3016592a59b8de87f90bb618b184bad9fe22add3
scott snyder <sss@karma>
2019-09-19 09:36:53 -0400

    LWHists: Suppress cppcheck warnings.
    Suppress some cppcheck warnings that were new with cppcheck 1.89.
    Add some missing virtual/override keywords.

--- Tools/LWHists/LWHists/TProfile2D_LW.h
+++ Tools/LWHists/LWHists/TProfile2D_LW.h
@@ -89,9 +89,11 @@ public:
 private:
   friend class LWHistInt;
   friend class LWHistVal;
+  // cppcheck-suppress virtualCallInConstructor
   virtual void clear() override;
   virtual const TH1* getROOTHistBaseNoAlloc() const override;
   virtual       TH1* getROOTHistBaseNoAlloc() override;
+  // cppcheck-suppress virtualCallInConstructor
   virtual void clearKeptROOTHist() override;//Does nothing if root-backend.
   const float * getVarBinsX() const;//null if fixed bin-widths
   const float * getVarBinsY() const;//null if fixed bin-widths
