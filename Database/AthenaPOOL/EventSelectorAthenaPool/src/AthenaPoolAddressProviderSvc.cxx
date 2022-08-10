7bc50a329c9d6b85fac5d17c28a9bb7c1b9b8ffa
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-08-10 16:58:53 +0200

    AthenaPoolAddressProviderSvc: remove usage of DataHandle

--- Database/AthenaPOOL/EventSelectorAthenaPool/src/AthenaPoolAddressProviderSvc.cxx
+++ Database/AthenaPOOL/EventSelectorAthenaPool/src/AthenaPoolAddressProviderSvc.cxx
@@ -127,7 +127,7 @@ StatusCode AthenaPoolAddressProviderSvc::loadAddresses(StoreID::type storeID,
    }
    // second data header
    if (m_attrListKey.value() != "") {
-      const DataHandle<AthenaAttributeList> attrList;
+      const AthenaAttributeList* attrList;
       std::string tokenStr;
       if (eventStore()->retrieve(attrList, m_attrListKey.value()).isSuccess()) {
          try {
