38317d5db48720f20597d9cf4d5a12ee3759ead3
Walter Lampl <walter.lampl@cern.ch>
2022-08-16 10:00:09 +0200

    remove obsolete jobO-only packate Calorimeter/CaloTestBeam/CaloTBRec
    remove obsolete jobO-only packate Calorimeter/CaloTestBeam/CaloTBRec

--- Calorimeter/CaloTestBeam/CaloTBRec/share/CaloTBRec_H6_TopoCluster_jobOptions.py
+++
@@ -1,3 +0,0 @@
-# 
-include( "CaloRec/CaloTopoCluster_jobOptions.py" )
-include( "CaloRec/EMTopoCluster_jobOptions.py" )
--- Calorimeter/CaloTestBeam/CaloTBRec/share/CaloTBRec_H8_reco_jobOptions.py
+++
@@ -1,9 +0,0 @@
-#*******************************************************************
-#
-# JobOption to analyse Combined testbeam data : Calo reconstruction
-#   
-#  same jobOpt as RecExCommon, with DetFlags.makeRIO.Calo_on()
-#*******************************************************************
-
-include( "CaloTBRec/CaloTBRec_H8_Cell_jobOptions.py" )
-include( "CaloTBRec/CaloTBRec_H8_Cluster_jobOptions.py" )
