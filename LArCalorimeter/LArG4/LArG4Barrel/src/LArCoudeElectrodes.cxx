9716273c75d93ad2c3187e91d58ae95e4478b2aa
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-08-29 09:48:45 +0200

    LArCoudeElectrodes: remove lazy initialization of members
    Analogues as in previous commit for LArCoudeAbsorbers.

--- LArCalorimeter/LArG4/LArG4Barrel/src/LArCoudeElectrodes.cxx
+++ LArCalorimeter/LArG4/LArG4Barrel/src/LArCoudeElectrodes.cxx
@@ -3,90 +3,45 @@
 */
 
 #include "LArCoudeElectrodes.h"
+#include "PhysicalVolumeAccessor.h"
 
-LArCoudeElectrodes* LArCoudeElectrodes::s_instance=nullptr;
 
-PhysicalVolumeAccessor* LArCoudeElectrodes::s_theCoudes=nullptr;
-
-std::once_flag LArCoudeElectrodes::s_flag;
-
-LArCoudeElectrodes*  LArCoudeElectrodes::GetInstance(const std::string& strDetector)
+const LArCoudeElectrodes* LArCoudeElectrodes::GetInstance(const std::string& strDetector)
 {
-  std::call_once( s_flag, [&](){ s_instance = new LArCoudeElectrodes(strDetector); } );
-  return s_instance;
+  static const LArCoudeElectrodes instance(strDetector);
+  return &instance;
 }
 
 
 LArCoudeElectrodes::LArCoudeElectrodes(const std::string& strDetector)
 {
-  if (s_theCoudes==nullptr)
-    {
-      if (strDetector.empty()) {
-        s_theCoudes=
-          new PhysicalVolumeAccessor("LAr::EMB::STAC",
-                                     "LAr::EMB::Electrode::CornerDownFold");
-        s_theCoudes->SetPhysicalVolumeList("LAr::EMB::Electrode::CornerUpFold");
-      }
-      else {
-        s_theCoudes=
-          new PhysicalVolumeAccessor(strDetector+"::LAr::EMB::STAC",
-                                     strDetector+"::LAr::EMB::Electrode::CornerDownFold");
-        s_theCoudes->SetPhysicalVolumeList(strDetector+"::LAr::EMB::Electrode::CornerUpFold");
-      }
-    }
-  m_filled=false;
+  static const PhysicalVolumeAccessor theCoudes = [&]() {
+    const std::string prefix = strDetector.empty() ? "" : strDetector+"::";
+    PhysicalVolumeAccessor pva(prefix+"LAr::EMB::STAC",
+                               prefix+"LAr::EMB::Electrode::CornerDownFold");
+    pva.SetPhysicalVolumeList(prefix+"LAr::EMB::Electrode::CornerUpFold");
+    return pva;
+  }();
+
   for (int stackid=0; stackid<15; stackid++) {
     for (int cellid=0; cellid<1024; cellid++) {
-      m_xcent[cellid][stackid] = XCentCoude(stackid,cellid);
-      m_ycent[cellid][stackid] = YCentCoude(stackid,cellid);
-      m_phirot[cellid][stackid] = PhiRot(stackid,cellid);
+      const int id=cellid+stackid*10000;
+      const G4VPhysicalVolume *pv=theCoudes.GetPhysicalVolume(id);
+      if (pv) {
+        const G4ThreeVector& tv=pv->GetTranslation();
+        m_xcent[cellid][stackid] = tv.x();
+        m_ycent[cellid][stackid] = tv.y();
 
+        // Calculate phirot
+        const G4RotationMatrix *rm=pv->GetRotation();
+        double alpha;
+        if (!rm) alpha=0.;
+        else alpha = rm->phiX();
+        // for down fold
+        if (pv->GetName().find("DownFold") != std::string::npos) alpha=alpha-3.14159;
+        // old way was assuming we start with a down fold if (stackid%2==0) alpha=alpha-3.14159;
+        m_phirot[cellid][stackid] = alpha;
+      }
     }
-  }
-  m_filled=true;
-}
-double LArCoudeElectrodes::XCentCoude(int stackid, int cellid) const
-{
-  if (m_filled) {
-    return m_xcent[cellid][stackid];
-  }
-  else {
-    const int id=cellid+stackid*10000;
-    const G4VPhysicalVolume *pv=s_theCoudes->GetPhysicalVolume(id);
-    if (!pv) return 0.;
-    const G4ThreeVector& tv=pv->GetTranslation();
-    return tv.x();
-  }
-}
-double LArCoudeElectrodes::YCentCoude(int stackid, int cellid) const
-{
-  if (m_filled) {
-    return m_ycent[cellid][stackid];
-  }
-  else {
-    const int id=cellid+stackid*10000;
-    const G4VPhysicalVolume *pv=s_theCoudes->GetPhysicalVolume(id);
-    if (!pv) return 0.;
-    const G4ThreeVector& tv=pv->GetTranslation();
-    return tv.y();
-  }
-}
-double LArCoudeElectrodes::PhiRot(int stackid, int cellid) const
-{
-  if (m_filled) {
-    return m_phirot[cellid][stackid];
-  }
-  else {
-    const int id=cellid+stackid*10000;
-    const G4VPhysicalVolume *pv=s_theCoudes->GetPhysicalVolume(id);
-    if (!pv) return 0.;
-    const G4RotationMatrix *rm=pv->GetRotation();
-    double alpha;
-    if (!rm) alpha=0.;
-    else alpha = rm->phiX();
-    // for down fold
-    if (pv->GetName().find("DownFold") != std::string::npos) alpha=alpha-3.14159;
-    // old way was assuming we start with a down fold if (stackid%2==0) alpha=alpha-3.14159;
-    return alpha;
   }
 }
