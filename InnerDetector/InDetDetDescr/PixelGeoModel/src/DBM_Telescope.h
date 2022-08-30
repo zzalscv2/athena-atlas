82cc12fa25141f5aeac50352ace9522569f3737d
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2022-08-31 01:01:56 +0200

    DBM_Telescope: added support for building geometry description from SQLite file

--- InnerDetector/InDetDetDescr/PixelGeoModel/src/DBM_Telescope.h
+++ InnerDetector/InDetDetDescr/PixelGeoModel/src/DBM_Telescope.h
@@ -1,29 +1,22 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 
-#ifndef DBMTelescope_H
-#define DBMTelescope_H
+#ifndef PIXELGEOMODEL_DBMTELESCOPE_H
+#define PIXELGEOMODEL_DBMTELESCOPE_H
+
+/**
+ * @class DBM_Telescope
+ * @brief Diamond Beam Monitor telescope builder
+ */
 
 #include "GeoVPixelFactory.h"
-#include "GaudiKernel/MsgStream.h"
-#include <iostream>
-
-/*** @class DBM_Telescope
- *
- * Diamond Beam Monitor telescope builder
- *
- */
 
 class DBM_Telescope : public GeoVPixelFactory {
  public:
   using GeoVPixelFactory::GeoVPixelFactory;
   virtual GeoVPhysVol* Build() override;
-
-  
- private:
-  
 };
 
 #endif
