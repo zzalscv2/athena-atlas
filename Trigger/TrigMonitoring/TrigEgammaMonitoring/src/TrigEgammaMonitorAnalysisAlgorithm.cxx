968da948ca53c3aa436ba0b998dc4af93a50371a
Edmar Egidio Purcino De Souza <edmar.egidio.purcino.de.souza@cern.ch>
2022-08-30 11:00:39 +0200

    [trigEGam] TrigEgammaMonitorPhoton - Applying tight WP in the offline photons
    [trigEGam] TrigEgammaMonitorPhoton - Applying tight WP in the offline photons

--- Trigger/TrigMonitoring/TrigEgammaMonitoring/src/TrigEgammaMonitorAnalysisAlgorithm.cxx
+++ Trigger/TrigMonitoring/TrigEgammaMonitoring/src/TrigEgammaMonitorAnalysisAlgorithm.cxx
@@ -408,8 +408,7 @@ void TrigEgammaMonitorAnalysisAlgorithm::fillDistributions( const std::vector< s
   }
 
   if ( info.signature == "Electron" ){
-      
-      // L2 Electron
+      // Fast Electron
       {
           std::string key = match()->key("FastElectrons");
           if(info.lrt) key = match()->key("FastElectrons_LRT");
@@ -445,6 +444,20 @@ void TrigEgammaMonitorAnalysisAlgorithm::fillDistributions( const std::vector< s
           fillTracking( trigger, el_vec, true );
       }
   }else if ( info.signature == "Photon"){
+    // Fast Photon
+      {
+          std::string key = match()->key("FastPhotons");         
+          std::vector<const xAOD::TrigPhoton*> ph_vec;
+          // Get only passed objects
+          auto vec =  tdt()->features<xAOD::TrigPhotonContainer>(trigger,condition,key );      
+          for( auto &featLinkInfo : vec ){
+              if(! featLinkInfo.isValid() ) continue;
+              const auto *feat = *(featLinkInfo.link);
+              if(!feat) continue;
+              ph_vec.push_back(feat);
+          }
+          fillL2Photon( trigger, ph_vec );
+      }
       // HLT Photon
       {
           std::vector<const xAOD::Egamma*> ph_vec;
@@ -574,6 +587,31 @@ void TrigEgammaMonitorAnalysisAlgorithm::fillL2Electron(const std::string &trigg
       highet_vec.push_back( el->pt()/Gaudi::Units::GeV );
       eta_vec.push_back( el->eta() );
       phi_vec.push_back( el->phi() );
+    }
+
+    fill( monGroup, et_col, eta_col, phi_col, highet_col );
+}
+
+
+void TrigEgammaMonitorAnalysisAlgorithm::fillL2Photon(const std::string &trigger, const std::vector< const xAOD::TrigPhoton* >& ph_vec) const
+{
+ 
+    auto monGroup = getGroup(trigger+"_Distributions_L2Photon");
+    
+    std::vector<float> et_vec, eta_vec, phi_vec, highet_vec;
+    
+    auto et_col   = Monitored::Collection("et" , et_vec  ); 
+    auto highet_col   = Monitored::Collection("highet" , highet_vec  );    
+    auto eta_col  = Monitored::Collection("eta", eta_vec );    
+    auto phi_col  = Monitored::Collection("phi", phi_vec );    
+    
+    for ( const auto *ph : ph_vec )
+    {
+      if(!ph)  continue;
+      et_vec.push_back( ph->pt()/Gaudi::Units::GeV );
+      highet_vec.push_back( ph->pt()/Gaudi::Units::GeV );
+      eta_vec.push_back( ph->eta() );
+      phi_vec.push_back( ph->phi() );
     }
 
     fill( monGroup, et_col, eta_col, phi_col, highet_col );
