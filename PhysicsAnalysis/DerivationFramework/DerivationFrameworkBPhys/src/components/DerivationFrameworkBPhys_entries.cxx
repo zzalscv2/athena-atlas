rolandjansky/athena

Update BPHY20: add BPhysBGammaFinder, modification JpsiPlus1Track for Jpsi plus e

PhysicsAnalysis/DerivationFramework/DerivationFrameworkBPhys/src/components/DerivationFrameworkBPhys_entries.cxx
@@ -40,6 +40,8 @@
 #include "DerivationFrameworkBPhys/Cascade3Plus1.h"
 #include "DerivationFrameworkBPhys/MuPlusDsCascade.h"
 #include "DerivationFrameworkBPhys/MuPlusDpstCascade.h"
+#include "DerivationFrameworkBPhys/BPhysBGammaFinder.h"
+
 using namespace DerivationFramework;
 
 DECLARE_TOOL_FACTORY( Reco_mumu )
@@ -83,6 +85,7 @@ DECLARE_TOOL_FACTORY( BPhysConversionFinder )
 DECLARE_TOOL_FACTORY( Cascade3Plus1 )
 DECLARE_TOOL_FACTORY( MuPlusDsCascade )
 DECLARE_TOOL_FACTORY( MuPlusDpstCascade )
+DECLARE_TOOL_FACTORY( BPhysBGammaFinder )
 
 DECLARE_FACTORY_ENTRIES( DerivationFrameworkBPhys ) {
    DECLARE_TOOL( Reco_mumu )
@@ -126,4 +129,5 @@ DECLARE_FACTORY_ENTRIES( DerivationFrameworkBPhys ) {
    DECLARE_TOOL( Cascade3Plus1 )
    DECLARE_TOOL( MuPlusDsCascade )
    DECLARE_TOOL( MuPlusDpstCascade )
+   DECLARE_TOOL( BPhysBGammaFinder )
 }

zzalscv2/athena-atlas

PhysicsAnalysis/DerivationFramework/DerivationFrameworkBPhys/src/components/DerivationFrameworkBPhys_entries.cxx
@@ -37,6 +37,7 @@
 #include "DerivationFrameworkBPhys/ReVertex.h"
 #include "DerivationFrameworkBPhys/BPhysConversionFinder.h"
 #include "DerivationFrameworkBPhys/Cascade3Plus1.h"
+#include "DerivationFrameworkBPhys/BPhysBGammaFinder.h"
 
 using namespace DerivationFramework;
 
@@ -79,4 +80,4 @@ DECLARE_COMPONENT( JpsiPlusDs1Cascade )
 DECLARE_COMPONENT( ReVertex )
 DECLARE_COMPONENT( BPhysConversionFinder )
 DECLARE_COMPONENT( Cascade3Plus1 )
-
+DECLARE_COMPONENT( BPhysBGammaFinder )