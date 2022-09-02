1fce2d4147cf080765211de69bbb35668c98e953
Shaun Roe <shaun.roe@cern.ch>
2022-09-02 12:32:49 +0200

    23.0-cppcheck-G4UserActions
    23.0-cppcheck-G4UserActions

--- Simulation/G4Utilities/G4UserActions/src/FluxRecorder.h
+++ Simulation/G4Utilities/G4UserActions/src/FluxRecorder.h
@@ -35,8 +35,8 @@ namespace G4UA
                         LMDTo,LMDTm,LMDTi,BMDTo,BMDTm,BMDTi,SWo,SWm,SWi,SWc,SWt,
                         bflz,bfhz,bslz,bshz,btlz,bthz,ffle,ffme,ffhe,fsle,fsme,fshe,ftle,ftme,fthe,
                         CY1,CY2,lastVol};
-    TH1D * m_flux[lastVol][9][2];
-    TH1D * m_fluxE[lastVol][9];
+    TH1D * m_flux[lastVol][9][2] = {};
+    TH1D * m_fluxE[lastVol][9] = {};
 
     double m_nev;
     std::vector<int> m_list;
