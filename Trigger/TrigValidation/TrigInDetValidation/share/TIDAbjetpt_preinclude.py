dad31b8ed9ee1762697622a048331db583364503
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-09-02 15:13:05 +0200

    Merge branch 'master-Xi2max' into 'master'
    Update TIDAbjetpt_preinclude.py to include change of Xi2max
    
    See merge request atlas/athena!56443
    
    (cherry picked from commit 1a86c121e366690faabc1693770ee36269cafaa7)
    
    bf22cd8f Update TIDAbjetpt_preinclude.py to include change of Xi2max

--- Trigger/TrigValidation/TrigInDetValidation/share/TIDAbjetpt_preinclude.py
+++ Trigger/TrigValidation/TrigInDetValidation/share/TIDAbjetpt_preinclude.py
@@ -1,4 +1,3 @@
-
 from AthenaCommon.Logging import logging 
 log = logging.getLogger("TrigInDetValidation")
 
@@ -8,7 +7,8 @@ from TrigInDetConfig.ConfigSettings import getInDetTrigConfig
 from AthenaCommon.SystemOfUnits import GeV
 
 getInDetTrigConfig("bjet")._pTmin = 0.8*GeV
+getInDetTrigConfig("bjet")._Xi2max = 12.0
 
 log.info( "ID Trigger pTmin: "+str(getInDetTrigConfig("bjet").pTmin) )
-
+log.info( "ID Trigger Xi2max: "+str(getInDetTrigConfig("bjet").Xi2max) )
 
