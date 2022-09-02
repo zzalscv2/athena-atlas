09e439237ffb21ac2352f5e177d614746da5efde
Sebastian Tapia Araya <s.tapia@cern.ch>
2022-09-02 16:07:26 +0200

    HI jet config for ComponentAccumulator
    adding trackjets, others R's jets, JetConstituentsModifiers and cell-level subtraction for egamma reco

--- Reconstruction/HeavyIonRec/HIRecConfig/python/HIRecConfig.py
+++ Reconstruction/HeavyIonRec/HIRecConfig/python/HIRecConfig.py
@@ -14,8 +14,9 @@ def HIRecCfg(flags):
         acc.merge(HIGlobalRecCfg(flags))
 
     if flags.HeavyIon.doJet:
-        #        acc.merge(HIJetRecCfg(flags))
-        pass
+        from HIJetRec.HIJetRecConfigCA import HIJetRecCfg
+        acc.merge(HIJetRecCfg(flags))    
+     #   pass
 
     if flags.HeavyIon.doEgamma:
         #        acc.merge(HIEgammaCfg(flags))
@@ -47,17 +48,23 @@ if __name__ == "__main__":
     flags.Input.Files = [defaultTestFiles.d + "/RecJobTransformTests/data18_hi.00367384.physics_HardProbes.daq.RAW._lb0145._SFO-8._0001.data"]
     flags.Exec.MaxEvents=5
     flags.Concurrency.NumThreads=1
+    flags.Output.doWriteAOD = True
+    flags.Output.AODFileName = "myAOD.pool.root"
 
     flags.fillFromArgs() # enable unit tests to switch only parts of reco: python -m HIRecConfig.HIRecConfig HeavyIon.doGlobal = 0 and so on
+
+    flags.HeavyIon.Jet.doTrackJetSeed = False # set to false in self-test because indetparticles are reconestructed 
+
     flags.lock()
 
     from AthenaConfiguration.MainServicesConfig import MainServicesCfg
     acc = MainServicesCfg(flags)
-    #      acc.getEventAlgo("SGInputLoader").FailIfNoProxy = True # enforce no missing data
-#    acc.getService("PrecedenceSvc").DumpPrecedenceRules = True
     from TriggerJobOpts.TriggerByteStreamConfig import ByteStreamReadCfg
     readBSAcc = ByteStreamReadCfg(flags)
     acc.merge(HIRecCfg(flags))
+
+    from AthenaPoolCnvSvc.PoolWriteConfig import PoolWriteCfg
+    acc.merge(PoolWriteCfg(flags))
 
     import sys
     sys.exit(acc.run().isFailure())
