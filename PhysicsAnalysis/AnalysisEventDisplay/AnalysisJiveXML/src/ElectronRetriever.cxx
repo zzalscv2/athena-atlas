c52320a0ee6b41f2ce7d69009c0de2824ac49095
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-09-22 14:29:31 +0200

    AnalysisJiveXML: remove deprecated uses of DataHandle

--- PhysicsAnalysis/AnalysisEventDisplay/AnalysisJiveXML/src/ElectronRetriever.cxx
+++ PhysicsAnalysis/AnalysisEventDisplay/AnalysisJiveXML/src/ElectronRetriever.cxx
@@ -40,7 +40,7 @@ namespace JiveXML {
     
     if (msgLvl(MSG::DEBUG)) msg(MSG::DEBUG)  << "in retrieveAll()" << endmsg;
     
-    const DataHandle<ElectronContainer> iterator, end;
+    SG::ConstIterator<ElectronContainer> iterator, end;
     const ElectronContainer* electrons;
     
     //obtain the default collection first
@@ -66,7 +66,7 @@ namespace JiveXML {
     for (; iterator!=end; iterator++) {
        if (iterator.key()!=m_sgKey) {
           if (msgLvl(MSG::DEBUG)) msg(MSG::DEBUG)  << "Trying to retrieve all " << dataTypeName() << " (" << iterator.key() << ")" << endmsg;
-            DataMap data = getData(iterator);
+            DataMap data = getData(&(*iterator));
             if ( FormatTool->AddToEvent(dataTypeName(), iterator.key(), &data).isFailure()){
 	       if (msgLvl(MSG::WARNING)) msg(MSG::WARNING) << "Collection " << iterator.key() << " not found in SG " << endmsg;
 	    }else{
