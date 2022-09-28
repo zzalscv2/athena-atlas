a1f043319f4b80243313d067311a46932472559b
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-09-28 14:31:51 +0200

    FastChainPileup: remove unused static in Samplers
    Remove unused static and enable thread-checker.

--- Simulation/FastSimulation/FastChainPileup/FastChainPileup/Samplers.h
+++ Simulation/FastSimulation/FastChainPileup/FastChainPileup/Samplers.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 // Helper for MultiParticleGunPileup
@@ -41,7 +41,6 @@ class MomSampler {
   ~MomSampler(){};
   virtual TLorentzVector shoot() {return m_val;};
   TLorentzVector m_val;
-  static TRandom m_random;
   ConstSampler * m_mass;
 };
 
