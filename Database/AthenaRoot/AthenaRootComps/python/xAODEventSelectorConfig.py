3ebd2dd1cd780d6d0a7272803aca3729504af3af
Teng Jian Khoo <teng.jian.khoo@cern.ch>
2022-09-30 10:43:23 +0200

    Add Athena TEvent-based file access
    Propagate AccessMode correctly, add main function for testing, propagate MetaDataSvc to ProxyProviderSvc for metadata @ initialize

---
+++ Database/AthenaRoot/AthenaRootComps/python/xAODEventSelectorConfig.py
@@ -0,0 +1,96 @@
+# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+
+# @file xAODHybridSelectorConfig
+# @purpose make the Athena framework read a set of xAOD files to emulate the
+#          usual TEvent event loop ... BUT READ METADATA WITH POOL!
+#          Converted from ReadAthenaxAODHybrid.py
+# @author Teng Jian Khoo
+#
+
+from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
+from AthenaConfiguration.ComponentFactory import CompFactory
+from AthenaCommon import Logging
+from AthenaCommon import Constants
+from AthenaServices.MetaDataSvcConfig import MetaDataSvcCfg
+
+# suppress the event loop heartbeat as it is somewhat I/O hungry for
+# no real gain in n-tuple reading/writing scenarii
+# if not hasattr(svcMgr, theApp.EventLoop): svcMgr += getattr(CfgMgr, theApp.EventLoop)()
+# evtloop = getattr(svcMgr, theApp.EventLoop)
+# try:
+#     evtloop.EventPrintoutInterval = 10000
+# except Exception:
+#     msg.info('disabling event loop heartbeat... [failed]')
+#     msg.info('performances might be sub-par... sorry.')
+#     pass
+
+from enum import IntEnum
+# Duplicate here because it's probably more efficient than importing from ROOT
+class xAODAccessMode(IntEnum):
+    BRANCH_ACCESS = 0
+    CLASS_ACCESS = 1
+    ATHENA_ACCESS = 2
+
+# Default to the most efficient access method
+def xAODReadCfg(configFlags,AccessMode=xAODAccessMode.CLASS_ACCESS):
+    """
+    Creates a ComponentAccumulator instance containing the 
+    athena services required for xAOD file reading
+    """
+
+    msg = Logging.logging.getLogger( 'ReadAthenaxAODHybrid' )
+    msg.debug("Configuring Athena for reading xAOD files (via TEvent, with POOL for Metadata)...")
+
+
+    result=ComponentAccumulator()
+    
+    result.addService(CompFactory.EvtPersistencySvc("EventPersistencySvc",)) #No service handle yet???
+    result.merge(MetaDataSvcCfg(configFlags))
+
+    result.addService(CompFactory.StoreGateSvc("MetaDataStore"))
+    # Suppress some uninformative messages
+    result.addService(CompFactory.PoolSvc("PoolSvc",OutputLevel=Constants.WARNING))
+
+    # result.addService(CompFactory.THistSvc())
+    result.addService(CompFactory.Athena.xAODCnvSvc())
+
+    result.addService(CompFactory.ProxyProviderSvc("ProxyProviderSvc",ProviderNames=[ "MetaDataSvc"]))
+    result.addService(
+        CompFactory.Athena.xAODEventSelector('EventSelector',
+            InputCollections = configFlags.Input.Files,
+            SkipEvents=configFlags.Exec.SkipEvents,
+            AccessMode=AccessMode,
+            ReadMetaDataWithPool=True
+            ))
+    evSel = result.getService("EventSelector")
+
+    result.setAppProperty("EvtSel",evSel.getFullJobOptName())
+
+    msg.debug("Configuring Athena for reading ROOT files (via TEvent, with POOL for Metadata)... [OK]")
+
+    return result
+
+
+
+if __name__=="__main__":
+    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    import os
+    asg_test_file_mc = os.environ['ASG_TEST_FILE_MC']
+    ConfigFlags.Input.Files=[asg_test_file_mc]
+
+    import sys
+    if len(sys.argv)>1:
+        ConfigFlags.Input.Files=[sys.argv[1]]
+    ConfigFlags.lock()
+
+    AccessMode = xAODAccessMode.CLASS_ACCESS
+    if len(sys.argv)>2:
+        AccessMode = xAODAccessMode(int(sys.argv[2]))
+
+    from AthenaConfiguration.MainServicesConfig import MainServicesCfg
+    from AthenaCommon.Configurable import ConfigurableRun3Behavior
+    with ConfigurableRun3Behavior():
+        cfg = MainServicesCfg(ConfigFlags)
+        cfg.merge(xAODReadCfg(ConfigFlags,AccessMode))
+
+    cfg.run(10)
