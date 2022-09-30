d25165b6db2f3cdffc75f72e4a783b11f2c5102c
Scott Snyder <scott.snyder@cern.ch>
2022-09-30 14:56:41 +0200

    L1CaloFEXByteStream: Fix thread-safety checker warnings.
    L1CaloFEXByteStream: Fix thread-safety checker warnings.
        
    Fix some new thread-safety checker warnings.
    Missing const qualifiers.

--- Trigger/TrigT1/L1CaloFEX/L1CaloFEXByteStream/src/bytestreamDecoder/bytestreamDecoder/L1CaloBsDecoderRun3.h
+++ Trigger/TrigT1/L1CaloFEX/L1CaloFEXByteStream/src/bytestreamDecoder/bytestreamDecoder/L1CaloBsDecoderRun3.h
@@ -26,34 +26,34 @@ public:
 
    void decodeEfexData( const uint32_t* beg, const uint32_t* end,
                         std::list<L1CaloRdoEfexTower>& dat,
-                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    void decodeEfexTobs( const uint32_t* beg, const uint32_t* end,
                         std::list<L1CaloRdoEfexTob>& tob,
-                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 #ifndef OFFLINE_DECODER
    void decodeJfexData( const uint32_t* beg, const uint32_t* end,
                         std::list<L1CaloRdoJfexTower>& dat,
-                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    void decodeJfexTobs( const uint32_t* beg, const uint32_t* end,
                         std::list<L1CaloRdoJfexTob>& tob,
-                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    void decodeGfexData( const uint32_t* beg, const uint32_t* end,
                         std::list<L1CaloRdoGfexTower>& dat,
-                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    void decodeGfexTobs( const uint32_t* beg, const uint32_t* end,
                         std::list<L1CaloRdoGfexTob>& dat,
-                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                        std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    void decodePh1TopoData( const uint32_t* beg, const uint32_t* end,
                            std::list<L1CaloRdoEfexTob>& etob,
                            std::list<L1CaloRdoJfexTob>& jtob,
                            std::list<L1CaloRdoGfexTob>& gtob,
                            std::list<L1CaloRdoMuonTob>& mtob,
-                           std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                           std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    void decodePh1TopoHits( const uint32_t* beg, const uint32_t* end,
                            std::list<L1CaloRdoPh1TopoHit>& hit,
@@ -68,40 +68,40 @@ private:
                                 const uint32_t shelfNumber,
                                 const uint32_t errorMask,
                                 std::list<L1CaloRdoEfexTower>& dat,
-                                std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                                std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    bool decodeEfexTobSlice( const uint32_t payload[], size_t& index,
                             const uint32_t efexNumber, const uint32_t shelfNumber,
                             const uint32_t numSlices, const uint32_t errorMask,
                             std::list<L1CaloRdoEfexTob>& tob,
-                            std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                            std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 #ifndef OFFLINE_DECODER
    uint32_t decodeJfexDataChan( const uint32_t payload[],
                                 const uint32_t jfexNumber,
                                 const uint32_t fpgaNumber,
                                 const uint32_t errorMask,
                                 std::list<L1CaloRdoJfexTower>& dat,
-                                std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                                std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    bool decodeJfexTobSlice( const uint32_t payload[], size_t blockSize, size_t& index,
                             const uint32_t jfexNumber, const uint32_t fpgaNumber,
                             const uint32_t sliceNumber, const uint32_t numSlices,
                             const uint32_t errorMask,
                             std::list<L1CaloRdoJfexTob>& tob,
-                            std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                            std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    uint32_t decodeGfexDataChan( const uint32_t payload[],
                                 const uint32_t fpgaNumber,
                                 const uint32_t chanNumber,
                                 const uint32_t errorMask,
                                 std::list<L1CaloRdoGfexTower>& dat,
-                                std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                                std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    bool decodeGfexTobSlice( const uint32_t payload[], const uint32_t blockType,
                             const uint32_t sliceNumber, const uint32_t numSlices,
                             const uint32_t errorMask,
                             std::list<L1CaloRdoGfexTob>& tob,
-                            std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                            std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 #endif
    void decodeOneEfexTob( const uint32_t word[], const uint32_t shelfNumber,
                           const uint32_t efexNumber, const uint32_t fpgaNumber,
@@ -110,7 +110,7 @@ private:
                           L1CaloRdoFexTob::TobType tobType,
                           L1CaloRdoFexTob::TobSource tobSource,
                           std::list<L1CaloRdoEfexTob>& tob,
-                          std::list<L1CaloRdoRodInfo>::const_iterator rodInfo );
+                          std::list<L1CaloRdoRodInfo>::const_iterator rodInfo ) const;
 
    bool checkFibreCRC( std::vector<uint32_t>& data ) const;
 
