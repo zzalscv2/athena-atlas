68ba51f5f9bc8e7fa63bb4d314e1b59fe06a7078
David Richard Shope <david.richard.shope@cern.ch>
2022-10-04 10:21:27 +0200

    Add dictionary for InDetTrackSystematicsTools
    Add dictionary for InDetTrackSystematicsTools

---
+++ PhysicsAnalysis/TrackingID/InDetTrackSystematicsTools/InDetTrackSystematicsTools/InDetTrackSystematicsToolsDict.h
@@ -0,0 +1,26 @@
+/*
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef INDETTRACKSYSTEMATICSTOOLS_INDETTRACKSYSTEMATICSTOOLSDICT_H
+#define INDETTRACKSYSTEMATICSTOOLS_INDETTRACKSYSTEMATICSTOOLSDICT_H
+
+#include "InDetTrackSystematicsTools/IInDetTrackSmearingTool.h"
+#include "InDetTrackSystematicsTools/InDetTrackSmearingTool.h"
+
+#include "InDetTrackSystematicsTools/IInDetTrackBiasingTool.h"
+#include "InDetTrackSystematicsTools/InDetTrackBiasingTool.h"
+
+#include "InDetTrackSystematicsTools/IInclusiveTrackFilterTool.h"
+#include "InDetTrackSystematicsTools/InclusiveTrackFilterTool.h"
+
+#include "InDetTrackSystematicsTools/IJetTrackFilterTool.h"
+#include "InDetTrackSystematicsTools/JetTrackFilterTool.h"
+
+#include "InDetTrackSystematicsTools/IInDetTrackTruthFilterTool.h"
+#include "InDetTrackSystematicsTools/InDetTrackTruthFilterTool.h"
+
+#include "InDetTrackSystematicsTools/IInDetTrackTruthOriginTool.h"
+#include "InDetTrackSystematicsTools/InDetTrackTruthOriginTool.h"
+
+#endif // not INDETTRACKSYSTEMATICSTOOLS_INDETTRACKSYSTEMATICSTOOLSDICT_H
