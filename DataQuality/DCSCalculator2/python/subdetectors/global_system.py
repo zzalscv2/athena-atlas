1ab9043007b2b664bb635d6b963c409e76c2467d
Peter Onyisi <ponyisi@utexas.edu>
2022-10-04 09:24:51 +0200

    Run 3 DB tag update for livetime in DCS Calculator
    Run 3 DB tag update for livetime in DCS Calculator

--- DataQuality/DCSCalculator2/python/subdetectors/global_system.py
+++ DataQuality/DCSCalculator2/python/subdetectors/global_system.py
@@ -39,8 +39,7 @@ class TDAQ_Busy(DCSC_Defect_Global_Variable):
     as run-LB
     """
     input_db = 'COOLOFL_TRIGGER/CONDBR2'
-    #fetch_args = dict(tag="OflLumiAcct-13TeV-004")
-    fetch_args = dict(tag="OflLumiAcct-001")
+    fetch_args = dict(tag="OflLumiAcct-Run3-001")
     deadfraction_threshold = 0.9
     
     def make_good_iovs(self, iovs):
