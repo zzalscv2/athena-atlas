57a7411474198436337478e7c9266494a5b04010
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2022-10-13 17:19:16 +0200

    Update conditions tags in reconstruction cosmic data ART tests.
    Update conditions tags in reconstruction cosmic data ART tests.
    Fixes ATLASRECTS=7318

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data22_IDCosmic.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data22_IDCosmic.sh
@@ -6,6 +6,6 @@
 # art-include: master/Athena
 # art-include: 22.0/Athena
 
-Reco_tf.py  --AMI=x636 --athenaopts="--threads=8" --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data22_cos/data22_cos.00412343.physics_IDCosmic.merge.RAW/data22_cos.00412343.physics_IDCosmic.merge.RAW._lb0763._SFO-ALL._0001.1 --maxEvents=300 --outputESDFile=myESD.pool.root --outputAODFile=myAOD.pool.root
+Reco_tf.py  --AMI=x636 --athenaopts="--threads=8" --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data22_cos/data22_cos.00412343.physics_IDCosmic.merge.RAW/data22_cos.00412343.physics_IDCosmic.merge.RAW._lb0763._SFO-ALL._0001.1 --maxEvents=300 --outputESDFile=myESD.pool.root --outputAODFile=myAOD.pool.root --conditionsTag=CONDBR2-ES1PA-2022-06
 RES=$?
 echo "art-result: $RES Reco"
