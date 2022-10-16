7cbd446c5b39fa235d00c40b9c94fbe6bb9e5348
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2022-10-16 10:33:01 +0200

    make PFlowLeptonSelector Re-entrant
    make PFlowLeptonSelector Re-entrant

--- Reconstruction/eflowRec/eflowRec/PFLeptonSelector.h
+++ Reconstruction/eflowRec/eflowRec/PFLeptonSelector.h
@@ -7,7 +7,7 @@
 /** Algorithm which will select leptons for downstream algorithms to use within eflowRec */
 
 #include "AthContainers/ConstDataVector.h"
-#include "AthenaBaseComps/AthAlgorithm.h"
+#include "AthenaBaseComps/AthReentrantAlgorithm.h"
 #include "StoreGate/DataHandle.h"
 
 #include "xAODEgamma/ElectronContainer.h"
@@ -15,69 +15,104 @@
 
 class CaloCellContainer;
 
-class PFLeptonSelector : public AthAlgorithm {
+class PFLeptonSelector : public AthReentrantAlgorithm
+{
 
 public:
   /** Default constructor */
   PFLeptonSelector(const std::string& name, ISvcLocator* pSvcLocator);
   /** Default destructor */
-  ~PFLeptonSelector() {};
+  ~PFLeptonSelector(){};
 
   /** Gaudi AthAlgorithm hooks */
   StatusCode initialize();
-  StatusCode execute();
+  StatusCode execute(const EventContext& ctx) const;
   StatusCode finalize();
 
 private:
-
   /** Select electrons to use */
-  StatusCode selectElectrons(SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer >>& selectedElectronsWriteHandle, const SG::WriteHandle<ConstDataVector<CaloCellContainer> >& leptonCaloCellsWriteHandle);
+  StatusCode selectElectrons(SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer>>& selectedElectronsWriteHandle,
+                             const SG::WriteHandle<ConstDataVector<CaloCellContainer>>& leptonCaloCellsWriteHandle) const;
 
   /** store the cells of the electrons */
-  void storeElectronCells(const xAOD::Egamma& electron, SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle);
+  void storeElectronCells(const xAOD::Egamma& electron,
+                          SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const;
 
   /** select  muons to use */
-  StatusCode selectMuons(SG::WriteHandle<ConstDataVector<xAOD::MuonContainer> >& selectedMuonsWriteHandle, const SG::WriteHandle<ConstDataVector<CaloCellContainer> >& leptonCaloCellsWriteHandle);
+  StatusCode selectMuons(SG::WriteHandle<ConstDataVector<xAOD::MuonContainer>>& selectedMuonsWriteHandle,
+                         const SG::WriteHandle<ConstDataVector<CaloCellContainer>>& leptonCaloCellsWriteHandle) const;
 
   /** store the cells of the muons */
-  void storeMuonCells(const xAOD::Muon& muon, SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle);
+  void storeMuonCells(const xAOD::Muon& muon,
+                      SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const;
 
   /** puts set of lepton cells into the lepton container */
-  void storeLeptonCells(const xAOD::CaloCluster& theCluster, SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle);
+  void storeLeptonCells(const xAOD::CaloCluster& theCluster,
+                        SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const;
 
   /** Put lepton containers and list of lepton cells into Storegate */
-  StatusCode recordLeptonContainers(SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer >>& selectedElectronsWriteHandle, SG::WriteHandle<ConstDataVector<xAOD::MuonContainer> >& selectedMuonsWriteHandle, SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle );
-  
+  StatusCode recordLeptonContainers(
+    SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer>>& selectedElectronsWriteHandle,
+    SG::WriteHandle<ConstDataVector<xAOD::MuonContainer>>& selectedMuonsWriteHandle,
+    SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const;
+
   /** ReadHandle for the ElectronContainer to be used as input */
-  SG::ReadHandleKey<xAOD::ElectronContainer> m_electronsReadHandleKey{this,"inputElectronsName","Electrons","ReadHandle for the ElectronContainer to be used as input"};
+  SG::ReadHandleKey<xAOD::ElectronContainer> m_electronsReadHandleKey{
+    this,
+    "inputElectronsName",
+    "Electrons",
+    "ReadHandle for the ElectronContainer to be used as input"
+  };
 
   /** ReadHandle for the MuonContainer to be used as input */
-  SG::ReadHandleKey<xAOD::MuonContainer> m_muonsReadHandleKey{this,"inputMuonsName","Muons","ReadHandle for the MuonContainer to be used as input"};
+  SG::ReadHandleKey<xAOD::MuonContainer> m_muonsReadHandleKey{ this,
+                                                               "inputMuonsName",
+                                                               "Muons",
+                                                               "ReadHandle for the MuonContainer to be used as input" };
 
-  /** WriteHandle for the ElectronContainer, that will be filled with electrons passing the electron ID in PFLeptonSelector::selectElectrons */
-  SG::WriteHandleKey<ConstDataVector<xAOD::ElectronContainer >> m_selectedElectronsWriteHandleKey{this,"outputElectronsName","eflowRec_selectedElectrons","WriteHandle for the ElectronContainer, that will be filled with electrons passing the electron ID in PFLeptonSelector::selectElectrons"};
+  /** WriteHandle for the ElectronContainer, that will be filled with electrons passing the electron ID in
+   * PFLeptonSelector::selectElectrons */
+  SG::WriteHandleKey<ConstDataVector<xAOD::ElectronContainer>> m_selectedElectronsWriteHandleKey{
+    this,
+    "outputElectronsName",
+    "eflowRec_selectedElectrons",
+    "WriteHandle for the ElectronContainer, that will be filled with electrons passing the electron ID in "
+    "PFLeptonSelector::selectElectrons"
+  };
 
-  /** WriteHandle for the MuonContainer, that will be filled with muons passing the muon ID in PFLeptonSelector::selectMuons */
-  SG::WriteHandleKey<ConstDataVector<xAOD::MuonContainer> > m_selectedMuonsWriteHandleKey{this,"outputMuonsName","eflowRec_selectedMuons","WriteHandle for the MuonContainer, that will be filled with muons passing the muon ID in PFLeptonSelector::selectMuons"};
-  
+  /** WriteHandle for the MuonContainer, that will be filled with muons passing the muon ID in
+   * PFLeptonSelector::selectMuons */
+  SG::WriteHandleKey<ConstDataVector<xAOD::MuonContainer>> m_selectedMuonsWriteHandleKey{
+    this,
+    "outputMuonsName",
+    "eflowRec_selectedMuons",
+    "WriteHandle for the MuonContainer, that will be filled with muons passing the muon ID in "
+    "PFLeptonSelector::selectMuons"
+  };
+
   /** WriteHandle for the CaloCellContainer, that will store calorimeter cells associated to leptons */
-  SG::WriteHandleKey<ConstDataVector<CaloCellContainer> > m_leptonCaloCellsWriteHandleKey{this,"outputLeptonCellsName","eflowRec_leptonCellContainer","WriteHandle for the CaloCellContainer, that will store calorimeter cells associated to leptons"};
+  SG::WriteHandleKey<ConstDataVector<CaloCellContainer>> m_leptonCaloCellsWriteHandleKey{
+    this,
+    "outputLeptonCellsName",
+    "eflowRec_leptonCellContainer",
+    "WriteHandle for the CaloCellContainer, that will store calorimeter cells associated to leptons"
+  };
 
   /** Toggle storage of lepton CaloCells */
-  Gaudi::Property<bool> m_storeLeptonCells{this,"storeLeptonCells",false,"Toggle storage of lepton CaloCells"};
+  Gaudi::Property<bool> m_storeLeptonCells{ this, "storeLeptonCells", false, "Toggle storage of lepton CaloCells" };
 
-  Gaudi::Property<std::string> m_electronID{this,"electronID","LHMedium","Select electron ID"};
+  Gaudi::Property<std::string> m_electronID{ this, "electronID", "LHMedium", "Select electron ID" };
 
   /** Toggle to determine whether we select any electrons or not - if selected then tracks matched to those electrons
-  * in PFTrackSelector are not used in particle flow. If not selected then an empty electron container is created,
-  * and hence PFTrackSelector won't find any electrons to match its selected tracks to.
-  */
-  Gaudi::Property<bool> m_selectElectrons{this,"selectElectrons",true,"Toggle usage of electron ID"};
+   * in PFTrackSelector are not used in particle flow. If not selected then an empty electron container is created,
+   * and hence PFTrackSelector won't find any electrons to match its selected tracks to.
+   */
+  Gaudi::Property<bool> m_selectElectrons{ this, "selectElectrons", true, "Toggle usage of electron ID" };
 
   /** Toggle to determine whether we select any muons or not - if selected then tracks matched to those muons
-  * in PFTrackSelector are not used in particle flow. If not selected then an empty muon container is created,
-  * and hence PFTrackSelector won't find any muons to match its selected tracks to.
-  */
-  Gaudi::Property<bool> m_selectMuons{this,"selectMuons",true,"Toggle usage of muon ID"};
+   * in PFTrackSelector are not used in particle flow. If not selected then an empty muon container is created,
+   * and hence PFTrackSelector won't find any muons to match its selected tracks to.
+   */
+  Gaudi::Property<bool> m_selectMuons{ this, "selectMuons", true, "Toggle usage of muon ID" };
 };
 #endif
--- Reconstruction/eflowRec/src/PFLeptonSelector.cxx
+++ Reconstruction/eflowRec/src/PFLeptonSelector.cxx
@@ -2,13 +2,10 @@
   Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 #include <utility>
-
-
-
 #include "eflowRec/PFLeptonSelector.h"
 
 PFLeptonSelector::PFLeptonSelector(const std::string& name, ISvcLocator* pSvcLocator):
-  AthAlgorithm(name, pSvcLocator)
+  AthReentrantAlgorithm(name, pSvcLocator)
 {
 }
 
@@ -24,11 +21,11 @@ StatusCode PFLeptonSelector::initialize(){
   return StatusCode::SUCCESS;
 }
 
-StatusCode PFLeptonSelector::execute(){
+StatusCode PFLeptonSelector::execute(const EventContext& ctx) const{
 
-  SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer >> selectedElectronsWriteHandle(m_selectedElectronsWriteHandleKey);
-  SG::WriteHandle<ConstDataVector<xAOD::MuonContainer> > selectedMuonsWriteHandle(m_selectedMuonsWriteHandleKey);
-  SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle(m_leptonCaloCellsWriteHandleKey);
+  SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer >> selectedElectronsWriteHandle(m_selectedElectronsWriteHandleKey,ctx);
+  SG::WriteHandle<ConstDataVector<xAOD::MuonContainer> > selectedMuonsWriteHandle(m_selectedMuonsWriteHandleKey,ctx);
+  SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle(m_leptonCaloCellsWriteHandleKey,ctx);
   
   if (recordLeptonContainers(selectedElectronsWriteHandle,selectedMuonsWriteHandle,leptonCaloCellsWriteHandle).isFailure()) {
       return StatusCode::SUCCESS;
@@ -53,50 +50,64 @@ StatusCode PFLeptonSelector::execute(){
 
 StatusCode PFLeptonSelector::finalize(){ return StatusCode::SUCCESS; }
 
-StatusCode PFLeptonSelector::selectElectrons(SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer >>& selectedElectronsWriteHandle, const SG::WriteHandle<ConstDataVector<CaloCellContainer> >& leptonCaloCellsWriteHandle ){
+StatusCode
+PFLeptonSelector::selectElectrons(
+  SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer>>& selectedElectronsWriteHandle,
+  const SG::WriteHandle<ConstDataVector<CaloCellContainer>>& leptonCaloCellsWriteHandle) const
+{
 
-  SG::ReadHandle<xAOD::ElectronContainer>  electronsReadHandle(m_electronsReadHandleKey);
-  
+  SG::ReadHandle<xAOD::ElectronContainer> electronsReadHandle(m_electronsReadHandleKey);
+
   if (!electronsReadHandle.isValid()){
     ATH_MSG_WARNING("Invalid read handle to electron container with name: " << electronsReadHandle.key());
     return StatusCode::FAILURE;
   }
 
-  for (const auto *theElectron : *electronsReadHandle){
-    
-    if (theElectron){
-      if (theElectron->pt() > 10000){
-	bool passElectronID = false;
-	bool gotID = theElectron->passSelection(passElectronID, m_electronID);
-	if (!gotID) {
-	  ATH_MSG_WARNING("Could not get Electron ID");
-	  continue;
-	}
-	if (passElectronID){
-	  if (selectedElectronsWriteHandle.isValid()) selectedElectronsWriteHandle->push_back(theElectron);
-	  else ATH_MSG_WARNING("Do not have valid WriteHandle for ElectronContainer with name: " << selectedElectronsWriteHandle.key());
-	  if (true == m_storeLeptonCells) this->storeElectronCells(*theElectron,leptonCaloCellsWriteHandle);
-	}//mediumPP
-      }//10GeV pt cut
-    }//valid egamma pointer
-    else ATH_MSG_WARNING("This electron is a NULL pointer");
-    
-  }//electron loop
+  for (const auto* theElectron : *electronsReadHandle) {
+
+    if (theElectron) {
+      if (theElectron->pt() > 10000) {
+        bool passElectronID = false;
+        bool gotID = theElectron->passSelection(passElectronID, m_electronID);
+        if (!gotID) {
+          ATH_MSG_WARNING("Could not get Electron ID");
+          continue;
+        }
+        if (passElectronID) {
+          if (selectedElectronsWriteHandle.isValid())
+            selectedElectronsWriteHandle->push_back(theElectron);
+          else
+            ATH_MSG_WARNING(
+              "Do not have valid WriteHandle for ElectronContainer with name: " << selectedElectronsWriteHandle.key());
+          if (true == m_storeLeptonCells)
+            this->storeElectronCells(*theElectron, leptonCaloCellsWriteHandle);
+        } // mediumPP
+      }   // 10GeV pt cut
+    }     // valid egamma pointer
+    else
+      ATH_MSG_WARNING("This electron is a NULL pointer");
+
+  } // electron loop
 
   return StatusCode::SUCCESS;
 }
 
-void PFLeptonSelector::storeElectronCells(const xAOD::Egamma& electron, SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle){
+void
+PFLeptonSelector::storeElectronCells(const xAOD::Egamma& electron,
+                                     SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const
+{
 
   const xAOD::CaloCluster* electronCluster = electron.caloCluster(); 
   if (electronCluster){
     this->storeLeptonCells(*electronCluster,std::move(leptonCaloCellsWriteHandle));      
   }
   else ATH_MSG_WARNING("This electron has an invalid pointer to its cluster");
-
 }
 
-StatusCode PFLeptonSelector::selectMuons(SG::WriteHandle<ConstDataVector<xAOD::MuonContainer> >& selectedMuonsWriteHandle, const SG::WriteHandle<ConstDataVector<CaloCellContainer> >& leptonCaloCellsWriteHandle) {
+StatusCode
+PFLeptonSelector::selectMuons(SG::WriteHandle<ConstDataVector<xAOD::MuonContainer>>& selectedMuonsWriteHandle,
+                              const SG::WriteHandle<ConstDataVector<CaloCellContainer>>& leptonCaloCellsWriteHandle) const
+{
 
   SG::ReadHandle<xAOD::MuonContainer> muonsReadHandle(m_muonsReadHandleKey);
   
@@ -105,27 +116,35 @@ StatusCode PFLeptonSelector::selectMuons(SG::WriteHandle<ConstDataVector<xAOD::M
     return StatusCode::FAILURE;
   }
 
-  for (const auto *theMuon : *muonsReadHandle){  
-    
-    //Details of medium muons are here:
-    //https://twiki.cern.ch/twiki/bin/view/Atlas/MuonSelectionTool
-    //We only care about muons with ID tracks. Of the muon types which could be medium, only Combined have ID tracks (for looser selections other muon types could have ID tracks)
-    
+  for (const auto *theMuon : *muonsReadHandle){
+
+    // Details of medium muons are here:
+    // https://twiki.cern.ch/twiki/bin/view/Atlas/MuonSelectionTool
+    // We only care about muons with ID tracks. Of the muon types which could be medium, only Combined have ID tracks
+    // (for looser selections other muon types could have ID tracks)
+
     xAOD::Muon::Quality muonQuality = theMuon->quality();
-    if( muonQuality <= xAOD::Muon::Medium) {
+    if (muonQuality <= xAOD::Muon::Medium) {
       xAOD::Muon::MuonType muonType = theMuon->muonType();
-      if ( xAOD::Muon::Combined == muonType){
-	if (selectedMuonsWriteHandle.isValid()) selectedMuonsWriteHandle->push_back(theMuon);
-	else ATH_MSG_WARNING("Do not have valid WriteHandle for MuonContainer with name: " << selectedMuonsWriteHandle.key());
-	if (true == m_storeLeptonCells) this->storeMuonCells(*theMuon,leptonCaloCellsWriteHandle);
-      }//combined muons
-    }//Medium muons
-  } //muon loop
+      if (xAOD::Muon::Combined == muonType) {
+        if (selectedMuonsWriteHandle.isValid())
+          selectedMuonsWriteHandle->push_back(theMuon);
+        else
+          ATH_MSG_WARNING(
+            "Do not have valid WriteHandle for MuonContainer with name: " << selectedMuonsWriteHandle.key());
+        if (true == m_storeLeptonCells)
+          this->storeMuonCells(*theMuon, leptonCaloCellsWriteHandle);
+      } // combined muons
+    }   // Medium muons
+  }     // muon loop
 
   return StatusCode::SUCCESS;
 }
 
-void PFLeptonSelector::storeMuonCells(const xAOD::Muon& muon, SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle){
+void
+PFLeptonSelector::storeMuonCells(const xAOD::Muon& muon,
+                                 SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const
+{
 
   const ElementLink<xAOD::CaloClusterContainer>& theLink = muon.clusterLink(); 
   if (theLink.isValid()){
@@ -136,10 +155,12 @@ void PFLeptonSelector::storeMuonCells(const xAOD::Muon& muon, SG::WriteHandle<Co
     else ATH_MSG_WARNING("This muon has an invalid pointer to its cluster ");
   }
   else ATH_MSG_WARNING("This muon has an invalid element link to its cluster");
-
 }
 
-void PFLeptonSelector::storeLeptonCells(const xAOD::CaloCluster& theCluster, SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle){
+void
+PFLeptonSelector::storeLeptonCells(const xAOD::CaloCluster& theCluster,
+                                   SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const
+{
 
   const CaloClusterCellLink* theCellLink = theCluster.getCellLinks();
 
@@ -152,7 +173,12 @@ void PFLeptonSelector::storeLeptonCells(const xAOD::CaloCluster& theCluster, SG:
   else ATH_MSG_WARNING("This cluster has an invalid pointer to its cells, in storeLeptonCells");
 }
 
-StatusCode PFLeptonSelector::recordLeptonContainers(SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer >>& selectedElectronsWriteHandle, SG::WriteHandle<ConstDataVector<xAOD::MuonContainer> >& selectedMuonsWriteHandle,SG::WriteHandle<ConstDataVector<CaloCellContainer> > leptonCaloCellsWriteHandle){
+StatusCode
+PFLeptonSelector::recordLeptonContainers(
+  SG::WriteHandle<ConstDataVector<xAOD::ElectronContainer>>& selectedElectronsWriteHandle,
+  SG::WriteHandle<ConstDataVector<xAOD::MuonContainer>>& selectedMuonsWriteHandle,
+  SG::WriteHandle<ConstDataVector<CaloCellContainer>> leptonCaloCellsWriteHandle) const
+{
 
   StatusCode sc = selectedElectronsWriteHandle.record(std::make_unique<ConstDataVector<xAOD::ElectronContainer >>(SG::VIEW_ELEMENTS));
   
@@ -180,5 +206,4 @@ StatusCode PFLeptonSelector::recordLeptonContainers(SG::WriteHandle<ConstDataVec
   }
 
   return StatusCode::SUCCESS;
-
 }
