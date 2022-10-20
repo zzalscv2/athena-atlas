8f347c2abb2efb7f772a906241dea0c297638b7c
scott snyder <sss@karma>
2022-10-20 11:16:24 -0400

    GoodRunsLists: Remove dependency on EventInfo.
    Remove dependency on the old EventInfo class.

--- DataQuality/GoodRunsLists/GoodRunsLists/GoodRunsListSelectorTool.h
+++ DataQuality/GoodRunsLists/GoodRunsLists/GoodRunsListSelectorTool.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef GoodRunsLists_GoodRunsListSelectorTool_H
@@ -26,7 +26,6 @@
  **/
 
 class TFormula;
-class EventInfo;
 
 namespace Root {
   class TGRLCollection;
