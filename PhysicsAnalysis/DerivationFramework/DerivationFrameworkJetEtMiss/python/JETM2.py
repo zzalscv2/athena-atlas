544b94d4d88cfad8974ae34015eb5223cad8eca2
Chris Malena Delitzsch <chris.malena.delitzsch@cern.ch>
2022-10-25 14:02:00 +0200

    Merge JETM8 and JETM13 into JETM2 - CA version only
    Merge JETM8 and JETM13 into JETM2 - CA version only

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/JETM2.py
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkJetEtMiss/python/JETM2.py
@@ -1,15 +1,15 @@
 # Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 #!/usr/bin/env python
 #====================================================================
-# DAOD_JETM13.py
+# DAOD_JETM2.py
 #====================================================================
 
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
 from AthenaConfiguration.ComponentFactory import CompFactory
 
 # Main algorithm config
-def JETM13KernelCfg(ConfigFlags, name='JETM13Kernel', **kwargs):
-    """Configure the derivation framework driving algorithm (kernel) for JETM13"""
+def JETM2KernelCfg(ConfigFlags, name='JETM2Kernel', **kwargs):
+    """Configure the derivation framework driving algorithm (kernel) for JETM2"""
     acc = ComponentAccumulator()
 
     # Common augmentations
@@ -18,7 +18,7 @@ def JETM13KernelCfg(ConfigFlags, name='JETM13Kernel', **kwargs):
 
     if ConfigFlags.Input.isMC:
         # thinning tools: 
-        truthThinningTool = CompFactory.DerivationFramework.MenuTruthThinning(name               = "JETM13TruthThinning",
+        truthThinningTool = CompFactory.DerivationFramework.MenuTruthThinning(name               = "JETM2TruthThinning",
                                                                               StreamName         = kwargs['StreamName'],
                                                                               WriteAllStable     = True,
                                                                               WritePartons       = False,
@@ -34,12 +34,12 @@ def JETM13KernelCfg(ConfigFlags, name='JETM13Kernel', **kwargs):
     acc.addEventAlgo(DerivationKernel(name, ThinningTools = [truthThinningTool] if ConfigFlags.Input.isMC else []))
 
     # Extra jet content:
-    acc.merge(JETM13ExtraContentCfg(ConfigFlags))
+    acc.merge(JETM2ExtraContentCfg(ConfigFlags))
 
     return acc
 
 
-def JETM13ExtraContentCfg(ConfigFlags):
+def JETM2ExtraContentCfg(ConfigFlags):
 
     acc = ComponentAccumulator()
 
@@ -83,13 +83,15 @@ def JETM13ExtraContentCfg(ConfigFlags):
     #=======================================
 
     if ConfigFlags.Input.isMC:
-        from DerivationFrameworkMCTruth.MCTruthCommonConfig import AddTopQuarkAndDownstreamParticlesCfg
-        acc.merge(AddTopQuarkAndDownstreamParticlesCfg(generations=4,rejectHadronChildren=True))
+        from DerivationFrameworkMCTruth.MCTruthCommonConfig import AddTopQuarkAndDownstreamParticlesCfg, AddTruthCollectionNavigationDecorationsCfg
+        acc.merge(AddTopQuarkAndDownstreamParticlesCfg())
+        acc.merge(AddTruthCollectionNavigationDecorationsCfg(TruthCollections=["TruthTopQuarkWithDecayParticles","TruthBosonsWithDecayParticles"],prefix='Top'))
+
 
     return acc
 
 
-def JETM13Cfg(ConfigFlags):
+def JETM2Cfg(ConfigFlags):
 
     acc = ComponentAccumulator()
 
@@ -98,10 +100,10 @@ def JETM13Cfg(ConfigFlags):
     # for actually configuring the matching, so we create it here and pass it down
     # TODO: this should ideally be called higher up to avoid it being run multiple times in a train
     from DerivationFrameworkPhys.TriggerListsHelper import TriggerListsHelper
-    JETM13TriggerListsHelper = TriggerListsHelper()
+    JETM2TriggerListsHelper = TriggerListsHelper()
 
     # Skimming, thinning, augmentation, extra content
-    acc.merge(JETM13KernelCfg(ConfigFlags, name="JETM13Kernel", StreamName = 'StreamDAOD_JETM13', TriggerListsHelper = JETM13TriggerListsHelper))
+    acc.merge(JETM2KernelCfg(ConfigFlags, name="JETM2Kernel", StreamName = 'StreamDAOD_JETM2', TriggerListsHelper = JETM2TriggerListsHelper))
 
     # ============================
     # Define contents of the format
@@ -109,17 +111,22 @@ def JETM13Cfg(ConfigFlags):
     from OutputStreamAthenaPool.OutputStreamConfig import OutputStreamCfg
     from DerivationFrameworkCore.SlimmingHelper import SlimmingHelper
     
-    JETM13SlimmingHelper = SlimmingHelper("JETM13SlimmingHelper", NamesAndTypes = ConfigFlags.Input.TypedCollections)
+    JETM2SlimmingHelper = SlimmingHelper("JETM2SlimmingHelper", NamesAndTypes = ConfigFlags.Input.TypedCollections)
 
-    JETM13SlimmingHelper.SmartCollections = ["EventInfo",
+    JETM2SlimmingHelper.SmartCollections = ["EventInfo",
                                              "Electrons", "Photons", "Muons", "TauJets",
                                              "InDetTrackParticles", "PrimaryVertices",
                                              "MET_Baseline_AntiKt4EMPFlow",
                                              "AntiKt4EMTopoJets","AntiKt4EMPFlowJets",
                                              "AntiKt4TruthJets","AntiKt10TruthJets",
-                                             "AntiKt10UFOCSSKSoftDropBeta100Zcut10Jets"]
+                                             
+                                             "AntiKt10UFOCSSKSoftDropBeta100Zcut10Jets",
+                                             "BTagging_AntiKt4EMPFlow",
+                                             "BTagging_AntiKtVR30Rmax4Rmin02Track",
 
-    JETM13SlimmingHelper.AllVariables = ["CaloCalTopoClusters", "CaloCalFwdTopoTowers",
+    ]
+
+    JETM2SlimmingHelper.AllVariables = ["CaloCalTopoClusters", "CaloCalFwdTopoTowers",
                                          "GlobalChargedParticleFlowObjects", "GlobalNeutralParticleFlowObjects",
                                          "CHSGChargedParticleFlowObjects","CHSGNeutralParticleFlowObjects",
                                          "Kt4EMTopoOriginEventShape","Kt4EMPFlowEventShape","Kt4EMPFlowPUSBEventShape","Kt4EMPFlowNeutEventShape","Kt4UFOCSSKEventShape","Kt4UFOCSSKNeutEventShape",
@@ -127,68 +134,76 @@ def JETM13Cfg(ConfigFlags):
                                          "TruthVertices",
                                          "TruthEvents"]
 
-    JETM13SlimmingHelper.ExtraVariables = ["UFOCSSK.pt.eta.phi.m.signalType",
+    JETM2SlimmingHelper.ExtraVariables = ["UFOCSSK.pt.eta.phi.m.signalType",
                                            "UFO.pt.eta.phi.m.signalType",
                                            "InDetTrackParticles.particleHypothesis.vx.vy.vz",
                                            "GSFTrackParticles.particleHypothesis.vx.vy.vz",
                                            "PrimaryVertices.x.y.z",
                                            "TauJets.clusterLinks",
                                            "Muons.energyLossType.EnergyLoss.ParamEnergyLoss.MeasEnergyLoss.EnergyLossSigma.MeasEnergyLossSigma.ParamEnergyLossSigmaPlus.ParamEnergyLossSigmaMinus.clusterLinks.FSR_CandidateEnergy",
-                                           "MuonSegments.x.y.z.px.py.pz"]
+                                           "MuonSegments.x.y.z.px.py.pz",
+                                           "CSSKGChargedParticleFlowObjects.pt.eta.phi.m.matchedToPV.originalObjectLink",
+                                           "CSSKGNeutralParticleFlowObjects.pt.eta.phi.m.originalObjectLink"]
 
-    JETM13SlimmingHelper.AppendToDictionary['GlobalChargedParticleFlowObjects'] ='xAOD::FlowElementContainer'
-    JETM13SlimmingHelper.AppendToDictionary['GlobalChargedParticleFlowObjectsAux'] ='xAOD::FlowElementAuxContainer'
-    JETM13SlimmingHelper.AppendToDictionary['GlobalNeutralParticleFlowObjects'] = 'xAOD::FlowElementContainer'
-    JETM13SlimmingHelper.AppendToDictionary['GlobalNeutralParticleFlowObjectsAux'] = 'xAOD::FlowElementAuxContainer'
-    JETM13SlimmingHelper.AppendToDictionary['CHSGChargedParticleFlowObjects'] = 'xAOD::FlowElementContainer'
-    JETM13SlimmingHelper.AppendToDictionary['CHSGChargedParticleFlowObjectsAux'] = 'xAOD::ShallowAuxContainer'
-    JETM13SlimmingHelper.AppendToDictionary['CHSGNeutralParticleFlowObjects'] = 'xAOD::FlowElementContainer'
-    JETM13SlimmingHelper.AppendToDictionary['CHSGNeutralParticleFlowObjectsAux'] = 'xAOD::ShallowAuxContainer'
-    JETM13SlimmingHelper.AppendToDictionary['UFOCSSK'] = 'xAOD::FlowElementContainer'
-    JETM13SlimmingHelper.AppendToDictionary['UFOCSSKAux'] = 'xAOD::FlowElementAuxContainer'
-    JETM13SlimmingHelper.AppendToDictionary['UFO'] = 'xAOD::FlowElementContainer'
-    JETM13SlimmingHelper.AppendToDictionary['UFOAux'] = 'xAOD::FlowElementAuxContainer'
-    JETM13SlimmingHelper.AppendToDictionary['Kt4UFOCSSKEventShape'] = 'xAOD::EventShape'
-    JETM13SlimmingHelper.AppendToDictionary['Kt4UFOCSSKEventShapeAux'] = 'xAOD::EventShapeAuxInfo'
-    JETM13SlimmingHelper.AppendToDictionary['Kt4UFOCSSKNeutEventShape'] = 'xAOD::EventShape'
-    JETM13SlimmingHelper.AppendToDictionary['Kt4UFOCSSKNeutEventShapeAux'] = 'xAOD::EventShapeAuxInfo'
+
+    JETM2SlimmingHelper.AppendToDictionary['GlobalChargedParticleFlowObjects'] ='xAOD::FlowElementContainer'
+    JETM2SlimmingHelper.AppendToDictionary['GlobalChargedParticleFlowObjectsAux'] ='xAOD::FlowElementAuxContainer'
+    JETM2SlimmingHelper.AppendToDictionary['GlobalNeutralParticleFlowObjects'] = 'xAOD::FlowElementContainer'
+    JETM2SlimmingHelper.AppendToDictionary['GlobalNeutralParticleFlowObjectsAux'] = 'xAOD::FlowElementAuxContainer'
+    JETM2SlimmingHelper.AppendToDictionary['CHSGChargedParticleFlowObjects'] = 'xAOD::FlowElementContainer'
+    JETM2SlimmingHelper.AppendToDictionary['CHSGChargedParticleFlowObjectsAux'] = 'xAOD::ShallowAuxContainer'
+    JETM2SlimmingHelper.AppendToDictionary['CHSGNeutralParticleFlowObjects'] = 'xAOD::FlowElementContainer'
+    JETM2SlimmingHelper.AppendToDictionary['CHSGNeutralParticleFlowObjectsAux'] = 'xAOD::ShallowAuxContainer'
+    JETM2SlimmingHelper.AppendToDictionary["CSSKGNeutralParticleFlowObjects"]='xAOD::FlowElementContainer'
+    JETM2SlimmingHelper.AppendToDictionary["CSSKGNeutralParticleFlowObjectsAux"]='xAOD::ShallowAuxContainer'
+    JETM2SlimmingHelper.AppendToDictionary['UFOCSSK'] = 'xAOD::FlowElementContainer'
+    JETM2SlimmingHelper.AppendToDictionary['UFOCSSKAux'] = 'xAOD::FlowElementAuxContainer'
+    JETM2SlimmingHelper.AppendToDictionary['UFO'] = 'xAOD::FlowElementContainer'
+    JETM2SlimmingHelper.AppendToDictionary['UFOAux'] = 'xAOD::FlowElementAuxContainer'
+    JETM2SlimmingHelper.AppendToDictionary['Kt4UFOCSSKEventShape'] = 'xAOD::EventShape'
+    JETM2SlimmingHelper.AppendToDictionary['Kt4UFOCSSKEventShapeAux'] = 'xAOD::EventShapeAuxInfo'
+    JETM2SlimmingHelper.AppendToDictionary['Kt4UFOCSSKNeutEventShape'] = 'xAOD::EventShape'
+    JETM2SlimmingHelper.AppendToDictionary['Kt4UFOCSSKNeutEventShapeAux'] = 'xAOD::EventShapeAuxInfo'
+
+    from DerivationFrameworkJetEtMiss.JetCommonConfig import addOriginCorrectedClustersToSlimmingTool
+    addOriginCorrectedClustersToSlimmingTool(JETM2SlimmingHelper,writeLC=True,writeEM=True)
 
     # Truth containers
     if ConfigFlags.Input.isMC:
 
         from DerivationFrameworkMCTruth.MCTruthCommonConfig import addTruth3ContentToSlimmerTool
-        addTruth3ContentToSlimmerTool(JETM13SlimmingHelper)
+        addTruth3ContentToSlimmerTool(JETM2SlimmingHelper)
 
-        JETM13SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayParticles'] = 'xAOD::TruthParticleContainer'
-        JETM13SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayParticlesAux'] = 'xAOD::TruthParticleAuxContainer'
-        JETM13SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayVertices'] = 'xAOD::TruthVertexContainer'
-        JETM13SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayVerticesAux'] = 'xAOD::TruthVertexAuxContainer'
-        JETM13SlimmingHelper.AppendToDictionary['TruthParticles'] = 'xAOD::TruthParticleContainer'
-        JETM13SlimmingHelper.AppendToDictionary['TruthParticlesAux'] = 'xAOD::TruthParticleAuxContainer'
+        JETM2SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayParticles'] = 'xAOD::TruthParticleContainer'
+        JETM2SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayParticlesAux'] = 'xAOD::TruthParticleAuxContainer'
+        JETM2SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayVertices'] = 'xAOD::TruthVertexContainer'
+        JETM2SlimmingHelper.AppendToDictionary['TruthTopQuarkWithDecayVerticesAux'] = 'xAOD::TruthVertexAuxContainer'
+        JETM2SlimmingHelper.AppendToDictionary['TruthParticles'] = 'xAOD::TruthParticleContainer'
+        JETM2SlimmingHelper.AppendToDictionary['TruthParticlesAux'] = 'xAOD::TruthParticleAuxContainer'
         
-        JETM13SlimmingHelper.AllVariables += ["TruthTopQuarkWithDecayParticles","TruthTopQuarkWithDecayVertices"]
-        JETM13SlimmingHelper.AllVariables += ["AntiKt4TruthJets", "InTimeAntiKt4TruthJets", "OutOfTimeAntiKt4TruthJets", "TruthParticles"]
-        JETM13SlimmingHelper.SmartCollections += ["AntiKt4TruthWZJets"]
+        JETM2SlimmingHelper.AllVariables += ["TruthTopQuarkWithDecayParticles","TruthTopQuarkWithDecayVertices","TruthHFWithDecayParticles"]
+        JETM2SlimmingHelper.AllVariables += ["AntiKt4TruthJets", "InTimeAntiKt4TruthJets", "OutOfTimeAntiKt4TruthJets", "TruthParticles"]
+        JETM2SlimmingHelper.SmartCollections += ["AntiKt4TruthWZJets"]
 
     # Trigger content
-    JETM13SlimmingHelper.IncludeTriggerNavigation = False
-    JETM13SlimmingHelper.IncludeJetTriggerContent = False
-    JETM13SlimmingHelper.IncludeMuonTriggerContent = False
-    JETM13SlimmingHelper.IncludeEGammaTriggerContent = False
-    JETM13SlimmingHelper.IncludeJetTauEtMissTriggerContent = False
-    JETM13SlimmingHelper.IncludeTauTriggerContent = False
-    JETM13SlimmingHelper.IncludeEtMissTriggerContent = False
-    JETM13SlimmingHelper.IncludeBJetTriggerContent = False
-    JETM13SlimmingHelper.IncludeBPhysTriggerContent = False
-    JETM13SlimmingHelper.IncludeMinBiasTriggerContent = False
+    JETM2SlimmingHelper.IncludeTriggerNavigation = False
+    JETM2SlimmingHelper.IncludeJetTriggerContent = False
+    JETM2SlimmingHelper.IncludeMuonTriggerContent = False
+    JETM2SlimmingHelper.IncludeEGammaTriggerContent = False
+    JETM2SlimmingHelper.IncludeJetTauEtMissTriggerContent = False
+    JETM2SlimmingHelper.IncludeTauTriggerContent = False
+    JETM2SlimmingHelper.IncludeEtMissTriggerContent = False
+    JETM2SlimmingHelper.IncludeBJetTriggerContent = False
+    JETM2SlimmingHelper.IncludeBPhysTriggerContent = False
+    JETM2SlimmingHelper.IncludeMinBiasTriggerContent = False
 
     jetOutputList = ["AntiKt4UFOCSSKNoPtCutJets"]
     from DerivationFrameworkJetEtMiss.JetCommonConfig import addJetsToSlimmingTool
-    addJetsToSlimmingTool(JETM13SlimmingHelper, jetOutputList, JETM13SlimmingHelper.SmartCollections)
+    addJetsToSlimmingTool(JETM2SlimmingHelper, jetOutputList, JETM2SlimmingHelper.SmartCollections)
 
     # Output stream    
-    JETM13ItemList = JETM13SlimmingHelper.GetItemList()
-    acc.merge(OutputStreamCfg(ConfigFlags, "DAOD_JETM13", ItemList=JETM13ItemList, AcceptAlgs=["JETM13Kernel"]))
+    JETM2ItemList = JETM2SlimmingHelper.GetItemList()
+    acc.merge(OutputStreamCfg(ConfigFlags, "DAOD_JETM2", ItemList=JETM2ItemList, AcceptAlgs=["JETM2Kernel"]))
 
     return acc
 
