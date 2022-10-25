7cc5fd73686605750315bba48c439347a76b467a
Scott Snyder <scott.snyder@cern.ch>
2022-10-25 08:54:42 +0200

    TrigInDetPattRecoEvent: Naming cleanup.
    TrigInDetPattRecoEvent: Naming cleanup.
    
    Use TrigInDetSiLayer rather than a C-style typedef.
    In passing, should fix some clang warnings.

--- Trigger/TrigEvent/TrigInDetPattRecoEvent/TrigInDetPattRecoEvent/TrigInDetSiLayer.h
+++ Trigger/TrigEvent/TrigInDetPattRecoEvent/TrigInDetPattRecoEvent/TrigInDetSiLayer.h
@@ -1,16 +1,16 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
-#ifndef __TRIG_INDET_SI_LAYER_H__
-#define __TRIG_INDET_SI_LAYER_H__
+#ifndef TRIGINDETPATTRECOEVENT_TRIGINDETSILAYER_H
+#define TRIGINDETPATTRECOEVENT_TRIGINDETSILAYER_H
 
-typedef struct TrigInDetSiLayer {
+class TrigInDetSiLayer {
 public:
   int m_subdet;//1 : Pixel, 2 : SCT
   int m_type;//0: barrel, +/-n : endcap
   float m_refCoord;
   float m_minBound, m_maxBound;
-} TRIG_INDET_SI_LAYER;
+};
 
 #endif
