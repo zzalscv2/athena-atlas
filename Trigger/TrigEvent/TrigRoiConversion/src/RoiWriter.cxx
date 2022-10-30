9d584c69375072a4e903cad68bd7e2360e5d5cc5
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-10-30 18:55:06 +0100

    TrigRoIConversion: make RoiWriter reentrant
    Make the `RoiWriter` reentrant. Also remove the unused (and not MT-safe)
    overwrite feature.

--- Trigger/TrigEvent/TrigRoiConversion/src/RoiWriter.cxx
+++ Trigger/TrigEvent/TrigRoiConversion/src/RoiWriter.cxx
@@ -1,29 +1,36 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 //
 //   @file    RoiWriter.cxx
-//   
 //
 //   @author M.Sutton
+//   @author F. Winklmeier
 // 
 
 // EDM include(s):
 #include "xAODTrigger/RoiDescriptorStore.h"
 #include "xAODTrigger/RoiDescriptorStoreAuxInfo.h"
 
+// Athena include(s):
+#include "StoreGate/ReadHandle.h"
+#include "StoreGate/WriteHandle.h"
+
 // Local include(s):
 #include "RoiWriter.h"
 #include "TrigRoiConversion/RoiSerialise.h"
 
-RoiWriter::RoiWriter( const std::string& name, ISvcLocator* pSvcLocator )
-   : AthAlgorithm( name, pSvcLocator ) {
-
-   // Declare the property:
-   declareProperty( "ForceOverwrite", m_forceOverwrite = false );
+namespace {
+  // Prefix of the AOD container:
+  static const std::string prefix = "HLT_TrigRoiDescriptorCollection";
+  static const std::string newPrefix = "HLT_xAOD__RoiDescriptorStore";
 }
 
-StatusCode RoiWriter::execute() { 
+RoiWriter::RoiWriter( const std::string& name, ISvcLocator* pSvcLocator )
+   : AthReentrantAlgorithm( name, pSvcLocator ) {
+}
+
+StatusCode RoiWriter::execute(const EventContext& /*ctx*/) const {
 
    ATH_MSG_DEBUG( "In execute()..." );
 
@@ -31,58 +38,43 @@ StatusCode RoiWriter::execute() {
    std::vector< std::string > keys;
    evtStore()->keys< TrigRoiDescriptorCollection >( keys );
 
+   // Buffer for serialisation:
+   std::vector< std::vector< uint32_t > > roiserial;
+
    bool just_dandy = true;
 
    // Loop over these container(s):
    for( const std::string& key : keys ) {
 
-      // Prefix of the AOD container:
-      static const std::string prefix = "HLT_TrigRoiDescriptorCollection";
-      static const std::string newPrefix = "HLT_xAOD__RoiDescriptorStore";
-
       // Construct the key of the new container:
       const std::string newKey = ( ( key.find( prefix ) == 0 ) ?
-				   ( newPrefix + key.substr( prefix.size() ) ) :
+                                   ( newPrefix + key.substr( prefix.size() ) ) :
                                    key );
-      
-      // Check if we need to do anything:
-      if( evtStore()->contains< xAOD::RoiDescriptorStore >( newKey ) &&
-          !m_forceOverwrite ) {
-         continue;
-      }
 
-      // Get the roi desciptor collection
-      const TrigRoiDescriptorCollection* roiCollection = 0;
+      SG::ReadHandle< TrigRoiDescriptorCollection > rh(key);
+      SG::WriteHandle< xAOD::RoiDescriptorStore >   wh(newKey);
 
-      /// check object actually points to something - make sure that we do all 
+      /// check object actually points to something - make sure that we do all
       /// collections that we can, and not barf on the first one that might fail  
-      if ( evtStore()->retrieve( roiCollection, key ).isSuccess() && roiCollection!=0 ) {
+      if ( rh.isValid() ) {
 
-	// Create the "new payload" from it:
-	std::vector< std::vector< uint32_t > > roiserial;
-	RoiUtil::serialise( *roiCollection, roiserial );
-	
-	// Create the xAOD objects:
-	std::unique_ptr< xAOD::RoiDescriptorStore >         store( new xAOD::RoiDescriptorStore() );
-	std::unique_ptr< xAOD::RoiDescriptorStoreAuxInfo >    aux( new xAOD::RoiDescriptorStoreAuxInfo() );
-	store->setStore( aux.get() );
-	store->setSerialised( roiserial );
-	
-	// Record the new container:
-	if( evtStore()->contains< xAOD::RoiDescriptorStore >( newKey ) ) {
-	  ATH_CHECK( evtStore()->overwrite( std::move( store ), newKey, false ) );
-	  ATH_CHECK( evtStore()->overwrite( std::move( aux ),   newKey + "Aux.", false ) );
-	  
-	} else {
-	  ATH_CHECK( evtStore()->record( std::move( store ), newKey ) );
-	  ATH_CHECK( evtStore()->record( std::move( aux ),   newKey + "Aux." ) );
-	}
+         // Create the "new payload" from it:
+         roiserial.clear();
+         RoiUtil::serialise( *rh, roiserial );
+
+         // Create the xAOD objects:
+         auto store = std::make_unique< xAOD::RoiDescriptorStore >();
+         auto aux   = std::make_unique< xAOD::RoiDescriptorStoreAuxInfo >();
+         store->setStore( aux.get() );
+         store->setSerialised( roiserial );
+
+         // Record the new container:
+         ATH_CHECK( wh.record( std::move( store ), std::move( aux ) ) );
       }
       else just_dandy = false;
    }
 
    // Return gracefully:
-   if ( just_dandy ) return StatusCode::SUCCESS;
-   else              return StatusCode::FAILURE;
+   return just_dandy ? StatusCode::SUCCESS : StatusCode::FAILURE;
 }
 
--- Trigger/TrigEvent/TrigRoiConversion/src/RoiWriter.h
+++ Trigger/TrigEvent/TrigRoiConversion/src/RoiWriter.h
@@ -1,17 +1,9 @@
 // emacs: this is -*- c++ -*-
 
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
-//
-//   @file    RoiWriter.h        
-//
-//                   
-//  
-//
-//   $Id: RoiWriter.h 781778 2016-11-01 23:42:26Z sutt $
-//
 #ifndef TRIGROICONVERSION_ROIWRITER_H
 #define TRIGROICONVERSION_ROIWRITER_H
 
@@ -19,7 +11,7 @@
 #include <string>
 
 // Athena include(s):
-#include "AthenaBaseComps/AthAlgorithm.h"
+#include "AthenaBaseComps/AthReentrantAlgorithm.h"
 
 // EDM include(s):
 #include "TrigSteeringEvent/TrigRoiDescriptorCollection.h"
@@ -29,23 +21,17 @@
 /// This algorithm creates xAOD::RoiDescriptorStore objects during AOD writing.
 ///
 /// @author M. Sutton
+/// @author F. Winklmeier
 ///
-/// $Revision: 781778 $
-/// $Date: 2016-11-02 00:42:26 +0100 (Wed, 02 Nov 2016) $
-///
-class RoiWriter : public AthAlgorithm {
+class RoiWriter : public AthReentrantAlgorithm {
 
 public:
   /// Algorithm constructor
   RoiWriter( const std::string& name, ISvcLocator* pSvcLocator );
   
   /// Execute the conversion
-  StatusCode execute();
+  virtual StatusCode execute(const EventContext& ctx) const override;
   
-private:
-   /// Force the overwrite of the xAOD container(s)
-   bool m_forceOverwrite;
-
 }; // class RoiWriter
 
 #endif // TRIGROICONVERSION_ROIWRITER_H
