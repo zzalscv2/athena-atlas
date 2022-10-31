59cb9d1fb19c409a1f17d3a738ec7e6e10d9b0bb
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2022-10-31 16:39:42 +0100

    Externals Update, master branch (2022.10.28.)
    Updated all projects to atlasexternals-2.0.142.
    
    Also updated all build_externals.sh scripts to the new formalism for
    overriding the version of the externals to be built.

--- Projects/AthSimulation/build_externals.sh
+++ Projects/AthSimulation/build_externals.sh
@@ -11,8 +11,8 @@ ATLAS_EXT_PROJECT_NAME="AthSimulationExternals"
 ATLAS_BUILDTYPE="RelWithDebInfo"
 ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=101
                         -DLCG_VERSION_POSTFIX="_ATLAS_26"
-                        -DATLAS_GAUDI_TAG="v36r6.002"
-                        -DATLAS_GEOMODEL_TAG="4.4.0")
+                        -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v36r6.002/Gaudi-v36r6.002.tar.gz;URL_MD5;db01383a77b482a881970d834c3af091"
+                        -DATLAS_GEOMODEL_SOURCE="URL;https://gitlab.cern.ch/GeoModelDev/GeoModel/-/archive/4.4.0/GeoModel-4.4.0.tar.bz2;URL_MD5;c339b961e1d2f0c48b9807314568c8c2")
 ATLAS_EXTRA_MAKE_ARGS=()
 
 # Let "the common script" do all the heavy lifting.
