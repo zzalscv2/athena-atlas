e63d035e8e1d52e7e96540181b54fd98905dbcf3
Jon Burr <jon.burr@cern.ch>
2022-11-02 13:35:07 +0100

    Allow setting container selections directly in the MET maker
    Allow setting container selections directly in the MET maker

--- PhysicsAnalysis/Algorithms/MetAnalysisAlgorithms/Root/MetMakerAlg.cxx
+++ PhysicsAnalysis/Algorithms/MetAnalysisAlgorithms/Root/MetMakerAlg.cxx
@@ -15,6 +15,8 @@
 #include <xAODMissingET/MissingETAuxContainer.h>
 #include <xAODMissingET/MissingETAssociationHelper.h>
 
+#include "AthContainers/ConstDataVector.h"
+
 //
 // method implementations
 //
@@ -53,6 +55,10 @@ namespace CP
                          &m_muonsHandle, &m_tausHandle, &m_invisHandle}) {
       ANA_CHECK (handle->initialize (m_systematicsList, SG::AllowEmpty));
     }
+    ANA_CHECK (m_electronsSelection.initialize(m_systematicsList, m_electronsHandle, SG::AllowEmpty));
+    ANA_CHECK (m_muonsSelection.initialize(m_systematicsList, m_muonsHandle, SG::AllowEmpty));
+    ANA_CHECK (m_photonsSelection.initialize(m_systematicsList, m_photonsHandle, SG::AllowEmpty));
+    ANA_CHECK (m_tausSelection.initialize(m_systematicsList, m_tausHandle, SG::AllowEmpty));
     ANA_CHECK (m_jetsHandle.initialize (m_systematicsList));
     ANA_CHECK (m_metHandle.initialize (m_systematicsList));
 
@@ -99,6 +105,7 @@ namespace CP
       // (and photons).
       auto processParticles =
         [&] (SysReadHandle<xAOD::IParticleContainer>& handle,
+             SysReadSelectionHandle &selection,
              xAOD::Type::ObjectType type,
              const std::string& term) -> StatusCode {
           if (!handle) {
@@ -106,19 +113,24 @@ namespace CP
           }
           const xAOD::IParticleContainer* particles = nullptr;
           ANA_CHECK (handle.retrieve (particles, sys));
+          ConstDataVector<xAOD::IParticleContainer> selected(SG::VIEW_ELEMENTS);
+          for (const xAOD::IParticle *particle : *particles)
+              if (selection.getBool(*particle, sys))
+                  selected.push_back(particle);
           ANA_CHECK (m_makerTool->rebuildMET (term, type, met.get(),
-                                              particles, metHelper));
+                                              selected.asDataVector(), metHelper));
           return StatusCode::SUCCESS;
         };
 
       // Calculate the terms coming from the user's selected objects.
-      ANA_CHECK (processParticles (m_electronsHandle, xAOD::Type::Electron,
-                                   m_electronsKey));
-      ANA_CHECK (processParticles (m_photonsHandle, xAOD::Type::Photon,
-                                   m_photonsKey));
-      ANA_CHECK (processParticles (m_tausHandle, xAOD::Type::Tau, m_tausKey));
-      ANA_CHECK (processParticles (m_muonsHandle, xAOD::Type::Muon,
-                                   m_muonsKey));
+      ANA_CHECK (processParticles (m_electronsHandle, m_electronsSelection, 
+                                   xAOD::Type::Electron, m_electronsKey));
+      ANA_CHECK (processParticles (m_photonsHandle, m_photonsSelection, 
+                                   xAOD::Type::Photon, m_photonsKey));
+      ANA_CHECK (processParticles (m_tausHandle, m_tausSelection, 
+                                   xAOD::Type::Tau, m_tausKey));
+      ANA_CHECK (processParticles (m_muonsHandle, m_muonsSelection, 
+                                   xAOD::Type::Muon, m_muonsKey));
 
       const xAOD::JetContainer *jets {nullptr};
       ANA_CHECK (m_jetsHandle.retrieve (jets, sys));
