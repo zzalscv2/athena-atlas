6f3663934946174b91b46f0ee18bda5288aae92e
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-11-02 10:59:18 +0100

    JetEventTPCnv: make JetCnvProperties const
    None of the `JetCnvProperties` are actually set. Remove them or make
    them const to avoid thread-checker warnings.

--- Reconstruction/Jet/JetEventTPCnv/JetEventTPCnv/JetCnvProperties.h
+++ Reconstruction/Jet/JetEventTPCnv/JetEventTPCnv/JetCnvProperties.h
@@ -1,26 +1,21 @@
 ///////////////////////// -*- C++ -*- /////////////////////////////
 
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef JETEVENTTPCNV_JETCNVPROPERTIES_H 
 #define JETEVENTTPCNV_JETCNVPROPERTIES_H 
 
-#include <string>
-#include <vector>
-
-
-
 class JetCnvProperties {
-public:
-
-  std::vector<std::string> & get_no_constit_colls(){return s_write0constit_colls;}
-  void set_no_constit(bool v){s_write0constit = v;}
-
 protected:
-  static std::vector<std::string> s_write0constit_colls;
-  static bool s_write0constit;
+  /**
+   * Write Jet constituents.
+   *
+   * Used in JetCnv_p5/6. This flag used to be configurable but
+   * since it was never set to true, it is now const.
+   */
+  inline static const bool s_write0constit{false};
   
 };
 
