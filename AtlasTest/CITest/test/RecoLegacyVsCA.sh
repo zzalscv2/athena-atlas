6c6fc1a3031ff1a78b7ae727bc2d25ca6d60a160
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2022-11-02 13:48:14 +0100

    CITest: add diff-root in CA vs. legacy reco configuration comparison
    Add RecoRun3Data_CAConfig and  RecoRun3Data_LegacyVsCA test, increase the CA reco tests to 25 events, add a diff-root test in the CA vs. Legacy configuration comparison

--- AtlasTest/CITest/test/RecoLegacyVsCA.sh
+++ AtlasTest/CITest/test/RecoLegacyVsCA.sh
@@ -7,3 +7,6 @@ xAODDigest.py --extravars "../${1}/run_${2}/myAOD.pool.root" "${1}_${2}_digest_l
 xAODDigest.py --extravars "../${1}_CAConfig/run_${2}/myAOD.pool.root" "${1}_${2}_digest_CA.txt"
 
 comparexAODDigest.py "${1}_${2}_digest_legacy.txt" "${1}_${2}_digest_CA.txt" --ignoreMuons --ignoreMET
+
+
+acmd.py diff-root "../${1}/run_${2}/myAOD.pool.root" "../${1}_CAConfig/run_${2}/myAOD.pool.root" --nan-equal --error-mode resilient --ignore-leaves RecoTimingObj_p1_HITStoRDO_timings RecoTimingObj_p1_RAWtoESD_mems RecoTimingObj_p1_RAWtoESD_timings RAWtoESD_mems RAWtoESD_timings ESDtoAOD_mems ESDtoAOD_timings HITStoRDO_timings RAWtoALL_mems RAWtoALL_timings RecoTimingObj_p1_RAWtoALL_mems RecoTimingObj_p1_RAWtoALL_timings RecoTimingObj_p1_EVNTtoHITS_timings index_ref MuonsLRTAux MuonTruthParticlesAuxDyn MSOnlyExtrapolatedMuonTrackParticlesAux ExtrapolatedMuonTrackParticlesAux CombinedMuonTrackParticlesAux ExtraPolatedMuonsLRTTrackParticlesAux EMEO_CombinedMuonTrackParticlesAux EMEO_ExtrapolatedMuonTrackParticlesAux CombinedMuonsLRTTrackParticlesAux CombinedStauTrackParticlesAux EMEO_MSOnlyExtrapolatedMuonTrackParticlesAux SlowMuonsAux InDetForwardTrackParticlesAux TrigNavigationAux EventInfo --order-trees --entries 5 --mode semi-detailed
