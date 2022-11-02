41f4606e41fdfbba5d709d89ed748c69835cd88b
Scott Snyder <scott.snyder@cern.ch>
2022-11-02 10:55:39 +0100

    AthenaMonitoring: Remove references to EventInfo.
    AthenaMonitoring: Remove references to EventInfo.
    
    Remove a couple remaining references to the old EventInfo package.

--- Control/AthenaMonitoring/src/DQFilledBunchFilterTool.cxx
+++ Control/AthenaMonitoring/src/DQFilledBunchFilterTool.cxx
@@ -4,7 +4,6 @@
 
 #include "DQFilledBunchFilterTool.h"
 #include "AthenaKernel/errorcheck.h"
-#include "EventInfo/EventID.h"
 
 DQFilledBunchFilterTool::DQFilledBunchFilterTool(const std::string& type,const std::string& name,const IInterface* parent)
 : AthAlgTool( type, name, parent )
@@ -31,7 +30,7 @@ bool DQFilledBunchFilterTool::accept() const {
     return true;
   } else {
     const EventContext& ctx = Gaudi::Hive::currentContext();
-    EventID::number_type bcid = ctx.eventID().bunch_crossing_id();
+    EventIDBase::number_type bcid = ctx.eventID().bunch_crossing_id();
     SG::ReadCondHandle<BunchCrossingCondData> bcData(m_bcDataKey, ctx);
     bool value = bcData->isFilled(bcid) ^ m_invert;
     ATH_MSG_VERBOSE("Filled bunch DQ tool accept called, value " << value);
