622a3d57357d00016010e062010eac59c030155c
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2022-11-03 16:59:42 +0100

    TrfTestsARTPlots: Add mc20 and mc21 AODtoDAOD CA tests
    TrfTestsARTPlots: Add mc20 and mc21 AODtoDAOD CA tests

---
+++ Tools/TrfTestsARTPlots/test/test_trf_mc20_phys_physlite_ca.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# art-description: Derivation_tf.py mc20 in MP and CA
+# art-type: grid
+# art-include: master/Athena
+# art-athena-mt: 8
+# art-output: dcube*
+# art-html: dcube_physlite
+
+export ATHENA_CORE_NUMBER=8
+AODFILE=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/input/v0/1000events_mc20_ttbar.AOD.pool.root 
+
+Derivation_tf.py \
+  --CA \
+  --inputAODFile $AODFILE \
+  --outputDAODFile art.pool.root \
+  --formats PHYS PHYSLITE \
+  --multiprocess True \
+  --sharedWriter True \
+  --postExec 'from AthenaAuditors.AthenaAuditorsConf import FPEAuditor;FPEAuditor.NStacktracesOnFPE=10;' \
+  --maxEvents 1000
+
+rcderiv=$?
+echo "art-result: ${rcderiv} Derivation_tf_mc20_phys_physlite_ca" 
+
+# Check for FPEs in the logiles
+test_trf_check_fpe.sh
+fpeStat=$?
+
+echo "art-result: ${fpeStat} FPEs in logfiles"
+
+echo "============ checkxAOD AOD.27162646._000001.pool.root.1"
+checkxAOD $AODFILE
+echo "============ checkxAOD DAOD_PHYS.art.pool.root"
+checkxAOD DAOD_PHYS.art.pool.root
+echo "============ checkxAOD DAOD_PHYSLITE.art.pool.root"
+checkxAOD DAOD_PHYSLITE.art.pool.root
+rc2=$?
+echo "art-result: ${rc2} checkxAOD" 
+
+echo "============ xAODHist DAOD_PHYSLITE.art.pool.root"
+xAODHist.py --analysis --outputHISTFile hist_physlite_latest.root DAOD_PHYSLITE.art.pool.root 
+rc3=$?
+echo "art-result: ${rc3} xAODHist DAOD_PHYSLITE.art.pool.root" 
+
+echo "============ xAODHistSize DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,tmp.AOD"
+xAODHistSize.py --outputHISTFile hist_physlite_latest.root --xAODFiles DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,$AODFILE
+rc4=$?
+echo "art-result: ${rc4} xAODHistSize" 
+
+# dcube references
+echo "============ dcube references"
+dcubeRef="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/dcube/mc20/v0/hist_physlite_2308.root"
+dcubeXML="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/dcube/mc20/v0/dcube_config_hist_physlite_2308.xml"
+echo ${dcubeRef}
+echo ${dcubeXML}
+
+# Run dcube comparison
+echo "============ dcube"
+$ATLAS_LOCAL_ROOT/dcube/current/DCubeClient/python/dcube.py -p --jobId PHYSLITETest -c ${dcubeXML} -r ${dcubeRef} -x dcube_physlite hist_physlite_latest.root
+rc5=$?
+echo "art-result: ${rc5} dcube_physlite" 
---
+++ Tools/TrfTestsARTPlots/test/test_trf_mc21_phys_physlite_ca.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# art-description: Derivation_tf.py mc21 in MP and CA
+# art-type: grid
+# art-include: master/Athena
+# art-athena-mt: 8
+# art-output: dcube*
+# art-html: dcube_physlite
+
+export ATHENA_CORE_NUMBER=8
+AODFILE=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/input/v0/1000events_mc21_ttbar.AOD.pool.root 
+Derivation_tf.py \
+  --CA \
+  --inputAODFile $AODFILE \
+  --outputDAODFile art.pool.root \
+  --formats PHYS PHYSLITE \
+  --multiprocess True \
+  --sharedWriter True \
+  --postExec 'from AthenaAuditors.AthenaAuditorsConf import FPEAuditor;FPEAuditor.NStacktracesOnFPE=10;' \
+  --maxEvents 1000
+
+rcderiv=$?
+echo "art-result: ${rcderiv} Derivation_tf_phys_physlite_ca" 
+
+# Check for FPEs in the logiles
+test_trf_check_fpe.sh
+fpeStat=$?
+
+echo "art-result: ${fpeStat} FPEs in logfiles"
+
+echo "============ checkxAOD ${AODFILE}"
+checkxAOD $AODFILE
+echo "============ checkxAOD DAOD_PHYS.art.pool.root"
+checkxAOD DAOD_PHYS.art.pool.root
+echo "============ checkxAOD DAOD_PHYSLITE.art.pool.root"
+checkxAOD DAOD_PHYSLITE.art.pool.root
+rc2=$?
+echo "art-result: ${rc2} checkxAOD" 
+
+echo "============ xAODHist DAOD_PHYSLITE.art.pool.root"
+xAODHist.py --analysis --outputHISTFile hist_physlite_latest.root DAOD_PHYSLITE.art.pool.root 
+rc3=$?
+echo "art-result: ${rc3} xAODHist DAOD_PHYSLITE.art.pool.root" 
+
+echo "============ xAODHistSize DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,AOD.pool.root"
+xAODHistSize.py --outputHISTFile hist_physlite_latest.root --xAODFiles DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,$AODFILE
+rc4=$?
+echo "art-result: ${rc4} xAODHistSize" 
+
+# dcube references
+echo "============ dcube references"
+dcubeRef="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/dcube/mc21/v0/hist_physlite_2308.root"
+dcubeXML="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/dcube/mc21/v0/dcube_config_hist_physlite_2308.xml"
+echo ${dcubeRef}
+echo ${dcubeXML}
+
+# Run dcube comparison
+echo "============ dcube"
+$ATLAS_LOCAL_ROOT/dcube/current/DCubeClient/python/dcube.py -p --jobId PHYSLITETest -c ${dcubeXML} -r ${dcubeRef} -x dcube_physlite hist_physlite_latest.root
+rc5=$?
+echo "art-result: ${rc5} dcube_physlite" 
