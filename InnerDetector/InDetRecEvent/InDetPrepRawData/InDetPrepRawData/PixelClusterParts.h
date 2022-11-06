fdea7459fa352a7e5830e9cbe43d1e6d3e8d97d3
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2022-11-06 20:01:26 +0100

    PixelClusterParts avoid allocations default ctors
    PixelClusterParts avoid allocations default ctors

--- InnerDetector/InDetRecEvent/InDetPrepRawData/InDetPrepRawData/PixelClusterParts.h
+++ InnerDetector/InDetRecEvent/InDetPrepRawData/InDetPrepRawData/PixelClusterParts.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -12,130 +12,119 @@
 #ifndef TRKPREPRAWDATA_PIXELCLUSTERPARTS_H
 #define TRKPREPRAWDATA_PIXELCLUSTERPARTS_H
 
-#include "Identifier/Identifier.h"
-#include "GeoPrimitives/GeoPrimitives.h"
 #include "EventPrimitives/EventPrimitives.h"
+#include "GeoPrimitives/GeoPrimitives.h"
+#include "Identifier/Identifier.h"
 
 namespace InDet {
-    /** @class PixelClusterParts 
-   
-   groups Idetifier, tot vector and lvl1 vector together 
-   facilitates interface of IClusterSplitter
-   
-   @author Andreas.Salzburger@cern.ch  
-   */
-  class PixelClusterParts {
+/** @class PixelClusterParts
 
-    public :
-        /** constructor */
-    PixelClusterParts(const std::vector<Identifier>& group,
-        const std::vector<int>& totgroup,
-        const std::vector<int>& lvl1group) :
-        m_idgroup(group),
-        m_totgroup(totgroup),
-        m_lvl1group(lvl1group),
-        m_localPosition(0),
-        m_errorMatrix(0)
-        {}    
+groups Idetifier, tot vector and lvl1 vector together
+facilitates interface of IClusterSplitter
 
-        /** constructor */
-    PixelClusterParts(const std::vector<Identifier>& group,
-        const std::vector<int>& totgroup,
-        const std::vector<int>& lvl1group,
-        const Amg::Vector2D& position) :
-        m_idgroup(group),
-        m_totgroup(totgroup),
-        m_lvl1group(lvl1group),
-        m_localPosition(new Amg::Vector2D(position)),
-        m_errorMatrix(0)
-        {}
+@author Andreas.Salzburger@cern.ch
+@author Christos Anastopoulos MT related changes
+*/
+class PixelClusterParts
+{
 
-        /** constructor */
-    PixelClusterParts(const std::vector<Identifier>& group,
-        const std::vector<int>& totgroup,
-        const std::vector<int>& lvl1group,
-        const Amg::Vector2D& position,
-        const Amg::MatrixX& error) :
-        m_idgroup(group),
-        m_totgroup(totgroup),
-        m_lvl1group(lvl1group),
-        m_localPosition(new Amg::Vector2D(position)),
-        m_errorMatrix(new Amg::MatrixX(error))
-        {}
+public:
+  /** constructor */
+  PixelClusterParts(const std::vector<Identifier>& group,
+                    const std::vector<int>& totgroup,
+                    const std::vector<int>& lvl1group)
+    : m_idgroup(group)
+    , m_totgroup(totgroup)
+    , m_lvl1group(lvl1group)
+    , m_localPosition(Amg::Vector2D::Zero())
+    , m_errorMatrix{}
+  {
+  }
 
+  /** constructor */
+  PixelClusterParts(const std::vector<Identifier>& group,
+                    const std::vector<int>& totgroup,
+                    const std::vector<int>& lvl1group,
+                    const Amg::Vector2D& position)
+    : m_idgroup(group)
+    , m_totgroup(totgroup)
+    , m_lvl1group(lvl1group)
+    , m_localPosition(position)
+    , m_errorMatrix{}
+  {
+  }
 
-        /** copy constructor */
-    PixelClusterParts(const PixelClusterParts& pcp) : 
-        m_idgroup(pcp.m_idgroup),
-        m_totgroup(pcp.m_totgroup),
-        m_lvl1group(pcp.m_lvl1group),
-        m_localPosition( pcp.m_localPosition ? new Amg::Vector2D(*pcp.m_localPosition) : 0),
-        m_errorMatrix(pcp.m_errorMatrix ? new Amg::MatrixX(*pcp.m_errorMatrix) : 0)
-        {}    
+  /** constructor */
+  PixelClusterParts(const std::vector<Identifier>& group,
+                    const std::vector<int>& totgroup,
+                    const std::vector<int>& lvl1group,
+                    const Amg::Vector2D& position,
+                    const Amg::MatrixX& error)
+    : m_idgroup(group)
+    , m_totgroup(totgroup)
+    , m_lvl1group(lvl1group)
+    , m_localPosition(position)
+    , m_errorMatrix(error)
+  {
+  }
 
-        /** move constructor */
-    PixelClusterParts(PixelClusterParts&& pcp) noexcept :
-        m_idgroup(std::move(pcp.m_idgroup)),
-        m_totgroup(std::move(pcp.m_totgroup)),
-        m_lvl1group(std::move(pcp.m_lvl1group)),
-        m_localPosition( pcp.m_localPosition ),
-        m_errorMatrix(pcp.m_errorMatrix)
-        {
-            pcp.m_localPosition= nullptr;
-            pcp.m_errorMatrix  = nullptr;
-        }
+  /** default ctors and move  */
+  PixelClusterParts(const PixelClusterParts& pcp) = default;
+  PixelClusterParts(PixelClusterParts&& pcp) noexcept = default;
+  PixelClusterParts& operator=(const PixelClusterParts& pcp) = default;
+  PixelClusterParts& operator=(PixelClusterParts&& pcp) noexcept = default;
+  /** destructor */
+  ~PixelClusterParts() = default;
 
+  /** return method identifiers */
+  const std::vector<Identifier>& identifierGroup() const;
 
-        /** assignment operator*/
-    PixelClusterParts& operator=(const PixelClusterParts& pcp){
-        if (this != &pcp){
-            m_idgroup = pcp.m_idgroup;
-            m_totgroup = pcp.m_totgroup;
-            m_lvl1group = pcp.m_lvl1group;
-            m_localPosition =  pcp.m_localPosition ? new Amg::Vector2D(*pcp.m_localPosition) : 0 ;
-            m_errorMatrix = pcp.m_errorMatrix ? new Amg::MatrixX(*pcp.m_errorMatrix) : 0 ;
-       
-        }
-        return (*this);
-    }
+  /** return method tot */
+  const std::vector<int>& totGroup() const;
 
-        /** destructor */
-    ~PixelClusterParts(){
-        delete m_localPosition;
-        delete m_errorMatrix;
-    }
+  /** return method lvl1 Group */
+  const std::vector<int>& lvl1Group() const;
 
-        /** return method identifiers */
-    const std::vector<Identifier>& identifierGroup() const;
+  /** return lcoal positions */
+  const Amg::Vector2D& localPosition() const;
 
-        /** return method tot */
-    const std::vector<int>&        totGroup() const;
+  /** return error description */
+  const Amg::MatrixX& errorMatrix() const;
 
-        /** return method lvl1 Group */
-    const std::vector<int>&        lvl1Group() const;
-
-        /** return lcoal positions */
-    const Amg::Vector2D* localPosition() const;
-
-        /** return error description */
-    const Amg::MatrixX* errorMatrix() const;
-
-    private :    
-
-    std::vector<Identifier>             m_idgroup;
-    std::vector<int>                    m_totgroup;
-    std::vector<int>                    m_lvl1group; 
-    Amg::Vector2D*                 m_localPosition;
-    Amg::MatrixX*                   m_errorMatrix;
-
-};    
+private:
+  std::vector<Identifier> m_idgroup;
+  std::vector<int> m_totgroup;
+  std::vector<int> m_lvl1group;
+  Amg::Vector2D m_localPosition;
+  Amg::MatrixX m_errorMatrix;
+};
 
 // inline implementation of return methods
-inline const std::vector<Identifier>& PixelClusterParts::identifierGroup() const { return m_idgroup; }
-inline const std::vector<int>& PixelClusterParts::totGroup() const { return m_totgroup; }
-inline const std::vector<int>& PixelClusterParts::lvl1Group() const { return m_lvl1group; }
-inline const Amg::Vector2D* PixelClusterParts::localPosition() const { return (m_localPosition); }
-inline const Amg::MatrixX* PixelClusterParts::errorMatrix() const { return (m_errorMatrix); }
+inline const std::vector<Identifier>&
+PixelClusterParts::identifierGroup() const
+{
+  return m_idgroup;
+}
+inline const std::vector<int>&
+PixelClusterParts::totGroup() const
+{
+  return m_totgroup;
+}
+inline const std::vector<int>&
+PixelClusterParts::lvl1Group() const
+{
+  return m_lvl1group;
+}
+inline const Amg::Vector2D&
+PixelClusterParts::localPosition() const
+{
+  return m_localPosition;
+}
+inline const Amg::MatrixX&
+PixelClusterParts::errorMatrix() const
+{
+  return m_errorMatrix;
+}
 
 }
 
