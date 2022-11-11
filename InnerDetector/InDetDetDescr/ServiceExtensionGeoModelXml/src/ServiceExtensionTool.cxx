f68c68b1ee66a993ff2aa9ad75dd0cfacad193b5
Nicholas Styles <nicholas.styles@desy.de>
2022-11-11 07:05:47 +0100

    Upgrade support for geometry building from .sqlite files
    Upgrade support for geometry building from .sqlite files

--- InnerDetector/InDetDetDescr/ServiceExtensionGeoModelXml/src/ServiceExtensionTool.cxx
+++ InnerDetector/InDetDetDescr/ServiceExtensionGeoModelXml/src/ServiceExtensionTool.cxx
@@ -27,20 +27,25 @@ StatusCode ServiceExtensionTool::create()
   GeoModelExperiment *theExpt = nullptr;
   ATH_CHECK(detStore()->retrieve(theExpt, "ATLAS"));
   auto *manager = new ServiceExtensionManager();
-  if (!isAvailable(m_node, m_table)) {
-      ATH_MSG_ERROR("No ServiceExtension geometry found. ServiceExtension can not be built.");
-      return StatusCode::FAILURE;
+
+  const GeoModelIO::ReadGeoModel* sqlreader = getSqliteReader();
+   
+  if(!sqlreader){ 
+      if (!isAvailable(m_node, m_table)) {
+          ATH_MSG_ERROR("No ServiceExtension geometry found. ServiceExtension can not be built.");
+          return StatusCode::FAILURE;
+      }
   }
   ATH_MSG_INFO("Building Service Extension");
   
   GeoPhysVol *world = &*theExpt->getPhysVol();
 
-  // Load the geometry, create the volume,
-  // and then find the volume index within the world to allow it to be added
-  // last two arguments are the location in the DB to look for the clob
-  // (may want to make those configurables)
+  // Load the geometry, create the volume, 
+  // node,table are the location in the DB to look for the clob
+  // empty strings are the (optional) containing detector and envelope names
+  // allowed to pass a null sqlreader ptr - it will be used to steer the source of the geometry
   GmxInterface gmxInterface;
-  const GeoVPhysVol * topVol = createTopVolume(world, gmxInterface, m_node,m_table, m_containingDetectorName, m_envelopeVolumeName);
+  const GeoVPhysVol * topVol = createTopVolume(world, gmxInterface, m_node,m_table, m_containingDetectorName, m_envelopeVolumeName,sqlreader);
   if (!topVol) {
     ATH_MSG_FATAL("Could not find the Top Volume!!!");
     return StatusCode::FAILURE;
