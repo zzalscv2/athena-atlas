8dd4bb631eabdb53d4940c691e30c023e9b0c981
leggett <leggett@cern.ch>
2022-11-15 09:45:26 -0800

    update Gaudi version to v36r6.003

--- Projects/AthSimulation/build_externals.sh
+++ Projects/AthSimulation/build_externals.sh
@@ -11,7 +11,7 @@ ATLAS_EXT_PROJECT_NAME="AthSimulationExternals"
 ATLAS_BUILDTYPE="RelWithDebInfo"
 ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=101
                         -DLCG_VERSION_POSTFIX="_ATLAS_26"
-                        -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v36r6.002/Gaudi-v36r6.002.tar.gz;URL_MD5;db01383a77b482a881970d834c3af091"
+                        -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v36r6.003/Gaudi-v36r6.003.tar.gz;URL_MD5;e7b279804438a7c68b190084fe2592a7"
                         -DATLAS_GEOMODEL_SOURCE="URL;https://gitlab.cern.ch/GeoModelDev/GeoModel/-/archive/4.4.0/GeoModel-4.4.0.tar.bz2;URL_MD5;c339b961e1d2f0c48b9807314568c8c2")
 ATLAS_EXTRA_MAKE_ARGS=()
 
