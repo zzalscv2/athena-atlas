29891eb25f0ae547a824f1a60cd8b8c8ef18361a
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-11-18 12:15:02 +0100

    Less expensive calls to barcode

--- Simulation/ISF/ISF_HepMC/ISF_HepMC_Tools/src/GenParticleSimWhiteList.cxx
+++ Simulation/ISF/ISF_HepMC/ISF_HepMC_Tools/src/GenParticleSimWhiteList.cxx
@@ -87,10 +87,11 @@ bool ISF::GenParticleSimWhiteList::pass(HepMC::ConstGenParticlePtr particle) con
   bool so_far_so_good = pass( particle , vertices );
 
   // Test all parent particles
+  const int barcode_of_particle = HepMC::barcode(particle);
   if (so_far_so_good && particle->production_vertex() && m_qs){
     for (auto pit: particle->production_vertex()->particles_in()){
       // Loop breaker
-      if ( HepMC::barcode(pit) == HepMC::barcode(particle) ) continue;
+      if ( HepMC::barcode(pit) == barcode_of_particle ) continue;
       // Check this particle
       vertices.clear();
       bool parent_all_clear = pass( pit , vertices );
