8c45db95f2feb246e780955e58d9d3e35454a341
Cristiano Alpigiani <cristiano.alpigiani@cern.ch>
2022-11-21 09:21:01 +0100

    Moving master derivation ARTs to CA
    Updating master derivation ARTs to CA
    
    This MR updates all derivation ARTs in master to use the CA. Given all format have been migrated to CA, legacy tests are not needed anymore. I took this opportunity to update also the input files, now containing 1k events.

---
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkJetEtMissART/test/test_mc21JETM10.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# art-include: master/Athena
+# art-description: DAOD building JETM10 mc21
+# art-type: grid
+# art-output: *.pool.root
+# art-output: checkFile*.txt
+# art-output: checkxAOD*.txt
+# art-output: checkIndexRefs*.txt
+
+set -e
+
+Derivation_tf.py \
+--CA True \
+--inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/CampaignInputs/mc21/AOD/mc21_13p6TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.AOD.e8453_s3873_r13829/1000events.AOD.29787656._000153.pool.root.1 \
+--outputDAODFile art.pool.root \
+--formats JETM10 \
+--maxEvents -1 \
+
+echo "art-result: $? reco"
+
+checkFile.py DAOD_JETM10.art.pool.root > checkFile_JETM10.txt
+
+echo "art-result: $?  checkfile"
+
+checkxAOD.py DAOD_JETM10.art.pool.root > checkxAOD_JETM10.txt
+
+echo "art-result: $?  checkxAOD"
+
+checkIndexRefs.py DAOD_JETM10.art.pool.root > checkIndexRefs_JETM10.txt 2>&1
+
+echo "art-result: $?  checkIndexRefs"
