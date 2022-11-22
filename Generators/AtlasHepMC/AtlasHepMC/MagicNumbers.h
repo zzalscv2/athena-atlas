1eb7d296530be7a3dcfe2916cf6bb34f9505b5d2
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2022-11-22 14:20:31 +0100

    Add header with magic numbers that will be used for simulation particle selsection
    Add header with magic numbers that will be used for simulation particle selsection

---
+++ Generators/AtlasHepMC/AtlasHepMC/MagicNumbers.h
@@ -0,0 +1,27 @@
+/*
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+*/
+/* Author: Andrii Verbytskyi andrii.verbytskyi@mpp.mpg.de */
+
+#ifndef ATLASHEPMC_MAGICNUMBERS_H
+#define ATLASHEPMC_MAGICNUMBERS_H
+namespace HepMC {
+/// @brief Constant defining the barcode threshold distinguishing generator record entries from detector sim ones
+/// @todo The sim barcodes start at 1M in MC15, so we should update the 200k threshold,
+///   but >= 200k is still a valid test for b = 1M so let's keep it this way until MC12 is long-dead.
+constexpr int SIM_BARCODE_THRESHOLD = 200000;
+constexpr int SIM_REGENERATION_INCREMENT = 1000000;
+
+constexpr int PARTONPDGMAX = 43;
+constexpr int NPPDGMIN = 1000000;
+constexpr int NPPDGMAX = 8999999;
+constexpr int PHOTOSMIN = 10000;
+
+template <class T>  inline bool is_simulation_particle(T p){ return (barcode(p)>SIM_BARCODE_THRESHOLD);}
+template <>  inline bool is_simulation_particle(int b){ return (b>SIM_BARCODE_THRESHOLD);}
+
+template <class T>  inline bool is_simulation_vertex(T p){ return (barcode(p)<-SIM_BARCODE_THRESHOLD);}
+template <>  inline bool is_simulation_vertex(int b){ return (b<-SIM_BARCODE_THRESHOLD);}
+
+}
+#endif
