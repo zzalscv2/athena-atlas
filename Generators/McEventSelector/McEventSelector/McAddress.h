622e137e521a9daae7b5c9debdd131daabb1ff9f
scott snyder <sss@karma>
2022-12-01 12:12:43 -0500

    McEventSelector: Fix compilation with gcc13.
    Missing #include <cstdint>.

--- Generators/McEventSelector/McEventSelector/McAddress.h
+++ Generators/McEventSelector/McEventSelector/McAddress.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 //====================================================================
@@ -17,6 +17,7 @@
 
 // C/C++ include files
 #include <string>
+#include <cstdint>
 
 // Framework include files
 #include "GaudiKernel/Kernel.h"
