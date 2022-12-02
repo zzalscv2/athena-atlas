fb48af12c77876e2b6d6a6f04cbb154de2281007
scott snyder <sss@karma>
2022-12-01 23:47:48 -0500

    TrigT1CaloByteStream: Fix compilation with gcc13.
    Missing #include <cstdint>.

--- Trigger/TrigT1/TrigT1CaloByteStream/src/xaod/WordDecoder.h
+++ Trigger/TrigT1/TrigT1CaloByteStream/src/xaod/WordDecoder.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef TRIGT1CALOBYTESTREAM_WORDDECODER_H
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <vector>
+#include <cstdint>
 
 namespace LVL1BS {
 
