ead78f6ee329702e24b21c73ec5c5221d4e8c37a
Marco Rimoldi <marco.rimoldi@cern.ch>
2022-12-03 20:06:38 +0100

    SUSYTools - add ART Test
    SUSYTools - add ART Test

--- PhysicsAnalysis/SUSYPhys/SUSYTools/scripts/TestSUSYToolsAlg.py
+++ PhysicsAnalysis/SUSYPhys/SUSYTools/scripts/TestSUSYToolsAlg.py
@@ -20,13 +20,13 @@ parser.add_option('-n', '--dryrun', dest = 'dryrun', default=False, action = 'st
 parser.add_option('--log-level', dest = 'log_level', default = 'INFO', choices = ['ALWAYS','FATAL','ERROR','WARNING','INFO','DEBUG','VERBOSE']) 
 parser.add_option('--dosyst', dest = 'dosyst', default = False, action = 'store_true')
 parser.add_option( '-s', '--submission-dir', dest = 'submission_dir', default = 'submitDir', help = 'Submission directory for EventLoop' )
-parser.add_option('-t', '--type', dest = 'type', default = 'mc16e', help = 'Job type. (mc16a, mc16d, mc16e, mc20a, mc20d, mc20e, mc21a, data18, data22)', choices = ['mc16a', 'mc16d', 'mc16e', 'mc20a', 'mc20d', 'mc20e','mc21a', 'data18', 'data22'])
+parser.add_option('-t', '--type', dest = 'type', default = 'mc20e', help = 'Job type. (mc20a, mc20d, mc20e, mc21a, data18, data22)', choices = ['mc20a', 'mc20d', 'mc20e', 'mc21a', 'data18', 'data22'])
 parser.add_option('--AFII', dest = 'AFII', default = False, action = 'store_true' )
 parser.add_option('-d', '--daod', dest = 'daod', type = 'int', default = 0, help = 'input DAOD type. Do not specify for xAOD input' )
 parser.add_option('-f', '--flav', dest = 'flav', default = 'PHYSVAL', help = 'input DAOD flavour' )
 parser.add_option('-m', '--maxEvts', dest = 'maxEvts', type = 'int', default = 500, help = 'Max events (-1 is all)' )
 parser.add_option('-M', '--maxEvtsManual', dest = 'maxEvtsManual', type = 'int')
-parser.add_option('-p', '--ptag', dest = 'ptag', default = 'p5057', help = 'ptag' )
+parser.add_option('-p', '--ptag', dest = 'ptag', default = 'p5226', help = 'ptag' )
 parser.add_option('--grl', dest = 'grl')
 parser.add_option('--inputDir', dest = 'inputDir')
 parser.add_option('--inputFile', dest = 'inputFile')
@@ -35,7 +35,7 @@ parser.add_option('--inputXRD', dest = 'inputXRD')
 parser.add_option('--overwrite', dest = 'overwrite', default = False, action = 'store_true' )
 ( options, args ) = parser.parse_args()
 print("Configured input data ptag: %s"%(options.ptag))
-ptageqdata = {'p4016':'p4017','p4095':'p4096','p4237':'p4238','p4441':'p4441','p4631':'p4441','p4853':'p4853','p5057':'p5057','p5226':'p5226'}
+ptageqdata = {'p5226':'p5226','p5278':'p5267'}
 if 'data' in options.type and options.ptag in ptageqdata: 
    options.ptag = ptageqdata[options.ptag]
    print("Overriding ptag to equivalent data ptag: -> %s"%(options.ptag))
@@ -65,16 +65,12 @@ cvmfsInputArea = [
 '/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/dev/SUSYTools/ART/ARTInput/',
 ]
 inputFiles = {}
-inputFiles['mc16e_AFII'] = 'DAOD_PHYSVAL.mc16_13TeV.410470.AFII_mc16e_%s.PHYSVAL.pool.root'%(options.ptag)
-inputFiles['mc16a']      = 'DAOD_PHYSVAL.mc16_13TeV.410470.FS_mc16a_%s.PHYSVAL.pool.root'%(options.ptag)
-inputFiles['mc16d']      = 'DAOD_PHYSVAL.mc16_13TeV.410470.FS_mc16d_%s.PHYSVAL.pool.root'%(options.ptag)
-inputFiles['mc16e']      = 'DAOD_PHYSVAL.mc16_13TeV.410470.FS_mc16e_%s.PHYSVAL.pool.root'%(options.ptag)
 inputFiles['mc20a']      = 'DAOD_mc20aPHYS.%s.art.merge.root'%(options.ptag)
 inputFiles['mc20d']      = 'DAOD_mc20dPHYS.%s.art.merge.root'%(options.ptag)
 inputFiles['mc20e']      = 'DAOD_PHYS.mc20_13TeV.410470.FS_mc20e_%s.PHYS.pool.root'%(options.ptag)
 inputFiles['mc21a']      = 'DAOD_PHYS.mc21_13p6TeV.601229.FS_mc21a_%s.PHYS.pool.root'%(options.ptag)
 inputFiles['data18']     = 'DAOD_PHYS.data18_13TeV.358031.data18_%s.PHYS.pool.root'%(options.ptag)
-inputFiles['data22']     = ''
+inputFiles['data22']     = 'DAOD_PHYS.data22_13p6TeV.430542.data22_%s.PHYS.pool.root'%(options.ptag)
 if options.daod == 0 and not '%s%s'%(options.type,'_AFII' if options.AFII else '') in inputFiles: sys.exit('No input file configured for type %s%s. Exiting.'%(options.type,'_AFII' if options.AFII else ''))
 
 inputDir = ''
@@ -117,6 +113,8 @@ job.options().setDouble( ROOT.EL.Job.optMaxEvents, options.maxEvts )
 from AnaAlgorithm.AnaAlgorithmConfig import AnaAlgorithmConfig
 config = AnaAlgorithmConfig( 'SUSYToolsAlg' )
 
+config.STConfigFile = "SUSYTools/SUSYTools_Default.conf"
+if (options.type == "data22" or "mc21" in options.type): config.STConfigFile = "SUSYTools/SUSYTools_Default_Run3.conf"
 config.DoSyst = options.dosyst
 config.DataSource = 1
 config.OutputLevel = outputlvl[options.log_level]
@@ -132,16 +130,15 @@ if options.AFII:
 # set mcCampaign
 if 'mc' in options.type:
    config.mcCampaign = options.type
-else:
-   config.mcCampaign = 'mc16e'
+elif options.type == 'data18':
+   config.mcCampaign = 'mc20e'
+   config.DataSource = 0
+elif options.type == 'data22':
+   config.mcCampaign = 'mc21a'
    config.DataSource = 0
 
 # set lumicalc info
 PRWLumiCalc = {}
-PRWLumiCalc['mc16a'] = ['/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/GoodRunsLists/data16_13TeV/20180129/PHYS_StandardGRL_All_Good_25ns_297730-311481_OflLumi-13TeV-009.root', 
-                        '/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/GoodRunsLists/data15_13TeV/20170619/PHYS_StandardGRL_All_Good_25ns_276262-284484_OflLumi-13TeV-008.root']
-PRWLumiCalc['mc16d'] = ['/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/GoodRunsLists/data17_13TeV/20180619/physics_25ns_Triggerno17e33prim.lumicalc.OflLumi-13TeV-010.root']
-PRWLumiCalc['mc16e'] = ['/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/GoodRunsLists/data18_13TeV/20190318/ilumicalc_histograms_None_348885-364292_OflLumi-13TeV-010.root']
 PRWLumiCalc['mc20a'] = ['/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/GoodRunsLists/data16_13TeV/20180129/PHYS_StandardGRL_All_Good_25ns_297730-311481_OflLumi-13TeV-009.root',
                         '/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/GoodRunsLists/data15_13TeV/20170619/PHYS_StandardGRL_All_Good_25ns_276262-284484_OflLumi-13TeV-008.root']
 PRWLumiCalc['mc20d'] = ['/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/GoodRunsLists/data17_13TeV/20180619/physics_25ns_Triggerno17e33prim.lumicalc.OflLumi-13TeV-010.root']
---
+++ PhysicsAnalysis/SUSYPhys/SUSYTools/test/test_SUSYTools_ART_data18-AnaBase-grid.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# art-description: SUSYTools ART test - TestSUSYToolsAlg.py
+# art-type: grid
+# art-include: master/AnalysisBase
+# art-output: hist-data18_DAOD_PHYS.root
+# art-output: dcube
+
+# Create empty pool file
+art.py createpoolfile
+
+echo "Running SUSYTools test: \'TestSUSYToolsAlg.py -f PHYS -t data18 -p p5226 --inputFile DAOD_PHYS.data18_13TeV.356250.data18_p5226.PHYS.pool.root --inputDir /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/SUSYTools/'"
+TestSUSYToolsAlg.py -f PHYS -t data18 -p p5226 --inputFile DAOD_PHYS.data18_13TeV.356250.data18_p5226.PHYS.pool.root --inputDir /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/SUSYTools/
+echo  "art-result: $? TEST"
+
+mv submitDir/hist-*.root ./hist-data18_DAOD_PHYS.root
+
+echo "Running DCube post-processing"
+
+tName="data18"
+dcubeRef=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/dev/SUSYTools/ART/References/hist-data18_DAOD_PHYS-rel22.root
+dcubeXml=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData/dev/SUSYTools/ART/xml/dcube_config_master_rel22.xml
+
+/cvmfs/atlas.cern.ch/repo/sw/art/dcube/bin/art-dcube ${tName} hist-data18_DAOD_PHYS.root ${dcubeXml} ${dcubeRef}
+
+echo "art-result: $? DCUBE"
+
+echo "Done."
