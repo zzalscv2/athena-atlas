eea6f279ae6a1bd51e1864b61ec99fca59595311
scott snyder <sss@karma>
2022-12-04 21:35:28 -0500

    ISF_Services: Fix thread-safety checker warnings.
    Disable thread-safety checking for tests using gtest.

--- Simulation/ISF/ISF_Core/ISF_Services/test/InputConverter_test.cxx
+++ Simulation/ISF/ISF_Core/ISF_Services/test/InputConverter_test.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 /**
@@ -7,6 +7,9 @@
  * @date June, 2016
  * @brief Tests for ISF::InputConverter.
  */
+
+#include "CxxUtils/checker_macros.h"
+ATLAS_NO_CHECK_FILE_THREAD_SAFETY;
 
 #undef NDEBUG
 
