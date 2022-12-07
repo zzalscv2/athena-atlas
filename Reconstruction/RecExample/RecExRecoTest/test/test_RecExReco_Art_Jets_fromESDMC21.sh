7e0a2bd78f360e78cccd083a4261b91606930c0b
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2022-12-07 16:38:21 +0100

    Add new jets from ESD reco test using CA and mc21. Remove old style jets from ESD test.

--- Reconstruction/RecExample/RecExRecoTest/test/test_RecExReco_Art_Jets_fromESDMC21.sh
+++ Reconstruction/RecExample/RecExRecoTest/test/test_RecExReco_Art_Jets_fromESDMC21.sh
@@ -4,10 +4,9 @@
 # art-type: grid
 # art-athena-mt: 8
 # art-include: master/Athena
-# art-include: 22.0/Athena
 # art-output: *.log   
 
-test_StandardJets.py  --filesIn /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecExRecoTest/mc20e_13TeV/valid1.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.ESD.e4993_s3227_r12689/myESD.pool.root -n 50 | tee temp.log
+python $Athena_DIR/python/RecExRecoTest/Jets_ESDMC21.py | tee temp.log
 echo "art-result: ${PIPESTATUS[0]}"
 test_postProcessing_Errors.sh temp.log
 
