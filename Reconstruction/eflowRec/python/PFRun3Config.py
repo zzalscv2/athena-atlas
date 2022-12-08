7e45b4d5d25dbc65921b2989529277fdd46e483d
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2022-12-08 12:28:34 +0100

    Update conditions tag for pflow test.

--- Reconstruction/eflowRec/python/PFRun3Config.py
+++ Reconstruction/eflowRec/python/PFRun3Config.py
@@ -127,7 +127,9 @@ if __name__=="__main__":
     from AthenaConfiguration.AllConfigFlags import ConfigFlags as cfgFlags
 
     cfgFlags.Input.isMC=True
-    ConfigFlags.Input.Files = ["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecExRecoTest/mc21_13p6TeV/ESDFiles/mc21_13p6TeV.421450.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_fct.recon.ESD.e8445_e8447_s3822_r13565/ESD.28877240._000046.pool.root.1"]
+    cfgFlags.Input.Files = ["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecExRecoTest/mc21_13p6TeV/ESDFiles/mc21_13p6TeV.421450.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_fct.recon.ESD.e8445_e8447_s3822_r13565/ESD.28877240._000046.pool.root.1"]
+    #Use latest MC21 tag to pick up latest muon folders apparently needed
+    cfgFlags.IOVDb.GlobalTag = "OFLCOND-MC21-SDR-RUN3-10"
     cfgFlags.Output.AODFileName="output_AOD.root"
     cfgFlags.Output.doWriteAOD=True
     cfgFlags.fillFromArgs()
