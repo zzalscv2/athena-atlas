8a9aa32d421f6372839dff2db7517856865d5fb6
Frank Winklmeier <frank.winklmeier@cern.ch>
2022-12-12 17:29:09 +0100

    TrigAFPHypo: pass string by reference

--- Trigger/TrigHypothesis/TrigAFPHypo/src/IAFPProtonTransportTool.h
+++ Trigger/TrigHypothesis/TrigAFPHypo/src/IAFPProtonTransportTool.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef TRIGAFPHYPO_IAFPPROTONTRANSPORTTOOL_H
@@ -16,7 +16,7 @@ class IAFPProtonTransportTool : virtual public IAlgTool {
   }
 
   virtual StatusCode load() = 0;
-  virtual void setParamFile(std::string paramFile) = 0;
+  virtual void setParamFile(const std::string& paramFile) = 0;
 
   virtual double x(double x0, double y0, double z0, double sx0, double sy0, double E) const = 0;
 
