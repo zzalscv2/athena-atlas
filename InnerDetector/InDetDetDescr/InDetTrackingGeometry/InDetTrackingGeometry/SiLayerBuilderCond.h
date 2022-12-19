f0157856bf2fbdd14ada5602a9af910e6eadd6ec
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2022-12-19 11:25:40 +0100

    Share common code between SiLayerBuilder and SiLayerBuilderCond
    Share common code between SiLayerBuilder and SiLayerBuilderCond

--- InnerDetector/InDetDetDescr/InDetTrackingGeometry/InDetTrackingGeometry/SiLayerBuilderCond.h
+++ InnerDetector/InDetDetDescr/InDetTrackingGeometry/InDetTrackingGeometry/SiLayerBuilderCond.h
@@ -2,19 +2,12 @@
   Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 */
 
-///////////////////////////////////////////////////////////////////
-// SiLayerBuilderCond.h, (c) ATLAS Detector software
-///////////////////////////////////////////////////////////////////
-
-#ifndef INDETTRACKINGGEOMETRY_SILAYERBUILDERNCOND_H
-#define INDETTRACKINGGEOMETRY_SILAYERBUILDERNCOND_H
+#ifndef INDETTRACKINGGEOMETRY_SILAYERBUILDERCOND_H
+#define INDETTRACKINGGEOMETRY_SILAYERBUILDERCOND_H
 
 // Athena
-#include "AthenaBaseComps/AthAlgTool.h"
-#include "CxxUtils/checker_macros.h"
+#include "InDetTrackingGeometry/SiLayerBuilderImpl.h"
 #include "InDetReadoutGeometry/SiDetectorElementCollection.h"
-// Amg
-#include "GeoPrimitives/GeoPrimitives.h"
 // Trk
 #include "TrkDetDescrInterfaces/ILayerBuilderCond.h"
 #include "TrkDetDescrUtils/SharedObject.h"
@@ -22,146 +15,83 @@
 #include "TrkGeometry/TrackingGeometry.h"
 // STL
 #include <vector>
-#include <utility> //for std::pair
-
-class PixelID;
-class SCT_ID;
-
-#ifndef TRKDETDESCR_TAKESMALLERBIGGER
-#define TRKDETDESCR_TAKESMALLERBIGGER
-#define takeSmaller(current,test) current = current < test ? current : test
-#define takeBigger(current,test)  current = current > test ? current : test
-#define takeSmallerBigger(cSmallest, cBiggest, test) takeSmaller(cSmallest, test); takeBigger(cBiggest, test)
-#endif
-
-namespace InDetDD {
-  class SiDetectorManager;
-}
 
 namespace Trk {
   class Surface;
   class CylinderLayer;
   class DiscLayer;
-  class PlaneLayer;
-  class BinnedLayerMaterial;
-  typedef std::pair< SharedObject<Surface>, Amg::Vector3D > SurfaceOrderPosition;
 }
 
 namespace InDet {
- 
+
   /** @class SiLayerBuilderCond
-  
-     The SiLayerBuilderCond parses the senstive detector elments and orders them onto a
-     Layer surface.
-  
-     It also uses the SiNumerology to construct the BinUtility and then orders the representing
-     detector surfaces on the layers.
-     
-     It performs an automated detector if an equidistant or non-equidistant binning
-     is to be used for the barrel case.
-     
-     @author Andreas.Salzburger@cern.ch
-    */
+
+      The SiLayerBuilderCond parses the senstive detector elments and orders them onto a
+      Layer surface.
+
+      It also uses the SiNumerology to construct the BinUtility and then orders the representing
+      detector surfaces on the layers.
+
+      It performs an automated detector if an equidistant or non-equidistant binning
+      is to be used for the barrel case.
+
+      @author Andreas.Salzburger@cern.ch
+  */
   class ATLAS_NOT_THREAD_SAFE SiLayerBuilderCond : // const_cast
-    public AthAlgTool, virtual public Trk::ILayerBuilderCond {
-    
-    public:
-    
-      /** AlgTool style constructor */
-      SiLayerBuilderCond(const std::string&,const std::string&,const IInterface*);
+    public extends<SiLayerBuilderImpl, Trk::ILayerBuilderCond> {
 
-      /** Destructor */
-      virtual ~SiLayerBuilderCond();
+  public:
 
-      /** AlgTool initialize method */
-      virtual StatusCode initialize() override;
-      /** AlgTool finalize method */
-      virtual StatusCode finalize() override;
+    /** AlgTool style constructor */
+    SiLayerBuilderCond(const std::string&,const std::string&,const IInterface*);
 
-      /** LayerBuilder interface method - returning Barrel-like layers */
-      virtual std::unique_ptr<const std::vector<Trk::CylinderLayer*> >
+    /** Destructor */
+    virtual ~SiLayerBuilderCond() = default;
+
+    /** AlgTool initialize method */
+    virtual StatusCode initialize() override;
+
+    /** LayerBuilder interface method - returning Barrel-like layers */
+    virtual std::unique_ptr<const std::vector<Trk::CylinderLayer*> >
       cylindricalLayers(const EventContext& ctx,
                         SG::WriteCondHandle<Trk::TrackingGeometry>& whandle) const override final;
 
-      /** LayerBuilder interface method - returning Endcap-like layers */
-      virtual std::unique_ptr<const std::vector<Trk::DiscLayer*> >
+    /** LayerBuilder interface method - returning Endcap-like layers */
+    virtual std::unique_ptr<const std::vector<Trk::DiscLayer*> >
       discLayers(const EventContext& ctx,
                  SG::WriteCondHandle<Trk::TrackingGeometry>& whandle) const override final;
 
-      /** LayerBuilder interface method - returning Planar-like layers */
-      virtual std::unique_ptr<const std::vector<Trk::PlaneLayer*> >
+    /** LayerBuilder interface method - returning Planar-like layers */
+    virtual std::unique_ptr<const std::vector<Trk::PlaneLayer*> >
       planarLayers(const EventContext& ctx,
                    SG::WriteCondHandle<Trk::TrackingGeometry>& whandle) const override final;
 
-      /** Name identification */
-      virtual const std::string& identification() const override final;
+    /** Name identification */
+    virtual const std::string& identification() const override final;
 
-    private:
-      SG::ReadCondHandle<InDetDD::SiDetectorElementCollection>
+  private:
+
+    SG::ReadCondHandle<InDetDD::SiDetectorElementCollection>
       retrieveSiDetElements(const EventContext& ctx) const;
-      std::unique_ptr<std::vector<Trk::CylinderLayer*> >
-      dressCylinderLayers(const std::vector<Trk::CylinderLayer*>& dLayers) const;
 
-      /** create the disc layers, if no vector is given, then it's the first
-       * pass, else it's the DBM for the Pixels */
-      std::unique_ptr<std::vector<Trk::DiscLayer*> >
+    /** create the disc layers, if no vector is given, then it's the first
+     * pass, else it's the DBM for the Pixels */
+    std::unique_ptr<std::vector<Trk::DiscLayer*> >
       createDiscLayers(const EventContext& ctx,
                        SG::WriteCondHandle<Trk::TrackingGeometry>& whandle,
                        std::unique_ptr<std::vector<Trk::DiscLayer*> > discLayers = nullptr) const;
 
-      /** create the disc layers, it is dedicated to ITk implementation of the
-       * endcap rings. Used for ITk specific case. */
-      std::unique_ptr<std::vector<Trk::DiscLayer*> >
+    /** create the disc layers, it is dedicated to ITk implementation of the
+     * endcap rings. Used for ITk specific case. */
+    std::unique_ptr<std::vector<Trk::DiscLayer*> >
       createRingLayers(const EventContext& ctx,
                        SG::WriteCondHandle<Trk::TrackingGeometry>& whandle) const;
 
-      //!< helper method to construct barrel material
-      const Trk::BinnedLayerMaterial barrelLayerMaterial(double r,
-                                                         double hz) const;
+    virtual void registerSurfacesToLayer(Trk::BinnedArraySpan<Trk::Surface * const >& surfaces, Trk::Layer& layer) const override; //!< layer association
 
-      //!< helper method to construct endcap material
-      const Trk::BinnedLayerMaterial endcapLayerMaterial(double rMin,
-                                                         double rMax) const;
+    SG::ReadCondHandleKey<InDetDD::SiDetectorElementCollection> m_SCT_ReadKey{this, "SCT_ReadKey", "SCT_DetectorElementCollection", "Key of output SiDetectorElementCollection for SCT"};
+    SG::ReadCondHandleKey<InDetDD::SiDetectorElementCollection> m_PixelReadKey{this, "PixelReadKey", "PixelDetectorElementCollection", "Key of output SiDetectorElementCollection for Pixel"};
 
-      void registerSurfacesToLayer(Trk::BinnedArraySpan<Trk::Surface * const >& surfaces, Trk::Layer& layer) const; //!< layer association
-
-      bool                                           m_pixelCase;                      //!< flag for pixel/sct
-                                                     
-      const InDetDD::SiDetectorManager*              m_siMgr;                          //!< the Si Detector Manager
-      std::string                                    m_siMgrLocation;                  //!< the location of the Pixel Manager
-      const PixelID*                                 m_pixIdHelper;                    //!< pixel Id Helper 
-      const SCT_ID*                                  m_sctIdHelper;                    //!< sct Id Helper
-                                                     
-      bool                                           m_setLayerAssociation;            //!< Set Layer Association
-                                                     
-      // barrel layer section                        
-      std::vector<double>                            m_barrelAdditionalLayerR;         //!< Create an additional layer at these radii
-      std::vector<int>                               m_barrelAdditionalLayerType;      //!< material layer 1 - navigation layer 0 
-      size_t                                         m_barrelLayerBinsZ;               //!< Barrel bins for the material in z 
-      size_t                                         m_barrelLayerBinsPhi;             //!< Barrel bins for the material in phi
-      double                                         m_barrelEnvelope;                 //!< envelope around rMin/rMax
-      double                                         m_barrelEdbTolerance;             //!< tolerance in percent how much the bin sizes can change
-
-      std::vector<double>                            m_endcapAdditionalLayerPosZ;      //!< Create additional endcaps at these z positions
-      std::vector<int>                               m_endcapAdditionalLayerType;      //!< material layer 1 - navigation layer 0 ( for volume adjustment )
-      size_t                                         m_endcapLayerBinsR;               //!< Barrel bins for the material in r
-      size_t                                         m_endcapLayerBinsPhi;             //!< Barrel bins for the material in phi
-      double                                         m_endcapEnvelope;                 //!< envelope around rMin/rMax
-      bool                                           m_endcapComplexRingBinning;       //!< make std::vector<R> rings, could be different for layers
-                                                     
-      std::string                                    m_identification;                  //!< string identification  
-      
-      bool                                           m_runGeometryValidation;           //!< run the validation of the geometry ( no empty bins)
-      SG::ReadCondHandleKey<InDetDD::SiDetectorElementCollection> m_SCT_ReadKey{this, "SCT_ReadKey", "SCT_DetectorElementCollection", "Key of output SiDetectorElementCollection for SCT"};
-      SG::ReadCondHandleKey<InDetDD::SiDetectorElementCollection> m_PixelReadKey{this, "PixelReadKey", "PixelDetectorElementCollection", "Key of output SiDetectorElementCollection for Pixel"};
-      
-       std::vector<int>                              m_layerIndicesBarrel; //!< indices to be used for layer creation (used for ITk specific case)
-       std::vector<int>                              m_layerIndicesEndcap; //!< indices to be used for layer creation (used for ITk specific case)
-       bool                                          m_useRingLayout;      //!< to enable creation of rings for ITk pixel geometry (used for ITk specific case)
-       bool                                          m_addMoreSurfaces   ;              //!< to add additional surfaces to the SCT_OverlapDescriptor (used for ITk specific case)
-      
-            
-                      
   };
 
   inline std::unique_ptr<const std::vector<Trk::PlaneLayer*> >
@@ -180,6 +110,4 @@ namespace InDet {
 } // end of namespace
 
 
-#endif // INDETTRACKINGGEOMETRY_SILAYERBUILDERNCOND_H
-
-
+#endif // INDETTRACKINGGEOMETRY_SILAYERBUILDERCOND_H
---
+++ InnerDetector/InDetDetDescr/InDetTrackingGeometry/InDetTrackingGeometry/SiLayerBuilderImpl.h
@@ -0,0 +1,143 @@
+/*
+  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+*/
+
+#ifndef INDETTRACKINGGEOMETRY_SILAYERBUILDERIMPL_H
+#define INDETTRACKINGGEOMETRY_SILAYERBUILDERIMPL_H
+
+// Athena
+#include "AthenaBaseComps/AthAlgTool.h"
+#include "CxxUtils/checker_macros.h"
+#include "InDetReadoutGeometry/SiDetectorElementCollection.h"
+// Amg
+#include "GeoPrimitives/GeoPrimitives.h"
+// Trk
+#include "TrkDetDescrUtils/SharedObject.h"
+#include "TrkDetDescrUtils/BinnedArray.h"
+#include "TrkGeometry/TrackingGeometry.h"
+// STL
+#include <vector>
+#include <utility> //for std::pair
+
+class PixelID;
+class SCT_ID;
+
+#ifndef TRKDETDESCR_TAKESMALLERBIGGER
+#define TRKDETDESCR_TAKESMALLERBIGGER
+#define takeSmaller(current,test) current = current < test ? current : test
+#define takeBigger(current,test)  current = current > test ? current : test
+#define takeSmallerBigger(cSmallest, cBiggest, test) takeSmaller(cSmallest, test); takeBigger(cBiggest, test)
+#endif
+
+namespace InDetDD {
+  class SiDetectorManager;
+}
+
+namespace Trk {
+  class Surface;
+  class CylinderLayer;
+  class DiscLayer;
+  class BinnedLayerMaterial;
+  typedef std::pair< SharedObject<Surface>, Amg::Vector3D > SurfaceOrderPosition;
+}
+
+namespace InDet {
+
+  /** @class SiLayerBuilderImpl
+
+      The SiLayerBuilderImpl parses the senstive detector elments and orders them onto a
+      Layer surface.
+
+      It also uses the SiNumerology to construct the BinUtility and then orders the representing
+      detector surfaces on the layers.
+
+      It performs an automated detector if an equidistant or non-equidistant binning
+      is to be used for the barrel case.
+
+      @author Andreas.Salzburger@cern.ch
+  */
+  class ATLAS_NOT_THREAD_SAFE SiLayerBuilderImpl : public AthAlgTool {
+
+  public:
+
+    /** Destructor */
+    virtual ~SiLayerBuilderImpl() = default;
+
+    /** AlgTool initialize method */
+    virtual StatusCode initialize() override;
+    /** AlgTool finalize method */
+    virtual StatusCode finalize() override;
+
+  protected:
+    /** AlgTool style constructor */
+    SiLayerBuilderImpl(const std::string&,const std::string&,const IInterface*);
+
+    virtual void registerSurfacesToLayer(Trk::BinnedArraySpan<Trk::Surface * const >& layerSurfaces, Trk::Layer& lay) const;
+
+    std::unique_ptr<const std::vector<Trk::CylinderLayer*> >
+      cylindricalLayersImpl(const InDetDD::SiDetectorElementCollection& siDetElementCollection) const;
+
+    std::unique_ptr<std::vector< Trk::DiscLayer*> >
+      createDiscLayersImpl(const InDetDD::SiDetectorElementCollection& siDetElementCollection, std::unique_ptr<std::vector< Trk::DiscLayer*> > discLayers = nullptr) const;
+
+    std::unique_ptr<std::vector< Trk::DiscLayer*> >
+      createRingLayersImpl(const InDetDD::SiDetectorElementCollection& siDetElementCollection) const;
+
+    std::unique_ptr<std::vector<Trk::CylinderLayer*> >
+      dressCylinderLayers(const std::vector<Trk::CylinderLayer* >& dLayers) const;
+
+    //!< helper method to construct barrel material
+    const Trk::BinnedLayerMaterial barrelLayerMaterial(double r,
+                                                       double hz) const;
+
+    //!< helper method to construct endcap material
+    const Trk::BinnedLayerMaterial endcapLayerMaterial(double rMin,
+                                                       double rMax) const;
+
+    /// Common properties
+    BooleanProperty m_pixelCase{this, "PixelCase", true}; //!< flag for pixel/sct
+    const InDetDD::SiDetectorManager *m_siMgr{}; //!< the Si Detector Manager
+    StringProperty m_siMgrLocation{this, "SiDetManagerLocation", "Pixel"}; //!< the location of the Pixel Manager
+    const PixelID *m_pixIdHelper{}; //!< pixel Id Helper
+    const SCT_ID *m_sctIdHelper{}; //!< sct Id Helper
+
+    BooleanProperty m_setLayerAssociation{this, "SetLayerAssociation", true}; //!< Set Layer Association
+
+    // For the Active Barrel Material
+    // barrel layer section
+    DoubleArrayProperty m_barrelAdditionalLayerR{this, "BarrelAdditionalLayerRadii", {} };         //!< Create an additional layer at these radii
+    IntegerArrayProperty m_barrelAdditionalLayerType{this, "BarrelAdditionalLayerType", {} };      //!< material layer 1 - navigation layer 0
+    UnsignedIntegerProperty m_barrelLayerBinsZ{this, "BarrelLayerBinsZ", 100};               //!< Barrel bins for the material in z
+    UnsignedIntegerProperty m_barrelLayerBinsPhi{this, "BarrelLayerBinsPhi", 1};             //!< Barrel bins for the material in phi
+    DoubleProperty m_barrelEnvelope{this, "BarrelEnvelope", 0.1};                 //!< envelope around rMin/rMax
+    DoubleProperty m_barrelEdbTolerance{this, "BarrelEdbTolerance", 0.05};             //!< tolerance in percent how much the bin sizes can change
+
+    // For the Active Endcap Material
+    DoubleArrayProperty m_endcapAdditionalLayerPosZ{this, "EndcapAdditionalLayerPositionsZ", {} };      //!< Create additional endcaps at these z positions
+    IntegerArrayProperty m_endcapAdditionalLayerType{this,"EndcapAdditionalLayerType" , {} };      //!< material layer 1 - navigation layer 0 ( for volume adjustment )
+    UnsignedIntegerProperty m_endcapLayerBinsR{this, "EndcapLayerBinsR", 100};               //!< Barrel bins for the material in r
+    UnsignedIntegerProperty m_endcapLayerBinsPhi{this, "EndcapLayerBinsPhi", 1};             //!< Barrel bins for the material in phi
+    DoubleProperty m_endcapEnvelope{this, "EndcapEnvelope", 0.1};                 //!< envelope around rMin/rMax
+    BooleanProperty m_endcapComplexRingBinning{this, "EndcapComplexRingBinning", true};       //!< make std::vector<R> rings, could be different for layers
+
+    StringProperty m_identification{this, "Identification", "Pixel"};                  //!< string identification
+
+    BooleanProperty m_runGeometryValidation{this, "GeometryValidation", true};           //!< run the validation of the geometry ( no empty bins)
+
+    IntegerArrayProperty m_layerIndicesBarrel{this, "LayerIndicesBarrel", {} }; //!< indices to be used for layer creation (used for ITk specific case)
+    IntegerArrayProperty m_layerIndicesEndcap{this, "LayerIndicesEndcap", {} }; //!< indices to be used for layer creation (used for ITk specific case)
+    BooleanProperty                                          m_useRingLayout{this, "UseRingLayout", false};      //!< to enable creation of rings for ITk pixel geometry (used for ITk specific case)
+    BooleanProperty                                          m_addMoreSurfaces   {this, "AddMoreSurfaces", false};              //!< to add additional surfaces to the SCT_OverlapDescriptor (used for ITk specific case)
+
+    // Properties only in SiLayerBuilderCond
+    SG::ReadCondHandleKey<InDetDD::SiDetectorElementCollection> m_SCT_ReadKey{this, "SCT_ReadKey", "SCT_DetectorElementCollection", "Key of output SiDetectorElementCollection for SCT"};
+    SG::ReadCondHandleKey<InDetDD::SiDetectorElementCollection> m_PixelReadKey{this, "PixelReadKey", "PixelDetectorElementCollection", "Key of output SiDetectorElementCollection for Pixel"};
+
+
+
+  };
+
+} // end of namespace
+
+
+#endif // INDETTRACKINGGEOMETRY_SILAYERBUILDERIMPL_H
