f6ae157a3a9949e0ea2b4064a2f4740fa300d6cd
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2023-01-03 20:21:43 +0100

    Update MagicNumbers.h -- try to use const ref for the HepMC::is_ functions
    Update MagicNumbers.h -- try to use const ref for the HepMC::is_ functions

--- Generators/AtlasHepMC/AtlasHepMC/MagicNumbers.h
+++ Generators/AtlasHepMC/AtlasHepMC/MagicNumbers.h
@@ -25,11 +25,11 @@ constexpr int PHOTOSMIN = 10000;
 /// interactions in standard MC Production
 constexpr int crazyParticleBarcode(std::numeric_limits<int32_t>::max());
 
-template <class T>  inline bool is_simulation_particle(T p){ return (barcode(p)>SIM_BARCODE_THRESHOLD);}
-template <>  inline bool is_simulation_particle(int b){ return (b>SIM_BARCODE_THRESHOLD);}
+template <class T>  inline bool is_simulation_particle(const T& p){ return (barcode(p)>SIM_BARCODE_THRESHOLD);}
+template <>  inline bool is_simulation_particle(const int& b){ return (b>SIM_BARCODE_THRESHOLD);}
 
-template <class T>  inline bool is_simulation_vertex(T p){ return (barcode(p)<-SIM_BARCODE_THRESHOLD);}
-template <>  inline bool is_simulation_vertex(int b){ return (b<-SIM_BARCODE_THRESHOLD);}
+template <class T>  inline bool is_simulation_vertex(const T& p){ return (barcode(p)<-SIM_BARCODE_THRESHOLD);}
+template <>  inline bool is_simulation_vertex(const int& b){ return (b<-SIM_BARCODE_THRESHOLD);}
 
 }
 #endif
