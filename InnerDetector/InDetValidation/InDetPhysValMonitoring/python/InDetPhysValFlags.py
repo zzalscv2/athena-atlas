9466f6faacc6465fda4bd0157889fcd289b0b8a5
Thomas Strebler <thomas.strebler@cern.ch>
2023-01-04 18:24:58 +0100

    Set default IDPVM flag requiredSiHits to 0

--- InnerDetector/InDetValidation/InDetPhysValMonitoring/python/InDetPhysValFlags.py
+++ InnerDetector/InDetValidation/InDetPhysValMonitoring/python/InDetPhysValFlags.py
@@ -27,7 +27,7 @@ def createIDPVMConfigFlags():
     icf.addFlag("jetsNameForHardScatter", 'AntiKt4EMTopoJets' ) # when building jets, what types of jets are built (used for hardScatterStrategy == 2)
     icf.addFlag("validateExtraTrackCollections", [] ) # List of extra track collection names to be validated in addition to Tracks.
     icf.addFlag("ancestorIDs", [] )
-    icf.addFlag("requiredSiHits", 1)
+    icf.addFlag("requiredSiHits", 0)
     icf.addFlag("maxProdVertRadius", 300)
     icf.addFlag("hardScatterStrategy", 0 ) # The hard-scatter vertex selection strategy to use when running hard-scatter efficiency / performance plots in IDPVM. 0 corresponds to sumPt^2, 1 corresponds to sumPt
     icf.addFlag("truthMinPt", lambda pcf : 500 if pcf.GeoModel.Run <= LHCPeriod.Run3 else 1000) # Configurable pT cut for determining a "reconstructable" particle
