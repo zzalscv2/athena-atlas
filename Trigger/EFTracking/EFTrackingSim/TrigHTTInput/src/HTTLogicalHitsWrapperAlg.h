d8165283542e1702870ff41db7d2725dd636084c
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2023-01-07 06:49:22 +0100

    cppcheck: Fix publicAllocationError
    cppcheck: Fix publicAllocationError

--- Trigger/EFTracking/EFTrackingSim/TrigHTTInput/src/HTTLogicalHitsWrapperAlg.h
+++ Trigger/EFTracking/EFTrackingSim/TrigHTTInput/src/HTTLogicalHitsWrapperAlg.h
@@ -11,9 +11,6 @@
 #include "TrigHTTMaps/HTTClusteringToolI.h"
 #include "TrigHTTInput/HTTRawToLogicalHitsTool.h"
 
-
-class TH1F;
-class TH2F;
 class HTTEventInputHeader;
 class HTTLogicalEventInputHeader;
 
@@ -34,20 +31,6 @@ private:
   ToolHandle<HTTRawToLogicalHitsTool>      m_hitMapTool      { this, "RawToLogicalHitsTool", "HTTRawToLogicalHitsTool/HTTRawToLogicalHitsTool", "Map Raw to Logical hit Tool" };
 
   Gaudi::Property<bool>  m_Clustering   {this, "Clustering", false, "flag to enable the clustering"};
-  Gaudi::Property<bool>  m_Monitor      {this, "Monitor",    false, "flag to enable internal monitor"};
- 
-
-  // some histograms for internal monitor
-  TH1F*   m_hits_barrelEC = nullptr;
-  TH2F*   m_hits_r_vs_z = nullptr;
-
-  TH1F*   m_nMissedHit = nullptr;
-  TH1F*   m_nMissedHitFrac = nullptr;
-  TH1F*   m_MissedHit_eta = nullptr ;
-  TH1F*   m_MissedHit_phi = nullptr;
-  TH1F*   m_MissedHit_type = nullptr;
-  TH1F*   m_MissedHit_barrelEC = nullptr;
-  TH1F*   m_MissedHit_layer = nullptr;
 
 };
 
