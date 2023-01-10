970ccc17e3c77cd0e2f4c8def53e2d4ec6148600
scott snyder <sss@karma>
2023-01-09 23:26:36 -0500

    AthExUnittest: Suppress cppcheck warnings.
    Suppress cppcheck syntax errors from gtest macros.

--- Control/AthenaExamples/AthExUnittest/test/gt_AthExUnittest.cxx
+++ Control/AthenaExamples/AthExUnittest/test/gt_AthExUnittest.cxx
@@ -1,6 +1,6 @@
 
 //
-//  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+//  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 //
 
 #include "CxxUtils/checker_macros.h"
@@ -65,6 +65,7 @@ namespace Athena_test {
 
   };
   
+  // cppcheck-suppress syntaxError
   TEST_F( AthExUnittestAlgTest, getDefaultPropertyValue ) {
     int prop= getIntProperty( "MyProperty" );
     EXPECT_EQ( prop, 1 );
