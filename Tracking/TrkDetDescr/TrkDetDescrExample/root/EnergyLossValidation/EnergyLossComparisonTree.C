bcde11272fa213921efca636b7a9c14a30d59e8e
Shaun Roe <shaun.roe@cern.ch>
2023-01-11 10:53:39 +0100

    23.0-cppcheck-TrkDetDescrExample
    23.0-cppcheck-TrkDetDescrExample

--- Tracking/TrkDetDescr/TrkDetDescrExample/root/EnergyLossValidation/EnergyLossComparisonTree.C
+++ Tracking/TrkDetDescr/TrkDetDescrExample/root/EnergyLossValidation/EnergyLossComparisonTree.C
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #define EnergyLossComparisonTree_cxx
@@ -283,7 +283,7 @@ void EnergyLossComparisonTree::Init(TTree *tree)
 
    //std::cout << " branch addresses set " << std::endl;
 
-   Notify();
+   EnergyLossComparisonTree::Notify();
 }
 
 Bool_t EnergyLossComparisonTree::Notify()
--- Tracking/TrkDetDescr/TrkDetDescrExample/root/LayerValidation/LayerValidationTree.C
+++ Tracking/TrkDetDescr/TrkDetDescrExample/root/LayerValidation/LayerValidationTree.C
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "LayerValidationTree.h"
@@ -80,7 +80,7 @@ void LayerValidationTree::Init(TTree *tree)
    fChain->SetBranchAddress("HitPositionR",HitPositionR);
 
 
-   Notify();
+   LayerValidationTree::Notify();
 }
 
 Bool_t LayerValidationTree::Notify()
