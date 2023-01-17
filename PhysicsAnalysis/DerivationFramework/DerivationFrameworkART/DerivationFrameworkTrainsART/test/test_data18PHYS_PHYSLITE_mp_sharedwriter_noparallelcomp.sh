409114e96ee249cc9056d9439a887f140f34a58f
Cristiano Alpigiani <cristiano.alpigiani@cern.ch>
2023-01-17 18:24:23 +0100

    Fixing an oversight in a sharedWriter ART
    Fixing sharedWriter ART
    
    Although I thought to have tripled checked the in , I forgot to add the CA option in one test :-/. This MR just fix that oversight.

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_data18PHYS_PHYSLITE_mp_sharedwriter_noparallelcomp.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkTrainsART/test/test_data18PHYS_PHYSLITE_mp_sharedwriter_noparallelcomp.sh
@@ -10,6 +10,7 @@
 # art-athena-mt: 8
 
 ATHENA_CORE_NUMBER=8 Derivation_tf.py \
+  --CA True \
   --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data18_13TeV.00357772.physics_Main.recon.AOD.r13286/AOD.27654050._000557.pool.root.1 \
   --outputDAODFile art.pool.root \
   --formats PHYS PHYSLITE \
