20aff5336d5c927c1258db4ab1dabb996b2176cf
Tadej Novak <tadej.novak@cern.ch>
2023-01-18 14:53:40 +0100

    DQ steering: do not use global config flags

--- Control/AthenaMonitoring/share/Run3DQTestingDriver.py
+++ Control/AthenaMonitoring/share/Run3DQTestingDriver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 #
 
 '''@file DQTestingDriver.py
@@ -12,11 +12,11 @@
 
 if __name__=='__main__':
     import sys
-    from AthenaConfiguration.AllConfigFlags import ConfigFlags
+    from AthenaConfiguration.AllConfigFlags import initConfigFlags
     from AthenaConfiguration.ComponentFactory import CompFactory
     from AthenaConfiguration.Enums import Format
-
-    parser = ConfigFlags.getArgumentParser()
+    flags = initConfigFlags()
+    parser = flags.getArgumentParser()
     parser.add_argument('--preExec', help='Code to execute before locking configs')
     parser.add_argument('--postExec', help='Code to execute after setup')
     parser.add_argument('--dqOffByDefault', action='store_true',
@@ -36,12 +36,12 @@ if __name__=='__main__':
     args, _ = parser.parse_known_args()
 
     # default input if nothing specified
-    ConfigFlags.Input.Files = ['/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/AthenaMonitoring/q431/21.0/f946/myESD.pool.root']
-    ConfigFlags.Output.HISTFileName = 'ExampleMonitorOutput.root'    
+    flags.Input.Files = ['/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/AthenaMonitoring/q431/21.0/f946/myESD.pool.root']
+    flags.Output.HISTFileName = 'ExampleMonitorOutput.root'    
     if args.dqOffByDefault:
         from AthenaMonitoring.DQConfigFlags import allSteeringFlagsOff
-        allSteeringFlagsOff()
-    ConfigFlags.fillFromArgs(parser=parser)
+        allSteeringFlagsOff(flags)
+    flags.fillFromArgs(parser=parser)
 
     # Setup logs
     from AthenaCommon.Logging import log
@@ -52,26 +52,26 @@ if __name__=='__main__':
     # override Input.Files with result from our own arguments
     # if --filesInput was specified as well (!) this will override
     if args.inputFiles is not None:
-        ConfigFlags.Input.Files = args.inputFiles.split(',')
+        flags.Input.Files = args.inputFiles.split(',')
     # if --evtMax was specified as well this will override
     if args.maxEvents is not None:
-        ConfigFlags.Exec.MaxEvents = args.maxEvents
+        flags.Exec.MaxEvents = args.maxEvents
 
-    if ConfigFlags.Input.Format is Format.BS:
-        if ConfigFlags.DQ.Environment not in ('tier0', 'tier0Raw', 'online'):
+    if flags.Input.Format is Format.BS:
+        if flags.DQ.Environment not in ('tier0', 'tier0Raw', 'online'):
             log.warning('Reading RAW file, but DQ.Environment set to %s',
-                        ConfigFlags.DQ.Environment)
+                        flags.DQ.Environment)
             log.warning('Will proceed but best guess is this is an error')
         log.info('Will schedule reconstruction, as best we know')
     else:
-        if ConfigFlags.DQ.Environment in ('tier0', 'tier0Raw', 'online'):
+        if flags.DQ.Environment in ('tier0', 'tier0Raw', 'online'):
             log.warning('Reading POOL file, but DQ.Environment set to %s',
-                        ConfigFlags.DQ.Environment)
+                        flags.DQ.Environment)
             log.warning('Will proceed but best guess is this is an error')
 
     # perfmon
     if args.perfmon:
-        ConfigFlags.PerfMon.doFullMonMT=True
+        flags.PerfMon.doFullMonMT=True
 
     if args.preExec:
         # bring things into scope
@@ -79,47 +79,47 @@ if __name__=='__main__':
         log.info('Executing preExec: %s', args.preExec)
         exec(args.preExec)
 
-    if hasattr(ConfigFlags, "DQ") and hasattr(ConfigFlags.DQ, "Steering") and hasattr(ConfigFlags, "Detector"):
-        if hasattr(ConfigFlags.DQ.Steering, "InDet"):
-            if ((ConfigFlags.DQ.Steering.InDet, "doAlignMon") and ConfigFlags.DQ.Steering.InDet.doAlignMon) or \
-               ((ConfigFlags.DQ.Steering.InDet, "doGlobalMon") and ConfigFlags.DQ.Steering.InDet.doGlobalMon) or \
-               ((ConfigFlags.DQ.Steering.InDet, "doPerfMon") and ConfigFlags.DQ.Steering.InDet.doPerfMon):
-                ConfigFlags.Detector.GeometryID = True
+    if hasattr(flags, "DQ") and hasattr(flags.DQ, "Steering") and hasattr(flags, "Detector"):
+        if hasattr(flags.DQ.Steering, "InDet"):
+            if ((flags.DQ.Steering.InDet, "doAlignMon") and flags.DQ.Steering.InDet.doAlignMon) or \
+               ((flags.DQ.Steering.InDet, "doGlobalMon") and flags.DQ.Steering.InDet.doGlobalMon) or \
+               ((flags.DQ.Steering.InDet, "doPerfMon") and flags.DQ.Steering.InDet.doPerfMon):
+                flags.Detector.GeometryID = True
 
     # Just assume we want the full ID geometry, if we are reading in geometry
-    ConfigFlags.Detector.GeometryPixel = True
-    ConfigFlags.Detector.GeometrySCT = True
-    ConfigFlags.Detector.GeometryTRT = True
+    flags.Detector.GeometryPixel = True
+    flags.Detector.GeometrySCT = True
+    flags.Detector.GeometryTRT = True
 
     log.info('FINAL CONFIG FLAGS SETTINGS FOLLOW')
     if args.loglevel is None or getattr(Constants, args.loglevel) <= Constants.INFO:
-        ConfigFlags.dump()
-    ConfigFlags.lock()
+        flags.dump()
+    flags.lock()
 
     # Initialize configuration object, add accumulator, merge, and run.
     from AthenaConfiguration.MainServicesConfig import MainServicesCfg
-    cfg = MainServicesCfg(ConfigFlags)
+    cfg = MainServicesCfg(flags)
 
     # add perfmon
     if args.perfmon:
         from PerfMonComps.PerfMonCompsConfig import PerfMonMTSvcCfg
-        cfg.merge(PerfMonMTSvcCfg(ConfigFlags))
+        cfg.merge(PerfMonMTSvcCfg(flags))
 
-    if ConfigFlags.Input.Format is Format.BS:
+    if flags.Input.Format is Format.BS:
         # attempt to start setting up reco ...
         from CaloRec.CaloRecoConfig import CaloRecoCfg
-        cfg.merge(CaloRecoCfg(ConfigFlags))
+        cfg.merge(CaloRecoCfg(flags))
     else:
         from AthenaPoolCnvSvc.PoolReadConfig import PoolReadCfg
-        cfg.merge(PoolReadCfg(ConfigFlags))
+        cfg.merge(PoolReadCfg(flags))
 
     # load DQ
     from AthenaMonitoring.AthenaMonitoringCfg import AthenaMonitoringCfg
-    dq = AthenaMonitoringCfg(ConfigFlags)
+    dq = AthenaMonitoringCfg(flags)
     cfg.merge(dq)
 
     # Force loading of conditions in MT mode
-    if ConfigFlags.Concurrency.NumThreads > 0:
+    if flags.Concurrency.NumThreads > 0:
         if len([_ for _ in cfg._conditionsAlgs if _.name=="PixelDetectorElementCondAlg"]) > 0:
             beginseq = cfg.getSequence("AthBeginSeq")
             beginseq.Members.append(CompFactory.ForceIDConditionsAlg("ForceIDConditionsAlg"))
