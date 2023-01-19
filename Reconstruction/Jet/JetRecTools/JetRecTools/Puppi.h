7dc5e4ec600a15f061807ff723860878651f84d2
scott snyder <sss@karma>
2023-01-19 11:00:21 -0500

    JetRecTools: Fix cppcheck warnings.
    Pass containers by const reference, not by value.

--- Reconstruction/Jet/JetRecTools/JetRecTools/Puppi.h
+++ Reconstruction/Jet/JetRecTools/JetRecTools/Puppi.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 #include "fastjet/internal/base.hh"
 #include "fastjet/PseudoJet.hh"
@@ -11,7 +11,7 @@
 class PuppiUserInfo: public fastjet::PseudoJet::UserInfoBase{
  public:
   PuppiUserInfo();
-  PuppiUserInfo(std::vector<double>);
+  PuppiUserInfo(const std::vector<double>&);
   PuppiUserInfo(double);
 
   std::vector<double> otherChi2Vec;
@@ -24,7 +24,7 @@ class Puppi{
  public:
   Puppi(double R0, double Rmin, double beta, double centralPTCutOffset, double centralPTCutSlope, double forwardPTCutOffset, double forwardPTCutSlope, double etaBoundary);
 
-  void setParticles(const std::vector<fastjet::PseudoJet> chargedHS, const std::vector<fastjet::PseudoJet> chargedPU, const std::vector<fastjet::PseudoJet> neutral, const std::vector<fastjet::PseudoJet> forward, int nPU);
+  void setParticles(const std::vector<fastjet::PseudoJet>& chargedHS, const std::vector<fastjet::PseudoJet>& chargedPU, const std::vector<fastjet::PseudoJet>& neutral, const std::vector<fastjet::PseudoJet>& forward, int nPU);
 
   // Returns the weight that would be applied to a particle (assuming it is neutral or forward)
   double getWeight(const fastjet::PseudoJet pfo);
