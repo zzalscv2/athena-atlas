0690bd9b598ea88b47b2c73d71731fa409e8ffbb
Scott Snyder <scott.snyder@cern.ch>
2023-01-19 08:42:55 +0100

    SiSPSeededTrackFinderData: Fix cppcheck warnings.
    SiSPSeededTrackFinderData: Fix cppcheck warnings.
    
    Prefer to initialize class members in an initialization list.
    Suppress cppcheck warnings about (apparently deliberately) not copying
    all members.

--- InnerDetector/InDetRecEvent/SiSPSeededTrackFinderData/src/SiTrajectoryElement_xk.cxx
+++ InnerDetector/InDetRecEvent/SiSPSeededTrackFinderData/src/SiTrajectoryElement_xk.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "SiSPSeededTrackFinderData/SiTrajectoryElement_xk.h"
@@ -1936,7 +1936,9 @@ InDet::SiTrajectoryElement_xk::SiTrajectoryElement_xk(const SiTrajectoryElement_
 {
   *this          = E;
 }
-  
+
+// deliberately not assigning all variables?
+// cppcheck-suppress operatorEqVarError
 InDet::SiTrajectoryElement_xk& InDet::SiTrajectoryElement_xk::operator = 
 (const InDet::SiTrajectoryElement_xk& E) 
 {
