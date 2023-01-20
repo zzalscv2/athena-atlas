fea3eb7f540f47f917c87a204552ef60bb0bc37a
Tadej Novak <tadej.novak@cern.ch>
2023-01-20 15:20:59 +0100

    Add initial Run 4 overlay ART tests

---
+++ Simulation/Tests/OverlayTests/test/test_MCOverlay_RUN4_ttbar_mu200.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# art-description: MC+MC Overlay without reco for Run 4, ttbar, mu=200
+# art-type: grid
+# art-architecture:  '#x86_64-intel'
+# art-include: master/Athena
+
+# art-output: *.root
+# art-output: log.*
+# art-output: mem.summary.*
+# art-output: mem.full.*
+# art-output: runargs.*
+
+if [ -z ${ATLAS_REFERENCE_DATA+x} ]; then
+  ATLAS_REFERENCE_DATA="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art"
+fi
+
+events=25
+HITS_File="${ATLAS_REFERENCE_DATA}/PhaseIIUpgrade/HITS/ATLAS-P2-RUN4-01-00-00/mc15_14TeV.600012.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.simul.HITS.e8185_s3856/HITS.29179779._000100.pool.root.1"
+RDO_BKG_File="${ATLAS_REFERENCE_DATA}/PhaseIIUpgrade/RDO_BKG/ATLAS-P2-RUN4-01-00-00/mu200.25events.RDO.pool.root"
+OverlayOutFile="RUN4_ttbar.mu200.overlay.RDO.pool.root"
+
+Overlay_tf.py \
+--CA \
+--conditionsTag OFLCOND-MC15c-SDR-14-05 \
+--digiSeedOffset1 170 --digiSeedOffset2 170 \
+--geometryVersion ATLAS-P2-RUN4-01-00-00 \
+--inputHITSFile ${HITS_File} \
+--inputRDO_BKGFile ${RDO_BKG_File} \
+--maxEvents ${events} \
+--outputRDOFile ${OverlayOutFile} \
+--preInclude 'HITtoRDO:Campaigns.PhaseIIPileUp200' \
+--postInclude 'PyJobTransforms.UseFrontier' \
+--skipEvents 0
+
+rc=$?
+status=$rc
+echo "art-result: $rc overlay"
+
+rc2=-9999
+if [ $rc -eq 0 ]; then
+  RunRDOAnalysis.py -i "$OverlayOutFile"
+  rc2=$?
+  status=$rc2
+fi
+echo "art-result: $rc2 analysis"
+
+if command -v art.py >/dev/null 2>&1; then
+  rc3=-9999
+  if [ $rc -eq 0 ]; then
+    art.py compare grid --entries 10 "${1}" "${2}" --mode=semi-detailed --file="$OverlayOutFile"
+    rc3=$?
+    status=$rc3
+  fi
+  echo "art-result: $rc3 regression"
+fi
+
+exit $status
