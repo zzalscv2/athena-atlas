0b5dfed9beda981c9f811189d89bf473322e7010
Andrew Caldon Smith <andrew.caldon.smith@cern.ch>
2023-01-20 14:40:52 +0100

    Add electron OR into SUSYTools
    cleaned up working version; now it checks whether StdWithLRTMuons has been created already before making a new one

--- PhysicsAnalysis/SUSYPhys/SUSYTools/SUSYTools/SUSYObjDef_xAOD.h
+++ PhysicsAnalysis/SUSYPhys/SUSYTools/SUSYTools/SUSYObjDef_xAOD.h
@@ -29,6 +29,7 @@
 // Tool handles
 #include "AsgTools/AnaToolHandle.h"
 #include "MuonAnalysisInterfaces/IMuonLRTOverlapRemovalTool.h"
+#include "EgammaAnalysisInterfaces/IElectronLRTOverlapRemovalTool.h"
 
 // Configuration
 #include "TEnv.h"
@@ -165,7 +166,7 @@ namespace ST {
     StatusCode GetFatJets(xAOD::JetContainer*& copy, xAOD::ShallowAuxContainer*& copyaux, const bool recordSG = false, const std::string& jetkey = "", const bool doLargeRdecorations = false, const xAOD::JetContainer* containerToBeCopied = nullptr) override final;
     StatusCode GetTaus(xAOD::TauJetContainer*& copy, xAOD::ShallowAuxContainer*& copyaux, const bool recordSG = true, const std::string& taukey = "TauJets", const xAOD::TauJetContainer* containerToBeCopied = nullptr) override final;
     StatusCode GetMuons(xAOD::MuonContainer*& copy, xAOD::ShallowAuxContainer*& copyaux, const bool recordSG = true, const std::string& muonkey = "Muons", const std::string& lrtmuonkey = "MuonsLRT", const xAOD::MuonContainer* containerToBeCopied = nullptr) override final;
-    StatusCode GetElectrons(xAOD::ElectronContainer*& copy, xAOD::ShallowAuxContainer*& copyaux, const bool recordSG = true, const std::string& elekey = "Electrons", const xAOD::ElectronContainer* containerToBeCopied = nullptr) override final;
+    StatusCode GetElectrons(xAOD::ElectronContainer*& copy, xAOD::ShallowAuxContainer*& copyaux, const bool recordSG = true, const std::string& elekey = "Electrons", const std::string& lrtelekey = "LRTElectrons", const xAOD::ElectronContainer* containerToBeCopied = nullptr) override final;
     StatusCode GetPhotons(xAOD::PhotonContainer*& copy, xAOD::ShallowAuxContainer*& copyaux, const bool recordSG = true, const std::string& photonkey = "Photons", const xAOD::PhotonContainer* containerToBeCopied = nullptr) override final;
     StatusCode GetMET(xAOD::MissingETContainer& met,
                       const xAOD::JetContainer* jet,
@@ -192,6 +193,11 @@ namespace ST {
     const xAOD::MuonContainer* prompt_muons = nullptr;
     const xAOD::MuonContainer* lrt_muons = nullptr;
     mutable xAOD::Muon* newMuon = nullptr;
+
+    StatusCode MergeElectrons(const  xAOD::ElectronContainer & electrons, xAOD::ElectronContainer* outputCol, const std::set<const xAOD::Electron *> &ElectronsToRemove) const override final;
+    const xAOD::ElectronContainer* prompt_electrons = nullptr;
+    const xAOD::ElectronContainer* lrt_electrons = nullptr;
+    mutable xAOD::Electron* newElectron = nullptr;
 
     bool IsPFlowCrackVetoCleaning(const xAOD::ElectronContainer* elec = nullptr, const xAOD::PhotonContainer* gamma = nullptr) const override final;
 
@@ -591,6 +597,8 @@ namespace ST {
     std::string m_eleIsoHighPt_WP;
     double      m_eleIsoHighPtThresh;
     std::string m_eleChID_WP;
+    bool m_eleLRT; 
+    int m_eleLRT_strat;
     bool        m_eleChIso; // use Charge ID SF with/without Iso applied
     bool        m_eleChID_signal; // allows to run ECID but remove it from signal definition
     bool        m_runECIS; //run ChargeIDSelector if valid WP was selected
@@ -794,7 +802,6 @@ namespace ST {
     ToolHandleArray<CP::IMuonTriggerScaleFactors> m_muonTrigSFTools;
     asg::AnaToolHandle<CP::IMuonLRTOverlapRemovalTool> m_muonLRTORTool;
     SG::WriteHandleKey<xAOD::MuonContainer>     m_outMuonLocation{this, "OutputMuonLocation", "StdWithLRTMuons", "name of the muon container to write"};   /** Combined muon collection.   */
-
     //
     asg::AnaToolHandle<IAsgElectronEfficiencyCorrectionTool> m_elecEfficiencySFTool_reco;
     asg::AnaToolHandle<IAsgElectronEfficiencyCorrectionTool> m_elecEfficiencySFTool_id;
@@ -808,6 +815,9 @@ namespace ST {
     std::vector<asg::AnaToolHandle<IAsgElectronEfficiencyCorrectionTool>> m_elecEfficiencySFTool_trigEff_mixLep;
     ToolHandleArray<IAsgElectronEfficiencyCorrectionTool> m_elecTrigSFTools;
     ToolHandleArray<IAsgElectronEfficiencyCorrectionTool> m_elecTrigEffTools;
+    asg::AnaToolHandle<CP::IElectronLRTOverlapRemovalTool> m_elecLRTORTool;
+    SG::WriteHandleKey<xAOD::ElectronContainer>     m_outElectronLocation{this, "OutputElectronLocation", "StdWithLRTElectrons", "name of the muon container to write"};   /** Combined electron collection.   */
+
     //
     asg::AnaToolHandle<CP::IEgammaCalibrationAndSmearingTool> m_egammaCalibTool;
     asg::AnaToolHandle<IAsgElectronLikelihoodTool> m_elecSelLikelihood;
