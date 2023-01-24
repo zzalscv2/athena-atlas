a8e8127c4c009666956cf8de86812fe0989f964b
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2023-01-24 11:09:59 +0100

    Introduce CA based AODtoDAOD in q442 test, remove doRAWtoALL

--- Tools/TrfTestsARTPlots/test/test_trf_q442_phys_physlite_ca.sh
+++ Tools/TrfTestsARTPlots/test/test_trf_q442_phys_physlite_ca.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# art-description: Reco_tf.py q442, RAWtoALL in MT and AODtoDAOD in MP
+# art-description: Reco_tf.py q442, RAWtoALL in MT and AODtoDAOD in MP with CA
 # art-type: grid
 # art-include: master/Athena
 # art-athena-mt: 8
@@ -10,9 +10,7 @@
 export ATHENA_CORE_NUMBER=8
 Reco_tf.py \
   --AMI q442 \
-  --sharedWriter True \
-  --outputDAODFile art.pool.root \
-  --reductionConf PHYS PHYSLITE \
+  --outputAODFile myAOD.pool.root \
   --athenaopts "RAWtoALL:--threads=${ATHENA_CORE_NUMBER} --nprocs=0" "AODtoDAOD:--threads=0 --nprocs=${ATHENA_CORE_NUMBER}" \
   --postExec 'FPEAuditor.NStacktracesOnFPE=10; from DerivationFrameworkJetEtMiss.JetCommon import swapAlgsInSequence;swapAlgsInSequence(topSequence,"jetalg_ConstitModCorrectPFOCSSKCHS_GPFlowCSSK", "UFOInfoAlgCSSK" );' \
   --maxEvents -1
@@ -20,14 +18,28 @@ Reco_tf.py \
 rc1=$?
 echo "art-result: ${rc1} Reco_tf_q442_phys_physlite_mt_mp" 
 
+Derivation_tf.py \
+  --CA \
+  --inputAODFile myAOD.pool.root \
+  --outputDAODFile art.pool.root \
+  --sharedWriter True \
+  --multiprocess True \
+  --formats PHYS PHYSLITE \
+  --postExec 'from AthenaAuditors.AthenaAuditorsConf import FPEAuditor;FPEAuditor.NStacktracesOnFPE=10' \
+  --maxEvents -1
+
+rcderiv=$?
+echo "art-result: ${rcderiv} Derivation_tf_q449_phys_physlite_mp_ca" 
+
+
 # Check for FPEs in the logiles
 test_trf_check_fpe.sh
 fpeStat=$?
 
 echo "art-result: ${fpeStat} FPEs in logfiles"
 
-echo "============ checkxAOD tmp.AOD"
-checkxAOD tmp.AOD
+echo "============ checkxAOD myAOD.pool.root"
+checkxAOD myAOD.pool.root
 echo "============ checkxAOD DAOD_PHYS.art.pool.root"
 checkxAOD DAOD_PHYS.art.pool.root
 echo "============ checkxAOD DAOD_PHYSLITE.art.pool.root"
@@ -40,8 +52,8 @@ xAODHist.py --analysis --outputHISTFile hist_physlite_latest.root DAOD_PHYSLITE.
 rc3=$?
 echo "art-result: ${rc3} xAODHist DAOD_PHYSLITE.art.pool.root" 
 
-echo "============ xAODHistSize DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,tmp.AOD"
-xAODHistSize.py --outputHISTFile hist_physlite_latest.root --xAODFiles DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,tmp.AOD
+echo "============ xAODHistSize DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,myAOD.pool.root"
+xAODHistSize.py --outputHISTFile hist_physlite_latest.root --xAODFiles DAOD_PHYSLITE.art.pool.root,DAOD_PHYS.art.pool.root,myAOD.pool.root
 rc4=$?
 echo "art-result: ${rc4} xAODHistSize" 
 
