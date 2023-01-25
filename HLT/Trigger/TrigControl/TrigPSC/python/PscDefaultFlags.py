86b48cabef0b083669166ffc95f4c6fe32226556
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-01-25 10:26:47 +0100

    TrigServices: CA configuration for basic services
    Replace `setupCommonServices` with a CA function. This is the
    equivalent of `MainServicesConfig`.
    
    Notable changes during refactoring:
    - remove `ThreadPoolSvc` and `ThreadInitTool` as these are not needed
    - remove a few ineffective property settings (just re-setting defaults)
    
    Relates to ATR-26800.

--- HLT/Trigger/TrigControl/TrigPSC/python/PscDefaultFlags.py
+++ HLT/Trigger/TrigControl/TrigPSC/python/PscDefaultFlags.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 #
 '''Functions setting default flags for generating online HLT python configuration'''
 
@@ -19,6 +19,10 @@ def setDefaultOnlineFlagsNewStyle(flags):
     flags.Input.isMC = False
     flags.Input.Format = Format.BS
     flags.Trigger.doHLT = True  # This distinguishes the HLT setup from online reco (GM, EventDisplay)
+    flags.Scheduler.ShowDataDeps = False
+    flags.Scheduler.ShowControlFlow = False
+    flags.Scheduler.ShowDataFlow = False
+    flags.Scheduler.EnableVerboseViews = False
 
 
 def defaultOnlineFlags(flags = None):
