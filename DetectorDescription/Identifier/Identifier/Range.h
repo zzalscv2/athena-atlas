e9b36f3ac20451aedabebcf3c90e04bb4fde73bf
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2023-02-16 15:57:03 +0100

    ExpandedIdentifier: Try to avoid multiple calls to resize()
    ExpandedIdentifier: Try to avoid multiple calls to resize()

--- DetectorDescription/Identifier/Identifier/Range.h
+++ DetectorDescription/Identifier/Identifier/Range.h
@@ -1,11 +1,12 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef IDENTIFIER_RANGE_H
 #define IDENTIFIER_RANGE_H
  
 #include <Identifier/ExpandedIdentifier.h> 
+#include<vector>
 #include <cassert>
 #include <stdexcept>
  
