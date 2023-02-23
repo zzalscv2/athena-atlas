6d0340f90e1a5a39c82e6164f137158cdce0ab08
James Catmore <james.catmore@cern.ch>
2023-02-23 18:51:40 +0100

    Read/Write/Decor handles for TruthDressingTool
    Draft: Read/Write/Decor handles for TruthDressingTool
    
    This provides read/write/decor handles for the TruthDressingTool, as part of the effort to prepare the derivation framework for running in AthenaMT.

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkMCTruth/DerivationFrameworkMCTruth/TruthDressingTool.h
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkMCTruth/DerivationFrameworkMCTruth/TruthDressingTool.h
@@ -11,7 +11,11 @@
 #include "DerivationFrameworkInterfaces/IAugmentationTool.h"
 #include "DerivationFrameworkMCTruth/DecayGraphHelper.h"
 #include "xAODTruth/TruthParticleContainer.h"
+#include "Gaudi/Property.h"
 #include "GaudiKernel/ToolHandle.h"
+#include "StoreGate/ReadHandleKey.h"
+#include "StoreGate/WriteDecorHandleKey.h"
+#include "StoreGate/ReadDecorHandleKey.h"
 
 namespace DerivationFramework {
 
@@ -24,22 +28,52 @@ namespace DerivationFramework {
       virtual StatusCode addBranches() const;
 
     private:
-      /// Parameter: input collection key
-      std::string m_particlesKey;
-      /// Parameter: collection key of particles to be dressed
-      std::string m_dressParticlesKey;
+      /// ReadHandleKey input collection key
+      SG::ReadHandleKey<xAOD::TruthParticleContainer> m_particlesKey
+         {this, "particlesKey", "TruthParticles", "ReadHandleKey for TruthParticles for photon list input"};
+      /// ReadHandleKey for particles to be dressed
+      SG::ReadHandleKey<xAOD::TruthParticleContainer> m_dressParticlesKey
+         {this, "dressParticlesKey", "TruthParticles", "ReadHandleKey for input particles to be dressed.  If taus are selected, everything in this input key will be used"};
+      /// WriteDecorHandleKeys for decorations
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_eKey
+         {this, "e_dressed", "TruthParticles.e_dressed", "e_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_ptKey
+         {this, "pt_dressed", "TruthParticles.pt_dressed", "pt_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_etaKey
+         {this, "eta_dressed", "TruthParticles.eta_dressed", "eta_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_phiKey 
+         {this, "phi_dressed", "TruthParticles.phi_dressed", "phi_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_pt_visKey
+         {this, "pt_vis_dressed", "TruthParticles.pt_vis_dressed", "pt_vis_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_eta_visKey
+         {this, "eta_vis_dressed", "TruthParticles.eta_vis_dressed", "eta_vis_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_phi_visKey
+         {this, "phi_vis_dressed", "TruthParticles.phi_vis_dressed", "phi_vis_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_m_visKey
+         {this, "m_vis_dressed", "TruthParticles.m_vis_dressed", "m_vis_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorator_nphotonKey
+         {this, "nPhotons_dressed", "TruthParticles.nPhotons_dressed", "nPhotons_dressed decoration"};
+      SG::WriteDecorHandleKey<xAOD::TruthParticleContainer> m_decorationKey
+         {this, "DressingKey", "TruthParticles.dressedPhoton", "Dressed photon decoration"};
+
       /// Parameter: Use photons from hadron decays?
-      bool m_usePhotonsFromHadrons;
+      Gaudi::Property<bool> m_usePhotonsFromHadrons 
+         {this, "usePhotonsFromHadrons", false,  "Add photons coming from hadron decays while dressing"};  
       /// Parameter: Use leptons from hadron decays?
-      bool m_useLeptonsFromHadrons;
+      Gaudi::Property<bool> m_useLeptonsFromHadrons 
+         {this, "useLeptonsFromHadrons", false, "Consider leptons coming from hadron decays?"};
       /// Parameter: Cone size for dressing
-      float m_coneSize;
+      Gaudi::Property<float> m_coneSize 
+         {this, "dressingConeSize", 0.1, "Size of dR cone in which to include FSR photons in dressing"}; 
       /// Parameter: List of pdgIDs of particles to dress
-      std::vector<int> m_listOfPIDs;
+      Gaudi::Property< std::vector<int> > m_listOfPIDs 
+         {this, "particleIDsToDress", {11,13}, "List of the pdgID's of particles to be dressed (usually 11,13).  Special treatment for taus (15)"};
       /// Parameter: Use antikT algorithm for dressing?
-      bool m_useAntiKt;
+      Gaudi::Property<bool> m_useAntiKt 
+         {this, "useAntiKt", false, "use anti-k_T instead of fixed-cone dressing"};
       /// Parameter: Name of the decoration to apply
-      std::string m_decorationName;
+      Gaudi::Property<std::string> m_decorationName
+         {this, "decorationName", "", "Name of the decoration for photons that were used in dressing"};
   }; 
 }
 
