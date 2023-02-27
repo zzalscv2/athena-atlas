be08aa9ce218e0fe3a9573762fac491833554246
Rui Wang <rui.wang@cern.ch>
2023-02-27 14:35:06 +0100

    Add ComponentAccumulator tool configuration for ISF_ActsTools
    Add ComponentAccumulator tool configuration for ISF_ActsTools

--- Simulation/ISF/ISF_Acts/ISF_ActsTools/python/ISF_ActsToolsConfigDb.py
+++ Simulation/ISF/ISF_Acts/ISF_ActsTools/python/ISF_ActsToolsConfigDb.py
@@ -3,4 +3,4 @@
 
 from AthenaCommon.CfgGetter import addTool
 
-addTool("ISF_ActsTools.ISF_ActsToolsConfig.getActsFatrasSimTool",  "ISF_ActsFatrasSimTool")
+addTool("ISF_ActsTools.ISF_ActsToolsConfigLegacy.getActsFatrasSimTool",  "ISF_ActsFatrasSimTool")
