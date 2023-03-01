3de6f473f1474efc1d49dbbd4dfcb19042671207
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2023-03-01 14:13:04 +0100

    MC23 (and MC21) configuration for the 2022 low mu run (420000)
    MC23 (and MC21) configuration for the 2022 low mu run (420000)

---
+++ Tools/Campaigns/share/MC23NoPileUp.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.BeamFlags import jobproperties as bf
+bf.Beam.numberOfCollisions.set_Value_and_Lock(0)
+
+from Digitization.DigitizationFlags import digitizationFlags
+from SimulationConfig.SimEnums import PixelRadiationDamageSimulationType
+digitizationFlags.pixelPlanarRadiationDamageSimulationType.set_Value_and_Lock(PixelRadiationDamageSimulationType.RamoPotential.value)
+digitizationFlags.dataRunNumber.set_Value_and_Lock(410000)
+
+from AthenaCommon.Resilience import protectedInclude
+protectedInclude('LArConfiguration/LArConfigRun3Old_NoPileup.py')
+
+protectedInclude('PyJobTransforms/HepMcParticleLinkVerbosity.py')
