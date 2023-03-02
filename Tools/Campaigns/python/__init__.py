1a02003664555d9ec675ba6d58de3ae01d413965
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2023-03-02 15:48:12 +0100

    MC21 configuration for the 2022 low mu run (420000)
    MC21 configuration for the 2022 low mu run (420000)

--- Tools/Campaigns/python/__init__.py
+++ Tools/Campaigns/python/__init__.py
@@ -1,14 +1,14 @@
-# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 
 from .MC16 import MC16a, MC16d, MC16e, MC16NoPileUp, MC16SimulationNoIoV, MC16SimulationSingleIoV, MC16Simulation
 from .MC20 import MC20a, MC20d, MC20e, MC20NoPileUp
-from .MC21 import MC21a, MC21aSingleBeamspot, MC21NoPileUp, BeamspotSplitMC21a, MC21SimulationNoIoV, MC21SimulationSingleIoV, MC21Simulation, MC21SimulationMultipleIoV, MC21SimulationMultiBeamSpot, MC21SimulationSingleIoVCalibrationHits, MC21SimulationCalibrationHits, MC21SimulationMultipleIoVCalibrationHits, MC21SimulationMultiBeamSpotCalibrationHits
+from .MC21 import MC21a, MC21aSingleBeamspot, MC21NoPileUp, MC21NoPileUpLowMuRun, BeamspotSplitMC21a, MC21SimulationNoIoV, MC21SimulationLowMuRun, MC21SimulationSingleIoV, MC21Simulation, MC21SimulationMultipleIoV, MC21SimulationMultiBeamSpot, MC21SimulationSingleIoVCalibrationHits, MC21SimulationCalibrationHits, MC21SimulationMultipleIoVCalibrationHits, MC21SimulationMultiBeamSpotCalibrationHits
 from .PhaseII import PhaseIIPileUp60, PhaseIIPileUp140, PhaseIIPileUp200, PhaseIINoPileUp, PhaseIISimulationNoIoV, PhaseIISimulationSingleIoV, PhaseIISimulation
 
 __all__ = [
   'MC16a', 'MC16d', 'MC16e', 'MC16NoPileUp', 'MC16SimulationNoIoV', 'MC16SimulationSingleIoV', 'MC16Simulation',
   'MC20a', 'MC20d', 'MC20e', 'MC20NoPileUp',
-  'MC21a', 'MC21aSingleBeamspot', 'MC21NoPileUp', 'BeamspotSplitMC21a', 'MC21SimulationNoIoV', 'MC21SimulationSingleIoV', 'MC21Simulation',
+  'MC21a', 'MC21aSingleBeamspot', 'MC21NoPileUp', 'MC21NoPileUpLowMuRun', 'BeamspotSplitMC21a', 'MC21SimulationNoIoV', 'MC21SimulationLowMuRun', 'MC21SimulationSingleIoV', 'MC21Simulation',
   'MC21SimulationMultipleIoV', 'MC21SimulationMultiBeamSpot',
   'MC21SimulationSingleIoVCalibrationHits', 'MC21SimulationCalibrationHits',
   'MC21SimulationMultipleIoVCalibrationHits', 'MC21SimulationMultiBeamSpotCalibrationHits',
---
+++ Tools/Campaigns/share/MC21NoPileUpLowMuRun.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+
+from AthenaCommon.BeamFlags import jobproperties as bf
+bf.Beam.numberOfCollisions.set_Value_and_Lock(0)
+
+from Digitization.DigitizationFlags import digitizationFlags
+digitizationFlags.doPixelPlanarRadiationDamage.set_Value_and_Lock(True)
+digitizationFlags.dataRunNumber.set_Value_and_Lock(420000)
+
+from AthenaCommon.Resilience import protectedInclude
+protectedInclude('LArConfiguration/LArConfigRun3Old_NoPileup.py')
+
+protectedInclude('PyJobTransforms/HepMcParticleLinkVerbosity.py')
