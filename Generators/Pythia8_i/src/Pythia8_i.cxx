9e84ebcfbac66cb4dadbcb3e1578643eaa159bea
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2023-03-06 14:55:35 +0100

    Use access by reference in the loops in HepMC codes
    Use access by reference in the loops in HepMC codes

--- Generators/Pythia8_i/src/Pythia8_i.cxx
+++ Generators/Pythia8_i/src/Pythia8_i.cxx
@@ -667,7 +667,7 @@ void Pythia8_i::addLHEToHepMC(HepMC::GenEvent *evt){
   }
 
   //This code and the HepMC2 version below assume a correct input, e.g. beams[0]->end_vertex() exists.
-  for(auto  v: procEvent->vertices()) v->set_status(1);
+  for(auto&  v: procEvent->vertices()) v->set_status(1);
   auto beams=evt->beams();
   auto procBeams=procEvent->beams();
   if(beams[0]->momentum().pz() * procBeams[0]->momentum().pz() < 0.) std::swap(procBeams[0],procBeams[1]);
