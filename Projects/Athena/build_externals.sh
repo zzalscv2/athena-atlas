2652b763dc7f5cc8b4c7b90572874fde61d3678a
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2023-03-06 18:16:56 +0100

    Projects: Update to Gaudi v36r11.000
    Projects: Update to Gaudi v36r11.000

--- Projects/Athena/build_externals.sh
+++ Projects/Athena/build_externals.sh
@@ -11,7 +11,7 @@ ATLAS_EXT_PROJECT_NAME="AthenaExternals"
 ATLAS_BUILDTYPE="RelWithDebInfo"
 ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=102
                         -DLCG_VERSION_POSTFIX="b_ATLAS_11"
-                        -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v36r10.000/Gaudi-v36r10.000.tar.gz;URL_MD5;8ec6a34071aacf7f796f74010df69d37"
+                        -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v36r11.000/Gaudi-v36r11.000.tar.gz;URL_MD5;75c37c23a1e210d8fcbf98d988bf7fcd"
                         -DATLAS_ACTS_SOURCE="URL;https://github.com/acts-project/acts/archive/refs/tags/v23.4.0.tar.gz;URL_MD5;b2a4293abcd36470b781392bdd936da4")
 ATLAS_EXTRA_MAKE_ARGS=()
 
