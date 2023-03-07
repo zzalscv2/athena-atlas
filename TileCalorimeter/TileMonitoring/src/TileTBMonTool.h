993aeecd708793ff00e02a688fd1fe4da4087b59
Siarhei Harkusha <siarhei.harkusha@cern.ch>
2023-03-07 14:03:02 +0100

    TileMonitoring: Add topological analysis histograms for TB
    TileMonitoring: Add topological analysis histograms for TB
    
    TileTBMonTool has been updated for TB2022 campaigns to add
    topological analysis histograms and timging histograms per module.

--- TileCalorimeter/TileMonitoring/src/TileTBMonTool.h
+++ TileCalorimeter/TileMonitoring/src/TileTBMonTool.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 // ********************************************************************
@@ -15,6 +15,8 @@
 #define TILEMONITORING_TILETBMONTOOL_H
 
 #include "TileFatherMonTool.h"
+
+#include "Identifier/IdentifierHash.h"
 
 class TileDQstatus;
 
@@ -44,6 +46,7 @@ class ATLAS_NOT_THREAD_SAFE TileTBMonTool : public TileFatherMonTool {  // depre
 
     void fillHitMap(int side, int section, int module, int tower, int sample, double energy);
     void initFirstEvent(void);
+    void bookTimeHistograms(int ros, int drawer);
 
     std::string m_cellContainer; 
 
@@ -52,7 +55,14 @@ class ATLAS_NOT_THREAD_SAFE TileTBMonTool : public TileFatherMonTool {  // depre
 
     TProfile2D* m_tileTBHitMapLBC01;
     TProfile2D* m_tileTBHitMapEBC02;
+    TProfile2D* m_tileTBHitMap;
     TH2F* m_tileEventEnergyVsCellsNumber;
+    TH2F* m_CtotVsClong;
+    TH1F* m_Ctot;
+    TH1F* m_Clong;
+    TH1F* m_hotCell_LBC02_A{nullptr};
+
+    TProfile* m_tileTBChannelTime[4][64];
 
     bool m_isFirstEvent;
     unsigned char m_maskedChannels[276][48]; // max drawerIdx = 276 and max channel = 48
@@ -60,6 +70,9 @@ class ATLAS_NOT_THREAD_SAFE TileTBMonTool : public TileFatherMonTool {  // depre
 
     double m_maxTotalEnergy;
     double m_cellEnergyThreshold;
+    float m_energyThresholdForTime;
+    double m_beamEnergy;
+    std::vector<std::vector<IdentifierHash>> m_cellsNearTower;
 };
 
 #endif
