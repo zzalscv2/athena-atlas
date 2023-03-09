229f56875a33ad00e407d9472b75f3ae392eb2e3
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-03-09 10:16:30 +0100

    TriggerTest: fix conditions tag in HI and lowMu ART tests
    Set an explicit conditions tag for lowMu (ATR-27091) and
    HI samples (ATR-27088).

--- Trigger/TrigValidation/TriggerTest/test/test_trig_mc_v1lowMu_grid.py
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_mc_v1lowMu_grid.py
@@ -27,7 +27,13 @@ ex.job_options = 'TriggerJobOpts/runHLT_standalone.py'
 ex.input = 'minbias'
 ex.threads = 4
 ex.concurrent_events = 4
-ex.args = '-c "setMenu=\'PhysicsP1_pp_lowMu_run3_v1\';doWriteBS=False;doWriteRDOTrigger=True;"'
+precommand = ''.join([
+  "setMenu='PhysicsP1_pp_lowMu_run3_v1';",
+  "doWriteBS=False;",
+  "doWriteRDOTrigger=True;",
+  "from AthenaConfiguration.AllConfigFlags import ConfigFlags;ConfigFlags.IOVDb.GlobalTag='OFLCOND-MC21-SDR-RUN3-07'"
+])
+ex.args = '-c "{:s}"'.format(precommand)
 
 test = Test.Test()
 test.art_type = 'grid'
