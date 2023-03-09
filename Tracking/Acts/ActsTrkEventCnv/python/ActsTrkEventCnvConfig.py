4940f316e4bca7cc8d664b93565e1ed513a7c390
Edward Moyse <edward.moyse@cern.ch>
2023-03-09 16:39:52 +0100

    Create new ActsTrkEventCnv package, and copy ActsATLASConvertorTool there
    Create new ActTrkEventCnv package, and copy ActsATLASConvertorTool there
    Renamed and cleaned up methods to match ATLAS coding standards
    Added a new test (which currently doesn't do very much)

---
+++ Tracking/Acts/ActsTrkEventCnv/python/ActsTrkEventCnvConfig.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator 
+from AthenaConfiguration.ComponentFactory import CompFactory
+
+def ActsToTrkConverterToolCfg(flags, name="ActsToTrkConverterTool", **kwargs):
+    from ActsGeometry.ActsGeometryConfig import ActsTrackingGeometryToolCfg
+    result = ComponentAccumulator()
+    kwargs.setdefault("TrackingGeometryTool", result.popToolsAndMerge(ActsTrackingGeometryToolCfg(flags))) # PrivateToolHandle
+    result.setPrivateTools(CompFactory.ActsTrk.ActsToTrkConverterTool(name, **kwargs) )
+    return result
+
+def TrkToActsConvertorAlgCfg(flags, name="", **kwargs):
+    result = ComponentAccumulator()
+    kwargs.setdefault("ConvertorTool", result.popToolsAndMerge(ActsToTrkConverterToolCfg(flags))) # PrivateToolHandle
+    result.addEventAlgo(
+      CompFactory.ActsTrk.TrkToActsConvertorAlg(name, **kwargs) )
+    return result
+
+if __name__ == "__main__":
+# To run this, do e.g.
+    # python -m ActsTrkEventCnv.ActsTrkEventCnvConfig --threads=1
+
+    from AthenaConfiguration.AllConfigFlags import initConfigFlags
+    from TrkConfig.TrackCollectionReadConfig import TrackCollectionReadCfg
+    from AthenaPoolCnvSvc.PoolReadConfig import PoolReadCfg
+    flags = initConfigFlags()
+    args = flags.fillFromArgs()
+
+    flags.Input.Files = [
+        '/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/ActsTrkEventCnv/RUN4.ESD.pool.root']
+    flags.IOVDb.GlobalTag = "OFLCOND-MC21-SDR-RUN3-07"
+    flags.Scheduler.ShowDataDeps=True
+    flags.Scheduler.ShowDataFlow=True
+    flags.Scheduler.CheckDependencies=True
+    flags.lock()
+    flags.dump()
+
+    if not args.config_only:
+        from AthenaConfiguration.MainServicesConfig import MainServicesCfg
+        cfg = MainServicesCfg(flags)
+    else:
+        cfg=ComponentAccumulator()
+    
+    # Set up to read ESD and tracks
+    track_collections = ['CombinedITkTracks']
+    cfg.merge(PoolReadCfg(flags))
+    for collection in track_collections:
+        cfg.merge(TrackCollectionReadCfg(flags, collection))
+
+    # Needed to read tracks
+    from TrkEventCnvTools.TrkEventCnvToolsConfigCA import TrkEventCnvSuperToolCfg
+    cfg.merge(TrkEventCnvSuperToolCfg(flags))
+
+
+    # Muon geometry not yet in ActsTrackingGeometrySvcCfg
+    from MuonConfig.MuonGeometryConfig import MuonGeoModelCfg
+    cfg.merge(MuonGeoModelCfg(flags))
+
+    # Now setup the convertor
+    acc = TrkToActsConvertorAlgCfg(flags, OutputLevel=1, TrackCollectionKeys=track_collections)
+    cfg.merge(acc)
+
+    cfg.printConfig(withDetails=True, summariseProps=True)
+
+    if not args.config_only:
+        sc = cfg.run()
+        if not sc.isSuccess():
+            import sys
+            sys.exit("Execution failed")
