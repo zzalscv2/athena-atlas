ec649f20ad5e5bf59370f1d1a82f903aeda0243a
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-03-20 17:32:07 +0100

    TrigP1Test: remove unnecessary BFieldAutoConfig modifier

--- Trigger/TrigValidation/TrigP1Test/test/test_trigP1_v1Dev_noL1Sim_noHLTChains_MT_build.py
+++ Trigger/TrigValidation/TrigP1Test/test/test_trigP1_v1Dev_noL1Sim_noHLTChains_MT_build.py
@@ -15,7 +15,7 @@ ex.input = 'data'
 ex.forks = 2
 ex.threads = 2
 ex.concurrent_events = 2
-ex.args = '-c "setMenu=\'Dev_pp_run3_v1\';doL1Sim=False;doEmptyMenu=True;forceEnableAllChains=True;BFieldAutoConfig=False;"'
+ex.args = '-c "setMenu=\'Dev_pp_run3_v1\';doL1Sim=False;doEmptyMenu=True;forceEnableAllChains=True"'
 ex.args += ' --dump-config-reload'
 
 test = Test.Test()
