7b3fdaeb1d6ab5ed6f9708b49cb65357d7c089e9
Jiri Masik <jiri.masik@cern.ch>
2023-03-23 11:26:46 +0100

    Idtrig ca migration trig fast track finder config
    Idtrig ca migration trig fast track finder config

--- Trigger/TrigValidation/TrigInDetValidation/share/TIDAbjetpt_preinclude.py
+++ Trigger/TrigValidation/TrigInDetValidation/share/TIDAbjetpt_preinclude.py
@@ -7,8 +7,10 @@ from TrigInDetConfig.ConfigSettings import getInDetTrigConfig
 from AthenaCommon.SystemOfUnits import GeV
 
 getInDetTrigConfig("bjet")._pTmin = 0.8*GeV
-getInDetTrigConfig("bjet")._Xi2max = 12.0
+
+from AthenaConfiguration.AllConfigFlags import ConfigFlags as flags
+flags.Trigger.InDetTracking.bjet.Xi2max=12.
 
 log.info( "ID Trigger pTmin: "+str(getInDetTrigConfig("bjet").pTmin) )
-log.info( "ID Trigger Xi2max: "+str(getInDetTrigConfig("bjet").Xi2max) )
+log.info(f"ID Trigger Xi2max: {flags.Trigger.InDetTracking.bjet.Xi2max}")
 
