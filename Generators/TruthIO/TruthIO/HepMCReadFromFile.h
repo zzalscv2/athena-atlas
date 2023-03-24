c8fa6908b9df9bbaa98eda92667a84564e9e2a8d
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2023-03-24 19:07:08 +0100

    Use ReaderFactory for the ascii file input
    Use ReaderFactory for the ascii file input

--- Generators/TruthIO/TruthIO/HepMCReadFromFile.h
+++ Generators/TruthIO/TruthIO/HepMCReadFromFile.h
@@ -28,7 +28,7 @@ private:
   StoreGateSvc* m_sgSvc;
 
 #ifdef HEPMC3
-  std::unique_ptr<HepMC3::ReaderAsciiHepMC2> m_hepmcio;
+  std::shared_ptr<HepMC3::Reader> m_hepmcio;
 #else
   std::unique_ptr<HepMC::IO_GenEvent> m_hepmcio;
 #endif
