e0430bb34adb8153f4e8a769f4e853b4243602cd
Thomas Strebler <thomas.strebler@cern.ch>
2023-03-28 16:39:12 +0200

    MET Reco: workaround to avoid unmerged CA
    MET Reco: workaround to avoid unmerged CA

--- Reconstruction/MET/METReconstruction/python/METTruth_Cfg.py
+++ Reconstruction/MET/METReconstruction/python/METTruth_Cfg.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 
 from METReconstruction.METRecoCfg import BuildConfig, METConfig,getMETRecoAlg
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
@@ -15,6 +15,8 @@ def METTruth_Cfg(configFlags):
                            BuildConfig('IntMuons')],
                           doRegions=True
                           )
+    components.merge(cfg_truth.accumulator)
+
     recoAlg=getMETRecoAlg(algName='METRecoAlg_Truth',configs={"Truth":cfg_truth})
     components.addEventAlgo(recoAlg)
     return components
