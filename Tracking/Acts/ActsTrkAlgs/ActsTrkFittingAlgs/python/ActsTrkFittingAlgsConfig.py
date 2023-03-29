c8a08cf3c7e332ec277403ba3d4c984b818751dc
Arnaud Jean Maury <arnaud.maury@cern.ch>
2023-03-29 14:39:24 +0200

    Write additional tracks from ACTS refitting to output ESD/AOD files
    Write additional tracks from ACTS refitting to output ESD/AOD files

--- Tracking/Acts/ActsTrkAlgs/ActsTrkFittingAlgs/python/ActsTrkFittingAlgsConfig.py
+++ Tracking/Acts/ActsTrkAlgs/ActsTrkFittingAlgs/python/ActsTrkFittingAlgsConfig.py
@@ -21,7 +21,47 @@ def ActsReFitterAlgCfg(flags, name="ActsReFitterAlg", **kwargs):
         )
     )
 
+    if flags.Acts.writeTrackCollection:
+        result.merge(writeAdditionalTracks(flags))
+
     return result
 
 
+def writeAdditionalTracks(flags, trackName='ResolvedTracks', newTrackName='ReFitted_Tracks'):
+    result = ComponentAccumulator()
+    
+    from xAODTrackingCnv.xAODTrackingCnvConfig import ITkTrackParticleCnvAlgCfg
+
+    if flags.Tracking.doTruth:
+        from InDetConfig.ITkTrackTruthConfig import ITkTrackTruthCfg
+        result.merge(ITkTrackTruthCfg(flags,
+              Tracks = trackName,
+              DetailedTruth = f"{trackName}DetailedTruth",
+              TracksTruth = f"{trackName}TruthCollection"))
+        result.merge(ITkTrackTruthCfg(flags,
+              Tracks = newTrackName,
+              DetailedTruth = f"{newTrackName}DetailedTruth",
+              TracksTruth = f"{newTrackName}TruthCollection"))
+
+    result.merge(ITkTrackParticleCnvAlgCfg(flags,
+           name = f"{trackName}TrackParticleCnvAlg",
+           TrackContainerName = trackName,
+           xAODTrackParticlesFromTracksContainerName = f"{trackName}TrackParticles",
+           TrackTruthContainerName = f"{trackName}TruthCollection")) 
+    result.merge(ITkTrackParticleCnvAlgCfg(flags,
+           name = f"{newTrackName}TrackParticleCnvAlg",
+           TrackContainerName = newTrackName,
+           xAODTrackParticlesFromTracksContainerName = f"{newTrackName}TrackParticles",
+           TrackTruthContainerName = f"{newTrackName}TruthCollection")) 
+
+    from OutputStreamAthenaPool.OutputStreamConfig import addToESD, addToAOD
+    itemList = [f"xAOD::TrackParticleContainer#{trackName}TrackParticles",
+                f"xAOD::TrackParticleAuxContainer#{trackName}TrackParticlesAux.",
+                f"xAOD::TrackParticleContainer#{newTrackName}TrackParticles",
+                f"xAOD::TrackParticleAuxContainer#{newTrackName}TrackParticlesAux."]
+
+    result.merge(addToESD(flags, itemList))
+    result.merge(addToAOD(flags, itemList))
+    
+    return result
 
