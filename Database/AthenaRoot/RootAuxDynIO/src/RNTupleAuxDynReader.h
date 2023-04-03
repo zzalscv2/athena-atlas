3e0f760d1af73714c979a08534a06d9a566eb378
Marcin Jerzy Nowak <marcin.nowak@cern.ch>
2023-04-03 10:50:52 +0200

    Fix reading EventInfoAux::eventNumber (long long vs int64_t)
    Fix reading EventInfoAux::eventNumber (long long vs int64_t)

--- Database/AthenaRoot/RootAuxDynIO/src/RNTupleAuxDynReader.h
+++ Database/AthenaRoot/RootAuxDynIO/src/RNTupleAuxDynReader.h
@@ -5,6 +5,7 @@
 #ifndef RNTUPLEAUXDYNREADER_H
 #define RNTUPLEAUXDYNREADER_H
 
+#include "AthenaBaseComps/AthMessaging.h"
 #include "AthContainers/AuxStoreInternal.h" 
 #include "RootAuxDynIO/RootAuxDynIO.h" 
 
@@ -13,80 +14,84 @@
 
 class TClass;
 
-class RNTupleAuxDynReader :  public RootAuxDynIO::IRootAuxDynReader
+namespace RootAuxDynIO
 {
-public :
 
-  struct FieldInfo
-  {
-    enum Status { NotInitialized, Initialized, TypeError, NotFound };
+   class RNTupleAuxDynReader : public AthMessaging, public IRootAuxDynReader
+   {
+   public :
 
-    TClass*       tclass = 0;
-    TClass*       SE_tclass = 0;
+      struct FieldInfo
+      {
+         enum Status { NotInitialized, Initialized, TypeError, NotFound };
+
+         TClass*       tclass = 0;
+         TClass*       SE_tclass = 0;
     
-    bool          isPackedContainer = false;
-    bool          needsSE = false;
-    enum Status   status = NotInitialized;
+         bool          isPackedContainer = false;
+         bool          needsSE = false;
+         enum Status   status = NotInitialized;
 
-    SG::auxid_t   auxid;
-    std::string   attribName;
-    std::string   fieldName;
-    std::string   fieldType;
-  };
+         SG::auxid_t   auxid;
+         std::string   attribName;
+         std::string   fieldName;
+         std::string   fieldType;
+      };
 
    
-   RNTupleAuxDynReader(const std::string& store_key, RNTupleReader* rntReader);
+      RNTupleAuxDynReader(const std::string& store_key, RNTupleReader* rntReader);
 
-   void init(bool standalone);
+      void init(bool standalone);
 
-   virtual void addReaderToObject(void* object, size_t ttree_row, std::recursive_mutex* iomtx = nullptr ) override final;
+      virtual void addReaderToObject(void* object, size_t ttree_row, std::recursive_mutex* iomtx = nullptr ) override final;
 
-   virtual const SG::auxid_set_t& auxIDs() const override final;
+      virtual const SG::auxid_set_t& auxIDs() const override final;
 
-   void addBytes(size_t bytes);
+      void addBytes(size_t bytes);
 
-   virtual size_t getBytesRead() const override final;
+      virtual size_t getBytesRead() const override final;
 
-   virtual void resetBytesRead() override final;
+      virtual void resetBytesRead() override final;
 
-   const FieldInfo& getFieldInfo(const SG::auxid_t& auxid, const SG::AuxStoreInternal& store);
+      const FieldInfo& getFieldInfo(const SG::auxid_t& auxid, const SG::AuxStoreInternal& store);
 
-   virtual ~RNTupleAuxDynReader() {}
+      virtual ~RNTupleAuxDynReader() {}
 
-protected:
-   // auxids that could be found in registry for attribute names from the file
-   SG::auxid_set_t                       m_auxids;
+   protected:
+      // auxids that could be found in registry for attribute names from the file
+      SG::auxid_set_t                       m_auxids;
   
-   std::string                           m_storeFieldName;
-   // counter for bytes read
-   size_t                                m_bytesRead = 0;
-   // offset of the AxuStoreHolder base class in the objects read by the Reader
-   int                                   m_storeHolderOffset = -1;
-   bool                                  m_initialized = false;
-   std::string                           m_key;
+      std::string                           m_storeFieldName;
+      // counter for bytes read
+      size_t                                m_bytesRead = 0;
+      // offset of the AxuStoreHolder base class in the objects read by the Reader
+      int                                   m_storeHolderOffset = -1;
+      bool                                  m_initialized = false;
+      std::string                           m_key;
 
-   // map auxid -> branch info. not sure if it can be different from m_branchMap
-   std::map<SG::auxid_t, FieldInfo>     m_fieldInfos;
+      // map auxid -> branch info. not sure if it can be different from m_branchMap
+      std::map<SG::auxid_t, FieldInfo>     m_fieldInfos;
 
-   RNTupleReader*                       m_ntupleReader;
-};
+      RNTupleReader*                       m_ntupleReader;
+   };
 
 
 
-inline void  RNTupleAuxDynReader::addBytes(size_t bytes) {
-   m_bytesRead += bytes;
-}
+   inline void  RNTupleAuxDynReader::addBytes(size_t bytes) {
+      m_bytesRead += bytes;
+   }
 
-inline size_t RNTupleAuxDynReader::getBytesRead() const {
-   return m_bytesRead;
-}
+   inline size_t RNTupleAuxDynReader::getBytesRead() const {
+      return m_bytesRead;
+   }
 
-inline void RNTupleAuxDynReader::resetBytesRead() {
-   m_bytesRead = 0;
-}
+   inline void RNTupleAuxDynReader::resetBytesRead() {
+      m_bytesRead = 0;
+   }
 
-inline const SG::auxid_set_t& RNTupleAuxDynReader::auxIDs() const {
-    return m_auxids;
-}
+   inline const SG::auxid_set_t& RNTupleAuxDynReader::auxIDs() const {
+      return m_auxids;
+   }
 
+} //namespace
 #endif
