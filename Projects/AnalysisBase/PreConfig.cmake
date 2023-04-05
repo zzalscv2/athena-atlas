cf383f4fae85c53625fc06019affa3a8f4ff7fb1
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2023-04-05 19:31:22 +0200

    Externals Update, 21.2 branch (2023.03.30.)
    Setting ATLAS_USE_CUSTOM_CPACK_INSTALL_SCRIPT for analysis release clients.
    
    This way analysis projects should have an easier time detecting when
    something goes wrong during TGZ package making for grid running.

---
+++ Projects/AnalysisBase/PreConfig.cmake
@@ -0,0 +1,8 @@
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+#
+# Pre-configuration setup for AnalysisBase.
+#
+
+# Child projects should not use the ATLAS CPack install scripts.
+set( ATLAS_USE_CUSTOM_CPACK_INSTALL_SCRIPT FALSE
+   CACHE BOOL "Don't use the custom ATLAS CPack install scripts" )
