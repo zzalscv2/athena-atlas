828c9228bcdfaca87c528c57321e841e8080b9a9
Bastian Schlag <bastian.schlag@cern.ch>
2023-04-06 12:09:01 +0200

    Update gfex monitoring algorithm
    Update gfex monitoring algorithm

--- Trigger/TrigT1/TrigT1CaloMonitoring/src/GfexMonitorAlgorithm.h
+++ Trigger/TrigT1/TrigT1CaloMonitoring/src/GfexMonitorAlgorithm.h
@@ -7,13 +7,10 @@
 #include "AthenaMonitoring/AthMonitorAlgorithm.h"
 #include "AthenaMonitoringKernel/Monitored.h"
 #include "StoreGate/ReadHandleKey.h"
-
 #include "xAODTrigger/gFexJetRoI.h"
 #include "xAODTrigger/gFexJetRoIContainer.h"
 #include "xAODTrigger/gFexGlobalRoI.h"
 #include "xAODTrigger/gFexGlobalRoIContainer.h"
-
-
 
 class GfexMonitorAlgorithm : public AthMonitorAlgorithm {
 public:GfexMonitorAlgorithm( const std::string& name, ISvcLocator* pSvcLocator );
@@ -22,20 +19,19 @@ public:GfexMonitorAlgorithm( const std::string& name, ISvcLocator* pSvcLocator )
   virtual StatusCode fillHistograms( const EventContext& ctx ) const override;
 
 private:
+  // Private members
+  enum class FPGAType {FPGAa, FPGAb, FPGAc, None};
+  std::map<std::string, std::pair<std::string, std::string>> m_globTobVarMap;
   StringProperty m_packageName{this,"PackageName","gfexMonitor","group name for histograming"};
-  // container keys including this, steering parameter, default value and help description
-  SG::ReadHandleKey< xAOD::gFexJetRoIContainer > m_gFexLRJetContainerKey {this,"mygFexLRJetRoIContainer","L1_gFexLRJetRoI","SG key of the input gFex LR Jet Roi container"};
-  SG::ReadHandleKey< xAOD::gFexJetRoIContainer > m_gFexRhoContainerKey {this,"mygFexRhoContainer","L1_gFexRhoRoI","SG key of the input gFex Rho container"};
-  SG::ReadHandleKey< xAOD::gFexJetRoIContainer > m_gFexSRJetContainerKey {this,"mygFexSRJetOutputContainer","L1_gFexSRJetRoI","SG key of the input gFex SR Jet Roi container"};
-
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gScalarEJwojContainerKey {this,"mygScalarEJwojContainer","L1_gScalarEJwoj","SG key of the inputScalar MET and SumET (from Jets without Jets algo) container"};
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gMETComponentsJwojContainerKey {this,"mygMETComponentsJwojOutputContainer","L1_gMETComponentsJwoj","SG key of the input total MET components (from Jets without Jets algo) container"};
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gMHTComponentsJwojContainerKey {this,"mygMHTComponentsJwojOutputContainer","L1_gMHTComponentsJwoj","SG key of the inputhard MET components (from Jets without Jets algo) container"};
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gMSTComponentsJwojContainerKey {this,"mygMSTComponentsJwojOutputContainer","L1_gMSTComponentsJwoj","SG key of the input soft MET components (from Jets without Jets algo) container"};
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gMETComponentsNoiseCutContainerKey {this,"my_gMETComponentsNoiseCutOutputContainer","L1_gMETComponentsNoiseCut","SG key for MET_x and MET_y components obtained with the Noise Cut algorithm"};
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gMETComponentsRmsContainerKey {this,"my_gMETComponentsRmsOutputContainer","L1_gMETComponentsRms","SG key for MET_x and MET_y components obtained with the Components Rms algorithm"};
-
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gScalarENoiseCutContainerKey {this,"mygScalarENoiseCutContainer","L1_gScalarENoiseCut","SG key of the inputScalar MET and SumET contianer obtained with the Noise Cut algorithm"};
-  SG::ReadHandleKey< xAOD::gFexGlobalRoIContainer > m_gScalarERmsContainerKey {this,"mygScalarERmsContainer","L1_gScalarERms","SG key of the inputScalar MET and SumET contianer obtained with the Components Rms algorithm"};
+  FloatArrayProperty m_ptCutValues{this, "ptCutValues", {-1., 0., 10., 50., 100.}, "List of lower pt cut values, -1. for not cut applied."};
+  // Define read handles
+  Gaudi::Property<SG::ReadHandleKeyArray<xAOD::gFexJetRoIContainer>> m_gFexJetTobKeyList{this,"gFexJetTobKeyList",{"L1_gFexLRJetRoI", "L1_gFexSRJetRoI"},"Array of gFEX jet ReadHandleKeys to fill histograms for"};
+  Gaudi::Property<SG::ReadHandleKeyArray<xAOD::gFexJetRoIContainer>> m_gFexRhoTobKeyList{this,"gFexRhoTobKeyList",{"L1_gFexRhoRoI"},"Array of gFEX rho ReadHandleKeys to fill histograms for"};
+  Gaudi::Property<SG::ReadHandleKeyArray<xAOD::gFexGlobalRoIContainer>> m_gFexGlobalTobKeyList{this,"gFexGlobalTobKeyList", {"L1_gScalarEJwoj","L1_gMETComponentsJwoj","L1_gMHTComponentsJwoj","L1_gMSTComponentsJwoj","L1_gMETComponentsNoiseCut","L1_gMETComponentsRms","L1_gScalarENoiseCut","L1_gScalarERms"},"Array of gFEX global TOBs ReadHandleKeys to fill histograms for"};
+  // Define private methods
+  StatusCode fillJetHistograms(const std::string& handleKey, const xAOD::gFexJetRoIContainer* container, const float& ptCutValue) const;
+  StatusCode fillRhoHistograms(const std::string& handleKey, const xAOD::gFexJetRoIContainer* container) const;
+  StatusCode fillGlobalTobHistograms(const std::string& handleKey, const xAOD::gFexGlobalRoIContainer* container) const;
+  FPGAType getFPGAType(const float& eta) const;
 };
 #endif
