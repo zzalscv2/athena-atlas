31bbb1468a8e0c76b72005f4da2a7f7c2c876533
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2023-04-11 19:18:37 +0200

    egamma only. Disable also Lucid and AFP
    egamma only. Disable also Lucid and AFP

--- Reconstruction/egamma/egammaConfig/python/egammaOnlyFromRawFlags.py
+++ Reconstruction/egamma/egammaConfig/python/egammaOnlyFromRawFlags.py
@@ -3,13 +3,15 @@
 def egammaOnlyFromRaw(flags):
     """egammaOnlyFromRaw flags for Reco_tf with CA"""
     flags.Detector.GeometryMuon = False
+    flags.Detector.EnableAFP = False
+    flags.Detector.EnableLucid = False
+    flags.Reco.EnableTrigger = False
     flags.Reco.EnableCombinedMuon = False
     flags.Reco.EnablePFlow = False
     flags.Reco.EnableTau = False
     flags.Reco.EnableJet = False
     flags.Reco.EnableBTagging = False
     flags.Reco.EnableCaloRinger = False
-    flags.Reco.EnableTrigger = False
     flags.Egamma.doTrackThinning = False
     flags.Reco.PostProcessing.GeantTruthThinning = False
     flags.Reco.PostProcessing.TRTAloneThinning = False
