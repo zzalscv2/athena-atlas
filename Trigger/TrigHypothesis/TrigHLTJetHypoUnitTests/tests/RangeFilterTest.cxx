a18b9ace6f3725b8d7a9d024ca9991eb3c90a63b
Scott Snyder <scott.snyder@cern.ch>
2023-04-12 17:41:08 +0200

    TrigHLTJetHypoUnitTests: Fix thread-safety checker warnings.
    TrigHLTJetHypoUnitTests: Fix thread-safety checker warnings.
    
    Disable thread-safety checking for tests used gtest.
    Otherwise we have (disabled by default) warnings about calls to
    unchecked code.

--- Trigger/TrigHypothesis/TrigHLTJetHypoUnitTests/tests/RangeFilterTest.cxx
+++ Trigger/TrigHypothesis/TrigHLTJetHypoUnitTests/tests/RangeFilterTest.cxx
@@ -1,6 +1,9 @@
 /*
-  Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
+
+#include "CxxUtils/checker_macros.h"
+ATLAS_NO_CHECK_FILE_THREAD_SAFETY;
 
 #include "gtest/gtest.h"
 #include "TrigHLTJetHypo/../src/RangeFilter.h"
