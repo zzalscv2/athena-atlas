2f94a94896d2f509f4558715a25acccf277156a5
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-04-24 13:02:09 +0200

    prepare_release_notes: support "main" branch

--- Build/AtlasBuildScripts/prepare_release_notes.py
+++ Build/AtlasBuildScripts/prepare_release_notes.py
@@ -100,7 +100,7 @@ def main():
             print('Is there a ticket associated with the release build request e.g. ATLINFR-XXXX? (press return to skip)')
             ticket = input(': ')
             print('Do you wish to add a description of the release?')
-            print('e.g. "Master release for derivations and upgrade", or "Production release for data-taking. ')
+            print('e.g. "Release for derivations and upgrade", or "Production release for data-taking. ')
             print('(press return to skip)')
             message = input(': ')
             if ticket:
@@ -124,7 +124,7 @@ def sanitize_args(target_release, nightly_tag, keep_going=False):
     rel_match = re.search(
         r'release/(?P<ver>\d+)\.(?P<maj>\d+?)\.(?P<rev>\d+?)', target_release)
     nig_match = re.search(
-        r'nightly/(?P<branch>((\d+\.\d+)|master|22\.0-mc20))/(?P<date>\d{4}-\d{2}-\d{2})T(?P<time>\d{4})', nightly_tag)
+        r'nightly/(?P<branch>((\d+\.\d+)|master|main|22\.0-mc20))/(?P<date>\d{4}-\d{2}-\d{2})T(?P<time>\d{4})', nightly_tag)
 
     if not rel_match:
         print("The target release is not formatted as xx.y.z (version.major.revision semantic)")
@@ -137,7 +137,7 @@ def sanitize_args(target_release, nightly_tag, keep_going=False):
     else:
         branch_rel = '.'.join([rel_match.group('ver'), rel_match.group('maj')])
         branch_nig = nig_match.group('branch')
-        if branch_rel != branch_nig and branch_rel != '22.0' and branch_nig != 'master':
+        if branch_rel != branch_nig and branch_rel not in ('22.0', 'master', 'main'):
             print("You are creating a tag for %s from a nightly from %s" %
                   (branch_rel, branch_nig))
             if not keep_going:
