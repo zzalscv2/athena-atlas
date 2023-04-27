7394fbf4062b7509c477b44072cde621a536eb08
Christopher Young <christopher.young@cern.ch>
2023-04-27 16:14:42 +0200

    AF3: Change logic for energy deposits outside of calorimeters
    AF3: Change logic for energy deposits outside of calorimeters

--- Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/src/TFCSHitCellMappingFCal.cxx
+++ Simulation/ISF/ISF_FastCaloSim/ISF_FastCaloSimEvent/src/TFCSHitCellMappingFCal.cxx
@@ -14,8 +14,8 @@ FCSReturnCode TFCSHitCellMappingFCal::simulate_hit(
     Hit &hit, TFCSSimulationState &simulstate, const TFCSTruthState * /*truth*/,
     const TFCSExtrapolationState * /*extrapol*/) {
   int cs = calosample();
-  const CaloDetDescrElement *cellele =
-      m_geo->getFCalDDE(cs, hit.x(), hit.y(), hit.z());
+  float distance;
+  const CaloDetDescrElement* cellele=m_geo->getFCalDDE(cs,hit.x(),hit.y(),hit.z(),&distance);
   ATH_MSG_DEBUG("HIT: cellele=" << cellele << " E=" << hit.E() << " cs=" << cs
                                 << " x=" << hit.x() << " y=" << hit.y()
                                 << " z=" << hit.z());
@@ -29,7 +29,15 @@ FCSReturnCode TFCSHitCellMappingFCal::simulate_hit(
     return (FCSReturnCode)(FCSRetry + 5); // retry simulation up to 5 times
   }
 
-  simulstate.deposit(cellele, hit.E());
+  // If the distance is positive then we are using the nearest cell rather than are inside a cell
+  // If we are more than 2.25mm from the nearest cell we don't create a hit to avoid the build-up of energy in edge cells
+  // For FCSV2 another hit can be created but with a cutoff to avoid looping, 
+  // for FastCaloGAN the rest of the hits in the layer will be scaled up by the energy renormalization step.
+  if (distance<2.25){
+    simulstate.deposit(cellele,hit.E());
+  }else{
+    hit.setXYZE(hit.x(),hit.y(),hit.z(),0.0);
+  }
 
   return FCSSuccess;
 }
