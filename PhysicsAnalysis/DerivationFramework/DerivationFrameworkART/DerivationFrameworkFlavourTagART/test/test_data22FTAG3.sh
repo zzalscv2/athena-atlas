42869cdf3b97a3eb7f73f690bc417d60287429f6
Cristiano Alpigiani <cristiano.alpigiani@cern.ch>
2023-04-28 16:49:24 +0200

    Activating derivation ARTs in 23.0 and updating some of them
    Updating derivation ARTs and activating the 23.0 ones
    
    This MR has the main target to activate the relevant derivation tests in 23.0. I took this opportunity to also add TRIG8 test and update the train configuration.

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_data22FTAG3.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_data22FTAG3.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # art-include: master/Athena
-# art-description: DAOD building JETM6 FTAG2 data22
+# art-include: 23.0/Athena
+# art-description: DAOD building FTAG3 data22
 # art-type: grid
 # art-output: *.pool.root
 # art-output: checkFile*.txt
@@ -14,31 +15,19 @@ Derivation_tf.py \
 --CA True \
 --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/CampaignInputs/data22/AOD/data22_13p6TeV.00431906.physics_Main.merge.AOD.r13928_p5279/1000events.AOD.30220215._001367.pool.root.1 \
 --outputDAODFile art.pool.root \
---formats JETM6 FTAG2 \
+--formats FTAG3 \
 --maxEvents -1 \
 
 echo "art-result: $? reco"
 
-checkFile.py DAOD_JETM6.art.pool.root > checkFile_JETM6.txt
+checkFile.py DAOD_FTAG3.art.pool.root > checkFile_FTAG3.txt
 
 echo "art-result: $?  checkfile"
 
-checkxAOD.py DAOD_JETM6.art.pool.root > checkxAOD_JETM6.txt
+checkxAOD.py DAOD_FTAG3.art.pool.root > checkxAOD_FTAG3.txt
 
 echo "art-result: $?  checkxAOD"
 
-checkIndexRefs.py DAOD_JETM6.art.pool.root > checkIndexRefs_JETM6.txt 2>&1
-
-echo "art-result: $?  checkIndexRefs"
-
-checkFile.py DAOD_FTAG2.art.pool.root > checkFile_FTAG2.txt
-
-echo "art-result: $?  checkfile"
-
-checkxAOD.py DAOD_FTAG2.art.pool.root > checkxAOD_FTAG2.txt
-
-echo "art-result: $?  checkxAOD"
-
-checkIndexRefs.py DAOD_FTAG2.art.pool.root > checkIndexRefs_FTAG2.txt 2>&1
+checkIndexRefs.py DAOD_FTAG3.art.pool.root > checkIndexRefs_FTAG3.txt 2>&1
 
 echo "art-result: $?  checkIndexRefs"
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_mc20FTAG3.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_mc20FTAG3.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # art-include: master/Athena
-# art-description: DAOD building EGAM7 JETM3 mc20
+# art-include: 23.0/Athena
+# art-description: DAOD building FTAG3 mc20
 # art-type: grid
 # art-output: *.pool.root
 # art-output: checkFile*.txt
@@ -14,31 +15,19 @@ Derivation_tf.py \
 --CA True \
 --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/CampaignInputs/mc20/AOD/mc20_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.recon.AOD.e6337_s3681_r13145/1000events.AOD.27121237._002005.pool.root.1 \
 --outputDAODFile art.pool.root \
---formats EGAM7 JETM3 \
+--formats FTAG3 \
 --maxEvents -1 \
 
 echo "art-result: $? reco"
 
-checkFile.py DAOD_EGAM7.art.pool.root > checkFile_EGAM7.txt
+checkFile.py DAOD_FTAG3.art.pool.root > checkFile_FTAG3.txt
 
 echo "art-result: $?  checkfile"
 
-checkxAOD.py DAOD_EGAM7.art.pool.root > checkxAOD_EGAM7.txt
+checkxAOD.py DAOD_FTAG3.art.pool.root > checkxAOD_FTAG3.txt
 
 echo "art-result: $?  checkxAOD"
 
-checkIndexRefs.py DAOD_EGAM7.art.pool.root > checkIndexRefs_EGAM7.txt 2>&1
-
-echo "art-result: $?  checkIndexRefs"
-
-checkFile.py DAOD_JETM3.art.pool.root > checkFile_JETM3.txt
-
-echo "art-result: $?  checkfile"
-
-checkxAOD.py DAOD_JETM3.art.pool.root > checkxAOD_JETM3.txt
-
-echo "art-result: $?  checkxAOD"
-
-checkIndexRefs.py DAOD_JETM3.art.pool.root > checkIndexRefs_JETM3.txt 2>&1
+checkIndexRefs.py DAOD_FTAG3.art.pool.root > checkIndexRefs_FTAG3.txt 2>&1
 
 echo "art-result: $?  checkIndexRefs"
