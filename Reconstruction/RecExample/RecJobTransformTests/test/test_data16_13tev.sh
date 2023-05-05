dbb81d967ac59aed566f42a9c6b1585a1fb08b51
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2023-05-05 18:22:15 +0200

    Update Run 2 global tags to latest versions.
    Update Run 2 global tags to latest versions.

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data16_13tev.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data16_13tev.sh
@@ -8,7 +8,7 @@
 
 export ATHENA_CORE_NUMBER=8
 #The setup for run2 data is described in test_data15_13TeV.sh
-Reco_tf.py  --CA --multithreaded --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data16_13TeV.00310809.physics_Main.daq.RAW._lb1219._SFO-2._0001.data --maxEvents 300 --conditionsTag="CONDBR2-BLKPA-RUN2-10" --geometryVersion="ATLAS-R2-2016-01-00-01" --preExec="flags.DQ.Steering.doHLTMon=False" --ignorePatterns 'LArRawDataReadingAlg.+ERROR.+Found.+unsupported.+Rod.+block.+type.+0|LArRawDataReadingAlg.+\|.+ERROR.+\|.|ERROR.+message.+limit.+LArRawDataReadingAlg.'  --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root
+Reco_tf.py  --CA --multithreaded --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data16_13TeV.00310809.physics_Main.daq.RAW._lb1219._SFO-2._0001.data --maxEvents 300 --conditionsTag="CONDBR2-BLKPA-RUN2-11" --geometryVersion="ATLAS-R2-2016-01-00-01" --preExec="flags.DQ.Steering.doHLTMon=False" --ignorePatterns 'LArRawDataReadingAlg.+ERROR.+Found.+unsupported.+Rod.+block.+type.+0|LArRawDataReadingAlg.+\|.+ERROR.+\|.|ERROR.+message.+limit.+LArRawDataReadingAlg.'  --outputESDFile myESD.pool.root --outputAODFile myAOD.pool.root --outputHISTFile myHist.root
 
 RES=$?
 echo "art-result: $RES Reco"
