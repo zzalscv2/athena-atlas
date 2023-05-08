c9a8be95aa49679a3a10cfb0d806d9b91a4724f3
Julien Maurer <julien.maurer@cern.ch>
2023-05-08 13:05:30 +0200

    lighter TCT ART output
    lighter TCT ART output

--- Tools/Tier0ChainTests/test/test_Run00287281_express_HICollisions.sh
+++ Tools/Tier0ChainTests/test/test_Run00287281_express_HICollisions.sh
@@ -9,12 +9,11 @@
 # art-include: master/Athena
 # art-include: 21.3/Athena
 # art-include: 21.9/Athena
+# art-output: log.*
 
 Reco_tf.py --outputHISTFile=myMergedMonitoring_0.root --outputESDFile=myESD_0.pool.root --outputAODFile=myAOD_0.AOD.pool.root --outputTAGFile=myTAG_0.root --inputBSFile=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/Tier0ChainTests/data15_hi.00287281.express_express.merge.RAW._lb0124._SFO-ALL._0001.1 --AMI=q314 --maxEvents=25 --imf False
 echo "art-result: $? Reco"
 
-ArtPackage=$1
-ArtJobName=$2
-art.py compare grid --entries 10 --file=*AOD*.pool.root ${ArtPackage} ${ArtJobName}
+ArtRef=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/$1/TCT_21.0-mc16d_references/$2
+art.py compare ref --entries 10 --file=*AOD*.pool.root . $ArtRef
 echo "art-result: $? Diff"
-
--- Tools/Tier0ChainTests/test/test_q431.sh
+++ Tools/Tier0ChainTests/test/test_q431.sh
@@ -9,12 +9,18 @@
 # art-include: master/Athena
 # art-include: 21.3/Athena
 # art-include: 21.9/Athena
+# art-output: log.*
 
 Reco_tf.py --AMI=q431 --outputTAGFile=myTAG.pool.root --outputAODFile=myAOD.pool.root --outputESDFile=myESD.pool.root --outputHISTFile=myHIST.root --imf False
-echo "art-result: $? Reco"
+rc1=$?
+echo "art-result: $rc1 Reco"
 
-ArtPackage=$1
-ArtJobName=$2
-art.py compare grid --entries 10 ${ArtPackage} ${ArtJobName}
-echo "art-result: $? Diff"
+rc2=-9999
+if [ ${rc1} -eq 0 ]
+then
+    ArtRef=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/$1/TCT_21.0-mc16d_references/$2
+    art.py compare ref --entries 10 . $ArtRef
+    rc2=$?
+fi
+echo  "art-result: ${rc2} Diff"
 
