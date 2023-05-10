7eb23c2c225567e4ce4e8306a494fcafdaac1a29
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2023-05-10 10:02:14 +0200

    Remove barcodes from derivation framework
    Remove barcodes from derivation framework

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkMCTruth/src/GenericTruthThinning.cxx
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkMCTruth/src/GenericTruthThinning.cxx
@@ -31,8 +31,7 @@ m_partString(""),
 m_preserveDescendants(false),
 m_preserveGeneratorDescendants(false),
 m_preserveAncestors(false),
-m_tauHandling(true),
-m_geantOffset(200000)
+m_tauHandling(true)
 {
     declareProperty("EventsKey", m_eventsKey);
     declareProperty("ParticleSelectionString", m_partString);
@@ -41,7 +40,6 @@ m_geantOffset(200000)
     declareProperty("PreserveGeneratorDescendants", m_preserveGeneratorDescendants);
     declareProperty("PreserveAncestors", m_preserveAncestors);
     declareProperty("TauHandling", m_tauHandling);
-    declareProperty("SimBarcodeOffset", m_geantOffset); 
 }
 
 // Destructor
@@ -163,7 +161,7 @@ StatusCode DerivationFramework::GenericTruthThinning::doThinning() const
     // - update the masks including the descendants/ancestors
     // To ensure graph completeness, this  over-rides anything set by the special treatment 
     // of taus in the section above 
-    DerivationFramework::DecayGraphHelper decayHelper(m_geantOffset);
+    DerivationFramework::DecayGraphHelper decayHelper;
     std::unordered_set<int> encounteredBarcodes; // to enable loop handling
     if (m_preserveDescendants || m_preserveGeneratorDescendants  || m_preserveAncestors) {
         for (unsigned int i=0; i<nParticles; ++i) {
