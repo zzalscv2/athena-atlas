c2c6eda27b72f28ce8638da84c73fe8e23b27672
Maximilian Emanuel Goblirsch-Kolb <maximilian.goblirsch-kolb@cern.ch>
2023-05-12 17:47:24 +0200

    Updates to TrigGlobalEfficiencyCorrections for Run-3 (found during HZZ Run-3 development)
    Updates to TrigGlobalEfficiencyCorrections for Run-3 (found during HZZ Run-3 development)

--- Trigger/TrigAnalysis/TrigGlobalEfficiencyCorrection/Root/TrigGlobalEfficiencyCorrectionTool.cxx
+++ Trigger/TrigAnalysis/TrigGlobalEfficiencyCorrection/Root/TrigGlobalEfficiencyCorrectionTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2018 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 // contact: jmaurer@cern.ch
@@ -742,11 +742,6 @@ CP::CorrectionCode TrigGlobalEfficiencyCorrectionTool::getEfficiency(unsigned ru
 	efficiencyMc = 0.;
 	
 	ATH_MSG_DEBUG("Computing global trigger efficiency for this event with  " << particles.size() << " lepton(s) as input; run number = " << runNumber);
-	
-	if(runNumber<266904 || runNumber>364292)
-	{
-		ATH_MSG_WARNING("Suspicious run number provided " << runNumber << ", continuing anyway");
-	}
 	
 	#ifdef XAOD_STANDALONE
 	if(!m_initialized)
