d856e41169fd4f7589afe24224504175b18c3739
Vakhtang Tsulaia <vakhtang.tsulaia@cern.ch>
2023-05-26 18:40:45 +0200

    argFile::_mergeArgs(). Make sure --threads is not passed to merger in hybrid MP+MT jobs
    argFile::_mergeArgs(). Make sure --threads is not passed to merger in hybrid MP+MT jobs
    
    Fixes the crash in merge step of hybrid MP+MT caused by the fact that the merger is
    configured to run athenaMT, while the number of concurrent events in runargs is set to 0

--- Tools/PyJobTransforms/python/trfArgClasses.py
+++ Tools/PyJobTransforms/python/trfArgClasses.py
@@ -1224,14 +1224,26 @@ class argFile(argList):
             # Need to ensure that "nprocs" is not passed to merger
             # and prevent multiple '--threads' options when there are multiple sub-steps in 'athenopts'
             for subStep in myargdict['athenaopts'].value:
+                hasNprocs = False
+                hasNthreads = False
                 for opt in myargdict['athenaopts'].value[subStep]:
                     if opt.startswith('--nprocs'):
+                        hasNprocs = True
                         continue
                     # Keep at least one '--threads'
                     elif opt.startswith('--threads'):
+                        hasNthreads = True
                         if opt in newopts:
                             continue
                     newopts.append(opt)
+                # If we have hybrid MP+MT job make sure --threads is not passed to merger
+                if hasNprocs and hasNthreads:
+                    tmpopts = []
+                    for opt in newopts:
+                        if opt.startswith('--threads'):
+                            continue
+                        tmpopts.append(opt)
+                    newopts = tmpopts
             myargdict['athenaopts'] = argSubstepList(newopts, runarg=False)
         return myargdict
 
