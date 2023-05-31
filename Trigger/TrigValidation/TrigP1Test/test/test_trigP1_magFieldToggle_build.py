43295eccdb77c825f6f8742ae10837d4550987dc
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-05-31 14:08:49 +0200

    TrigP1Test: migrate magFieldToggle ART test to CA

--- Trigger/TrigValidation/TrigP1Test/test/test_trigP1_magFieldToggle_build.py
+++ Trigger/TrigValidation/TrigP1Test/test/test_trigP1_magFieldToggle_build.py
@@ -55,10 +55,10 @@ ex_bs2.args = '-r %d -n 20 -o magField2 %s' % (run_no+2, input_file)
 
 ex = ExecStep.ExecStep()
 ex.type = 'athenaHLT'
-ex.job_options = 'TrigP1Test/testHLT_magField.py'
+ex.job_options = 'TrigP1Test.MagFieldReader.run Trigger.Online.BFieldAutoConfig=False'
 ex.input = ''
 ex.max_events = 20
-ex.args = '-f raw.data -i -M -ul -c "flags.Trigger.Online.BFieldAutoConfig=False"'
+ex.args = '-f raw.data -i -M -ul'
 ex.perfmon = False # perfmon currently not fully supported with athenaHLT -M
 
 # Pass the transitions file into athenaHLT -i
