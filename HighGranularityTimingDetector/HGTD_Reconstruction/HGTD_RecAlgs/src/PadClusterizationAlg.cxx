aef78a16841ab752f03f4bb5503a5e7d745a68fb
scott snyder <sss@karma>
2023-06-08 07:08:47 -0400

    HGTD_RecAlgs: Fix clang warning.
    Binding to temporary in range-for.

--- HighGranularityTimingDetector/HGTD_Reconstruction/HGTD_RecAlgs/src/PadClusterizationAlg.cxx
+++ HighGranularityTimingDetector/HGTD_Reconstruction/HGTD_RecAlgs/src/PadClusterizationAlg.cxx
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration.
+ * Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration.
  *
  * @file HGTD_RecAlgs/src/PadClusterizationAlg.cxx
  * @author Alexander Leopold <alexander.leopold@cern.ch>
@@ -51,7 +51,7 @@ StatusCode PadClusterizationAlg::execute(const EventContext& ctx) const {
   HGTD_ClusterContainer* locp_prd_container = prd_container_handle.ptr();
 
   // Transform the RDOs collection by collection
-  for (const auto& rdo_collection : *rdo_container) {
+  for (const auto rdo_collection : *rdo_container) {
     if (rdo_collection->empty()) {
       continue;
     }
