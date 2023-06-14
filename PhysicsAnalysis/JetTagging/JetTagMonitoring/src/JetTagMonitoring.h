5d16f08cd58bf51b6276af0db45ccd1d854f7b62
scott snyder <sss@karma>
2023-06-14 06:37:02 -0400

    JetTagMonitoring: Fix cppcheck warning.
    Duplicate member between base and derived classes.

--- PhysicsAnalysis/JetTagging/JetTagMonitoring/src/JetTagMonitoring.h
+++ PhysicsAnalysis/JetTagging/JetTagMonitoring/src/JetTagMonitoring.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 // *******************************************************
@@ -103,7 +103,7 @@ private:
 
     ToolHandle< Analysis::TrackSelector > m_trackSelectorTool{this, "TrackSelectorTool", "Analysis::TrackSelector"};
     ToolHandle<Reco::ITrackToVertex> m_trackToVertexTool{this, "TrackToVertexTool", "Reco::TrackToVertex"};
-    PublicToolHandle< Trig::TrigDecisionTool > m_trigDecTool{this, "JTMTrigDecisionTool", "Trig::TrigDecisionTool/TrigDecisionTool"}; // added by SARA
+    PublicToolHandle< Trig::TrigDecisionTool > m_jtmTrigDecTool{this, "JTMTrigDecisionTool", "Trig::TrigDecisionTool/TrigDecisionTool"}; // added by SARA
     bool m_histogramsCreated;
 
     /* const Trk::VxCandidate* m_priVtx; */
