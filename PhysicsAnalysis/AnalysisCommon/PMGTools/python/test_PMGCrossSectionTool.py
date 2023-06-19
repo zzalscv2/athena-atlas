72f4d7b8ea4a18c8976309aa194679d2d6f426d6
Andrii Verbytskyi <andrii.verbytskyi@cern.ch>
2023-06-19 13:04:59 +0200

    Use 2to3 on the old PMGTools scripts
    Use 2to3 on the old PMGTools scripts

--- PhysicsAnalysis/AnalysisCommon/PMGTools/python/test_PMGCrossSectionTool.py
+++ PhysicsAnalysis/AnalysisCommon/PMGTools/python/test_PMGCrossSectionTool.py
@@ -18,18 +18,18 @@ def main():
     # take a ttbar sample as example ( users should get this from the EventInfo )
     sample_id = 410000
 
-    print '%d sample loaded' % tool.getLoadedDSIDs().size()
-    print
-    print 'Sample dsid               = ', sample_id
-    print 'Sample name               = ', tool.getSampleName(sample_id)
-    print 'xsection [pb]             = ', tool.getSampleXsection(sample_id)
-    print 'filter eff                = ', tool.getFilterEff(sample_id)
-    print 'k factor                  = ', tool.getKfactor(sample_id)
-    print 'xsection uncertainty      = ', tool.getXsectionUncertainty(sample_id)
-    print 'xsection uncertainty up   = ', tool.getXsectionUncertaintyUP(sample_id)
-    print 'xsection uncertainty down = ', tool.getXsectionUncertaintyDOWN(sample_id)
+    print('%d sample loaded' % tool.getLoadedDSIDs().size())
+    print()
+    print('Sample dsid               = ', sample_id)
+    print('Sample name               = ', tool.getSampleName(sample_id))
+    print('xsection [pb]             = ', tool.getSampleXsection(sample_id))
+    print('filter eff                = ', tool.getFilterEff(sample_id))
+    print('k factor                  = ', tool.getKfactor(sample_id))
+    print('xsection uncertainty      = ', tool.getXsectionUncertainty(sample_id))
+    print('xsection uncertainty up   = ', tool.getXsectionUncertaintyUP(sample_id))
+    print('xsection uncertainty down = ', tool.getXsectionUncertaintyDOWN(sample_id))
        
-    print
+    print()
 
 if __name__ == '__main__':
     ROOT.gROOT.ProcessLine('.x $ROOTCOREDIR/scripts/load_packages.C')
