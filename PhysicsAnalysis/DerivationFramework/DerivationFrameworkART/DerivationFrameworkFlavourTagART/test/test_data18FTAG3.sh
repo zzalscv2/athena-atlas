73511f5c387767d07b6d96ea54745e450f629262
Cristiano Alpigiani <cristiano.alpigiani@cern.ch>
2023-06-19 13:04:00 +0200

    Updating derivation ARTs in 23.0
    Updating derivation ARTs in 23.0
    
    The deriv ARTs are not currently running in 23.0 since the # art-include: 23.0/Athena is not currently include. With this MR we are synchronising 23.0 with master.

--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_data18FTAG3.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_data18FTAG3.sh
@@ -2,7 +2,7 @@
 
 # art-include: master/Athena
 # art-include: 23.0/Athena
-# art-description: DAOD building FTAG2 data18
+# art-description: DAOD building FTAG3 data18
 # art-type: grid
 # art-output: *.pool.root
 # art-output: checkFile*.txt
@@ -15,19 +15,19 @@ Derivation_tf.py \
 --CA True \
 --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/CampaignInputs/data18/AOD/data18_13TeV.00357772.physics_Main.merge.AOD.r13286_p4910/1000events.AOD.27655096._000455.pool.root.1 \
 --outputDAODFile art.pool.root \
---formats FTAG2 \
+--formats FTAG3 \
 --maxEvents -1 \
 
 echo "art-result: $? reco"
 
-checkFile.py DAOD_FTAG2.art.pool.root > checkFile_FTAG2.txt
+checkFile.py DAOD_FTAG3.art.pool.root > checkFile_FTAG3.txt
 
 echo "art-result: $?  checkfile"
 
-checkxAOD.py DAOD_FTAG2.art.pool.root > checkxAOD_FTAG2.txt
+checkxAOD.py DAOD_FTAG3.art.pool.root > checkxAOD_FTAG3.txt
 
 echo "art-result: $?  checkxAOD"
 
-checkIndexRefs.py DAOD_FTAG2.art.pool.root > checkIndexRefs_FTAG2.txt 2>&1
+checkIndexRefs.py DAOD_FTAG3.art.pool.root > checkIndexRefs_FTAG3.txt 2>&1
 
 echo "art-result: $?  checkIndexRefs"
--- PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_mc20FTAG3.sh
+++ PhysicsAnalysis/DerivationFramework/DerivationFrameworkART/DerivationFrameworkFlavourTagART/test/test_mc20FTAG3.sh
@@ -2,7 +2,7 @@
 
 # art-include: master/Athena
 # art-include: 23.0/Athena
-# art-description: DAOD building FTAG1 mc20
+# art-description: DAOD building FTAG3 mc20
 # art-type: grid
 # art-output: *.pool.root
 # art-output: checkFile*.txt
@@ -15,19 +15,19 @@ Derivation_tf.py \
 --CA True \
 --inputAODFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/CampaignInputs/mc20/AOD/mc20_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.recon.AOD.e6337_s3681_r13145/1000events.AOD.27121237._002005.pool.root.1 \
 --outputDAODFile art.pool.root \
---formats FTAG1 \
+--formats FTAG3 \
 --maxEvents -1 \
 
 echo "art-result: $? reco"
 
-checkFile.py DAOD_FTAG1.art.pool.root > checkFile_FTAG1.txt
+checkFile.py DAOD_FTAG3.art.pool.root > checkFile_FTAG3.txt
 
 echo "art-result: $?  checkfile"
 
-checkxAOD.py DAOD_FTAG1.art.pool.root > checkxAOD_FTAG1.txt
+checkxAOD.py DAOD_FTAG3.art.pool.root > checkxAOD_FTAG3.txt
 
 echo "art-result: $?  checkxAOD"
 
-checkIndexRefs.py DAOD_FTAG1.art.pool.root > checkIndexRefs_FTAG1.txt 2>&1
+checkIndexRefs.py DAOD_FTAG3.art.pool.root > checkIndexRefs_FTAG3.txt 2>&1
 
 echo "art-result: $?  checkIndexRefs"
