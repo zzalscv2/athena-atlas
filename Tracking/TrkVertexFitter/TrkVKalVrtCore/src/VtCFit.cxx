8f9ea84b2368bc0c6a4354011f89457d22d15a67
Vadim Kostyukhin <vadim.kostyukhin@cern.ch>
2023-06-22 16:35:34 +0200

    Fix negative covariance
    Catch cases with negative diagonal elements of covarince matrix

--- Tracking/TrkVertexFitter/TrkVKalVrtCore/src/VtCFit.cxx
+++ Tracking/TrkVertexFitter/TrkVKalVrtCore/src/VtCFit.cxx
@@ -439,6 +439,7 @@ namespace Trk {
      }
 
 /*   covariance matrix on vertex coordinates */
+    if(wa[0]<0 || wa[2]<0 || wa[5]<0) return -2; // Matrix elements must be positive!
     IERR=cfdinv(wa, &(vk->fitVcov[0]), -3);
     if (IERR) {IERR=cfdinv(wa, &(vk->fitVcov[0]), 3);}  // last safety 
     if ( !vk->passNearVertex ) {  if (IERR) return IERR; } // If nothing helps - detect failure (not for passNearVertex!!!)
