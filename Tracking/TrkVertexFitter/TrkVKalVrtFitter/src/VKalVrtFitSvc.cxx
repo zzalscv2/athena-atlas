484e2f1f079eaaf706ac05a0c5693a3e4b20e610
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2023-07-01 18:42:52 +0200

    ATLASRECTS-7672 : Manage the memory ownership via unique_ptr to avoid leaks
    ATLASRECTS-7672 : Manage the memory ownership via unique_ptr to avoid leaks

--- Tracking/TrkVertexFitter/TrkVKalVrtFitter/src/VKalVrtFitSvc.cxx
+++ Tracking/TrkVertexFitter/TrkVKalVrtFitter/src/VKalVrtFitSvc.cxx
@@ -18,9 +18,9 @@
  extern void cfpest( int ntrk, double *vrt, long int *Charge, double (*part)[5], double (*par0)[3]);
  extern void xyztrp( const long int Charge, double* vrt, double* Mom, double* CovVrtMom, double BMAG, double* Perig, double* CovPerig );
 
- extern int CFit(VKalVrtControl *FitCONTROL, int ifCovV0, int NTRK, 
+ extern int CFit(VKalVrtControl *FitCONTROL, int ifCovV0, int NTRK,
 	      long int *ich, double xyz0[3], double (*par0)[3],
-	      double (*inp_Trk5)[5], double (*inp_CovTrk5)[15], 
+	      double (*inp_Trk5)[5], double (*inp_CovTrk5)[15],
 	      double xyzfit[3], double (*parfs)[3], double ptot[4],
               double covf[21], double & chi2, double *chi2tr);
 
@@ -51,7 +51,7 @@
     StatusCode sc = CvtPerigee(InpPerigee, ntrk, state);
     if(sc.isFailure())return StatusCode::FAILURE;
 
-    int ierr = VKalVrtFit3( ntrk, Vertex, Momentum, Charge, ErrorMatrix, 
+    int ierr = VKalVrtFit3( ntrk, Vertex, Momentum, Charge, ErrorMatrix,
                             Chi2PerTrk, TrkAtVrt,Chi2, state, ifCovV0 ) ;
     if (ierr) return StatusCode::FAILURE;
     return StatusCode::SUCCESS;
@@ -64,8 +64,8 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const xAOD::TrackParti
         Amg::Vector3D& Vertex,
 	TLorentzVector&   Momentum,
 	long int& Charge,
-	dvect& ErrorMatrix, 
-	dvect& Chi2PerTrk, 
+	dvect& ErrorMatrix,
+	dvect& Chi2PerTrk,
         std::vector< std::vector<double> >& TrkAtVrt,
 	double& Chi2,
         IVKalState& istate,
@@ -78,62 +78,82 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const xAOD::TrackParti
 //------  extract information about selected tracks
 //
     int ntrk=0;
+
+    // The tmpInputC will be not owning just holding plain ptr
+    // the ownership is handled via the TParamOwner
+    // and is unique_ptr so we do not leak
     std::vector<const TrackParameters*>   tmpInputC(0);
+    std::vector<std::unique_ptr<const TrackParameters>> TParamOwner(0);
     StatusCode sc;
     double closestHitR=1.e6;   //VK needed for FirstMeasuredPointLimit if this hit itself is absent
-    if(m_firstMeasuredPoint){               //First measured point strategy
+    if(m_firstMeasuredPoint){  //First measured point strategy
        //------
        if(!InpTrkC.empty()){
           if( m_InDetExtrapolator == nullptr ){
             if(msgLvl(MSG::WARNING))msg()<< "No InDet extrapolator given."<<
 	                                 "Can't use FirstMeasuredPoint with xAOD::TrackParticle!!!" << endmsg;
-            return StatusCode::FAILURE;        
+            return StatusCode::FAILURE;
           }
           std::vector<const xAOD::TrackParticle*>::const_iterator     i_ntrk;
           if(msgLvl(MSG::DEBUG))msg()<< "Start FirstMeasuredPoint handling"<<'\n';
           unsigned int indexFMP;
           for (i_ntrk = InpTrkC.begin(); i_ntrk < InpTrkC.end(); ++i_ntrk) {
-	    if ((*i_ntrk)->indexOfParameterAtPosition(indexFMP, xAOD::FirstMeasurement)){
+            if ((*i_ntrk)->indexOfParameterAtPosition(indexFMP, xAOD::FirstMeasurement)){
               if(msgLvl(MSG::DEBUG))msg()<< "FirstMeasuredPoint on track is discovered. Use it."<<'\n';
-	      tmpInputC.push_back(new CurvilinearParameters((*i_ntrk)->curvilinearParameters(indexFMP)));
-	    }else{
-              if(msgLvl(MSG::DEBUG))msg()<< "FirstMeasuredPoint on track is absent."<<
-	                                    "Try extrapolation from Perigee to FisrtMeasuredPoint radius"<<'\n';
-              tmpInputC.push_back(m_fitPropagator->myxAODFstPntOnTrk((*i_ntrk))); 
-              if( (*i_ntrk)->radiusOfFirstHit() < closestHitR ) closestHitR=(*i_ntrk)->radiusOfFirstHit();
-              if(tmpInputC[tmpInputC.size()-1]==nullptr){  //Extrapolation failure 
-              if(msgLvl(MSG::WARNING))msg()<< "InDetExtrapolator can't etrapolate xAOD::TrackParticle Perigee "<<
-                                              "to FirstMeasuredPoint radius! Stop vertex fit!" << endmsg;
-                for(unsigned int i=0; i<tmpInputC.size()-1; i++) delete tmpInputC[i]; 
-                return StatusCode::FAILURE;
-	      }
+              // create parameters
+              TParamOwner.emplace_back(std::make_unique<CurvilinearParameters>(
+                  (*i_ntrk)->curvilinearParameters(indexFMP)));
+              //For the last one we created, push also a not owning / view ptr to tmpInputC
+              tmpInputC.push_back((TParamOwner.back()).get());
+            }else{
+              if(msgLvl(MSG::DEBUG)){
+                msg()<< "FirstMeasuredPoint on track is absent."<<
+                  "Try extrapolation from Perigee to FisrtMeasuredPoint radius"<<endmsg;
+              }
+
+              TParamOwner.emplace_back(m_fitPropagator->myxAODFstPntOnTrk((*i_ntrk)));
+              //For the last one we created, push also a not owning / view ptr to tmpInputC
+              tmpInputC.push_back((TParamOwner.back()).get());
+
+              if( (*i_ntrk)->radiusOfFirstHit() < closestHitR ) {
+                closestHitR=(*i_ntrk)->radiusOfFirstHit();
+              }
+              if(tmpInputC[tmpInputC.size()-1]==nullptr){
+                //Extrapolation failure
+              if(msgLvl(MSG::WARNING)){
+                msg()<< "InDetExtrapolator can't etrapolate xAOD::TrackParticle Perigee "<<
+                  "to FirstMeasuredPoint radius! Stop vertex fit!" << endmsg;
+              }
+              return StatusCode::FAILURE;
+              }
             }
           }
           sc=CvtTrackParameters(tmpInputC,ntrk,state);
           if(sc.isFailure()){
-            for(unsigned int i=0; i<tmpInputC.size(); i++) delete tmpInputC[i]; 
             return StatusCode::FAILURE;
           }
        }
     }else{
-       if(!InpTrkC.empty()) sc=CvtTrackParticle(InpTrkC,ntrk,state);
+       if(!InpTrkC.empty()) {
+         sc=CvtTrackParticle(InpTrkC,ntrk,state);
+       }
     }
     if(sc.isFailure())return StatusCode::FAILURE;
     if(!InpTrkN.empty()){sc=CvtNeutralParticle(InpTrkN,ntrk,state); if(sc.isFailure())return StatusCode::FAILURE;}
-//--
+    //--
     int ierr = VKalVrtFit3( ntrk, Vertex, Momentum, Charge, ErrorMatrix, Chi2PerTrk, TrkAtVrt, Chi2, state, ifCovV0 ) ;
     if (ierr) return StatusCode::FAILURE;
-//
-//-- Check vertex position with respect to first measured hit and refit with plane constraint if needed
+    //
+    //-- Check vertex position with respect to first measured hit and refit with plane constraint if needed
     state.m_planeCnstNDOF = 0;
     if(m_firstMeasuredPointLimit && !ierr){
        Amg::Vector3D  cnstRefPoint(0.,0.,0.);
        //----------- Use as reference either hit(state.m_globalFirstHit) or its radius(closestHitR) if hit is absent
        if(state.m_globalFirstHit)cnstRefPoint=state.m_globalFirstHit->position();
        else if(closestHitR < 1.e6){
-	  Amg::Vector3D unitMom=Amg::Vector3D(Momentum.Vect().Unit().x(),Momentum.Vect().Unit().y(),Momentum.Vect().Unit().z());
-	  if((Vertex+unitMom).perp() < Vertex.perp()) unitMom=-unitMom;
-	  cnstRefPoint=Vertex+(closestHitR-Vertex.perp())*unitMom;
+         Amg::Vector3D unitMom=Amg::Vector3D(Momentum.Vect().Unit().x(),Momentum.Vect().Unit().y(),Momentum.Vect().Unit().z());
+         if((Vertex+unitMom).perp() < Vertex.perp()) unitMom=-unitMom;
+         cnstRefPoint=Vertex+(closestHitR-Vertex.perp())*unitMom;
        }
        //------------
        if(Vertex.perp()>cnstRefPoint.perp() && cnstRefPoint.perp()>0.){
@@ -143,8 +163,8 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const xAOD::TrackParti
           double D= pp[0]*(cnstRefPoint.x()-state.m_refFrameX)
                    +pp[1]*(cnstRefPoint.y()-state.m_refFrameY)
                    +pp[2]*(cnstRefPoint.z()-state.m_refFrameZ);
-          state.m_vkalFitControl.setUsePlaneCnst( pp[0], pp[1], pp[2], D); 
-	  std::vector<double> saveApproxV(3,0.); state.m_ApproximateVertex.swap(saveApproxV); 
+          state.m_vkalFitControl.setUsePlaneCnst( pp[0], pp[1], pp[2], D);
+          std::vector<double> saveApproxV(3,0.); state.m_ApproximateVertex.swap(saveApproxV);
           state.m_ApproximateVertex[0]=cnstRefPoint.x();
           state.m_ApproximateVertex[1]=cnstRefPoint.y();
           state.m_ApproximateVertex[2]=cnstRefPoint.z();
@@ -154,11 +174,10 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const xAOD::TrackParti
              ierr = VKalVrtFit3(ntrk,Vertex,Momentum,Charge,ErrorMatrix,Chi2PerTrk,TrkAtVrt,Chi2, state, ifCovV0); // if fit with it failed
              state.m_planeCnstNDOF = 0;
           }
-	  state.m_ApproximateVertex.swap(saveApproxV); 
+          state.m_ApproximateVertex.swap(saveApproxV);
        }
     }
-//--
-    for(unsigned int i=0; i<tmpInputC.size(); i++) delete tmpInputC[i]; 
+    //--
     if (ierr) return StatusCode::FAILURE;
     return StatusCode::SUCCESS;
 }
@@ -169,8 +188,8 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const TrackParameters*
         Amg::Vector3D& Vertex,
 	TLorentzVector&   Momentum,
 	long int& Charge,
-	dvect& ErrorMatrix, 
-	dvect& Chi2PerTrk, 
+	dvect& ErrorMatrix,
+	dvect& Chi2PerTrk,
         std::vector< std::vector<double> >& TrkAtVrt,
 	double& Chi2,
         IVKalState& istate,
@@ -192,7 +211,7 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const TrackParameters*
       sc=CvtNeutralParameters(InpTrkN,ntrk,state);
       if(sc.isFailure())return StatusCode::FAILURE;
     }
-    
+
     if(state.m_ApproximateVertex.empty() && state.m_globalFirstHit){  //Initial guess if absent
 	state.m_ApproximateVertex.reserve(3);
         state.m_ApproximateVertex.push_back(state.m_globalFirstHit->position().x());
@@ -212,8 +231,8 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const TrackParameters*
           double D= pp[0]*(state.m_globalFirstHit->position().x()-state.m_refFrameX)
                    +pp[1]*(state.m_globalFirstHit->position().y()-state.m_refFrameY)
                    +pp[2]*(state.m_globalFirstHit->position().z()-state.m_refFrameZ);
-          state.m_vkalFitControl.setUsePlaneCnst( pp[0], pp[1], pp[2], D); 
-	  std::vector<double> saveApproxV(3,0.); state.m_ApproximateVertex.swap(saveApproxV); 
+          state.m_vkalFitControl.setUsePlaneCnst( pp[0], pp[1], pp[2], D);
+	  std::vector<double> saveApproxV(3,0.); state.m_ApproximateVertex.swap(saveApproxV);
           state.m_ApproximateVertex[0]=state.m_globalFirstHit->position().x();
           state.m_ApproximateVertex[1]=state.m_globalFirstHit->position().y();
           state.m_ApproximateVertex[2]=state.m_globalFirstHit->position().z();
@@ -223,7 +242,7 @@ StatusCode TrkVKalVrtFitter::VKalVrtFit(const std::vector<const TrackParameters*
 	     ierr = VKalVrtFit3(ntrk,Vertex,Momentum,Charge,ErrorMatrix,Chi2PerTrk,TrkAtVrt,Chi2, state, ifCovV0 ) ;     // if fit with it failed
              state.m_planeCnstNDOF = 0;
           }
-	  state.m_ApproximateVertex.swap(saveApproxV); 
+	  state.m_ApproximateVertex.swap(saveApproxV);
        }
     }
     if (ierr) return StatusCode::FAILURE;
@@ -242,8 +261,8 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
         Amg::Vector3D& Vertex,
 	TLorentzVector&   Momentum,
 	long int& Charge,
-	dvect& ErrorMatrix, 
-	dvect& Chi2PerTrk, 
+	dvect& ErrorMatrix,
+	dvect& Chi2PerTrk,
         std::vector< std::vector<double> >& TrkAtVrt,
 	double& Chi2,
         State& state,
@@ -257,7 +276,7 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
     double ptot[4]={0.};
     double xyzfit[3]={0.};
 //
-//--- Set field value at (0.,0.,0.) - some safety 
+//--- Set field value at (0.,0.,0.) - some safety
 //
     double Bx,By,Bz;
     state.m_fitField.getMagFld(-state.m_refFrameX,-state.m_refFrameY,-state.m_refFrameZ,Bx,By,Bz);
@@ -296,11 +315,11 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
       return ierr;
     }
     if(ptot[0]*ptot[0]+ptot[1]*ptot[1] == 0.) return -5; // Bad (divergent) fit
-//  
+//
 //  Postfit operation. Creation of array for different error calculations and full error matrix copy
 //
     state.m_FitStatus=ntrk;
-    if(ifCovV0 && state.m_vkalFitControl.getFullCovariance()){   //If full fit error matrix is returned by VKalVrtCORE 
+    if(ifCovV0 && state.m_vkalFitControl.getFullCovariance()){   //If full fit error matrix is returned by VKalVrtCORE
        int SymCovMtxSize=(3*ntrk+3)*(3*ntrk+4)/2;
        state.m_ErrMtx.assign (state.m_vkalFitControl.getFullCovariance(),
                                 state.m_vkalFitControl.getFullCovariance()+SymCovMtxSize);
@@ -319,18 +338,18 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
 
     if(Vertex.perp()>m_IDsizeR || std::abs(Vertex.z())>m_IDsizeZ)return -5; // Solution outside acceptable volume due to divergence
 
-    state.m_save_xyzfit[0]=xyzfit[0];    // saving of vertex position 
+    state.m_save_xyzfit[0]=xyzfit[0];    // saving of vertex position
     state.m_save_xyzfit[1]=xyzfit[1];    // for full error matrix
     state.m_save_xyzfit[2]=xyzfit[2];
 //
 // ------  Magnetic field in fitted vertex
 //
     double fx,fy,BMAG_CUR;
-    state.m_fitField.getMagFld(xyzfit[0] ,xyzfit[1] ,xyzfit[2] ,fx,fy,BMAG_CUR); 
+    state.m_fitField.getMagFld(xyzfit[0] ,xyzfit[1] ,xyzfit[2] ,fx,fy,BMAG_CUR);
     if(fabs(BMAG_CUR) < 0.01) BMAG_CUR=0.01;  // Safety
 
     Charge=0; for(i=0; i<ntrk; i++){Charge+=state.m_ich[i];};
-    Charge=-Charge; //VK 30.11.2009 Change sign acoording to ATLAS 
+    Charge=-Charge; //VK 30.11.2009 Change sign acoording to ATLAS
 
 
     TrkAtVrt.clear(); TrkAtVrt.reserve(ntrk);
@@ -360,7 +379,7 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
   {
     assert(dynamic_cast<State*> (&istate)!=nullptr);
     State& state = static_cast<State&> (istate);
-    int i,j,ij;				      
+    int i,j,ij;
     double Vrt[3],PMom[4],Cov0[21],Per[5],CovPer[15];
 
     for(i=0; i<3;  i++) Vrt[i]=Vertex[i];
@@ -377,7 +396,7 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
 // ------  Magnetic field in vertex
 //
     double fx,fy,BMAG_CUR;
-    state.m_fitField.getMagFld(Vrt[0], Vrt[1], Vrt[2] ,fx,fy,BMAG_CUR); 
+    state.m_fitField.getMagFld(Vrt[0], Vrt[1], Vrt[2] ,fx,fy,BMAG_CUR);
     if(fabs(BMAG_CUR) < 0.01) BMAG_CUR=0.01;  // Safety
 
     Trk::xyztrp( vkCharge, Vrt, PMom, Cov0, BMAG_CUR, Per, CovPer );
@@ -405,20 +424,20 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
            // 0 < Theta < pi   range
       while ( tp2 > pi) tp2 -= 2.*pi;
       while ( tp2 <-pi) tp2 += 2.*pi;
-      if    ( tp2 < 0.) { 
+      if    ( tp2 < 0.) {
         tp2 = fabs(tp2); tp1 += pi;
         while ( tp1 > pi) tp1 -= 2.*pi;
       }
 
-  } 
-  
+  }
 
-/* Returns a complete error matrix after fit 
-  useMom=0 def (V,Perigee1,Perigee2....PerigeeNTrk) 
-  useMom=1     (V,PxPyPz1,PxPyPz2....PxPyPzNTrk) 
+
+/* Returns a complete error matrix after fit
+  useMom=0 def (V,Perigee1,Perigee2....PerigeeNTrk)
+  useMom=1     (V,PxPyPz1,PxPyPz2....PxPyPzNTrk)
 */
 
- 
+
   StatusCode
   TrkVKalVrtFitter::VKalGetFullCov( long int NTrk, dvect& CovVrtTrk,
                                     IVKalState& istate,
@@ -445,7 +464,7 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
     for (std::vector<double>& v : Deriv) v.resize (DIM);
     std::vector<double> CovMtxOld(DIM*DIM);
 
- 
+
     CovVrtTrk.resize(DIM*(DIM+1)/2);
 
     ip=0;
@@ -464,13 +483,13 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
 
     int iSt=0;
     double Theta,invR,Phi;
-    for( iTrk=0; iTrk<NTrk; iTrk++){ 
+    for( iTrk=0; iTrk<NTrk; iTrk++){
       Theta=state.m_parfs[iTrk][0];
       Phi  =state.m_parfs[iTrk][1];
       invR =state.m_parfs[iTrk][2];
        /*-----------*/
        /* dNew/dOld */
-      iSt = 3 + iTrk*3;        
+      iSt = 3 + iTrk*3;
       if( !useMom ){
         Deriv[iSt  ][iSt+1] =   1;                                             //    Phi <-> Theta
         Deriv[iSt+1][iSt  ] =   1;                                             //    Phi <-> Theta
@@ -496,10 +515,10 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
     }
 //----------  Only upper half if filled and saved
     int ipnt=0;
-    double tmp, tmpTmp; 
+    double tmp, tmpTmp;
     for(i=0;i<DIM;i++){
      for(j=0;j<=i;j++){
-       tmp=0.; 
+       tmp=0.;
        for(ik=0;ik<DIM;ik++){
           if(Deriv[i][ik] == 0.) continue;
           tmpTmp=0;
@@ -522,7 +541,7 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
 
   StatusCode TrkVKalVrtFitter::VKalGetMassError( double& dM, double& MassError,
                                                  const IVKalState& istate) const
-  {    
+  {
     assert(dynamic_cast<const State*> (&istate)!=nullptr);
     const State& state = static_cast<const State&> (istate);
     if(!state.m_FitStatus) return StatusCode::FAILURE;
@@ -530,7 +549,7 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
     MassError = state.m_vkalFitControl.getVrtMassError();
     return StatusCode::SUCCESS;
   }
-  
+
 
   StatusCode  TrkVKalVrtFitter::VKalGetTrkWeights(dvect& trkWeights,
                                                   const IVKalState& istate) const
@@ -546,22 +565,22 @@ int TrkVKalVrtFitter::VKalVrtFit3( int ntrk,
 
     return StatusCode::SUCCESS;
   }
-  
 
-  int  TrkVKalVrtFitter::VKalGetNDOF(const State& state) 
-  {    
+
+  int  TrkVKalVrtFitter::VKalGetNDOF(const State& state)
+  {
     if(!state.m_FitStatus) return 0;
     int NDOF=2*state.m_FitStatus-3;
     if(state.m_usePointingCnst)         { NDOF+=2; }
     else if(state.m_useZPointingCnst)   { NDOF+=1; }
-    if( state.m_usePassNear || state.m_usePassWithTrkErr ) { NDOF+= 2; } 
+    if( state.m_usePassNear || state.m_usePassWithTrkErr ) { NDOF+= 2; }
 
     if( state.m_massForConstraint>0. )  { NDOF+=1; }
     if( !state.m_partMassCnst.empty() ) { NDOF+= state.m_partMassCnst.size(); }
-    if( state.m_useAprioriVertex )      { NDOF+= 3; }    
-    if( state.m_usePhiCnst )            { NDOF+=1; }  
-    if( state.m_useThetaCnst )          { NDOF+=1; }  
+    if( state.m_useAprioriVertex )      { NDOF+= 3; }
+    if( state.m_usePhiCnst )            { NDOF+=1; }
+    if( state.m_useThetaCnst )          { NDOF+=1; }
     return NDOF;
   }
-  
+
 }
