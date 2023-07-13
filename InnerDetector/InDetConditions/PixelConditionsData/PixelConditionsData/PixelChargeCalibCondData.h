76b04de11b078505d536a2fa73aa71ef86f85c8f
Shaun Roe <shaun.roe@cern.ch>
2023-07-13 15:17:44 +0200

    23.0-clang_warning-PixelConditionsData
    23.0-clang_warning-PixelConditionsData

--- InnerDetector/InDetConditions/PixelConditionsData/PixelConditionsData/PixelChargeCalibCondData.h
+++ InnerDetector/InDetConditions/PixelConditionsData/PixelConditionsData/PixelChargeCalibCondData.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef PIXELCHARGECALIBCONDDATA_H
@@ -12,10 +12,10 @@
 #include <vector>
 #include <array>
 namespace PixelChargeCalib{
-  class LegacyFitParameters;
-  class LinearFitParameters;
-  class Thresholds;
-  class Resolutions;
+  struct LegacyFitParameters;
+  struct LinearFitParameters;
+  struct Thresholds;
+  struct Resolutions;
 }
 
 class PixelChargeCalibCondData
