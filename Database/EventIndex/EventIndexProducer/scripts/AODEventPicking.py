53bad94e88791e790442ef35c361984f174849bc
Grigori Rybkin <grybkine@cern.ch>
2023-07-13 10:16:37 +0200

    EventIndexProducer: Fix event picking logic in case of Monte Carlo
    EventIndexProducer: Implement event picking in case of Monte Carlo
    
    For event picking, an event is identified by MC Channel Number and Event Number
    in case of Monte Carlo, by Run Number and Event Number in case of real data

--- Database/EventIndex/EventIndexProducer/scripts/AODEventPicking.py
+++ Database/EventIndex/EventIndexProducer/scripts/AODEventPicking.py
@@ -18,6 +18,7 @@ class PyxAODEvtFilter(Alg):
         kw['name'] = name
         super(PyxAODEvtFilter, self).__init__(**kw)
         self.evtList = kw.get('evtList',   None)
+        self.isMC = kw.get('isMC', False)
         return
 
     # Initialize the algorithm
@@ -31,7 +32,10 @@ class PyxAODEvtFilter(Alg):
         # Read the run/event number from xAOD::EventInfo
         if self.sg.contains('xAOD::EventInfo', 'EventInfo'):
             ei = self.sg.retrieve('xAOD::EventInfo', 'EventInfo')
-            runNumber = ei.runNumber()
+            if not self.isMC:
+                runNumber = ei.runNumber()
+            else:
+                runNumber = ei.mcChannelNumber()
             eventNumber = ei.eventNumber()
 
             # Check to see if we should accept or reject the event
@@ -57,7 +61,7 @@ if '__main__' in __name__:
     parser.add_argument('--outputAODFile', default=None, required=True,
                         help='Output AOD file')
     parser.add_argument('--eventList', default=None, required=True,
-                        help='Text file containing "run event guid" information (one per line)')
+                        help='Text file containing "run event [guid]" information (one per line)')
     args, _ = parser.parse_known_args()
 
     # Setup configuration logging
@@ -65,11 +69,11 @@ if '__main__' in __name__:
     log = logging.getLogger('AODEventPicking')
     log.info('== Picking events from AOD files w/ the CA Configuration')
 
-    # Parse input file that contains run_number, event_number, guid combinations
+    # Parse input file that contains run_number, event_number[, guid] combinations
     evtList = []
     with open(args.eventList) as f:
         for line in f:
-            run, evt, guid = line.rstrip().split()
+            run, evt, *guid = line.rstrip().split()
             evtList.append((int(run), int(evt)))
 
     # Set the configuration flags
@@ -96,12 +100,12 @@ if '__main__' in __name__:
     cfg.merge(PoolReadCfg(flags))
 
     # Setup event picking
-    cfg.addEventAlgo(PyxAODEvtFilter('EventFilterAlg', evtList = evtList), sequenceName = 'AthAlgSeq')
+    cfg.addEventAlgo(PyxAODEvtFilter('EventFilterAlg', evtList = evtList, isMC = flags.Input.isMC), sequenceName = 'AthAlgSeq')
 
     # Configure the output stream
     log.info('== Configuring Output Stream')
     from OutputStreamAthenaPool.OutputStreamConfig import OutputStreamCfg
-    cfg.merge(OutputStreamCfg(flags, 'AOD', takeItemsFromInput=True, AcceptAlgs = ['EventFilterAlg']))
+    cfg.merge(OutputStreamCfg(flags, 'AOD', takeItemsFromInput=True))
 
     # Configure metadata
     log.info('== Configuring metadata for the output stream')
@@ -111,6 +115,7 @@ if '__main__' in __name__:
     # Setup the output stream algorithm
     StreamAOD = cfg.getEventAlgo('OutputStreamAOD')
     StreamAOD.ForceRead = True
+    StreamAOD.AcceptAlgs += ['EventFilterAlg']
 
     # For (un)packing Cell Containers
     from LArGeoAlgsNV.LArGMConfig import LArGMCfg
