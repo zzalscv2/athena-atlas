fe70dac5bad8b5ad76f6ea5bc447caefa489ccbd
Carlo Varni <carlo.varni@cern.ch>
2023-07-21 16:50:18 +0200

    ACTS ART tests with 1 and 10 GeV muons
    ACTS ART tests with 1 and 10 GeV muons

--- InnerDetector/InDetValidation/InDetPhysValMonitoring/test/test_run4_acts_ckf_mu1GeV.sh
+++ InnerDetector/InDetValidation/InDetPhysValMonitoring/test/test_run4_acts_ckf_mu1GeV.sh
@@ -9,7 +9,7 @@
 
 lastref_dir=last_results
 dcubeXml=dcube_IDPVMPlots_ACTS_CKF_ITk.xml
-rdo_23p0=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PhaseIIUpgrade/RDO/ATLAS-P2-RUN4-03-00-00/mc21_14TeV.900498.PG_single_muonpm_Pt100_etaFlatnp0_43.recon.RDO.e8481_s4149_r14697/RDO.33675668._000016.pool.root.1
+rdo_23p0=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PhaseIIUpgrade/RDO/ATLAS-P2-RUN4-03-00-00/mc21_14TeV.900492.PG_single_muonpm_Pt1_etaFlatnp0_43.recon.RDO.e8481_s4149_r14697/RDO.33645151._000047.pool.root.1
 nEvents=1000
 
 # search in $DATAPATH for matching file
