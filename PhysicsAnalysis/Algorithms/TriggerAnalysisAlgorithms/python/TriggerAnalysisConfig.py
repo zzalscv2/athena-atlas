e95597101edb980f40503469aef32533a11b01d5
Baptiste Ravina <baptiste.ravina@cern.ch>
2023-07-24 14:50:40 +0200

    Algorithm for Global Trigger
    Algorithm for Global Trigger

--- PhysicsAnalysis/Algorithms/TriggerAnalysisAlgorithms/python/TriggerAnalysisConfig.py
+++ PhysicsAnalysis/Algorithms/TriggerAnalysisAlgorithms/python/TriggerAnalysisConfig.py
@@ -1,7 +1,8 @@
-# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 
 # AnaAlgorithm import(s):
 from AnalysisAlgorithmsConfig.ConfigBlock import ConfigBlock
+from AthenaConfiguration.Enums import LHCPeriod
 
 
 class TriggerAnalysisBlock (ConfigBlock):
@@ -9,59 +10,180 @@ class TriggerAnalysisBlock (ConfigBlock):
 
     def __init__ (self, configName) :
         super (TriggerAnalysisBlock, self).__init__ (configName)
-        self.addOption ('triggerChains', [], type=None)
+        self.addOption ('triggerChainsPerYear', {}, type=None)
+        self.addOption ('triggerChainsForSelection', [], type=None)
         self.addOption ('prescaleLumiCalcFiles', [], type=None)
         self.addOption ('noFilter', False, type=bool)
+        self.addOption ('electronID', '', type=str)
+        self.addOption ('electronIsol', '', type=str)
+        self.addOption ('photonIsol', '', type=str)
+        self.addOption ('muonID', '', type=str)
+        self.addOption ('electrons', '', type=str)
+        self.addOption ('muons', '', type=str)
+        self.addOption ('photons', '', type=str)
 
-
-    def makeAlgs (self, config) :
+    def makeTriggerDecisionAlg(self, config):
 
         # Create public trigger tools
         xAODConfTool = config.createPublicTool( 'TrigConf::xAODConfigTool', 'xAODConfigTool' )
-        decisionTool = config.createPublicTool( 'Trig::TrigDecisionTool', 'TrigDecisionTool')
+        decisionTool = config.createPublicTool( 'Trig::TrigDecisionTool', 'TrigDecisionTool' )
         decisionTool.ConfigTool = '%s/%s' % \
             ( xAODConfTool.getType(), xAODConfTool.getName() )
+        if config.geometry() == LHCPeriod.Run3:
+            decisionTool.NavigationFormat = 'TrigComposite' # Read Run 3 navigation (options are "TrigComposite" for R3 or "TriggElement" for R2, R2 navigation is not kept in most DAODs)
+            decisionTool.HLTSummary = 'HLTNav_Summary_DAODSlimmed' # Name of R3 navigation container (if reading from AOD, then "HLTNav_Summary_AODSlimmed" instead)
 
-        if self.triggerChains:
-            # Set up the trigger selection:
-            alg = config.createAlgorithm( 'CP::TrigEventSelectionAlg', 'TrigEventSelectorAlg' )
-            alg.tool = '%s/%s' % \
-                ( decisionTool.getType(), decisionTool.getName() )
-            alg.triggers = list(self.triggerChains)
-            alg.selectionDecoration = 'trigPassed'
-            alg.noFilter = self.noFilter
-            for t in self.triggerChains :
-                config.addOutputVar ('EventInfo', 'trigPassed_' + t, 'trigPassed_' + t, isEventLevel=True)
+        return decisionTool
 
-            # Calculate trigger prescales
-            if config.dataType() == 'data' and self.prescaleLumiCalcFiles:
-                alg = config.createAlgorithm( 'CP::TrigPrescalesAlg', 'TrigPrescalesAlg' )
-                config.addPrivateTool( 'pileupReweightingTool', 'CP::PileupReweightingTool' )
-                alg.pileupReweightingTool.LumiCalcFiles = self.prescaleLumiCalcFiles
-                alg.pileupReweightingTool.TrigDecisionTool = '%s/%s' % \
+    def makeTriggerMatchingAlg(self, config, decisionTool):
+
+        # Create public trigger tools
+        drScoringTool = config.createPublicTool( 'Trig::DRScoringTool', 'DRScoringTool' )
+        if config.geometry() == LHCPeriod.Run3:
+            matchingTool = config.createPublicTool( 'Trig::R3MatchingTool', 'MatchingTool' )
+            matchingTool.ScoringTool = '%s/%s' % \
+                    ( drScoringTool.getType(), drScoringTool.getName() )
+            matchingTool.TrigDecisionTool = '%s/%s' % \
                     ( decisionTool.getType(), decisionTool.getName() )
-                alg.triggers = [lumicalc.split(':')[-1] for lumicalc in self.prescaleLumiCalcFiles if ':' in lumicalc]
-                alg.triggersAll = list(self.triggerChains)
-                alg.prescaleDecoration = 'prescale'
+        else:
+            matchingTool = config.createPublicTool( 'Trig::MatchFromCompositeTool', 'MatchingTool' )
+            if config.isPhyslite():
+                matchingTool.InputPrefix = "AnalysisTrigMatch_"
 
+        return matchingTool
+
+    def makeTriggerSelectionAlg(self, config, decisionTool):
+
+        # Set up the trigger selection:
+        alg = config.createAlgorithm( 'CP::TrigEventSelectionAlg', 'TrigEventSelectorAlg' )
+        alg.tool = '%s/%s' % \
+            ( decisionTool.getType(), decisionTool.getName() )
+        alg.triggers = self.triggerChainsForSelection
+        alg.selectionDecoration = 'trigPassed'
+        alg.noFilter = self.noFilter
+
+        for t in self.triggerChainsForSelection :
+            config.addOutputVar ('EventInfo', 'trigPassed_' + t, 'trigPassed_' + t, isEventLevel=True)
+
+        # Calculate trigger prescales
+        if config.dataType() == 'data' and self.prescaleLumiCalcFiles:
+            alg = config.createAlgorithm( 'CP::TrigPrescalesAlg', 'TrigPrescalesAlg' )
+            config.addPrivateTool( 'pileupReweightingTool', 'CP::PileupReweightingTool' )
+            alg.pileupReweightingTool.LumiCalcFiles = self.prescaleLumiCalcFiles
+            alg.pileupReweightingTool.TrigDecisionTool = '%s/%s' % \
+                    ( decisionTool.getType(), decisionTool.getName() )
+            alg.triggers = [lumicalc.split(':')[-1] for lumicalc in self.prescaleLumiCalcFiles if ':' in lumicalc]
+            alg.triggersAll = self.triggerChainsForSelection
+            alg.prescaleDecoration = 'prescale'
+
+        return
+
+    def makeTriggerGlobalEffCorrAlg(self, config, decisionTool, matchingTool):
+
+        alg = config.createAlgorithm( 'CP::TrigGlobalEfficiencyAlg', 'TrigGlobalSFAlg' )
+        if config.geometry() == LHCPeriod.Run3:
+            alg.triggers_2022 = [trig.replace("HLT_","").replace(" || ", "_OR_") for trig in self.triggerChainsPerYear.get('2022',[])]
+        else:
+            alg.triggers_2015 = [trig.replace("HLT_","").replace(" || ", "_OR_") for trig in self.triggerChainsPerYear.get('2015',[])]
+            alg.triggers_2016 = [trig.replace("HLT_","").replace(" || ", "_OR_") for trig in self.triggerChainsPerYear.get('2016',[])]
+            alg.triggers_2017 = [trig.replace("HLT_","").replace(" || ", "_OR_") for trig in self.triggerChainsPerYear.get('2017',[])]
+            alg.triggers_2018 = [trig.replace("HLT_","").replace(" || ", "_OR_") for trig in self.triggerChainsPerYear.get('2018',[])]
+        alg.decisionTool = '%s/%s' % ( decisionTool.getType(), decisionTool.getName() )
+        alg.matchingTool = '%s/%s' % ( matchingTool.getType(), matchingTool.getName() )
+        alg.isRun3Geo = config.geometry() == LHCPeriod.Run3
+        alg.scaleFactorDecoration = 'globalTriggerEffSF_%SYS%'
+        alg.matchingDecoration = 'globalTriggerMatch_%SYS%'
+        alg.eventDecisionOutputDecoration = 'dontsave_%SYS%'
+        alg.doMatchingOnly = config.dataType() == 'data'
+        alg.noFilter = self.noFilter
+        alg.electronID = self.electronID
+        alg.electronIsol = self.electronIsol
+        alg.photonIsol = self.photonIsol
+        alg.muonID = self.muonID
+        alg.electrons, alg.electronSelection = config.readNameAndSelection(self.electrons)
+        alg.muons, alg.muonSelection = config.readNameAndSelection(self.muons)
+        alg.photons, alg.photonSelection = config.readNameAndSelection(self.photons)
+
+        if config.dataType != 'data' and not alg.doMatchingOnly:
+            config.addOutputVar ('EventInfo', alg.scaleFactorDecoration, 'globalTriggerEffSF', isEventLevel=True)
+        config.addOutputVar ('EventInfo', alg.matchingDecoration, 'globalTriggerMatch', isEventLevel=True)
+
+        return
+
+    def makeAlgs (self, config) :
+
+        # if we are only given the trigger dictionary, we fill the selection list automatically
+        if self.triggerChainsPerYear and not self.triggerChainsForSelection:
+            triggers = set()
+            for chain_list in self.triggerChainsPerYear.values():
+                for chain in chain_list:
+                    if '||' in chain:
+                        chains = chain.split('||')
+                        triggers.update(map(str.strip, chains))
+                    else:
+                        triggers.add(chain.strip())
+            self.triggerChainsForSelection = list(triggers)
+
+        # Create the decision algorithm, keeping track of the decision tool for later
+        decisionTool = self.makeTriggerDecisionAlg(config)
+
+        # Now pass it to the matching algorithm, keeping track of the matching tool for later
+        matchingTool = self.makeTriggerMatchingAlg(config, decisionTool)
+
+        if self.triggerChainsForSelection:
+            self.makeTriggerSelectionAlg(config, decisionTool)
+
+        # Calculate multi-lepton (electron/muon/photon) trigger efficiencies and SFs
+        if self.triggerChainsPerYear:
+            self.makeTriggerGlobalEffCorrAlg(config, decisionTool, matchingTool)
+
+        return
 
 
 def makeTriggerAnalysisConfig( seq,
-                               triggerChains = None,
+                               triggerChainsPerYear = None,
+                               triggerChainsForSelection = None,
                                prescaleLumiCalcFiles = None,
                                noFilter = None,
+                               electronWorkingPoint = None,
+                               muonWorkingPoint = None,
+                               photonWorkingPoint = None,
+                               electrons = None,
+                               muons = None,
+                               photons = None,
                                configName = 'Trigger'):
     """Create a basic trigger analysis algorithm sequence
 
     Keyword arguments:
       dataType -- The data type to run on ("data", "mc" or "afii")
-      triggerChains -- a list of trigger chains
+      triggerChainsPerYear -- a dictionary with key (str) the year and value (list of strings) the trigger chains
+      triggerChainsForSelection -- a list of trigger chains to be used for trigger selection, only set it if you need a different setup than for SFs!
       prescaleLumiCalcFiles -- a list of lumicalc files to calculate trigger prescales
       noFilter -- do not apply an event filter (i.e. don't skip any events)
     """
 
     config = TriggerAnalysisBlock (configName)
-    config.setOptionValue ('triggerChains', triggerChains, noneAction='ignore')
+    config.setOptionValue ('triggerChainsPerYear', triggerChainsPerYear, noneAction='ignore')
+    config.setOptionValue ('triggerChainsForSelection', triggerChainsForSelection, noneAction='ignore')
     config.setOptionValue ('prescaleLumiCalcFiles', prescaleLumiCalcFiles, noneAction='ignore')
     config.setOptionValue ('noFilter', noFilter, noneAction='ignore')
+    if electronWorkingPoint is not None:
+        splitWP = electronWorkingPoint.split ('.')
+        if len (splitWP) != 2 :
+            raise ValueError (f'electron working point should be of format "likelihood.isolation", not {electronWorkingPoint}')
+        config.setOptionValue ('electronID', splitWP[0], noneAction='ignore')
+        config.setOptionValue ('electronIsol', splitWP[1], noneAction='ignore')
+    if muonWorkingPoint is not None:
+        splitWP = muonWorkingPoint.split ('.')
+        if len (splitWP) != 2 :
+            raise ValueError (f'muon working point should be of format "likelihood.isolation", not {muonWorkingPoint}')
+        config.setOptionValue ('muonID', splitWP[0], noneAction='ignore')
+    if photonWorkingPoint is not None:
+        splitWP = photonWorkingPoint.split ('.')
+        if len (splitWP) != 2 :
+            raise ValueError (f'photon working point should be of format "likelihood.isolation", not {photonWorkingPoint}')
+        config.setOptionValue ('photonIsol', splitWP[1], noneAction='ignore')
+    config.setOptionValue('electrons', electrons, noneAction='ignore')
+    config.setOptionValue('muons', muons, noneAction='ignore')
+    config.setOptionValue('photons', photons, noneAction='ignore')
     seq.append (config)
