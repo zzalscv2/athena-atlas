c18b30d23575072fbaa48bf30b965ddb9960b1ee
Stewart Martin-Haugh <stewart.martin-haugh@cern.ch>
2023-07-25 09:56:58 +0200

    Add unconventional tracking tests to ART, ATR-27961
    Add unconventional tracking tests to ART, ATR-27961

---
+++ Trigger/TrigValidation/TriggerTest/test/test_trig_mc_v1Dev_slice_utt_build.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+
+# art-description: Trigger RDO->RDO_TRIG athena test of the unconventional tracking slice in Dev_pp_run3_v1 menu
+# art-type: build
+# art-include: main/Athena
+# art-include: 23.0/Athena
+# Skipping art-output which has no effect for build tests.
+# If you create a grid version, check art-output in existing grid tests.
+
+from TrigValTools.TrigValSteering import Test, ExecStep, CheckSteps
+
+ex = ExecStep.ExecStep()
+ex.type = 'athena'
+ex.job_options = 'TriggerJobOpts/runHLT_standalone.py'
+ex.input = 'ttbar'
+ex.threads = 1
+ex.args = '-c "setMenu=\'Dev_pp_run3_v1\';doEmptyMenu=True;doUnconventionalTrackingSlice=True;doWriteBS=False;doWriteRDOTrigger=True;"'
+
+test = Test.Test()
+test.art_type = 'build'
+test.exec_steps = [ex]
+test.check_steps = CheckSteps.default_check_steps(test)
+
+import sys
+sys.exit(test.run())
