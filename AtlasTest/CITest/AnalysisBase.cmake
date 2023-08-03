762daa6de5657c0c5c3395fb6fc4b24c5a6ca308
Tadej Novak <tadej.novak@cern.ch>
2023-08-03 14:31:02 +0200

    Rename AnalysisTop CI tests to something clearer

--- AtlasTest/CITest/AnalysisBase.cmake
+++ AtlasTest/CITest/AnalysisBase.cmake
@@ -1,12 +1,12 @@
-# Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 #
 # CI test definitions for the AnalysisBase project
 # --> README.md before you modify this file
 #
 
-atlas_add_citest( EMPFlowData
+atlas_add_citest( AnalysisTop_EMPFlowData
    LOG_IGNORE_PATTERN "Cannot interpolate outside histogram domain" # ANALYSISTO-1165
    SCRIPT CI_EMPFlowDatatest.py )
 
-atlas_add_citest( EMPFlowMC
+atlas_add_citest( AnalysisTop_EMPFlowMC
    SCRIPT CI_EMPFlowMCtest.py )
