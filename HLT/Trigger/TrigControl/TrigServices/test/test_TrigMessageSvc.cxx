9b62591a37effcdd879bf03b24cc30e9a39de4b7
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-08-09 15:55:19 +0200

    TrigServices: rewrite MessageSvc unit test
    Rewrite the unit test to use the regular state machine to avoid issues
    with finalization order. Also remove the rather useless check on the
    execution time which is way too generous to be useful.
    
    Closes ATR-27962.

--- HLT/Trigger/TrigControl/TrigServices/test/test_TrigMessageSvc.cxx
+++ HLT/Trigger/TrigControl/TrigServices/test/test_TrigMessageSvc.cxx
@@ -1,17 +1,10 @@
-#ifdef NDEBUG
-static const int timeout = 3000;
-#undef NDEBUG
-#else
-static const int timeout = 8000;
-#endif
+// Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 
-
-#include "../src/TrigMessageSvc.h"
-#include "TestTools/initGaudi.h"
-
-#include "GaudiKernel/IProperty.h"
-#include "GaudiKernel/IService.h"
+#include "GaudiKernel/IMessageSvc.h"
 #include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/IAppMgrUI.h"
+#include "GaudiKernel/ServiceHandle.h"
+#include "GaudiKernel/SmartIF.h"
 
 #include "TInterpreter.h"
 #include "CxxUtils/ubsan_suppress.h"
@@ -25,37 +18,30 @@ constexpr size_t numMessages = 50000;
 
 int main() {
   using namespace std;
-  ISvcLocator* pSvcLoc;
-  assert(Athena_test::initGaudi(pSvcLoc));
 
-   CxxUtils::ubsan_suppress ( []() { TInterpreter::Instance(); } );
+  CxxUtils::ubsan_suppress ( []() { TInterpreter::Instance(); } );
 
-  //--------------------------------------------------
-  // Create THistSvc and TrigMessageSvc
-  //--------------------------------------------------
-  IProperty* hsvcprop{nullptr};
-  assert(pSvcLoc->service("THistSvc","THistSvc",hsvcprop,true).isSuccess());
-  assert(hsvcprop->setProperty("Output","[\"EXPERT DATAFILE='expert-monitoring.root' OPT='RECREATE'\"]").isSuccess());
+  IAppMgrUI* appMgr = Gaudi::createApplicationMgr();
+  SmartIF<IProperty> propMgr(appMgr);
+  SmartIF<ISvcLocator> svcLoc(appMgr);
 
-  IService* hsvc{nullptr};
-  assert(pSvcLoc->service("THistSvc","THistSvc",hsvc,true).isSuccess());
+  propMgr->setProperty( "JobOptionsType", "NONE" ).
+    orThrow("Cannot set property JobOptionsType", "test");
 
-  TrigMessageSvc msgsvc("MessageSvc", pSvcLoc);
+  propMgr->setProperty("MessageSvcType", "TrigMessageSvc").
+    orThrow("Cannot set property MessageSvcType", "test");
 
-  //--------------------------------------------------
-  // Initialise and start the services
-  //--------------------------------------------------
-  StatusCode sc = msgsvc.initialize();
-  assert(sc.isSuccess());
-
-  sc = hsvc->start();
-  assert(sc.isSuccess());
-  sc = msgsvc.start();
-  assert(sc.isSuccess());
+  if ((appMgr->configure()).isFailure() ||
+      (appMgr->initialize()).isFailure()) {
+    return 1;
+  }
 
   //--------------------------------------------------
   // Prepare the benchmark
   //--------------------------------------------------
+  ServiceHandle<IMessageSvc> msgsvc("TrigMessageSvc/MessageSvc", "test");
+  assert( msgsvc.retrieve().isSuccess() );
+
   std::vector<std::string> names;
   names.reserve(numSources);
   for (size_t i=1; i<=numSources; ++i) {
@@ -73,26 +59,12 @@ int main() {
 
   for (size_t i=0; i<numMessages; ++i) {
     size_t j = distribution(generator);
-    msgsvc.reportMessage(names[j], 3, "This is a test message");
+    msgsvc->reportMessage(names[j], 3, "This is a test message");
   }
 
   auto t1 = std::chrono::high_resolution_clock::now();
   auto td = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-  msgsvc.reportMessage("Benchmark", 3, std::string("Time: ").append(std::to_string(td.count()).append(" ms")));
-  assert(td.count() < timeout);
-
-  //--------------------------------------------------
-  // Stop and finalise the services
-  //--------------------------------------------------
-  sc = msgsvc.stop();
-  assert(sc.isSuccess());
-  sc = hsvc->stop();
-  assert(sc.isSuccess());
-
-  sc = msgsvc.finalize();
-  assert(sc.isSuccess());
-  sc = hsvc->finalize();
-  assert(sc.isSuccess());
+  msgsvc->reportMessage("Benchmark", 3, std::string("Time: ").append(std::to_string(td.count()).append(" ms")));
 
   return 0;
 }
