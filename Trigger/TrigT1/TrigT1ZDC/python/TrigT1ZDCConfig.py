09a8bd210d49f7b182560c756d609343b6220180
Matthew Caleb Hoppesch <mch6@illinois.edu>
2023-08-11 19:32:44 +0200

    ATLASG-2538, changes for new ZdcLUT
    ATLASG-2538, changes for new ZdcLUT

--- Trigger/TrigT1/TrigT1ZDC/python/TrigT1ZDCConfig.py
+++ Trigger/TrigT1/TrigT1ZDC/python/TrigT1ZDCConfig.py
@@ -9,7 +9,7 @@ def L1ZDCSimCfg(flags):
     from ZdcRec.ZdcRecConfig import ZdcRecCfg
     acc.merge(ZdcRecCfg(flags))
     from AthenaConfiguration.ComponentFactory import CompFactory
-    acc.addEventAlgo(CompFactory.LVL1.TrigT1ZDC(filepath_LUT = "TrigT1ZDC/zdcRun3T1LUT_v1_30_05_2023.json",
+    acc.addEventAlgo(CompFactory.LVL1.TrigT1ZDC(filepath_LUT = flags.Trigger.ZdcLUT,
                                                 EnergyADCScale = 0.4))
     return acc
 
