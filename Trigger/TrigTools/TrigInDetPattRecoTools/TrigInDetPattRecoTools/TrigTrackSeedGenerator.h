65290a5a2aa627460fffcacac5aad9356bfe5b62
Marin Mlinarevic <marin.mlinarevic@cern.ch>
2023-08-14 17:20:06 +0200

    Variable redeclaration bug fix

--- Trigger/TrigTools/TrigInDetPattRecoTools/TrigInDetPattRecoTools/TrigTrackSeedGenerator.h
+++ Trigger/TrigTools/TrigInDetPattRecoTools/TrigInDetPattRecoTools/TrigTrackSeedGenerator.h
@@ -349,7 +349,6 @@ private:
 
   std::vector<TrigInDetTriplet> m_triplets;
 
-  float m_zMinus, m_zPlus, m_minCoord, m_maxCoord;
   float m_zMinus, m_zPlus, m_minCoord, m_maxCoord, m_zMinusEndcap, m_zPlusEndcap;
 
   int m_nInner, m_nOuter;
