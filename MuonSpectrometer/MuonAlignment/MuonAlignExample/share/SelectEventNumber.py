7592a4aa42b796802bdb993da03dfa5100ef9f53
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2023-08-18 11:44:18 +0200

    Delete MuonAlignExample
    Delete MuonAlignExample

--- MuonSpectrometer/MuonAlignment/MuonAlignExample/share/SelectEventNumber.py
+++
@@ -1,5 +0,0 @@
-from TrkAlignGenTools.AlignmentFLags import AlignmentFlags as align
-align.readEventList=True
-
-from TrkAlignGenAlgs.SelectEventNumberGetter import SelectEventNumberGetter
-selectEvtNumber = SelectEventNumberGetter().SelectEventNumberHandle()
