87f81f4322591e90a9239f6f3d354d81bfcf1597
scott snyder <snyder@bnl.gov>
2023-08-22 18:19:36 +0200

    L1CaloFEXToolInterfaces: Fix out-of-bounds write.
    FillgTowerEDMForward was writing tower Ets to the output array using the
    wrong indices, corrupting the stack.  But this is anyway redundant with
    what's already done in GetEnergyMatrix, so just remove that bit.

--- Trigger/TrigT1/L1CaloFEX/L1CaloFEXToolInterfaces/L1CaloFEXToolInterfaces/IgFEXFPGA.h
+++ Trigger/TrigT1/L1CaloFEX/L1CaloFEXToolInterfaces/L1CaloFEXToolInterfaces/IgFEXFPGA.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 
@@ -43,7 +43,7 @@ Interface definition for gFEXFPGA
     virtual void GetEnergyMatrix(gTowersForward &) const = 0;
 
     virtual void FillgTowerEDMCentral(SG::WriteHandle<xAOD::gFexTowerContainer> &) = 0;
-    virtual void FillgTowerEDMForward(SG::WriteHandle<xAOD::gFexTowerContainer> &, gTowersForward &) = 0;
+    virtual void FillgTowerEDMForward(SG::WriteHandle<xAOD::gFexTowerContainer> &) = 0;
 
 
   private:
