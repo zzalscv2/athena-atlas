7070629be87db1bfa0ddc2e851094f0a4cdde9e6
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2023-08-22 20:35:16 +0200

    CP::ShowerDepthTool Updates, main branch (2023.08.21.)
    Cleaned the interface and implementation of CP::ShowerDepthTool.
    
    Primitive types are now passed to the tool by value instead of
    const reference.
    
    Made the tool use std::unique_ptr for managing the memory of
    its histograms.
    
    Re-wrote the protection against out-of-bounds interpolation in
    the code to not hardcode any properties of the underlying TH2
    histogram.

--- PhysicsAnalysis/ElectronPhotonID/IsolationCorrections/IsolationCorrections/ShowerDepthTool.h
+++ PhysicsAnalysis/ElectronPhotonID/IsolationCorrections/IsolationCorrections/ShowerDepthTool.h
@@ -1,13 +1,14 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef ELECTRONISOLATIONSELECTION_SHOWERDEPTHTOOL_H
 #define ELECTRONISOLATIONSELECTION_SHOWERDEPTHTOOL_H
 
-// ROOT include(s):
-#include <TString.h>
+// System include(s).
+#include <memory>
 #include <optional>
+#include <utility>
 
 // Forward declaration(s):
 class TH1;
@@ -16,52 +17,47 @@ namespace CP {
 
   class ShowerDepthTool{
   public :
-    ShowerDepthTool(); 
-    ~ShowerDepthTool(); 
+    ShowerDepthTool();
+    ~ShowerDepthTool();
     /// Function initialising the tool
-    bool initialize();      
-      
+    bool initialize();
+
     /** Shower depth (in mm) on EM1 vs. eta, considering misalignments **/
-    float getCorrectedShowerDepthEM1(const float& etas1,const float& phi,const bool& isData = true) const;
+    float getCorrectedShowerDepthEM1(float etas1, float phi, bool isData = true) const;
 
     /** Shower depth (in mm) on EM2 vs. eta, considering misalignments **/
-    float getCorrectedShowerDepthEM2(const float& etas2,const float& phi,const bool& isData = true) const;
+    float getCorrectedShowerDepthEM2(float etas2, float phi, bool isData = true) const;
 
     /** Return the shower depth in R,Z considering misalignments **/
-    std::pair<float, float> getCorrectedRZ(const float& eta,const float& phi,const bool& isData = true,const int& sampling = 1) const;
-      
+    std::pair<float, float> getCorrectedRZ(float eta, float phi, bool isData = true, int sampling = 1) const;
+
     /** Return the calorimeter displacement in R(Z) for barrel (endcap) **/
-    float getRZCorrection(const float& eta,const float& phi,const bool& isData = true) const;     
-      
+    float getRZCorrection(float eta, float phi, bool isData = true) const;
+
     /** Eta direction from zvertex to the shower in the given sampling **/
-    float getCorrectedEtaDirection(const float& zvertex,const float& eta,const float& phi,const bool& isData=true,const int& sampling = 1) const;
-      
+    float getCorrectedEtaDirection(float zvertex, float eta, float phi, bool isData=true, int sampling = 1) const;
+
     /** Eta direction from samplings 1 and 2 (pointing) **/
-    std::optional<float> getCaloPointingEta(const float& etas1,const float& etas2,const float& phi,const bool& isData=true) const;
+    std::optional<float> getCaloPointingEta(float etas1, float etas2, float phi, bool isData=true) const;
 
     /** Shower depth (in mm) vs. eta on EM1 **/
-    static float getShowerDepthEM1(const float& etas1) ;
-      
+    static float getShowerDepthEM1(float etas1);
+
     /** Shower depth (in mm) vs. eta on EM2 **/
-    static float getShowerDepthEM2(const float& etas2) ;
+    static float getShowerDepthEM2(float etas2);
 
     /** Shower depth in R,Z for the given sampling **/
-    static std::pair<float, float> getRZ(const float& eta,const int& sampling) ;
+    static std::pair<float, float> getRZ(float eta, int sampling);
 
-    static float getEtaDirection(const float& zvertex,const float& R,const float& z) ;
-      
-      
-  protected:
+    static float getEtaDirection(float zvertex, float R, float z);
+
+  private:
 
     /** Return TH1* from file given fileName, histoName **/
-    static TH1* getHistoFromFile(const TString& fileName,const TString& histoName);      
-      
-  private:
-    TH1 *m_hData;
-    TH1 *m_hMC;    
-    std::string m_configFileName;
-    TString m_dataHistoName;
-    TString m_mcHistoName;
+    static std::unique_ptr<TH1> getHistoFromFile(const char* fileName, const char* histoName);
+
+    std::unique_ptr<TH1> m_hData;
+    std::unique_ptr<TH1> m_hMC;
   };
 
 } // namespace CP
