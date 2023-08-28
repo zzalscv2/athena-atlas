1806a579be3c55e0edd54741da0bf6fae55041a9
Walter Lampl <walter.lampl@cern.ch>
2023-08-28 17:26:50 +0200

    Delete some obsolete LArCell and LArCluster jobOptions
    Delete some obsolete LArCell and LArCluster jobOptions

--- LArCalorimeter/LArCellRec/share/LArCell_jobOptions.py
+++
@@ -1,4 +0,0 @@
-# default jobOptions file for LAr Cell Reconstruction
-# with full corrections and with the possibility to add the noise. 
-include( "LArCellRec/LArCellFromLArHit_MC_jobOptions.py" )
-
