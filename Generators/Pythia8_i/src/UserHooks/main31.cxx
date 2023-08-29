f3579a3f7bf99e85b2bf7711791273b708c9c4b0
Scott Snyder <scott.snyder@cern.ch>
2023-08-29 10:29:10 +0200

    Pythia8_i: Work around pythia 8.310 plugin issues.
    Pythia8_i: Work around plugin issues.
    
    Pythia 8.310 adds a plugin mechanism, allowing loading a class dynamically
    given the class and library name.  To make this work, some functions with
    known names are defined in the compilation unit containing the plugin
    via Pythia macros.  But if the same plugin is declared in more than one
    compilation unit, we'll get link errors.
    
    This comes up here because we have several compilation units that all include
    the PowhegHooks.h header defining that pluzgin.  However, we have our own
    mechanism to create the class and are not relying on the Pythia mechanism.
    So for now, we'll just null out the Pythia macros that define the plugin
    functions.  That should get things working as they were in 8.309.
    
    Once we're ready to drop versions older than 8.310, it would probably be worth
    revisiting this to see if we can change to using the Pythia plugin mechanism.

--- Generators/Pythia8_i/src/UserHooks/main31.cxx
+++ Generators/Pythia8_i/src/UserHooks/main31.cxx
@@ -1,10 +1,20 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "UserHooksUtils.h"
 #include "UserSetting.h"
 #include "Pythia8_i/UserHooksFactory.h"
+#if PYTHIA_VERSION_INTEGER >= 8310
+// Disable the pythia 8.310 plugin mechanism.
+// We use our own mechanism to create these classes, and we get link errors
+// if these are used in more than one compilation unit.
+# include "Pythia8/Plugins.h"
+# undef PYTHIA8_PLUGIN_CLASS
+# undef PYTHIA8_PLUGIN_VERSIONS
+# define PYTHIA8_PLUGIN_CLASS(BASE, CLASS, PYTHIA, SETTINGS, LOGGER)
+# define PYTHIA8_PLUGIN_VERSIONS(...)
+#endif
 #include "Pythia8Plugins/PowhegHooks.h"
 
 namespace Pythia8{
--- Generators/Pythia8_i/src/UserHooks/main31Vincia.cxx
+++ Generators/Pythia8_i/src/UserHooks/main31Vincia.cxx
@@ -6,6 +6,16 @@
 #include "UserSetting.h"
 #include "Pythia8_i/UserHooksFactory.h"
 #ifdef PYTHIA_VERSION_INTEGER
+#if PYTHIA_VERSION_INTEGER >= 8310
+// Disable the pythia 8.310 plugin mechanism.
+// We use our own mechanism to create these classes, and we get link errors
+// if these are used in more than one compilation unit.
+  # include "Pythia8/Plugins.h"
+  # undef PYTHIA8_PLUGIN_CLASS
+  # undef PYTHIA8_PLUGIN_VERSIONS
+  # define PYTHIA8_PLUGIN_CLASS(BASE, CLASS, PYTHIA, SETTINGS, LOGGER)
+  # define PYTHIA8_PLUGIN_VERSIONS(...)
+  #endif
   #if PYTHIA_VERSION_INTEGER > 8308
     #include "Pythia8Plugins/PowhegHooks.h"
   #else
