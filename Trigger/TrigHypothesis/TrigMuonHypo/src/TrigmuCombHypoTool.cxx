433ae429db19398d98e2c0256b3e14f3fc3d4d08
Scott Snyder <scott.snyder@cern.ch>
2023-08-29 10:24:56 +0200

    TrigMuonHypo: Fix cppcheck warnings.
    TrigMuonHypo: Fix cppcheck warnings.
    
    Prefer preincrement to postincrement for iterator classes.

--- Trigger/TrigHypothesis/TrigMuonHypo/src/TrigmuCombHypoTool.cxx
+++ Trigger/TrigHypothesis/TrigMuonHypo/src/TrigmuCombHypoTool.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #include "GaudiKernel/SystemOfUnits.h"
@@ -734,7 +734,7 @@ StatusCode TrigmuCombHypoTool::chooseBestMuon(std::vector<TrigmuCombHypoTool::Co
 
 	  // monitoring
 	  const xAOD::L2CombinedMuon* CombMf = (*input[j]).muComb;
-	  mucombNrOverlapped++;
+	  ++mucombNrOverlapped;
 	  mucombOverlappedPt = CombMf->pt()* CombMf->charge() /Gaudi::Units::GeV;
 	  mucombOverlappedEta = CombMf->eta();
 	  mucombOverlappedPhi = CombMf->phi();
