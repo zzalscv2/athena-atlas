a1906d94896b9025643ba8400a9041ba01d0eb23
Edward Moyse <edward.moyse@cern.ch>
2023-09-05 10:45:56 +0200

    Improvements to Acts / Trk convertors and tests
    Need to have surface to calculate the global position for ACTS parameters
    
    Print warning if no surface found
    
    Cache position, and improve debug output

--- Tracking/Acts/ActsEventCnv/test/testConversion.py
+++ Tracking/Acts/ActsEventCnv/test/testConversion.py
@@ -8,42 +8,53 @@ import math
 if "__main__" == __name__:
     RunConversion()
 
-    def _dparamsEqual(acts, trk):
-        for acts_track_params, trk_track_params in zip(acts, trk):
-            if (not math.isclose(acts_track_params, trk_track_params)):
+    def _valuesEqual(acts, trk):
+        for acts_values, trk_values in zip(acts, trk):
+            if (not math.isclose(acts_values, trk_values)):
                 return False
         return True
-
+   
     # Now compare outputs
     import json
     success = False
     with open('dump.json') as f:
         data = json.load(f)
         for event in data:
+            found_ni_differences = 0
             print('Processing', event)
             acts = data[event]['TrackContainers']['ConvertedVectorTrackContainer']
             trk = data[event]['Tracks']['CombinedITkTracks']
             if (acts != trk):
-                # We might need to make this comparison more sophisticated
-                # Since we do not necessarily expect binary identical results
-                # ... but we are a ways off of worrying about this right now
+                found_difference = False
+                # Okay, so simple comparison fails... let's try to find where
                 print('WARNING: Acts and Trk tracks differ')
                 print('We have ', len(acts), '[',
                       len(trk), '] Acts [ Trk ] tracks')
                 for i, (acts_track, trk_track) in enumerate(zip(acts, trk)):
-                    if (not _dparamsEqual(acts_track['dparams'], trk_track['dparams'])):
+                    if (not _valuesEqual(acts_track['dparams'], trk_track['dparams'])):
                         print('ERROR: Acts and Trk dparams differ for track', i)
                         print('Acts dparams:', acts_track['dparams'])
                         print('Trk dparams:', trk_track['dparams'])
-                    if (acts_track['pos'] != trk_track['pos']):
+                        found_difference = True
+                    if (not _valuesEqual(acts_track['pos'], trk_track['pos'])):
                         print('ERROR: Acts and Trk pos differ for track', i)
                         print('Acts pos:', acts_track['pos'])
                         print('Trk pos:', trk_track['pos'])
+                        found_difference = True
+                    if not found_difference:
+                        # Simple comparison failed, but no numerically significant difference found (in what we compare, at least)
+                        # (Of course, this does not exclude that there are important differences in quantities we are not checking!)
+                        found_ni_differences += 1
+                        success=True
             else:
                 print('SUCCESS: Acts and Trk tracks agree')
                 success = True
+    if found_ni_differences > 0:
+        print('WARNING: Found', found_ni_differences, 'tracks which have differences.')
     if not success:
         print('ERROR: the output of the conversion is not correct')
-        # Will uncomment this once the conversion is fixed
-        # import sys
-        # sys.exit(1)
+        import sys
+        sys.exit(1)
+    else:
+        print ('SUCCESS: the output of the conversion is correct (at least, to the precision we check)')
+
