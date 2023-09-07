3287cdcd5aaeb458c39857d5b645abb7ee3d19c1
scott snyder <sss@karma>
2023-09-06 23:05:41 -0400

    StoreGate: Fix cppcheck warning.
    Pass string by const reference, not by value.

--- Control/StoreGate/StoreGate/tools/hash_functions.h
+++ Control/StoreGate/StoreGate/tools/hash_functions.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef HASH_FUNCTIONS_H
@@ -13,7 +13,7 @@ namespace SG {
   ///@param str    the string to be hashed
   ///@param maxInt the maximum value the function returns. 
   ///              Hash collision probability is 1/min(maxInt, 2**127)
-  inline int simpleStringHash(std::string str, int maxInt=0xFFFF) {
+  inline int simpleStringHash(const std::string& str, int maxInt=0xFFFF) {
     int h(0), a(127);
     for (std::string::size_type iC=0; iC<str.size(); iC++)
       h = (a*h + str[iC]) % maxInt;    
