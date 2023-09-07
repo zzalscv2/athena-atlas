1f29426c117aca9bc4151e59ae0f9a392e2b039d
Carlo Varni <carlo.varni@cern.ch>
2023-09-07 08:40:59 +0200

    Use the same command as used in Acts SPOT also in the CI
    Use the same command as used in Acts SPOT also in the CI

--- AtlasTest/CITest/test/ActsBenchmarkWithSpot.sh
+++ AtlasTest/CITest/test/ActsBenchmarkWithSpot.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/bash
 # Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 
-# ttbar mu=200 input
-input_rdo=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PhaseIIUpgrade/RDO/ATLAS-P2-RUN4-03-00-00/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000047.pool.root.1
-n_events=10
+NTHREADS=${1}
+NEVENTS=${2}
+DATADIR="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PhaseIIUpgrade/RDO"
 
-Reco_tf.py --CA \
-  --preExec "flags.Exec.FPE=500;" \
-  --preInclude "InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude,ActsConfig.ActsCIFlags.actsBenchmarkSpotFlags" \
-  --postExec "cfg.getEventAlgo(\"ActsTrkITkPixelClusterizationAlg\").PixelClustersKey=\"xAODpixelClusters\";cfg.getEventAlgo(\"ActsTrkITkStripClusterizationAlg\").StripClustersKey=\"xAODstripClusters\";" \
-  --inputRDOFile ${input_rdo} \
-  --outputAODFile AOD.pool.root \
-  --maxEvents ${n_events}
+# Run the job
+export TRF_ECHO=1;
+ATHENA_CORE_NUMBER=${NTHREADS} Reco_tf.py \
+    --CA 'all:True' \
+    --maxEvents  ${NEVENTS} \
+    --perfmon 'fullmonmt' \
+    --multithreaded 'True' \
+    --autoConfiguration 'everything' \
+    --conditionsTag 'all:OFLCOND-MC15c-SDR-14-05' \
+    --geometryVersion 'all:ATLAS-P2-RUN4-03-00-00' \
+    --postInclude 'all:PyJobTransforms.UseFrontier' \
+    --preInclude "InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude,ActsConfig.ActsCIFlags.actsBenchmarkSpotFlags" \
+    --steering 'doRAWtoALL' \
+    --preExec 'all:ConfigFlags.Tracking.doITkFastTracking=False' \
+    --postExec 'all:cfg.getService("AlgResourcePool").CountAlgorithmInstanceMisses = True;cfg.getEventAlgo("ActsTrkITkPixelClusterizationAlg").PixelClustersKey="xAODpixelClusters";cfg.getEventAlgo("ActsTrkITkStripClusterizationAlg").StripClustersKey="xAODstripClusters";' \
+    --inputRDOFile ${DATADIR}"/ATLAS-P2-RUN4-03-00-00/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/*" \
+    --outputAODFile 'myAOD.pool.root' \
+    --jobNumber '1'
+
