b92326848dae3160b5246f2d58870d4ddce90d56
Scott Snyder <scott.snyder@cern.ch>
2023-09-12 09:36:07 +0200

    TruthD3PDMaker: Update truthjets for xAOD EDM.
    TruthD3PDMaker: Update truthjets for xAOD EDM.
    
    Update truthjets for xAOD EDM.

--- PhysicsAnalysis/D3PDMaker/TruthD3PDMaker/python/TruthJetD3PDObject.py
+++ PhysicsAnalysis/D3PDMaker/TruthD3PDMaker/python/TruthJetD3PDObject.py
@@ -1,12 +1,10 @@
-# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 
 import EventCommonD3PDMaker
-import JetD3PDMaker
 from D3PDMakerCoreComps.D3PDObject      import make_SGDataVector_D3PDObject
 from D3PDMakerConfig.D3PDMakerFlags     import D3PDMakerFlags
-from RecExConfig.RecFlags               import rec
 
-TruthJetD3PDObject = make_SGDataVector_D3PDObject ('JetCollection',
+TruthJetD3PDObject = make_SGDataVector_D3PDObject ('DataVector<xAOD::Jet_v1>',
                                                    D3PDMakerFlags.JetSGKey(),
                                                    'truthjet_',
                                                    'TruthJetD3PDObject')
@@ -14,11 +12,4 @@ TruthJetD3PDObject = make_SGDataVector_D3PDObject ('JetCollection',
 TruthJetD3PDObject.defineBlock(0, 'Kinematics',
                                EventCommonD3PDMaker.FourMomFillerTool,
                                WriteE  = True)
-
-TruthJetD3PDObject.defineBlock(2, 'JetShape',
-                               JetD3PDMaker.JetShapeFillerTool)
-
-if rec.doTruth:
-    TruthJetD3PDObject.defineBlock(2, 'TrueFlavorComponents',
-                                   JetD3PDMaker.JetTrueTagFillerTool)
 
