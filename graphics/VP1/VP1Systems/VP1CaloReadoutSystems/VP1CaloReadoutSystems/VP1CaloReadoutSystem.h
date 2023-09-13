c618872cdf61a3dc5cd03267e429eb29c64d4c76
scott snyder <sss@karma>
2023-09-13 16:03:31 -0400

    VP1CaloReadoutSystems: slots -> Q_SLOTS
    Rename the Qt slots 'keyword' to Q_SLOTS.
    
    Having this in the source confuses cppcheck, which reports syntax errors.
    We can define slots as a macro expanding to nothing in the cppcheck
    configuration, but if we do that globally, there are other packages that
    break because they use slots as an identifier.  Using Q_SLOTS instead
    prevents this name collision.

--- graphics/VP1/VP1Systems/VP1CaloReadoutSystems/VP1CaloReadoutSystems/VP1CaloReadoutSystem.h
+++ graphics/VP1/VP1Systems/VP1CaloReadoutSystems/VP1CaloReadoutSystems/VP1CaloReadoutSystem.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef VP1CALOREADOUTSYSTEM_H
@@ -39,7 +39,7 @@ class VP1CaloReadoutSystem: public IVP13DSystemSimple
   QByteArray saveState();
   void restoreFromState(QByteArray ba);
 
-  protected slots:
+  protected Q_SLOTS:
     void checkboxChanged();
     void checkboxChanged(QCheckBox*cb);
     void phiSectorTurnon(int);
