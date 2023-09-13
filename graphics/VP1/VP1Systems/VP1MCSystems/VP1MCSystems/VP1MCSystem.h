62f8c40073a736ada1e3239fd5b806fb5fd926a9
scott snyder <sss@karma>
2023-09-13 15:48:00 -0400

    VP1MCSystems: slots -> Q_SLOTS
    Rename the Qt slots 'keyword' to Q_SLOTS.
    
    Having this in the source confuses cppcheck, which reports syntax errors.
    We can define slots as a macro expanding to nothing in the cppcheck
    configuration, but if we do that globally, there are other packages that
    break because they use slots as an identifier.  Using Q_SLOTS instead
    prevents this name collision.

--- graphics/VP1/VP1Systems/VP1MCSystems/VP1MCSystems/VP1MCSystem.h
+++ graphics/VP1/VP1Systems/VP1MCSystems/VP1MCSystems/VP1MCSystem.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef VP1MCSYSTEM_H
@@ -39,7 +39,7 @@ public:
 
   void setTree(QTreeWidget *tw);
 
-  public slots:
+  public Q_SLOTS:
 
   void addParticle();
   void removeParticle();
