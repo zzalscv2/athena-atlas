fd1335383db361a3d3c9e32128bd176e7e7cda84
scott snyder <sss@karma>
2023-09-13 15:46:20 -0400

    VP1CaloClusterSystems: slots -> Q_SLOTS
    Rename the Qt slots 'keyword' to Q_SLOTS.
    
    Having this in the source confuses cppcheck, which reports syntax errors.
    We can define slots as a macro expanding to nothing in the cppcheck
    configuration, but if we do that globally, there are other packages that
    break because they use slots as an identifier.  Using Q_SLOTS instead
    prevents this name collision.

--- graphics/VP1/VP1Systems/VP1CaloClusterSystems/VP1CaloClusterSystems/VP1CaloClusterCollection.h
+++ graphics/VP1/VP1Systems/VP1CaloClusterSystems/VP1CaloClusterSystems/VP1CaloClusterCollection.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 
@@ -51,7 +51,7 @@ protected:
   QString provideSection() const;
 signals:
   void highestVisibleClusterEnergyChanged();
-private slots:
+private Q_SLOTS:
   void setConsiderTransverseEnergies(bool);
   void setScale(const QPair<bool,double>&);
   void setAllowedEnergies(const VP1Interval&);
