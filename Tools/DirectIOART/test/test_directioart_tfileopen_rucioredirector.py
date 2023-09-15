57f1c00ad98e2b5717d4ff345f99a5f1a5dbc462
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2023-09-15 15:12:52 +0200

    Update input file of redirector test to use AOD from hc_test.aft dataset

--- Tools/DirectIOART/test/test_directioart_tfileopen_rucioredirector.py
+++ Tools/DirectIOART/test/test_directioart_tfileopen_rucioredirector.py
@@ -10,31 +10,31 @@ import ROOT
 ROOT.gEnv.SetValue("Davix.Debug", 3.0)
 
 ### no redirection option ###
-f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc12_8TeV/AOD.00983781._000001.pool.root.1")
+f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc15_13TeV/AOD.05536542._000001.pool.root.1")
 if f: print("art-result: 0 DirectIOART_TFileOpen_RucioRedirect_noOpts")
 else: print("art-result: 1 DirectIOART_TFileOpen_RucioRedirect_noOpts")
 del f
 
 ### schemes=root ###
-f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc12_8TeV/AOD.00983781._000001.pool.root.1?schemes=root")
+f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc15_13TeV/AOD.05536542._000001.pool.root.1?schemes=root")
 if f: print("art-result: 0 DirectIOART_TFileOpen_RucioRedirect_schemesROOT")
 else: print("art-result: 1 DirectIOART_TFileOpen_RucioRedirect_schemesROOT")
 del f
 
 ### schemes=davs ###
-f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc12_8TeV/AOD.00983781._000001.pool.root.1?schemes=davs")
+f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc15_13TeV/AOD.05536542._000001.pool.root.1?schemes=davs")
 if f: print("art-result: 0 DirectIOART_TFileOpen_RucioRedirect_schemesDAVS")
 else: print("art-result: 1 DirectIOART_TFileOpen_RucioRedirect_schemesDAVS")
 del f
 
 ### select=goeip ###
-f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc12_8TeV/AOD.00983781._000001.pool.root.1?select=geoip")
+f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc15_13TeV/AOD.05536542._000001.pool.root.1?select=geoip")
 if f: print("art-result: 0 DirectIOART_TFileOpen_RucioRedirect_selectGEOIP")
 else: print("art-result: 1 DirectIOART_TFileOpen_RucioRedirect_selectGEOIP")
 del f
 
 ### rse=LRZ-LMU_DATADISK ###
-f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc12_8TeV/AOD.00983781._000001.pool.root.1?rse=LRZ-LMU_DATADISK")
+f = ROOT.TFile.Open("https://rucio-lb-prod.cern.ch/redirect/mc15_13TeV/AOD.05536542._000001.pool.root.1?rse=LRZ-LMU_DATADISK")
 if f: print("art-result: 0 DirectIOART_TFileOpen_RucioRedirect_rseLRZ-LMU_DATADISK")
 else: print("art-result: 1 DirectIOART_TFileOpen_RucioRedirect_rseLRZ-LMU_DATADISK")
 del f
