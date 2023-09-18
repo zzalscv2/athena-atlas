39dc66419599c56d4cdbc555e1f1d9d8b06768ed
Carlo Varni <carlo.varni@cern.ch>
2023-09-18 20:35:43 +0200

    Add art test for acts with ttbar pu200
    Add art test for acts with ttbar pu200

---
+++ InnerDetector/InDetValidation/InDetPhysValMonitoring/test/test_run4_acts_ckf_ttbar_PU200.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# art-description: Run 4 configuration, ITK only recontruction with ACTS, PU 200
+# art-input: mc21_14TeV:mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700
+# art-input-nfiles: 1
+# art-type: grid
+# art-include: main/Athena
+# art-output: *.root
+# art-output: *.xml
+# art-output: dcube*
+# art-html: dcube_ambi_last
+# art-memory: 6144
+
+lastref_dir=last_results
+dcubeXml=dcube_IDPVMPlots_ACTS_CKF_ITk.xml
+n_events=100
+
+# search in $DATAPATH for matching file
+dcubeXmlAbsPath=$(find -H ${DATAPATH//:/ } -mindepth 1 -maxdepth 1 -name $dcubeXml -print -quit 2>/dev/null)
+# Don't run if dcube config not found
+if [ -z "$dcubeXmlAbsPath" ]; then
+    echo "art-result: 1 dcube-xml-config"
+    exit 1
+fi
+
+run () {
+    name="${1}"
+    cmd="${@:2}"
+    ############
+    echo "Running ${name}..."
+    time ${cmd}
+    rc=$?
+    # Only report hard failures for 21.9 vs master tests since both
+    # branches are unlikely to ever match perfectly
+    [ "${name}" = "dcube-ckf-ambi" ] && [ $rc -ne 255 ] && rc=0
+    echo "art-result: $rc ${name}"
+    return $rc
+}
+
+ignore_pattern="ActsTrackFindingAlg.+ERROR.+Propagation.+reached.+the.+step.+count.+limit,ActsTrackFindingAlg.+ERROR.+Propapation.+failed:.+PropagatorError:3.+Propagation.+reached.+the.+configured.+maximum.+number.+of.+steps.+with.+the.+initial.+parameters"
+
+# Run w/o ambi. resolution
+run "Reconstruction-ckf" \
+    Reco_tf.py --CA \
+    --steering doRAWtoALL \
+    --preInclude "InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude,ActsConfig.ActsCIFlags.actsValidateTracksFlags" \
+    --ignorePatterns "${ignore_pattern}" \
+    --inputRDOFile ${ArtInFile} \
+    --outputAODFile AOD.ckf.root \
+    --maxEvents ${n_events}
+
+reco_rc=$?
+if [ $reco_rc != 0 ]; then
+    exit $reco_rc
+fi
+
+run "IDPVM-ckf" \
+    runIDPVM.py \
+    --filesInput AOD.ckf.root \
+    --outputFile idpvm.ckf.root
+
+reco_rc=$?
+if [ $reco_rc != 0 ]; then
+    exit $reco_rc
+fi
+
+# Run w/ ambi. resolution
+run "Reconstruction-ambi" \
+    Reco_tf.py --CA \
+    --steering doRAWtoALL \
+    --preInclude "InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude,ActsConfig.ActsCIFlags.actsValidateAmbiguityResolutionFlags" \
+    --ignorePatterns "${ignore_pattern}" \
+    --inputRDOFile ${ArtInFile} \
+    --outputAODFile AOD.ambi.root \
+    --perfmon fullmonmt \
+    --maxEvents ${n_events}
+
+reco_rc=$?
+if [ $reco_rc != 0 ]; then
+    exit $reco_rc
+fi
+
+run "IDPVM-ambi" \
+    runIDPVM.py \
+    --filesInput AOD.ambi.root \
+    --outputFile idpvm.ambi.root
+
+reco_rc=$?
+if [ $reco_rc != 0 ]; then
+    exit $reco_rc
+fi
+
+echo "download latest result..."
+art.py download --user=artprod --dst="$lastref_dir" "$ArtPackage" "$ArtJobName"
+ls -la "$lastref_dir"
+
+run "dcube-ckf-last" \
+    $ATLAS_LOCAL_ROOT/dcube/current/DCubeClient/python/dcube.py \
+    -p -x dcube_ckf_last \
+    -c ${dcubeXmlAbsPath} \
+    -r ${lastref_dir}/idpvm.ckf.root \
+    idpvm.ckf.root
+
+run "dcube-ambi-last" \
+    $ATLAS_LOCAL_ROOT/dcube/current/DCubeClient/python/dcube.py \
+    -p -x dcube_ambi_last \
+    -c ${dcubeXmlAbsPath} \
+    -r ${lastref_dir}/idpvm.ambi.root \
+    idpvm.ambi.root
+
+# Compare performance w/ and w/o ambi. resolution
+run "dcube-ckf-ambi" \
+    $ATLAS_LOCAL_ROOT/dcube/current/DCubeClient/python/dcube.py \
+    -p -x dcube_ckf_ambi \
+    -c ${dcubeXmlAbsPath} \
+    -r idpvm.ckf.root \
+    idpvm.ambi.root
