c94e584bdec1e8ea4b3c044e62d85ac330f85f26
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2023-09-20 18:01:33 +0200

    Fix crashes in ParticleJetTools::childrenRemoved function
    Fix crashes in ParticleJetTools::childrenRemoved function

--- PhysicsAnalysis/AnalysisCommon/ParticleJetTools/Root/ParticleJetLabelCommon.cxx
+++ PhysicsAnalysis/AnalysisCommon/ParticleJetTools/Root/ParticleJetLabelCommon.cxx
@@ -31,83 +31,52 @@ namespace ParticleJetTools {
   // minimal modification
   // --------------------------------------------------------------
 
-    // TODO
-    // can we do better by only looking at hadrons?
-    inline bool isChild
-        ( const xAOD::TruthParticle* p
-        , const xAOD::TruthParticle* c
-        ) {
+  // TODO
+  // can we do better by only looking at hadrons?
+  inline bool isChild
+  ( const xAOD::TruthParticle* p
+    , const xAOD::TruthParticle* c
+    ) {
 
-        if (p->barcode() == c->barcode())
-            return false;
+    if (p->barcode() == c->barcode()) { return false; }
 
+    for (size_t iC = 0; iC < p->nChildren(); iC++) {
+      const xAOD::TruthParticle* cc = p->child(iC);
+      if (!cc) { continue; }
 
-        for (size_t iC = 0; iC < p->nChildren(); iC++) {
-            const xAOD::TruthParticle* cc = p->child(iC);
-	    if(!cc) continue;
+      if (cc->barcode() == c->barcode()) { return true; }
 
-            if (cc->barcode() == c->barcode()) {
-                return true;
-            }
-
-            if (isChild(cc, c)) {
-                return true;
-            }
-        }
-
-        return false;
+      if (isChild(cc, c)) { return true; }
     }
 
+    return false;
+  }
 
-    void childrenRemoved
-        ( const std::vector<const xAOD::TruthParticle*>& parents
-        , std::vector<const xAOD::TruthParticle*>& children
-        ) {
 
-        // TODO
-        // this is probably very inefficient,
-        // but it's simple.
+  void childrenRemoved
+  ( const std::vector<const xAOD::TruthParticle*>& parents
+    , std::vector<const xAOD::TruthParticle*>& children
+    ) {
 
-        // for instance: if we remove a child from info on one parent,
-        // we still loop over the child again for the next parent.
-
-        // also, we're passing around vectors rather than their
-        // references.
-
-        // for each of the parents
-        for ( size_t ip = 0
-            ; ip != parents.size()
-            ; ip++ ) {
-
-            const xAOD::TruthParticle* p = parents[ip];
-	    if(!p) continue;
-
-            // the current child index
-            size_t ic = 0;
-
-            // (x) each of the potential children
-            while (ic != children.size()) {
-
-                const xAOD::TruthParticle* c = children[ic];
-		if (!c) continue;
-
-                // if citer is (recursively) a child of piter
-                // remove it.
-                if (isChild(p, c)) {
-                    children.erase(children.begin() + ic);
-                    // do not increment ic: we just removed a child.
-                    continue;
-
-                } else {
-                    // increment ic: we did *not* remove a child.
-                    ic++;
-                    continue;
-                }
-            }
-        }
-
-        return;
+    if ( &parents == &children ) {
+      // Same vector provided for both inputs. Extra care needed in
+      // this case...
+      const std::vector<const xAOD::TruthParticle*> copyParents = parents;
+      childrenRemoved(copyParents, children);
+      return;
     }
+    // We can now safely assume that parents will not be modified
+    // during this loop.
+    for ( const xAOD::TruthParticle* p : parents ) {
+      if (!p) continue;
+      children.erase(std::remove_if(children.begin(),
+                                    children.end(),
+                                    [p](const xAOD::TruthParticle* c) { return (c && isChild(p, c)); }),
+                     children.end());
+      // auto erased = std::erase_if(children, [p](const xAOD::TruthParticle* c) { return (c && isChild(p, c)); }); // C++20
+    }
+    return;
+  }
 
 
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
