cea9e7912f6bae2d22a3eba983d11b17a952d7f8
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2023-09-28 13:00:45 +0200

    NRPCRDO - Update decorator naming
    NRPCRDO - Update decorator naming

--- Event/xAOD/xAODMuonRDO/Root/NRPCRDOAuxContainer_v1.cxx
+++ Event/xAOD/xAODMuonRDO/Root/NRPCRDOAuxContainer_v1.cxx
@@ -3,16 +3,27 @@
 */
 
 #include "xAODMuonRDO/versions/NRPCRDOAuxContainer_v1.h"
+namespace {
+   static const std::string preFixStr{"MuonRdo_"};
+}
+#define RDO_AUXVARIABLE(VAR) \
+   do { \
+      static const std::string varName =preFixStr+#VAR; \
+      static const auxid_t auxid = getAuxID(varName, VAR); \
+      regAuxVar(auxid, varName, VAR); \
+    } while (false);
+
 
 namespace xAOD {
     NRPCRDOAuxContainer_v1::NRPCRDOAuxContainer_v1()
     : AuxContainerBase() {
-         AUX_VARIABLE(bcid);
-         AUX_VARIABLE(time);
-         AUX_VARIABLE(subdetector);
-         AUX_VARIABLE(tdcsector);
-         AUX_VARIABLE(tdc);
-         AUX_VARIABLE(channel);
-         AUX_VARIABLE(timeoverthr);
+         RDO_AUXVARIABLE(bcid);
+         RDO_AUXVARIABLE(time);
+         RDO_AUXVARIABLE(subdetector);
+         RDO_AUXVARIABLE(tdcsector);
+         RDO_AUXVARIABLE(tdc);
+         RDO_AUXVARIABLE(channel);
+         RDO_AUXVARIABLE(timeoverthr);
     }
 }
+#undef RDO_AUXVARIABLE
