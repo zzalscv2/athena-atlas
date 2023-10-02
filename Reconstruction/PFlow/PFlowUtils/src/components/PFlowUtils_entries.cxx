9027b0b452bce8a3e2c42bfa2f6dcc21c872d744
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2023-10-02 13:14:20 +0200

    Migrate PFlow CPData Decoratiing
    Migrate PFlow CPData Decoratiing

--- Reconstruction/PFlow/PFlowUtils/src/components/PFlowUtils_entries.cxx
+++ Reconstruction/PFlow/PFlowUtils/src/components/PFlowUtils_entries.cxx
@@ -1,13 +1,13 @@
 #include "PFlowUtils/WeightPFOTool.h"
 
 #if !defined(XAOD_ANALYSIS)
-#include "../CombinePFO.h"
+#include "../PFlowCellCPDataDecoratorAlgorithm.h"
 #include "../PFlowCalibPFODecoratorAlgorithm.h"
 #endif
 
 DECLARE_COMPONENT( CP::WeightPFOTool )
 
 #if !defined(XAOD_ANALYSIS)
-DECLARE_COMPONENT( CombinePFO )
+DECLARE_COMPONENT( PFlowCellCPDataDecoratorAlgorithm )
 DECLARE_COMPONENT( PFlowCalibPFODecoratorAlgorithm )
 #endif
