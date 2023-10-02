e737ee0f93814938a5f26a3c61789bc54fb60ba6
Jiri Masik <jiri.masik@cern.ch>
2023-10-02 14:13:37 +0200

    BjetTrackingConfig migrated to use InDetTrigSequence directly
    BjetTrackingConfig migrated to use InDetTrigSequence directly

--- Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Bjet/BjetTrackingConfig.py
+++ Trigger/TriggerCommon/TriggerMenuMT/python/HLT/Bjet/BjetTrackingConfig.py
@@ -2,15 +2,23 @@
 
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
 from AthenaConfiguration.ComponentFactory import CompFactory
+from AthenaCommon.Logging import logging
 
 def secondStageBjetTrackingCfg(flags, inputRoI: str, inputVertex: str, inputJets: str) -> ComponentAccumulator:
-    acc = ComponentAccumulator()
 
-    from TrigInDetConfig.TrigInDetConfig import trigInDetFastTrackingCfg
-    acc.merge(trigInDetFastTrackingCfg(flags, inputRoI, signatureName="bjet"))
+    log = logging.getLogger("BJetTrackingConfig")
+    
+    #safety measure to ensure we get the right instance of flags
+    from TrigInDetConfig.utils import getFlagsForActiveConfig
+    trkflags = getFlagsForActiveConfig(flags, "bjet", log)
 
-    from TrigInDetConfig.TrigInDetConfig import trigInDetPrecisionTrackingCfg
-    acc.merge(trigInDetPrecisionTrackingCfg(flags, inputRoI, signatureName="bjet"))
+    from TrigInDetConfig.InDetTrigSequence import InDetTrigSequence
+    seq = InDetTrigSequence(trkflags, 
+                            trkflags.Tracking.ActiveConfig.input_name, 
+                            rois   = inputRoI,
+                            inView = "VDVInDetFTF")
+    acc = seq.sequence("FastTrackFinder")
+    acc.merge(seq.sequenceAfterPattern())
 
     verifier = CompFactory.AthViews.ViewDataVerifier(name = 'VDVsecondStageBjetTracking',
                                                      DataObjects = [('xAOD::VertexContainer', f'StoreGateSvc+{inputVertex}'),
