60268f7787e3a0d0e1ce01a698839914fbec0361
Binbin Dong <binbin.dong@cern.ch>
2023-10-10 14:40:02 +0200

    Added unit test for btagging tools
    Added unit test for btagging tools

--- PhysicsAnalysis/JetTagging/JetTagPerformanceCalibration/xAODBTaggingEfficiency/util/BTaggingEfficiencyToolTester.cxx
+++ PhysicsAnalysis/JetTagging/JetTagPerformanceCalibration/xAODBTaggingEfficiency/util/BTaggingEfficiencyToolTester.cxx
@@ -1,12 +1,6 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
-
-
-//#ifdef XAOD_STANDALONE
-#   include "xAODRootAccess/TStore.h"
-//#endif // XAOD_STANDALONE
-
 #include <AsgTools/StandaloneToolHandle.h>
 #include "FTagAnalysisInterfaces/IBTaggingEfficiencyTool.h"
 
@@ -14,90 +8,89 @@
 #include <iomanip>
 
 using CP::CorrectionCode;
+ANA_MSG_HEADER(testBTagEfficiency)
+ANA_MSG_SOURCE(testBTagEfficiency, "BtaggingEfficiencyToolTester")
+using namespace testBTagEfficiency;
 
-int main() {
-  bool retval = true;
-  xAOD::TStore store;
-  
+int main(int argc, char* argv[]) {
 
-  std::string taggerName = "DL1";
-  std::string workingPointName = "FixedCutBEff_70";
-
-  asg::StandaloneToolHandle<IBTaggingEfficiencyTool> tool("BTaggingEfficiencyTool/BTagEffTest");
-  StatusCode code1 = tool.setProperty("ScaleFactorFileName","xAODBTaggingEfficiency/13TeV/2019-21-13TeV-MC16-CDI-2019-10-07_v1.root" );
-  StatusCode code2 = tool.setProperty("TaggerName",    taggerName  );
-  StatusCode code3 = tool.setProperty("OperatingPoint", workingPointName);
-  StatusCode code4 = tool.setProperty("JetAuthor",      "AntiKt4EMPFlowJets_BTagging201810" );
-  
-  //provide a file which tells the tool which efficiency maps to use, and connects sample DSIDs to the right efficiency map.
-  //you can find a tool to automatically create this config file here: https://gitlab.cern.ch/mstamenk/automate-hadronisation-information
-  StatusCode code5 = tool.setProperty("EfficiencyConfig","/afs/cern.ch/work/j/jshlomi/public/example_eff_config.txt");
-  
-  // specify an aggressive ("Tight") or not-so-aggressive ("Medium") eigenvector suppression scheme
-  // tool.setProperty("EigenvectorReductionB", "Medium");
-  // tool.setProperty("EigenvectorReductionC", "Medium");
-  // tool.setProperty("EigenvectorReductionLight", "Medium");
-  
-  // uncomment this if the "Envelope" systematics model is to be used instead of the eigenvector variations
-  // code = tool.setProperty("SystematicsStrategy", "Envelope");
-
-  StatusCode code6 = tool.initialize();
-
-  if (code1 != StatusCode::SUCCESS || code2 != StatusCode::SUCCESS || code3 != StatusCode::SUCCESS || code4 != StatusCode::SUCCESS 
-    || code5 != StatusCode::SUCCESS 
-    || code6 != StatusCode::SUCCESS) {
-    std::cout << "Initialization of tool " << tool->name() << " failed! " << std::endl;
-    return -1;
-  }
-  else {
-    std::cout << "Initialization of tool " << tool->name() << " finished." << std::endl;
+  const char* TEST_NAME = argv[0];
+  if (argc < 3) {
+    ANA_MSG_ERROR ( "No right inputs received!" );
+    ANA_MSG_ERROR ( "Usage: " << TEST_NAME << "[CDI path] [b-tagger name] [WP name]" );
+    return 1;
   }
 
-
+  std::string CDIPath = argv[1];
+  std::string taggerName = argv[2];
+  std::string workingPointName = argv[3];
+  std::string JetCollectionName = "AntiKt4EMPFlowJets";
   // select your efficiency map based on the DSID of your sample:
   unsigned int sample_dsid = 410470;
 
+  asg::StandaloneToolHandle<IBTaggingEfficiencyTool> tool("BTaggingEfficiencyTool/BTagEffTest");
+  StatusCode code1 = tool.setProperty("ScaleFactorFileName", CDIPath);
+  StatusCode code2 = tool.setProperty("TaggerName",          taggerName);
+  StatusCode code3 = tool.setProperty("OperatingPoint",      workingPointName);
+  StatusCode code4 = tool.setProperty("JetAuthor",           JetCollectionName);
+  StatusCode code5 = tool.setProperty("MinPt",               20000);
+  StatusCode code6 = tool.setProperty("OutputLevel",         MSG::WARNING);
+  StatusCode code7 = tool.initialize();
+  std::vector<StatusCode> codes = {code1, code2, code3, code4, code5, code6, code7};
+  for (const auto& code : codes) {
+    if (code != StatusCode::SUCCESS) {
+      ANA_MSG_ERROR ( "Initialization of tool " << tool->name() << " failed! ");
+      return 1;
+    }
+  }
+  ANA_MSG_INFO("Initialization of tool " << tool->name() << " finished.");
+  
   tool->setMapIndex(sample_dsid);
 
-
-  std::cout << "-----------------------------------------------------" << std::endl;
+  ANA_MSG_INFO( "-----------------------------------------------------");
   const std::map<CP::SystematicVariation, std::vector<std::string> > allowed_variations = tool->listSystematics();
-  std::cout << "Allowed systematics variations for tool " << tool->name() << ":" << std::endl;
+  ANA_MSG_INFO("Allowed systematics variations for tool " << tool->name() << ":");
   for (auto var : allowed_variations) {
-    std::cout << std::setw(40) << std::left << var.first.name() << ":";
-    for (auto flv : var.second) std::cout << " " << flv;
-    std::cout << std::endl;
+    ANA_MSG_INFO( std::setw(40) << std::left << var.first.name() << ":");
+    for (auto flv : var.second) 
+        ANA_MSG_INFO( " " << flv);
   }
-  std::cout << "-----------------------------------------------------" << std::endl;
+  ANA_MSG_DEBUG( "-----------------------------------------------------");
   
 
-
-  std::cout << "Creating a jet" << std::endl;
+  ANA_MSG_DEBUG( "Creating a jet");
   xAOD::JetFourMom_t p4(50000.,0.7,0.3,1000.);
 
   xAOD::Jet * jet = new xAOD::Jet();
   jet->makePrivateStore();
-  std::cout << "Setting jet 4 momentum" << std::endl;
+  ANA_MSG_DEBUG( "Setting jet 4 momentum");
   jet->setJetP4(p4);
-  std::cout << "Setting jet attribute" << std::endl;
-  // b jet (label==5)
+  ANA_MSG_DEBUG("Setting jet attribute");
   jet->setAttribute("HadronConeExclTruthLabelID", 5);
   float sf=0;
   float eff=0;
   CorrectionCode result;
-  std::cout << "Testing function calls without systematics..." << std::endl;
+  ANA_MSG_DEBUG( "Testing function calls without systematics...");
+
   result = tool->getEfficiency(*jet,eff);
-  if( result!=CorrectionCode::Ok) { std::cout << "b jet get efficiency failed"<<std::endl; retval=false;}
+  if( result!=CorrectionCode::Ok) { 
+    ANA_MSG_ERROR("b jet get efficiency failed!"); 
+    return 1;
+  }
   else {
-    std::cout << "b jet get efficiency succeeded: " << eff << std::endl;
+    ANA_MSG_DEBUG( "b jet get efficiency succeeded: " << eff );
   }
   result = tool->getScaleFactor(*jet,sf);
-  if( result!=CorrectionCode::Ok) { std::cout << "b jet get scale factor failed"<<std::endl; retval=false;}
+
+  if( result!=CorrectionCode::Ok) { 
+    ANA_MSG_ERROR("b jet get scale factor failed"); 
+    return 1;
+  }
   else {
-    std::cout << "b jet get scale factor succeeded: " << sf << std::endl;
+    ANA_MSG_INFO( "b jet get scale factor succeeded: " << sf );
   }
 
-  std::cout << "Testing function calls with systematics..." << std::endl;
+  ANA_MSG_DEBUG( "Testing function calls with systematics...");
   CP::SystematicSet systs = tool->affectingSystematics();
   for( CP::SystematicSet::const_iterator iter = systs.begin();
        iter!=systs.end(); ++iter) {
@@ -106,22 +99,21 @@ int main() {
     set.insert(var);
     StatusCode sresult = tool->applySystematicVariation(set);
     if( sresult !=StatusCode::SUCCESS) {
-      std::cout << var.name() << " apply systematic variation FAILED " << std::endl;
+      ANA_MSG_ERROR( var.name() << " apply systematic variation FAILED ");
   }
     result = tool->getScaleFactor(*jet,sf);
     if( result!=CorrectionCode::Ok) {
-      std::cout << var.name() << " getScaleFactor FAILED" << std::endl;
+      ANA_MSG_ERROR( var.name() << " getScaleFactor FAILED");
     } else {
-      std::cout << var.name() << " " << sf << std::endl;
+      ANA_MSG_DEBUG( var.name() << " " << sf);
     }
   }
+
   // don't forget to switch back off the systematics...
   CP::SystematicSet defaultSet;
   StatusCode dummyResult = tool->applySystematicVariation(defaultSet);
   if (dummyResult != StatusCode::SUCCESS)
-    std::cout << "problem disabling systematics setting!" << std::endl;
+    ANA_MSG_ERROR( "problem disabling systematics setting!");
 
-
-
-  return retval;
+  return 0;
 }
