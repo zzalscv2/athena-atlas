84f87cb714e015f0b84059c60d4759d9433e6d7a
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-10-10 16:06:11 +0200

    AthenaConfiguration: cleanup Deduplication
    Cleanup the `Deduplication` code:
     - update documentation
     - cleanup code
     - remove unused feature to deduplicate into `svcMgr`

--- Control/AthenaConfiguration/python/Deduplication.py
+++ Control/AthenaConfiguration/python/Deduplication.py
@@ -1,57 +1,56 @@
-# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 #
-# Functions used by the ComponentAccumulator to de-duplicate componentes defined multiple times
+# Functions used by the ComponentAccumulator to de-duplicate components.
 #
 
-from AthenaCommon.Logging import logging
 from AthenaConfiguration.DebuggingContext import raiseWithCurrentContext
-
-_msg=logging.getLogger('ComponentAccumulator') #'Deduplication' would the better name but breaks tons of unit-test log comparison
-
+from AthenaCommon.Logging import logging
 
 class DeduplicationFailed(RuntimeError):
     pass
 
-def deduplicate(newComp,compList):
-    #Check for duplicates:
+_msg = logging.getLogger('ComponentAccumulator')
 
-    for idx,comp in enumerate(compList):
+
+def deduplicate(newComp, compList):
+    """Merge newComp with compList. If a matching component is found it is updated
+    in compList. Otherwise a new component is added to compList.
+
+    Returns True if a new component has been added, otherwise False. If merging fails,
+    an exception is raised.
+    """
+
+    for idx, comp in enumerate(compList):
         if comp.__cpp_type__==newComp.__cpp_type__ and comp.name==newComp.name:
             exception = None
             try:
                 newComp.merge(comp)
             except Exception as e:
-                exception = e # the exception is not rised here to avoid python complaining that exception is raised while handling other one
+                exception = e  # remember exception and raise again outside the handler
             if exception:
                 raiseWithCurrentContext(exception)
-            #We found a service of the same type and name and could reconcile the two instances
-            _msg.debug("Reconciled configuration of component %s", comp.name)
-            #_deduplicated worked on 'newComp'. Overwrite the component in the list with the new, merged component
-            compList[idx]=newComp
-            return False #False means nothing got added
-        #end if same name & type
-    #end loop over existing components
 
-    #No component of the same type & name found, simply append
+            # We found a service of the same type and name and could reconcile the two instances.
+            # Overwrite the component in the list with the new (merged) component.
+            _msg.verbose("Reconciled configuration of component %s", comp.name)
+            compList[idx] = newComp
+            return False
+
+    # No component of the same type and name found, simply append
     _msg.debug("Adding component %s/%s to the job", newComp.__cpp_type__, newComp.name)
+    compList.append(newComp)
+    return True
 
-    #The following is to work with internal list of service as well as gobal svcMgr as second parameter
-    try:
-        compList.append(newComp)
-    except Exception:
-        compList+=newComp
-        pass
-    return True #True means something got added
 
 def deduplicateOne(newComp, oldComp):
+    """Merge newComp with oldComp and provide diagnostics on failure."""
+
     exception = None
     try:
-        assert oldComp.__cpp_type__ == newComp.__cpp_type__ , "Deduplicating components of different type"
-        assert oldComp.name == newComp.name, "Deduplicating components of differnet name"
+        assert oldComp.__cpp_type__ == newComp.__cpp_type__, "Deduplicating components of different type"
+        assert oldComp.name == newComp.name, "Deduplicating components of different name"
         oldComp.merge(newComp)
     except Exception as e:
-        exception = e # the exception is not rised here to avoid python complaining that exception is raised while handling other one
+        exception = e  # remember exception and raise again outside the handler
     if exception:
         raiseWithCurrentContext(exception)
-
-    
