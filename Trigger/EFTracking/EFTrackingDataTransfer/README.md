a653fcc14b55aae3f729f6215f5c7b1487b1dce2
Tomasz Bold <tomasz.bold@cern.ch>
2023-10-11 19:47:43 +0200

    Package to house EFTracking related utilities for data transfer
    Package to house EFTracking related utilities for data transfer

---
+++ Trigger/EFTracking/EFTrackingDataTransfer/README.md
@@ -0,0 +1,6 @@
+# Set of utilities to facilitate data transfer operations that are EFTracking specific
+
+# Benchmarks
+The package contains also a set of benchmark programs, they exercise various operations related to the data transfer.
+
+
---
+++ Trigger/EFTracking/EFTrackingDataTransfer/test/TransferBenchmark_test.cxx
@@ -0,0 +1,12 @@
+/*
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+*/
+#undef NDEBUG
+
+
+#include "Acts/EventData/Measurement.hpp"
+#include <iostream>
+
+int main() {
+  std::cout << "Start of data transfer speed test" << std::endl;
+}
