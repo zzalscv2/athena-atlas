cf579ecc51e343104ab977f32b6a4bbbc579547b
Dan Guest <daniel.hay.guest@cern.ch>
2023-10-16 11:29:16 +0200

    H5 Writer: allow double -> half
    H5 Writer: allow double -> half

--- PhysicsAnalysis/AnalysisCommon/HDF5Utils/HDF5Utils/CompressedTypes.h
+++ PhysicsAnalysis/AnalysisCommon/HDF5Utils/HDF5Utils/CompressedTypes.h
@@ -16,16 +16,26 @@
 namespace H5Utils {
 
   namespace internal {
+
+    H5::DataType halfPrecisionFloat(int ebias = 15);
+
     template <typename T>
     H5::DataType getCompressedType(Compression comp) {
+      if constexpr (std::is_floating_point<T>::value) {
+        switch (comp) {
+          case Compression::STANDARD: return H5Traits<T>::type;
+          case Compression::HALF_PRECISION: return halfPrecisionFloat();
+          case Compression::HALF_PRECISION_LARGE: return halfPrecisionFloat(5);
+          default: throw std::logic_error("unknown float compression");
+        }
+      }
       if (comp != Compression::STANDARD) {
         throw std::logic_error("compression not supported for this type");
       }
       return H5Traits<T>::type;
     }
-    template <>
-    H5::DataType getCompressedType<float>(Compression comp);
-  }
-}
+
+  } // end internal
+}   // end H5Utils
 
 #endif
--- PhysicsAnalysis/AnalysisCommon/HDF5Utils/Root/CompressedTypes.cxx
+++ PhysicsAnalysis/AnalysisCommon/HDF5Utils/Root/CompressedTypes.cxx
@@ -5,33 +5,24 @@
 #include "HDF5Utils/CompressedTypes.h"
 
 namespace {
-  H5::DataType halfPrecisionFloat(int ebias = 15) {
-    // start with native float
-    H5::FloatType type(H5Tcopy(H5::PredType::NATIVE_FLOAT.getId()));
-
-    // These definitions are copied from h5py, see:
-    //
-    //  https://github.com/h5py/h5py/blob/596748d52c351258c851bb56c8df1c25d3673110/h5py/h5t.pyx#L212-L217
-    //
-    type.setFields(15, 10, 5, 0, 10);
-    type.setSize(2);
-    type.setEbias(ebias);
-    return type;
-  }
 }
 
 namespace H5Utils {
 
   namespace internal {
 
-    template <>
-    H5::DataType getCompressedType<float>(Compression comp) {
-      switch (comp) {
-      case Compression::STANDARD: return H5Traits<float>::type;
-      case Compression::HALF_PRECISION: return halfPrecisionFloat();
-      case Compression::HALF_PRECISION_LARGE: return halfPrecisionFloat(5);
-      default: throw std::logic_error("unknown float compression");
-      }
+    H5::DataType halfPrecisionFloat(int ebias) {
+      // start with native float
+      H5::FloatType type(H5Tcopy(H5::PredType::NATIVE_FLOAT.getId()));
+
+      // These definitions are copied from h5py, see:
+      //
+      //  https://github.com/h5py/h5py/blob/596748d52c351258c851bb56c8df1c25d3673110/h5py/h5t.pyx#L212-L217
+      //
+      type.setFields(15, 10, 5, 0, 10);
+      type.setSize(2);
+      type.setEbias(ebias);
+      return type;
     }
 
   }
