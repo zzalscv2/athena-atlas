b475b5c76d91be9b9cc204dff55d041183c13dd8
Carlo Varni <carlo.varni@cern.ch>
2023-10-17 09:32:10 +0200

    Bump Acts Tag to v30.2.0
    Bump Acts Tag to v30.2.0

--- Projects/Athena/build_externals.sh
+++ Projects/Athena/build_externals.sh
@@ -12,7 +12,7 @@ ATLAS_BUILDTYPE="RelWithDebInfo"
 ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=102
                         -DLCG_VERSION_POSTFIX="b_ATLAS_29"
                         -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v36r14.000/Gaudi-v36r14.000.tar.gz;URL_MD5;1cc75461a7cdc87daf1f9b6cb48725b4"
-                        -DATLAS_ACTS_SOURCE="URL;https://github.com/acts-project/acts/archive/refs/tags/v30.1.0.tar.gz;URL_MD5;17c19f75e1c1a25dfc0d018c798eb860")
+                        -DATLAS_ACTS_SOURCE="URL;https://github.com/acts-project/acts/archive/refs/tags/v30.2.0.tar.gz;URL_MD5;89271d68d2ec413f01ef7c7f1aee3ef4")
 ATLAS_EXTRA_MAKE_ARGS=()
 
 # Let "the common script" do all the heavy lifting.
