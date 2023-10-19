1a759c998d5edd9fc62a4ac09246d35e00629098
Shaun Roe <shaun.roe@cern.ch>
2023-10-19 13:52:37 +0200

    main-ITk_dev3-PixelConditionsAlgorithms
    main-ITk_dev3-PixelConditionsAlgorithms

--- InnerDetector/InDetConditions/PixelConditionsAlgorithms/src/ITkPixChargeCalibAlg.cxx
+++ InnerDetector/InDetConditions/PixelConditionsAlgorithms/src/ITkPixChargeCalibAlg.cxx
@@ -31,9 +31,12 @@ namespace{
   //default values of ITk thresholds, hard coded for now
   Thresholds 
   defaultThresholds(int bec, int layer){
-    static const Thresholds t{600, 24, 75, 0}; //value, sigma, noise, inTimeThresh
-    static const Thresholds t0{900, 36, 110, 0}; //only different for first layer of barrel
-    if (bec==0 and layer==0) return t0;
+    static const Thresholds t{600, 24, 75, 1000}; //value, sigma, noise, inTimeThresh
+    static const Thresholds ec0{600, 24, 75, 1500};
+    static const Thresholds b0{900, 36, 110, 1000}; //different for first layer of barrel
+    if (layer == 0){
+      return bec==0 ? b0 : ec0;
+    }
     return t;
   }
   
@@ -42,7 +45,19 @@ namespace{
     static const LegacyFitParameters legacyFit{14.0f, -1000.f, 8000.f};
     return legacyFit;
   }
- 
+  
+  Resolutions
+  defaultResolutions(){
+    static const Resolutions r{0.4, 0.02};
+    return r;
+  }
+  
+  EventIDRange
+  infiniteEventRange(){
+    const EventIDBase start{EventIDBase::UNDEFNUM, EventIDBase::UNDEFEVT, 0,                       0,                       EventIDBase::UNDEFNUM, EventIDBase::UNDEFNUM};
+    const EventIDBase stop {EventIDBase::UNDEFNUM, EventIDBase::UNDEFEVT, EventIDBase::UNDEFNUM-1, EventIDBase::UNDEFNUM-1, EventIDBase::UNDEFNUM, EventIDBase::UNDEFNUM};
+    return EventIDRange{start, stop};
+  }
 }
 
 
@@ -72,20 +87,12 @@ StatusCode ITkPixChargeCalibAlg::execute(const EventContext& ctx) const {
     ATH_MSG_FATAL(m_pixelDetEleCollKey.fullKey() << " is not available.");
     return StatusCode::FAILURE;
   }
-
   static constexpr std::array<InDetDD::PixelDiodeType, enum2uint(InDetDD::PixelDiodeType::N_DIODETYPES)> diodeTypes
     = {InDetDD::PixelDiodeType::NORMAL, InDetDD::PixelDiodeType::LONG, InDetDD::PixelDiodeType::GANGED, InDetDD::PixelDiodeType::LARGE};
   
-
   // Construct the output Cond Object and fill it in
   auto pChargeCalibData = std::make_unique<PixelChargeCalibCondData>(m_pixelID->wafer_hash_max());
   //
-  const EventIDBase start{EventIDBase::UNDEFNUM, EventIDBase::UNDEFEVT, 0,                       0,                       EventIDBase::UNDEFNUM, EventIDBase::UNDEFNUM};
-  const EventIDBase stop {EventIDBase::UNDEFNUM, EventIDBase::UNDEFEVT, EventIDBase::UNDEFNUM-1, EventIDBase::UNDEFNUM-1, EventIDBase::UNDEFNUM, EventIDBase::UNDEFNUM};
-  EventIDRange rangeW{start, stop};
-
-  ATH_MSG_INFO("Range of input is " << rangeW);
-  
   for (unsigned int moduleHash{}; moduleHash < m_pixelID->wafer_hash_max(); moduleHash++) {
     IdentifierHash wafer_hash = IdentifierHash(moduleHash);
     const auto & [barrel_ec, layer] = getBecAndLayer(m_pixelID, wafer_hash);
@@ -102,26 +109,10 @@ StatusCode ITkPixChargeCalibAlg::execute(const EventContext& ctx) const {
       pChargeCalibData -> setLegacyFitParameters(type, moduleHash, allDefaultFitParams);
       pChargeCalibData -> setLinearFitParameters(type, moduleHash, allDefaultLinearParams);
     }
-    pChargeCalibData -> setTotResolutions(moduleHash, std::vector<Resolutions>(numFE, {0.f, 0.f}));
+    pChargeCalibData -> setTotResolutions(moduleHash, std::vector<Resolutions>(numFE, defaultResolutions()));
   }
   
-  
-
-  // Scan over if the DB contents need to be overwritten.
-  // This is useful for threshold study. So far only threshold value.
-  for (unsigned int moduleHash{}; moduleHash < m_pixelID->wafer_hash_max(); moduleHash++) {
-    IdentifierHash wafer_hash = IdentifierHash(moduleHash);
-    const auto & [barrel_ec, layer] = getBecAndLayer(m_pixelID, wafer_hash);
-    const InDetDD::SiDetectorElement *element = elements->getDetectorElement(wafer_hash);
-    const auto & [numFE, technology] = numChipsAndTechnology(element);
-    const Thresholds & defaults = defaultThresholds(barrel_ec, layer);
-    if (defaults.value > -0.1) {
-      for (InDetDD::PixelDiodeType type : diodeTypes) {
-        pChargeCalibData -> setThresholds(type, moduleHash, std::vector<Thresholds>(numFE, defaults));
-      }
-    }
-  }
-
+  const auto & rangeW = infiniteEventRange();
   if (writeHandle.record(rangeW, std::move(pChargeCalibData)).isFailure()) {
     ATH_MSG_FATAL("Could not record PixelChargeCalibCondData " << writeHandle.key() << " with EventRange " << rangeW << " into Conditions Store");
     return StatusCode::FAILURE;
