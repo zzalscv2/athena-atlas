da2c18f0f814b0a3ef645f3e4ed2efc8324eaa3c
Joseph Earl Lambert <joseph.earl.lambert@cern.ch>
2023-10-24 19:28:52 +0200

    TextConfig: config order determined when adding blocks; sub-blocks do not need unique names
    TextConfig: config order determined when adding blocks; sub-blocks do not need to be unique
    
    Previously, the order tht blocks would be configured was determined by the order that
    the blocks appeared in the yaml file. This could lead to some strange issues when the
    order that the blocks are configured in matters. The order that the blocks are
    configured now is by default the order that the blocks are added to the TextConfig
    class instance but, this can be changed by specifying a `pos` arguement that
    corresponds to the name of a previously added algorithm.
    
    The sub-blocks must now be associated with a super-block using the `superBlocks` arguement
    when adding the config block. This can be a list or a single string. Since the sub-blocks
    are now asscoiated with the super-blcoks, the same name in the yaml file can
    correspond to different config blocks depending on the context.

--- PhysicsAnalysis/Algorithms/AnalysisAlgorithmsConfig/python/ConfigText.py
+++ PhysicsAnalysis/Algorithms/AnalysisAlgorithmsConfig/python/ConfigText.py
@@ -2,40 +2,31 @@
 #
 # @author Joseph Lambert
 
+# constant
+ROOTNAME = 'root'
+
 import yaml
-import json
 import os
 import inspect
-from collections import OrderedDict
+from collections import namedtuple
 
 from AnalysisAlgorithmsConfig.ConfigSequence import ConfigSequence
+
+# class for config block information
+node = namedtuple("ConfigBlock", ['alg', 'algName', 'options', 'defaults', 'subAlgs'])
 
 
 def readYaml(yamlPath):
     """Loads YAML file into a dictionary"""
     if not os.path.isfile(yamlPath):
         raise ValueError(f"{yamlPath} is not a file.")
-
-    def ordered_load(stream, Loader=yaml.SafeLoader, object_pairs_hook=OrderedDict):
-        class OrderedLoader(Loader):
-            pass
-        def construct_mapping(loader, node):
-            loader.flatten_mapping(node)
-            return object_pairs_hook(loader.construct_pairs(node))
-        OrderedLoader.add_constructor(
-            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-            construct_mapping)
-        return yaml.load(stream, OrderedLoader)
-
     with open(yamlPath, 'r') as f:
-        textConfig = ordered_load(f, yaml.SafeLoader)
+        textConfig = yaml.safe_load(f)
     return textConfig
 
 
-def printYaml(d, sort=True, jsonFormat=False):
+def printYaml(d, sort=False, jsonFormat=False):
     """Prints a dictionary as YAML"""
-    # convert to normal dict for printing
-    d = json.loads(json.dumps(d))
     print(yaml.dump(d, default_flow_style=jsonFormat, sort_keys=sort))
 
 
@@ -62,6 +53,7 @@ def getClassArgs(func):
     return args
 
 
+
 class TextConfig():
     """This class provides a configuration manager that is intended to allow the user to:
         - define and configure functions that return an algSequence(?) object
@@ -73,10 +65,10 @@ class TextConfig():
     """
     def __init__(self, yamlPath=None):
         self._algs = {}
+        self._order = {ROOTNAME: []}
         self._textConfig = {}
         if yamlPath is not None:
             self.loadConfig(yamlPath)
-        # dict {<algNmae>: {'alg': *func, 'options': {...}, 'defaults': {...}}}
 
 
     def loadConfig(self, yamlPath):
@@ -87,7 +79,7 @@ class TextConfig():
         return 
 
 
-    def printConfig(self, sort=True, jsonFormat=False):
+    def printConfig(self, sort=False, jsonFormat=False):
         """Print YAML configuration file."""
         if self._textConfig is None:
             raise ValueError("No configuration has been loaded.")
@@ -106,17 +98,52 @@ class TextConfig():
         return
 
 
-    def addAlgConfigBlock(self, algName, alg, defaults=None):
+    def addAlgConfigBlock(self, algName, alg, defaults=None, pos=None, superBlocks=None):
         """Add class to list of available algorithms"""
-        if algName in self._algs:
-            raise ValueError(f"{algName} has already been added.")
         if not callable(alg):
             raise ValueError(f"{algName} is not a callable.")
         if type(alg) == type(dict):
             opts = getClassArgs(alg)
         else:
             opts = getFuncArgs(alg)    
-        self._algs[algName] = {'alg':alg, 'options':opts, 'defaults':defaults}
+
+        if superBlocks is None:
+            superBlocks = [ROOTNAME]
+        elif type(superBlocks) != list:
+            superBlocks = [superBlocks]
+
+        # add new alg block to subAlgs dict of super block
+        for block in superBlocks:
+            if block not in self._order:
+                self._order[block] = []
+            order = self._order[block]
+            
+            if block == ROOTNAME:
+                algs = self._algs
+            else:
+                if block not in self._algs:
+                    raise ValueError(f"{block} not added")
+                algs = self._algs[block].subAlgs
+
+            if alg in algs:
+                raise ValueError(f"{algName} has already been added.")
+
+            # create node with alg information
+            algs[algName] = node(
+                alg=alg,
+                algName=algName,
+                options=opts,
+                defaults=defaults,
+                subAlgs={}
+            )
+            # insert into order (list)
+            if pos is None:
+                order.append(algName)
+            elif pos in order:
+                order.insert(order.index(pos), algName)
+            else:
+                raise ValueError(f"{pos} does not exit in already added config blocks")
+
         return
 
 
@@ -124,9 +151,9 @@ class TextConfig():
         """Prints algorithms exposed to configuration"""
         algs = self._algs
         for alg, algInfo in algs.items():
-            algName = algInfo['alg'].__name__
-            algOptions = algInfo['options']
-            algDefaults = algInfo['defaults']
+            algName = algInfo.alg.__name__
+            algOptions = algInfo.options
+            algDefaults = algInfo.defaults
             print(f"{alg} -> {algName}")
             if printOpts and algOptions:
                 for opt in algOptions:
@@ -177,7 +204,7 @@ class TextConfig():
             return algOptions
 
 
-        def makeConfig(funcName, funcOptions):
+        def makeConfig(funcInfo, funcOptions):
             """
             Parameters
             ----------
@@ -193,12 +220,10 @@ class TextConfig():
             """
             configSeq = ConfigSequence()
 
-            available_funcs = self._algs
-            if funcName not in available_funcs:
-                raise ValueError(f"{funcName} has not been added")
-            func = available_funcs[funcName]['alg']
+            func = funcInfo.alg
+            funcName = funcInfo.algName
             funcDefaults = getDefaultArgs(func)
-            defaults = self._algs[funcName].get('defaults', None)
+            defaults = funcInfo.defaults
 
             args = {}
             # loop over all options for the function
@@ -227,7 +252,7 @@ class TextConfig():
             return configSeq, args.keys()
             
 
-        def configureAlg(configSeq, blockName, blockConfig, containerName=None):
+        def configureAlg(configSeq, block, blockConfig, containerName=None):
             if type(blockConfig) != list:
                 blockConfig = [blockConfig]
 
@@ -238,8 +263,8 @@ class TextConfig():
                 elif containerName is not None and 'containerName' not in options:
                     options['containerName'] = containerName
                 # will check which options are associated alg and not options
-                print(f"Configuring {blockName}")
-                seq, funcOpts = makeConfig(blockName, options)
+                print(f"Configuring {block.algName}")
+                seq, funcOpts = makeConfig(block, options)
                 if not seq._blocks:
                     continue
                 algOpts = setOptions(seq, options)
@@ -247,36 +272,59 @@ class TextConfig():
 
                 # check to see if there are unused parameters 
                 algOpts = [i['name'] for i in algOpts]
-                expectedOptions = set(funcOpts) | set(algOpts)  | set(self._algs.keys())
+                expectedOptions = set(funcOpts) 
+                expectedOptions |= set(algOpts)
+                expectedOptions |= set(block.subAlgs)
+
                 difference = set(options.keys()) - expectedOptions
                 if difference:
                     difference = "\n".join(difference)
                     raise ValueError(f"There are options set that are not used for "
-                                     f"{blockName}:\n{difference}\n"
+                                     f"{block.algName}:\n{difference}\n"
                                      "Please check your configuration.")
 
 
                 # check for sub-blocks and call this function recursively
-                for opt in options: # order is preserved
-                    if opt in self._algs:
-                        subAlg = opt
-                        configureAlg(configSeq, subAlg, options[subAlg], containerName)
+                for alg in self._order.get(block.algName, []):
+                    if alg in options:
+                        subAlg = block.subAlgs[alg]
+                        configureAlg(configSeq, subAlg, options[alg], containerName)
             return configSeq
 
 
         ### configure starts here ###
-        configSeq = ConfigSequence()
+        # make sure all blocks in yaml file are added (otherwise they would be ignored)
         for blockName in self._textConfig:
+            if blockName not in self._order[ROOTNAME]:
+                raise ValueError(f"Unkown block {blockName} in yaml file")
+
+        configSeq = ConfigSequence()
+        for blockName in self._order[ROOTNAME]:
+            # consistency check - should never happen
             if blockName not in self._algs:
                 raise ValueError(f"{blockName} not added")
-            blockConfig = self._textConfig[blockName]
-            configureAlg(configSeq, blockName, blockConfig)
+            # order only applies to root blocks
+            if blockName in self._textConfig:
+                blockConfig = self._textConfig[blockName]
+                alg = self._algs[blockName]
+                configureAlg(configSeq, alg, blockConfig)
+            else:
+                continue
         return configSeq
 
 
 
 def addDefaultAlgs(config, dataType, isPhyslite, noPhysliteBroken, noSystematics):
     """add algorithms and options"""
+
+    # CommonServices
+    from AsgAnalysisAlgorithms.AsgAnalysisConfig import makeCommonServicesConfig
+    def CommonServices(seq, noSystematics):
+        makeCommonServicesConfig(seq)
+        seq.setOptionValue ('.runSystematics', not noSystematics)
+        return seq
+    config.addAlgConfigBlock(algName="CommonServices", alg=CommonServices,
+        defaults={'noSystematics': noSystematics})
 
     # pileup
     from AsgAnalysisAlgorithms.AsgAnalysisAlgorithmsTest import pileupConfigFiles
@@ -298,38 +346,58 @@ def addDefaultAlgs(config, dataType, isPhyslite, noPhysliteBroken, noSystematics
     config.addAlgConfigBlock(algName="PileupReweighting", alg=makePileupConfig,
         defaults={'dataType':dataType,'isPhyslite':isPhyslite})
 
-    # muons
-    from MuonAnalysisAlgorithms.MuonAnalysisConfig import MuonCalibrationConfig
-    config.addAlgConfigBlock(algName="Muons", alg=MuonCalibrationConfig)
-    from MuonAnalysisAlgorithms.MuonAnalysisConfig import MuonWorkingPointConfig
-    config.addAlgConfigBlock(algName="MuonWP", alg=MuonWorkingPointConfig)    
-
-    # electrons
-    from EgammaAnalysisAlgorithms.ElectronAnalysisConfig import ElectronCalibrationConfig 
-    config.addAlgConfigBlock(algName="Electrons", alg=ElectronCalibrationConfig)
-    from EgammaAnalysisAlgorithms.ElectronAnalysisConfig import ElectronWorkingPointConfig
-    config.addAlgConfigBlock(algName="ElectronWP", alg=ElectronWorkingPointConfig)
-
-    # photons
-    from EgammaAnalysisAlgorithms.PhotonAnalysisConfig import PhotonCalibrationConfig
-    config.addAlgConfigBlock(algName="Photons", alg=PhotonCalibrationConfig)
-    from EgammaAnalysisAlgorithms.PhotonAnalysisConfig import PhotonWorkingPointConfig
-    config.addAlgConfigBlock(algName="PhotonWP", alg=PhotonWorkingPointConfig)
-
-    # tauJets
-    from TauAnalysisAlgorithms.TauAnalysisConfig import TauCalibrationConfig
-    config.addAlgConfigBlock(algName="TauJets", alg=TauCalibrationConfig)
-    from TauAnalysisAlgorithms.TauAnalysisConfig import TauWorkingPointConfig
-    config.addAlgConfigBlock(algName="TauJetWP", alg=TauWorkingPointConfig)
+    # event cleaning
+    from AsgAnalysisAlgorithms.EventCleaningConfig import EventCleaningBlock
+    config.addAlgConfigBlock(algName="EventCleaning", alg=EventCleaningBlock)
 
     # jets
     from JetAnalysisAlgorithms.JetAnalysisConfig import makeJetAnalysisConfig
     config.addAlgConfigBlock(algName="Jets", alg=makeJetAnalysisConfig)
     from JetAnalysisAlgorithms.JetJvtAnalysisConfig import JetJvtAnalysisConfig
-    config.addAlgConfigBlock(algName="JVT", alg=JetJvtAnalysisConfig)
+    config.addAlgConfigBlock(algName="JVT", alg=JetJvtAnalysisConfig,
+        superBlocks="Jets")
     from FTagAnalysisAlgorithms.FTagAnalysisConfig import makeFTagAnalysisConfig
     config.addAlgConfigBlock(algName="FlavourTagging", alg=makeFTagAnalysisConfig,
-        defaults={'selectionName': ''})
+        defaults={'selectionName': ''},
+        superBlocks="Jets")
+
+    # electrons
+    from EgammaAnalysisAlgorithms.ElectronAnalysisConfig import ElectronCalibrationConfig 
+    config.addAlgConfigBlock(algName="Electrons", alg=ElectronCalibrationConfig)
+    from EgammaAnalysisAlgorithms.ElectronAnalysisConfig import ElectronWorkingPointConfig
+    config.addAlgConfigBlock(algName="WorkingPoint", alg=ElectronWorkingPointConfig,
+        superBlocks="Electrons")
+
+    # photons
+    from EgammaAnalysisAlgorithms.PhotonAnalysisConfig import PhotonCalibrationConfig
+    config.addAlgConfigBlock(algName="Photons", alg=PhotonCalibrationConfig)
+    from EgammaAnalysisAlgorithms.PhotonAnalysisConfig import PhotonWorkingPointConfig
+    config.addAlgConfigBlock(algName="WorkingPoint", alg=PhotonWorkingPointConfig,
+        superBlocks="Photons")
+
+    # muons
+    from MuonAnalysisAlgorithms.MuonAnalysisConfig import MuonCalibrationConfig
+    config.addAlgConfigBlock(algName="Muons", alg=MuonCalibrationConfig)
+    from MuonAnalysisAlgorithms.MuonAnalysisConfig import MuonWorkingPointConfig
+    config.addAlgConfigBlock(algName="WorkingPoint", alg=MuonWorkingPointConfig,
+        superBlocks="Muons")
+
+    # tauJets
+    from TauAnalysisAlgorithms.TauAnalysisConfig import TauCalibrationConfig
+    config.addAlgConfigBlock(algName="TauJets", alg=TauCalibrationConfig)
+    from TauAnalysisAlgorithms.TauAnalysisConfig import TauWorkingPointConfig
+    config.addAlgConfigBlock(algName="WorkingPoint", alg=TauWorkingPointConfig,
+        superBlocks="TauJets")
+
+    # generator level analysis
+    from AsgAnalysisAlgorithms.AsgAnalysisConfig import makeGeneratorAnalysisConfig 
+    config.addAlgConfigBlock(algName="GeneratorLevelAnalysis", alg=makeGeneratorAnalysisConfig)
+
+    # pT/Eta Selection
+    from AsgAnalysisAlgorithms.AsgAnalysisConfig import makePtEtaSelectionConfig
+    config.addAlgConfigBlock(algName="PtEtaSelection", alg=makePtEtaSelectionConfig,
+        defaults={'selectionName': ''},
+        superBlocks=["Jets", "Electrons", "Photons", "Muons", "TauJets"])
 
     # met
     from MetAnalysisAlgorithms.MetAnalysisConfig import MetAnalysisConfig
@@ -340,60 +408,38 @@ def addDefaultAlgs(config, dataType, isPhyslite, noPhysliteBroken, noSystematics
     config.addAlgConfigBlock(algName="OverlapRemoval", alg=OverlapAnalysisConfig,
         defaults={'configName': 'OverlapRemoval'})
 
-    # event cleaning
-    from AsgAnalysisAlgorithms.EventCleaningConfig import EventCleaningBlock
-    config.addAlgConfigBlock(algName="EventCleaning", alg=EventCleaningBlock)
-
-    # generator level analysis
-    from AsgAnalysisAlgorithms.AsgAnalysisConfig import makeGeneratorAnalysisConfig 
-    config.addAlgConfigBlock(algName="GeneratorLevelAnalysis", alg=makeGeneratorAnalysisConfig)
-
-    # trigger
-    from TriggerAnalysisAlgorithms.TriggerAnalysisConfig import TriggerAnalysisBlock
-    config.addAlgConfigBlock(algName="Trigger", alg=TriggerAnalysisBlock,
-        defaults={'configName': 'Trigger'})
-
-    # pT/Eta Selection
-    from AsgAnalysisAlgorithms.AsgAnalysisConfig import makePtEtaSelectionConfig
-    config.addAlgConfigBlock(algName="PtEtaSelection", alg=makePtEtaSelectionConfig,
-        defaults={'selectionName': ''})
+    # object-based cutflow
+    from AsgAnalysisAlgorithms.AsgAnalysisConfig import ObjectCutFlowBlock
+    config.addAlgConfigBlock(algName='ObjectCutFlow', alg=ObjectCutFlowBlock)
 
     # thinning
     from AsgAnalysisAlgorithms.AsgAnalysisConfig import OutputThinningBlock
     config.addAlgConfigBlock(algName="Thinning", alg=OutputThinningBlock,
         defaults={'configName': 'Thinning'})
 
-    # output
-    from AsgAnalysisAlgorithms.OutputAnalysisConfig import OutputAnalysisConfig
-    config.addAlgConfigBlock(algName="Output", alg=OutputAnalysisConfig,
-        defaults={'configName': 'Output'})
-
-    # CommonServices
-    from AsgAnalysisAlgorithms.AsgAnalysisConfig import makeCommonServicesConfig
-    def CommonServices(seq, noSystematics):
-        makeCommonServicesConfig(seq)
-        seq.setOptionValue ('.runSystematics', not noSystematics)
-        return seq
-    config.addAlgConfigBlock(algName="CommonServices", alg=CommonServices,
-        defaults={'noSystematics': noSystematics})
-
-    # object-based cutflow
-    from AsgAnalysisAlgorithms.AsgAnalysisConfig import ObjectCutFlowBlock
-    config.addAlgConfigBlock(algName='ObjectCutFlow', alg=ObjectCutFlowBlock)
-
-    # event-based cutflow
-    from AsgAnalysisAlgorithms.AsgAnalysisConfig import EventCutFlowBlock
-    config.addAlgConfigBlock(algName='EventCutFlow', alg=EventCutFlowBlock,
-        defaults={'containerName': 'EventInfo', 'selectionName': ''})
+    # trigger
+    from TriggerAnalysisAlgorithms.TriggerAnalysisConfig import TriggerAnalysisBlock
+    config.addAlgConfigBlock(algName="Trigger", alg=TriggerAnalysisBlock,
+        defaults={'configName': 'Trigger'})
 
     # event selection
     from EventSelectionAlgorithms.EventSelectionConfig import EventSelectionConfig, EventSelectionMergerConfig
     config.addAlgConfigBlock(algName='EventSelection', alg=EventSelectionConfig)
     config.addAlgConfigBlock(algName='EventSelectionMerger', alg=EventSelectionMergerConfig)
 
+    # event-based cutflow
+    from AsgAnalysisAlgorithms.AsgAnalysisConfig import EventCutFlowBlock
+    config.addAlgConfigBlock(algName='EventCutFlow', alg=EventCutFlowBlock,
+        defaults={'containerName': 'EventInfo', 'selectionName': ''})
+
     # bootstraps
     from AsgAnalysisAlgorithms.BootstrapGeneratorConfig import BootstrapGeneratorConfig
     config.addAlgConfigBlock(algName='Bootstraps', alg=BootstrapGeneratorConfig)
+
+    # output
+    from AsgAnalysisAlgorithms.OutputAnalysisConfig import OutputAnalysisConfig
+    config.addAlgConfigBlock(algName="Output", alg=OutputAnalysisConfig,
+        defaults={'configName': 'Output'})
 
     return
 
