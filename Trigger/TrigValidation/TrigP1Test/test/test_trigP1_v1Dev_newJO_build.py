43dc25a74a2c84e3300e4f3d117be999a5de2e71
Mark Stockton <mark.stockton@cern.ch>
2023-10-26 20:43:05 +0200

    NewJO tests - add CA count comparison to dev and add PhysP1 test
    NewJO tests - add CA count comparison to dev and add PhysP1 test

--- Trigger/TrigValidation/TrigP1Test/test/test_trigP1_v1Dev_newJO_build.py
+++ Trigger/TrigValidation/TrigP1Test/test/test_trigP1_v1Dev_newJO_build.py
@@ -12,6 +12,7 @@ ex = ExecStep.ExecStep()
 ex.type = 'athenaHLT'
 ex.job_options = 'TriggerJobOpts.runHLT'
 ex.input = 'data'
+ex.max_events = 50
 ex.flags = ['Trigger.triggerMenuSetup="Dev_pp_run3_v1_TriggerValidation_prescale"',
             'Trigger.doLVL1=True']
 
@@ -19,6 +20,15 @@ test = Test.Test()
 test.art_type = 'build'
 test.exec_steps = [ex]
 test.check_steps = CheckSteps.default_check_steps(test)
+
+# Add a step comparing counts against legacy reference (from test_trigP1_v1Dev_decodeBS_build)
+chaindump = test.get_step("ChainDump")
+chaindump.args = '--json --yaml ref_v1Dev_decodeBS_build.new'
+refcomp = CheckSteps.ChainCompStep("CountRefComp")
+refcomp.input_file = 'ref_v1Dev_decodeBS_build.new'
+refcomp.reference_from_release = True # installed from TrigP1Test/share
+refcomp.required = False # Final exit code doesn't depend on this step
+CheckSteps.add_step_after_type(test.check_steps, CheckSteps.ChainDumpStep, refcomp)
 
 import sys
 sys.exit(test.run())
