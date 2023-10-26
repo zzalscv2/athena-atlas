31d24dc4556cce052c4fd67dbcd1bc757e0cd2d2
Oleg Kuprash <oleg.kuprash@cern.ch>
2023-10-26 20:53:36 +0200

    ATR-28452: Fix getInDetTrigConfig call for RDOtoRDOTrig
    ATR-28452: Fix getInDetTrigConfig call for RDOtoRDOTrig

--- Trigger/TrigTools/TrigInDetConfig/share/IDTrig_MC23a_preInclude.py
+++ Trigger/TrigTools/TrigInDetConfig/share/IDTrig_MC23a_preInclude.py
@@ -1,11 +1,16 @@
+from AthenaConfiguration.AllConfigFlags import ConfigFlags as flags
+from Campaigns.Utils import Campaign
+# do not let use this preInclude with campaigns other than MC23a
+if flags.Input.MCCampaign is not Campaign.MC23a:
+    raise Exception( "MC23a ID Trigger configuration is called when processing {}. Please remove this preInclude from the config/AMI tag".format(flags.Input.MCCampaign))
+
 from TrigInDetConfig.ConfigSettings import getInDetTrigConfig; 
 from AthenaCommon.SystemOfUnits import GeV; 
 getInDetTrigConfig('tauCore')._pTmin = 1*GeV; 
 getInDetTrigConfig('tauIso')._pTmin = 1*GeV; 
 getInDetTrigConfig('jetSuper')._zedHalfWidth = 150.;
-getInDetTrigConfig('bphysics')._zedHalfWidth = 225.;
-getInDetTrigConfig('bphysics')._SuperRoI = False;
+getInDetTrigConfig('bmumux')._zedHalfWidth = 225.;
+getInDetTrigConfig('bmumux')._SuperRoI = False;
 
-from AthenaConfiguration.AllConfigFlags import ConfigFlags as flags
 flags.Trigger.InDetTracking.RoiZedWidthDefault=0.0
 flags.Trigger.InDetTracking.bjet.Xi2max=9.
