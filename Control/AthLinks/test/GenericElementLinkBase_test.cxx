8cecab5716f4f0b62ab9a191edd9b1116684a543
scott snyder <snyder@bnl.gov>
2023-11-04 00:02:55 -0400

    AthLinks: Fix for initializing EL from default DL.
    When we initialize an ElementLink from a default DataLink, make sure the EL
    also ends up in the default state (thus ignoring any index that was provided).

--- Control/AthLinks/test/GenericElementLinkBase_test.cxx
+++ Control/AthLinks/test/GenericElementLinkBase_test.cxx
@@ -167,6 +167,7 @@ class DataLinkBase_test
 {
 public:
   using DataLinkBase::DataLinkBase;
+  DataLinkBase_test() {}
   DataLinkBase_test (sgkey_t key, CLID link_clid, IProxyDict* sg)
     : DataLinkBase (key, link_clid, sg) {}
 };
@@ -394,6 +395,10 @@ void test1 (SGTest::TestStore& store)
   assert (el14.key() == sgkey);
   assert (el14.source() == &store);
   assert (el14.storableBase (0, clid, true) == cont);
+
+  DataLinkBase_test dl14a;
+  Link el14a (dl14a, index1);
+  assert (el14a.isDefault());
 
   el12.reset();
   assert (el12.isDefault());
