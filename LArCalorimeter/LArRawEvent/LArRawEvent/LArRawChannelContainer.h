ca26c6e127f55b80907007d3b1a81bc0b938e00b
scott snyder <snyder@bnl.gov>
2023-11-05 23:10:30 +0100

    LArRawEvent: Add some missing dictionary entries.
    For use by PyDumper.
    And fix a typo in the comments.

--- LArCalorimeter/LArRawEvent/LArRawEvent/LArRawChannelContainer.h
+++ LArCalorimeter/LArRawEvent/LArRawEvent/LArRawChannelContainer.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 //Dear emacs, this is -*-c++-*-
@@ -29,7 +29,7 @@ class LArRawChannelContainer : public std::vector<LArRawChannel>
   /** constructor */
   LArRawChannelContainer() {} ;
 
-  /** desctructor */
+  /** destructor */
   virtual ~LArRawChannelContainer() { };
 
   inline void add (const LArRawChannel& rc) 
