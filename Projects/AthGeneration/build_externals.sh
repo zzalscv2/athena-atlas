7bafa88595103979c42646e0beb8ac39c67cff57
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2023-11-06 08:10:14 +0100

    Projects: Update to LCG_104b_ATLAS_3
    Projects: Update to LCG_104b_ATLAS_3

--- Projects/AthGeneration/build_externals.sh
+++ Projects/AthGeneration/build_externals.sh
@@ -10,7 +10,7 @@ ATLAS_PROJECT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 ATLAS_EXT_PROJECT_NAME="AthGenerationExternals"
 ATLAS_BUILDTYPE="RelWithDebInfo"
 ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=104
-                        -DLCG_VERSION_POSTFIX="b_ATLAS_1"
+                        -DLCG_VERSION_POSTFIX="b_ATLAS_3"
                         -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v37r1.000/Gaudi-v37r1.000.tar.gz;URL_MD5;37218f7d710d831699d3d1ba180ba434")
 ATLAS_EXTRA_MAKE_ARGS=()
 
