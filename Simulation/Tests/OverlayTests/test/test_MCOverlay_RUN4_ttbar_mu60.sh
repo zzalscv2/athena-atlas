5df7105d22d27c78f437190c098e0d39e04121dd
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2023-11-06 14:29:44 +0100

    Deactivate RUN4 OverlayTests ART jobs for 23.0
    Deactivate RUN4 OverlayTests ART jobs for 23.0

--- Simulation/Tests/OverlayTests/test/test_MCOverlay_RUN4_ttbar_mu60.sh
+++ Simulation/Tests/OverlayTests/test/test_MCOverlay_RUN4_ttbar_mu60.sh
@@ -3,7 +3,7 @@
 # art-description: MC+MC Overlay without reco for Run 4, ttbar, mu=60
 # art-type: grid
 # art-architecture:  '#x86_64-intel'
-# art-include: 23.0/Athena
+# art-include: 24.0/Athena
 # art-include: main/Athena
 
 # art-output: *.root
