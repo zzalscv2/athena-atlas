5ccab8924d074be0cb6ad140b63afc5e2e77e628
scott snyder <sss@karma>
2023-11-17 15:47:22 -0500

    RootUtils: Fix compilation with gcc14.
    Missing include.

--- Control/RootUtils/src/WithRootErrorHandler.cxx
+++ Control/RootUtils/src/WithRootErrorHandler.cxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002-2021 CERN for the benefit of the ATLAS collaboration.
+ * Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration.
  */
 /**
  * @file RootUtils/src/WithRootErrorHandler.cxx
@@ -11,6 +11,7 @@
 
 #include "RootUtils/WithRootErrorHandler.h"
 #include <vector>
+#include <cstdlib>
 
 
 namespace {
