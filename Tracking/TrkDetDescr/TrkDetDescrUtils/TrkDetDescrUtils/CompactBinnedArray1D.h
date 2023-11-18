2547664d24376b5f9f2cdae6a1929c27bcceffa8
scott snyder <sss@karma>
2023-11-17 21:10:13 -0500

    TrkDetDescrUtils: Fix compilation with gcc14.
    CompactBinnedArray1D::m_array can't be declared const, since it gets
    written by operator=.

--- Tracking/TrkDetDescr/TrkDetDescrUtils/TrkDetDescrUtils/CompactBinnedArray1D.h
+++ Tracking/TrkDetDescr/TrkDetDescrUtils/TrkDetDescrUtils/CompactBinnedArray1D.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 ///////////////////////////////////////////////////////////////////
@@ -184,7 +184,7 @@ public:
   }
 
 private:
-  const std::vector<size_t> m_array; //!< vector of indices to objects
+  std::vector<size_t> m_array;       //!< vector of indices to objects
   std::vector<T*> m_arrayObjects;    //!< objects
   BinUtility* m_binUtility;          //!< binUtility
 };
