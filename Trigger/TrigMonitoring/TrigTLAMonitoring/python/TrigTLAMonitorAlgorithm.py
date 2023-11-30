7cb3cb69c5fa2bcd8df53fb4823cc1d56542c7f8
Stefano Franchellucci <stefano.franchellucci@cern.ch>
2023-11-30 20:16:21 +0100

    Udate TLA monitoring
    Udate TLA monitoring

--- Trigger/TrigMonitoring/TrigTLAMonitoring/python/TrigTLAMonitorAlgorithm.py
+++ Trigger/TrigMonitoring/TrigTLAMonitoring/python/TrigTLAMonitorAlgorithm.py
@@ -9,284 +9,32 @@
 @brief Example trigger python configuration for the Run III AthenaMonitoring package, based on the example by C Burton and P Onyisi
 '''
 
-def histdefs_eventinfo(pprefix):
-    """
-    Return a list of histogram definitions for global event variables
+def tla_chains(inputFlags,log=None):
+    '''
+    Read in the TLA trigger chain names as submitted to the MonGroups 
+    '''
+    tla_triglist = []
+    # Trigger list from monitoring groups: signatures = TLAMon to be agreed w/ MonGroups management
+    from TrigConfigSvc.TriggerConfigAccess import getHLTMonitoringAccess
+    moniAccess=getHLTMonitoringAccess(inputFlags)
+    TLAChainsE=moniAccess.monitoredChains(signatures="tlaMon",monLevels=["t0"])
+    if log is not None:
+        log.info (" ==> tla_chainlist t0: %s", TLAChainsE)
+    for chain in TLAChainsE:
+        chain = "E_"+chain
+        tla_triglist.append(chain)
+    TLAChainsS=moniAccess.monitoredChains(signatures="tlaMon",monLevels=["shifter"])
+    if log is not None:
+        log.info (" ==> tla_chainlist shifter:  %s", TLAChainsS)
+    for chain in TLAChainsS:
+        chain = "S_"+chain
+        tla_triglist.append(chain)
+    if log is not None:
+        log.info (" ==> tla_triglist:  %s", tla_triglist)
+    return tla_triglist
 
-    @param pprefix Prefix of histogram names
-    """
-    histograms=[
-        {'name'  :f'{pprefix}_AvgMu',
-         'ylabel':'events',
-         'xlabel':'AvgMu',
-         'xmin'  :-0.5,
-         'xmax'  :69.5,
-         'xbins' :70},
-        {'name'  :f'{pprefix}_NumPV',
-         'ylabel':'events',
-         'xlabel':'NumPV',
-         'xmin'  :-0.5,
-         'xmax'  :39.5,
-         'xbins' :40},
-        {'name'  :f'{pprefix}_JetDensityEMTopo',
-         'ylabel':'events',
-         'xlabel':'JetDensityEMTopo',
-         'xmin'  :-0.5,
-         'xmax'  :25000,
-         'xbins' :100},
-        {'name'  :f'{pprefix}_JetDensityEMPFlow',
-         'ylabel':'events',
-         'xlabel':'JetDensityEMPFlow',
-         'xmin'  :-0.5,
-         'xmax'  :25000,
-         'xbins' :100}
-    ]
-    return histograms
 
-def histdefs_jetvariables(pprefix, plabel, pflow=False):
-    """
-    Return a list of jet-specific histogram definitions for jet moment distributions
-
-    @param pprefix Prefix of histogram names
-    @param plabel Human-readable name of the jet to use in labeling axes
-    @param pflow bool to indicate that this is the PFlow jet collection, so that the track-based variables are added. 
-                False indicates that this is the calo jet collection, and additional calo variables are monitored.
-    """
-
-    histograms=[
-       {'name'  :f'{pprefix}ActiveArea',
-        'ylabel':f'{plabel}s',
-        'xlabel':f'{plabel} ActiveArea',
-        'xunit' :'',
-        'xmin'  :0,
-        'xmax'  :2.,
-        'xbins' :200},
-    ]
-    if pflow:
-        histograms+=[
-           {'name'  :f'{pprefix}TrackWidthPt1000',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} TrackWidthPt1000',
-            'xmin'  :-0.5,
-            'xmax'  :0.95,
-            'xbins' :50},
-           {'name'  :f'{pprefix}NumTrkPt1000',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} NumTrkPt1000',
-            'xmin'  :-0.5,
-            'xmax'  :39.5,
-            'xbins' :40},
-           {'name'  :f'{pprefix}SumPtTrkPt500',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} SumPtTrkPt500',
-            'xmin'  :-0.5,
-            'xmax'  :500e3,
-            'xunit' : 'MeV',
-            'xbins' :200},
-           {'name'  :f'{pprefix}SumPtChargedPFOPt500',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} SumPtChargedPFOPt500',
-            'xmin'  :-0.5,
-            'xmax'  :500e3,
-            'xunit' : 'MeV',
-            'xbins' :200},
-           {'name'  :f'{pprefix}Jvt',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} Jvt',
-            'xmin'  :-0.2,
-            'xmax'  :1.2,
-            'xbins' :70},
-           {'name'  :f'{pprefix}JvtRpt',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} JvtRpt',
-            'xmin'  :-0.1,
-            'xmax'  :1.4,
-            'xbins' :75},
-           {'name'  :f'{pprefix}fastDIPS20211215_pu',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} fastDips_pu',
-            'xmin'  :-0.2,
-            'xmax'  :1.2,
-            'xbins' :70},
-           {'name'  :f'{pprefix}fastDIPS20211215_pb',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} fastDips_pb',
-            'xmin'  :-0.2,
-            'xmax'  :1.2,
-            'xbins' :70},
-           {'name'  :f'{pprefix}fastDIPS20211215_pc',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} fastDips_pc',
-            'xmin'  :-0.2,
-            'xmax'  :1.2,
-            'xbins' :70},
-            {'name'  :f'{pprefix}GN120230331_pu',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} GN120230331_pu',
-            'xmin'  :-0.2,
-            'xmax'  :1.2,
-            'xbins' :70},
-            {'name'  :f'{pprefix}GN120230331_pb',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} GN120230331_pb',
-            'xmin'  :-0.2,
-            'xmax'  :1.2,
-            'xbins' :70},
-            {'name'  :f'{pprefix}GN120230331_pc',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} GN120230331_pc',
-            'xmin'  :-0.2,
-            'xmax'  :1.2,
-            'xbins' :70},
-        ]
-    else:
-        histograms+=[
-           {'name'  :f'{pprefix}EMFrac',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} EMFrac',
-            'xmin'  :-0.1,
-            'xmax'  :1.4,
-            'xbins' :75},
-           {'name'  :f'{pprefix}HECFrac',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} HECFrac',
-            'xmin'  :-0.1,
-            'xmax'  :1.4,
-            'xbins' :75},
-           {'name'  :f'{pprefix}Timing',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} Timing',
-            'xunit' :'ns',
-            'xmin'  :-50,
-            'xmax'  :50,
-            'xbins' :50},
-           {'name'  :f'{pprefix}N90Constituents',
-            'ylabel':f'{plabel}s',
-            'xlabel':f'{plabel} N90Constituents',
-            'xunit' :'',
-            'xmin'  :0,
-            'xmax'  :20,
-            'xbins' :20},
-        ]
-    return histograms
-
-def histdefs_jetcalibscales(pprefix, plabel, pflow=False):
-    """
-    Return a list of jet-specific histogram definitions for jet pT distributions at different calibration scales
-
-    @param pprefix Prefix of histogram names
-    @param plabel Human-readable name of the jet to use in labeling axes
-    @param pflow bool to indicate that this is the PFlow jet collection, so that the track-based calibration scales are added.
-    """
-    histograms=[
-       {'name'  :f'{pprefix}JetConstitScaleMomentum_pt',
-        'ylabel':f'{plabel}s',
-        'xlabel':f'{plabel} JetConstitScaleMomentum_pt',
-        'xunit' :'GeV',
-        'xmin'  :0,
-        'xmax'  :750,
-        'xbins' :1500},
-       {'name'  :f'{pprefix}JetPileupScaleMomentum_pt',
-        'ylabel':f'{plabel}s',
-        'xlabel':f'{plabel} JetPileupScaleMomentum_pt',
-        'xunit' :'GeV',
-        'xmin'  :0,
-        'xmax'  :750,
-        'xbins' :1500},
-       {'name'  :f'{pprefix}JetEtaJESScaleMomentum_pt',
-        'ylabel':f'{plabel}s',
-        'xlabel':f'{plabel} JetEtaJESScaleMomentum_pt',
-        'xunit' :'GeV',
-        'xmin'  :0,
-        'xmax'  :750,
-        'xbins' :1500},
-    ]
-    if pflow:
-        histograms+= [
-          {'name'  :f'{pprefix}JetGSCScaleMomentum_pt',
-           'ylabel':f'{plabel}s',
-           'xlabel':f'{plabel} JetGSCScaleMomentum_pt',
-           'xunit' :'GeV',
-           'xmin'  :0,
-           'xmax'  :750,
-           'xbins' :1500}
-        ]
-    return histograms
-
-def histdefs_particle(pprefix, plabel):
-    """
-    Return a list of histogram definitions for particle kinematics.
-
-    @param pprefix Prefix of histogram names
-    @param plabel Human-readable name of the particle to use in labeling axes
-    """
-    histograms=[
-        {'name'  :f'n{pprefix}',
-         'ylabel':'events',
-         'xlabel':f'number of {plabel}s',
-         'xmin'  :-0.5,
-         'xmax'  :19.5,
-         'xbins' :20},
-        {'name'  :f'{pprefix}pt',
-         'ylabel':f'{plabel}s',
-         'xlabel':f'{plabel} p_{{T}}',
-         'xunit' :'GeV',
-         'xmin'  :0,
-         'xmax'  :750,
-         'xbins' :150},
-        {'name'  :f'{pprefix}eta',
-         'ylabel':f'{plabel}s',
-         'xlabel':f'{plabel} #eta',
-         'xmin'  :-4,
-         'xmax'  : 4,
-         'xbins' :100},
-        {'name'  :f'{pprefix}phi',
-         'ylabel':f'{plabel}s',
-         'xlabel':f'leading {plabel} #phi',
-         'xmin'  :-3.5,
-         'xmax'  : 3.5,
-         'xbins' :100},
-        {'name'  :f'{pprefix}0pt',
-         'ylabel':'events',
-         'xlabel':f'leading {plabel} p_{{T}}',
-         'xunit' :'GeV',
-         'xmin'  :0,
-         'xmax'  :750,
-         'xbins' :150},
-        {'name'  :f'{pprefix}0eta',
-         'ylabel':'events',
-         'xlabel':f'leading {plabel} #eta',
-         'xmin'  :-4,
-         'xmax'  : 4,
-         'xbins' :100},
-        {'name'  :f'{pprefix}0phi',
-         'ylabel':'events',
-         'xlabel':f'leading {plabel} #phi',
-         'xmin'  :-3.5,
-         'xmax'  : 3.5,
-         'xbins' :100}
-    ]
-    return histograms
-
-def histdefs_dR(p0prefix,p1prefix,p0label,p1label):
-    """
-    Return a list of histogram definitions for particle dR
-
-    @param p0prefix Prefix of first particle in histogram
-    @param p1prefix Prefix of second particle in histogram
-    @param p0label Human-readable name of the first particle to use in labeling axes
-    @param p1label Human-readable name of the second particle to use in labeling axes
-    """
-    histograms=[
-        {'name'  :f'{p0prefix}{p1prefix}dr',
-         'ylabel':'events',
-         'xlabel':f'#DeltaR_{{{p0label}, {p1label}}}',
-         'xmin'  :0,
-         'xmax'  :6,
-         'xbins' :60}]
-    return histograms
-
-def TrigTLAMonConfig(inputFlags):
+def TrigTLAMonConfig(inputFlags, tdt):
     '''Function to configures some algorithms in the monitoring system.'''
 
     from AthenaCommon.Logging import logging
@@ -304,7 +52,6 @@ def TrigTLAMonConfig(inputFlags):
     from AthenaMonitoring import AthMonitorCfgHelper
     helper = AthMonitorCfgHelper(inputFlags,'TrigTLAAthMonitorCfg')
 
-
     ### STEP 2 ###
     # Adding an algorithm to the helper.
 
@@ -317,32 +64,19 @@ def TrigTLAMonConfig(inputFlags):
     #trigTLAMonAlg.TriggerChain = 'HLT_mu26_ivarmedium'
     #trigTLAMonAlg.TriggerChain = 'HLT_e24_lhtight_nod0'
     trigTLAMonAlg.TriggerChain = ''
+    trigTLAMonAlg.TriggerDecisionTool = tdt
 
     ### STEP 4 ###
-    # Read in the TLA trigger chain names as submitted to the MonGroups
-
-    tla_triglist = []
-    # Trigger list from monitoring groups: signatures = TLAMon to be agreed w/ MonGroups management
-    from TrigConfigSvc.TriggerConfigAccess import getHLTMonitoringAccess
-    moniAccess=getHLTMonitoringAccess(inputFlags)
-    TLAChainsE=moniAccess.monitoredChains(signatures="tlaMon",monLevels=["t0"])
-    log.info (" ==> tla_chainlist t0: %s", TLAChainsE)
-    for chain in TLAChainsE:
-        chain = "E_"+chain
-        tla_triglist.append(chain)
-    TLAChainsS=moniAccess.monitoredChains(signatures="tlaMon",monLevels=["shifter"])
-    log.info (" ==> tla_chainlist shifter:  %s", TLAChainsS)
-    for chain in TLAChainsS:
-        chain = "S_"+chain
-        tla_triglist.append(chain)
-    log.info (" ==> tla_triglist:  %s", tla_triglist)
+    tla_triglist=tla_chains(inputFlags,log=None)
 
     # Add a generic monitoring tool (a "group" in old language). The returned 
     # object here is the standard GenericMonitoringTool.
     TLAMonGroup = helper.addGroup(trigTLAMonAlg,'TrigTLAMonitor','HLT/TLAMon/')
 
-
     ### STEP 5 ###
+    from TrigTLAMonitoring.TrigTLAMonitorHistograms import (
+        histdefs_eventinfo, histdefs_particle, histdefs_tracks, histdefs_dR, histdefs_jetcalibscales, histdefs_jetvariables
+    )
     # Configure histograms
     #NB! The histograms defined here must match the ones in the cxx file exactly
     histdefs_global=[]
@@ -352,6 +86,7 @@ def TrigTLAMonConfig(inputFlags):
     histdefs+=histdefs_particle('pfjet','particle-flow jet')
     histdefs+=histdefs_particle('ph'   ,'photon')
     histdefs+=histdefs_particle('muon' ,'muon')
+    histdefs+=histdefs_tracks  ('trk'  ,'track')
     histdefs+=histdefs_dR      ('jet0'  ,'jet1'  , 'leading jet'   ,'subleading jet'   )
     histdefs+=histdefs_dR      ('pfjet0','pfjet1', 'leading pf jet','subleading pf jet')
     histdefs+=histdefs_dR      ('jet0'  ,'ph0'   , 'leading jet'   ,'leading photon'   )
@@ -401,6 +136,7 @@ def TrigTLAMonConfig(inputFlags):
     # just return directly (and not create "result" above)
     return helper.result()
 
+
 if __name__=='__main__':
     # Get run options
     import argparse
@@ -435,7 +171,7 @@ if __name__=='__main__':
     # flags.Trigger.triggerMenuSetup="Physics_pp_v7_primaries"
     flags.Trigger.triggerMenuSetup = 'Physics_pp_run3_v1'
     flags.Trigger.triggerConfig = 'DB'
-    
+
     flags.lock()
 
     # Initialize configuration object, add accumulator, merge, and run.
@@ -444,11 +180,42 @@ if __name__=='__main__':
     cfg = MainServicesCfg(flags)
     cfg.merge(PoolReadCfg(flags))
 
-    trigTLAMonitorAcc = TrigTLAMonConfig(flags)
-    cfg.merge(trigTLAMonitorAcc)
+    # initialize the trigger decision tool
+    from TrigDecisionTool.TrigDecisionToolConfig import TrigDecisionToolCfg
+    tdt = cfg.getPrimaryAndMerge(TrigDecisionToolCfg(flags))
+
+    # Use sequeces to handle algorithms dependencies
+    from AthenaCommon.CFElements import seqAND, seqOR
+    cfg.addSequence( seqOR("monSeq")  )
+
+    # Sequence for augmenting tracks - needs to run only for pflow chains
+    cfg.addSequence( seqAND("trackAugmentSeq"), parentName="monSeq")
+    pfChains=[trig[2:] for trig in tla_chains(flags) if 'pf_ftf' in trig]
+    from AthenaConfiguration.ComponentFactory import CompFactory
+    cfg.addEventAlgo(
+        CompFactory.CP.TrigEventSelectionAlg(triggers=pfChains,tool=tdt,noL1=True),
+        sequenceName="trackAugmentSeq",
+    )
+    from BTagging.BTagTrackAugmenterAlgConfig import BTagTrackAugmenterAlgCfg
+    cfg.merge(
+        BTagTrackAugmenterAlgCfg(
+            flags,
+            TrackCollection='HLT_IDTrack_FS_FTF',
+            PrimaryVertexCollectionName='HLT_IDVertex_FS',
+        ),
+        sequenceName="trackAugmentSeq"
+    )
+    
+    # Add Monitoring Algorithm, after augmentations
+    trigTLAMonitorAcc = TrigTLAMonConfig(flags,tdt)
+    cfg.merge(
+        trigTLAMonitorAcc,
+        sequenceName="monSeq"
+    )
 
     # If you want to turn on more detailed messages ...
     #trigTLAMonitorAcc.getEventAlgo('TrigTLAMonAlg').OutputLevel = 2 # DEBUG
+    #cfg.getService("MessageSvc").debugLimit=100000 # for extensive print outs
     cfg.printConfig(withDetails=False) # set True for exhaustive info
 
     cfg.run()
--- Trigger/TrigMonitoring/TrigTLAMonitoring/src/TrigTLAMonitorAlgorithm.h
+++ Trigger/TrigMonitoring/TrigTLAMonitoring/src/TrigTLAMonitorAlgorithm.h
@@ -8,9 +8,12 @@
 #include "AthenaMonitoring/AthMonitorAlgorithm.h"
 #include "AthenaMonitoringKernel/Monitored.h"
 
+#include "TrigDecisionTool/TrigDecisionTool.h"
+
 #include "xAODJet/JetContainer.h"
 #include "xAODMuon/MuonContainer.h"
 #include "xAODEgamma/PhotonContainer.h"
+#include "xAODTracking/TrackParticleContainer.h"
 #include "xAODTrigger/TrigCompositeContainer.h"
 
 class TrigTLAMonitorAlgorithm : public AthMonitorAlgorithm {
@@ -27,7 +30,28 @@ class TrigTLAMonitorAlgorithm : public AthMonitorAlgorithm {
   SG::ReadHandleKey<xAOD::JetContainer   > m_pfjetContainerKey {this,"PFJetContainerName" ,"HLT_AntiKt4EMPFlowJets_subresjesgscIS_ftf_TLA", "Particle Flow Jet Container Name"};
   SG::ReadHandleKey<xAOD::PhotonContainer> m_photonContainerKey{this,"PhotonContainerName","HLT_egamma_Photons_TLA"                       , "Photon Container Name"};
   SG::ReadHandleKey<xAOD::MuonContainer  > m_muonContainerKey  {this,"MuonContainerName"  ,"HLT_MuonsCB_RoI_TLA"                          , "Muon Container Name"};
-  SG::ReadHandleKey<xAOD::TrigCompositeContainer  > m_tcEventInfoContainerKey  {this,"TCEventInfoContainerName"  ,"HLT_TCEventInfo_TLA"                          , "TCEventInfo Container Name"};
+  SG::ReadHandleKey<xAOD::TrackParticleContainer> m_trackParticleContainerKey{this,"TrackContainerName"      ,"HLT_IDTrack_FS_FTF"        , "Track Container Name"};
+  SG::ReadHandleKey<xAOD::TrigCompositeContainer> m_tcEventInfoContainerKey  {this,"TCEventInfoContainerName","HLT_TCEventInfo_TLA"       , "TCEventInfo Container Name"};
+
+  PublicToolHandle<Trig::TrigDecisionTool> m_trigDecisionTool{this,"TriggerDecisionTool","Trigger decision tool"};
+
+  //! Retrieve a container of type `T`.
+  /**
+   * \param container: output container
+   * \param key : name of container to be retrieved
+   * \param ctx: Event Context
+   */ 
+  template <typename T>
+  StatusCode readContainer(SG::ReadHandle<T> & container, SG::ReadHandleKey<T> key, const EventContext& ctx) const;
+
+  //! Fill an event info variable to a 1D histogram
+  /**
+   * \param tcEventInfo: SG:ReadHandle to TCEventInfoContainer
+   * \param varname: name of variable to be filled
+   * \param prefix: string appended as prefix to histogram name
+  */
+  template <typename T>
+  StatusCode fillEventInfoHistogram(SG::ReadHandle<DataVector<xAOD::TrigComposite_v1> > tcEventInfo, const std::string& varname, const std::string& prefix) const;
 
   //! Fill kinematic histograms for a given particle of type `T`
   /**
@@ -40,45 +64,21 @@ class TrigTLAMonitorAlgorithm : public AthMonitorAlgorithm {
    * \param trigName Suffix of the histogram name
    */
   template <class T>
-    StatusCode fillParticleHistograms(SG::ReadHandle<DataVector<T>> particles, const std::string& prefix, const std::string& trigName) const;
+  StatusCode fillParticleHistograms(SG::ReadHandle<DataVector<T>> particles, const std::string& prefix, const std::string& trigName) const;
 
-  //! Retrieve variable from TLA TCEventInfoContainer (xAOD::TrigComposite container).
+  //! Fill Analysis Object feature histogram`
   /**
-   * \param tcEI: const xAOD::TrigComposite pointer to TCEventInfoContainer
-   * \param varname : name of variable to be retrieved
-   * \param variable: variable to which value is stored 
-   */ 
-  template <typename T>
-  StatusCode getEventInfoDetail(const xAOD::TrigComposite_v1* tcEI, const std::string& varname, T& variable) const;
-
-  //! Fill an event info variable to a 1D histogram
-  /**
-   * \param tcEventInfo: SG:ReadHandle to TCEventInfoContainer
-   * \param varname: name of variable to be filled
-   * \param prefix: string appended as prefix to histogram name
-  */
-  template <typename T>
-    StatusCode fillEventInfoHistogram(SG::ReadHandle<DataVector<xAOD::TrigComposite_v1> > tcEventInfo, const std::string& varname, const std::string& prefix) const;
-
-  //! Fill jet pT to 1D histograms at different calibration scales.
-  /**
-   * \param jets: SG:ReadHandle to jets
-   * \param calibState: calibration state for which pT is retrieved and filled
-   * \param prefix: string appended as prefix to histogram name
-   * \param trigName: Name of trigger that event is required to have fired
-  */
-  StatusCode fillJetPtCalibStatesHistograms(SG::ReadHandle<xAOD::JetContainer> jets, const std::string& calibState, const std::string& prefix,  const std::string& trigName) const;
-
-  //! Fill jet variable to 1D histograms.
-  /**
-   * \param jets: SG:ReadHandle to jets
+   * The name of the histogram is `prefixVAR_trigName`, where the `VAR` is the name
+   * of the variable (ie: `pt`).
+   *
+   * \param container: Container to use
    * \param varname: name of variable to be filled
    * \param prefix: string appended as prefix to histogram name
    * \param trigName: Name of trigger that event is required to have fired
    * \param default_val: value filled in case variable cannot be retrieved. Defaults to -1.
-  */
-  template <typename T>
-  StatusCode fillJetVariableHistograms(SG::ReadHandle<xAOD::JetContainer> jets,  const std::string& varname,  const std::string& prefix,  const std::string& trigName, T default_val=-1) const;
+   */
+  template<class C, typename T>
+  StatusCode fillObjectVariableHistogram(SG::ReadHandle<DataVector<C>> container, const std::string& varname, const std::string& prefix , const std::string& trigName, T default_val=-1) const;
 
   //! Fill jet track variable to 1D histograms. Jet track variables are vectors where each element corresponds to the association of a different primary vertex.
   //  The first element (corresponding to the hard scatter vertex) is filled.
@@ -90,8 +90,8 @@ class TrigTLAMonitorAlgorithm : public AthMonitorAlgorithm {
    * \param default_val: value filled in case variable cannot be retrieved. Defaults to -1.
   */
   template <typename T>
-  StatusCode fillJetTrackVariableHistograms(SG::ReadHandle<xAOD::JetContainer> jets,  const std::string& varname,  const std::string& prefix,  const std::string& trigName, T default_val=-1) const;
-
+  StatusCode fillJetTrackVariableHistogram(SG::ReadHandle<xAOD::JetContainer> jets,  const std::string& varname,  const std::string& prefix,  const std::string& trigName, T default_val=-1) const;
+  
   //! A generic function to fill 2D histogram
   /**
    * \param varname1: Name of variable filled along x-axis
@@ -102,6 +102,15 @@ class TrigTLAMonitorAlgorithm : public AthMonitorAlgorithm {
   */
   template <typename U, typename T>
   StatusCode fill2DHistogram(const std::string& varname1, U var1, const std::string& varname2, T var2, const std::string& prefix) const;
+
+  //! Fill jet pT to 1D histograms at different calibration scales.
+  /**
+   * \param jets: SG:ReadHandle to jets
+   * \param calibState: calibration state for which pT is retrieved and filled
+   * \param prefix: string appended as prefix to histogram name
+   * \param trigName: Name of trigger that event is required to have fired
+  */
+  StatusCode fillJetPtCalibStatesHistograms(SG::ReadHandle<xAOD::JetContainer> jets, const std::string& calibState, const std::string& prefix,  const std::string& trigName) const;
 
   //! Fill kinematic histograms for a given particle of type `T`
   /**
@@ -115,6 +124,24 @@ class TrigTLAMonitorAlgorithm : public AthMonitorAlgorithm {
    */
   StatusCode fillDeltaRHistograms(const xAOD::IParticle* p0, const xAOD::IParticle* p1, const std::string& prefix, const std::string& trigName) const;
 };
+
+
+/*************************************************
+* Implementation of templated methods            *
+**************************************************/
+
+template <typename T>
+StatusCode TrigTLAMonitorAlgorithm::readContainer(SG::ReadHandle<T> & container, SG::ReadHandleKey<T> key, const EventContext& ctx) const{
+
+  container = SG::makeHandle( key, ctx );
+  if (! container.isValid() ) {
+    ATH_MSG_ERROR("evtStore() does not contain Collection with name "<< key);
+    return StatusCode::FAILURE;
+  }
+
+  return StatusCode::SUCCESS;
+}
+
 
 template <typename T>
 StatusCode TrigTLAMonitorAlgorithm::fillEventInfoHistogram(SG::ReadHandle<DataVector<xAOD::TrigComposite_v1> > tcEventInfo, const std::string& varname, const std::string& prefix) const {
@@ -132,6 +159,7 @@ StatusCode TrigTLAMonitorAlgorithm::fillEventInfoHistogram(SG::ReadHandle<DataVe
 
   return StatusCode::SUCCESS;
 }
+
 
 template <class T>
 StatusCode TrigTLAMonitorAlgorithm::fillParticleHistograms(SG::ReadHandle<DataVector<T>> particles, const std::string& prefix, const std::string& trigName) const {
@@ -171,6 +199,61 @@ StatusCode TrigTLAMonitorAlgorithm::fillParticleHistograms(SG::ReadHandle<DataVe
 
   return StatusCode::SUCCESS;
 
+}
+
+
+template<class C,typename T>
+StatusCode TrigTLAMonitorAlgorithm::fillObjectVariableHistogram(SG::ReadHandle<DataVector<C>> container, const std::string& varname, const std::string& prefix , const std::string& trigName, T default_val) const {
+  
+  // change var name to avoid confilcts with Monitoring standards ("var_trigname")
+  std::string hname = varname;
+  std::replace( hname.begin(), hname.end(), '_', '-' );
+
+  Monitored::Scalar<T> mon_var (prefix+hname+"_"+trigName,default_val);
+  SG::AuxElement::ConstAccessor<T> accessor(varname);
+  
+  unsigned cnt(0);
+  for(const auto element : *container) {
+    mon_var = accessor(*element);
+    fill("TrigTLAMonitor", mon_var);
+    if (cnt < 5) ATH_MSG_DEBUG(prefix<<" "<<varname<<" = "<<mon_var);
+    cnt++;
+  }
+  return StatusCode::SUCCESS;
+}
+
+
+template <typename T>
+StatusCode TrigTLAMonitorAlgorithm::fillJetTrackVariableHistogram(SG::ReadHandle<xAOD::JetContainer> jets,  const std::string& varname,  const std::string& prefix,  const std::string& trigName, T default_val) const {
+
+  std::vector<T> variable_vec;
+  Monitored::Scalar<T> variable  (prefix+varname+"_"+trigName,default_val);
+
+  unsigned cnt(0);
+  for(auto jet : *jets) {
+    auto status = jet->getAttribute<std::vector<T>>(varname, variable_vec);
+    if (!status){
+      ATH_MSG_WARNING("Failed retrieving "<<varname<<" for "<<prefix);
+    }
+    variable = variable_vec.at(0);
+    fill("TrigTLAMonitor",variable);
+    if (cnt < 3) ATH_MSG_DEBUG(prefix<<" "<<varname<<" = "<<variable);
+    cnt++;
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+
+template <typename U, typename T>
+StatusCode TrigTLAMonitorAlgorithm::fill2DHistogram(const std::string& varname1, U var1, const std::string& varname2, T var2, const std::string& prefix) const{
+
+  Monitored::Scalar<U> mon_var1(prefix+"_"+varname1, var1);
+  Monitored::Scalar<T> mon_var2(prefix+"_"+varname2, var2);
+  ATH_MSG_DEBUG("Filling 2D histogram. "<<varname1<<" = "<<var1<<" ; "<<varname2<<" = "<<var2);
+  fill("TrigTLAMonitor", mon_var1, mon_var2);
+
+  return StatusCode::SUCCESS;
 }
 
 
