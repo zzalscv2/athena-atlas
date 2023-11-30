5ae114d078ed58b658f4c0a6ccfaff2746dd57a3
John Chapman <jchapman@cern.ch>
2023-11-30 08:51:03 +0100

    Update classes in MCTruth package to handle Status and UniqueID (part of Barcode migration) (ATLASSIM-6978)

--- Simulation/G4Sim/MCTruth/MCTruth/TrackBarcodeInfo.h
+++ Simulation/G4Sim/MCTruth/MCTruth/TrackBarcodeInfo.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
 */
 
 #ifndef TrackBarcodeInfo_H
@@ -13,19 +13,23 @@ namespace ISF {
 
 class TrackBarcodeInfo: public VTrackInformation {
 public:
-	TrackBarcodeInfo(int bc, ISF::ISFParticle* baseIsp=0);
-	virtual int GetParticleBarcode() const override {return m_barcode;}
-	virtual const ISF::ISFParticle *GetBaseISFParticle() const override {return m_theBaseISFParticle;}
-	virtual ISF::ISFParticle *GetBaseISFParticle() override {return m_theBaseISFParticle;}
+  TrackBarcodeInfo(int bc, ISF::ISFParticle* baseIsp=0);
+  virtual int GetParticleBarcode() const override {return m_barcode;}  // TODO Drop this once UniqueID and Status are used instead
+  virtual int GetParticleUniqueID() const override {return m_uniqueID;}
+  virtual int GetParticleStatus() const override {return m_status;}
+  virtual const ISF::ISFParticle *GetBaseISFParticle() const override {return m_theBaseISFParticle;}
+  virtual ISF::ISFParticle *GetBaseISFParticle() override {return m_theBaseISFParticle;}
 
-	virtual void SetBaseISFParticle(ISF::ISFParticle*) override;
-	virtual void SetReturnedToISF(bool returned) override;
-	virtual bool GetReturnedToISF() const override {return m_returnedToISF;}
+  virtual void SetBaseISFParticle(ISF::ISFParticle*) override;
+  virtual void SetReturnedToISF(bool returned) override;
+  virtual bool GetReturnedToISF() const override {return m_returnedToISF;}
 
 private:
-	ISF::ISFParticle *m_theBaseISFParticle;
-	int m_barcode;
-	bool m_returnedToISF;
+  ISF::ISFParticle *m_theBaseISFParticle;
+  int m_barcode;  // TODO Drop this once UniqueID and Status are used instead
+  int m_uniqueID;
+  int m_status{0}; //FIXME
+  bool m_returnedToISF;
 };
 
 
