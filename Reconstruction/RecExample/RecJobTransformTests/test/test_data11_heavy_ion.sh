13b02bfb54f8abb0ce146aa7422c7f3fce808685
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2023-12-01 20:30:04 +0100

    ATLASRECTS-7847: Make data11 HI run in CA
    ATLASRECTS-7847: Make data11 HI run in CA

--- Reconstruction/RecExample/RecJobTransformTests/test/test_data11_heavy_ion.sh
+++ Reconstruction/RecExample/RecJobTransformTests/test/test_data11_heavy_ion.sh
@@ -6,17 +6,19 @@
 # art-include: main/Athena
 # art-include: 23.0/Athena
 
-Reco_tf.py --CA \
+export ATHENA_CORE_NUMBER=4
+Reco_tf.py \
+--CA \
+--multithreaded \
 --inputBSFile /cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/RecJobTransformTests/data11_hi.00193321.physics_HardProbes.merge.RAW._lb0050._SFO-9._0002.1 \
---athenaopts="--threads=1" \
 --maxEvents 10 \
 --conditionsTag "COMCOND-BLKPA-RUN1-07" \
 --geometryVersion "ATLAS-R1-2011-02-00-00" \
---outputESDFile myESD.pool.root \
+--autoConfiguration 'everything' \
 --outputAODFile myAOD.pool.root \
 --preInclude "all:HIRecConfig.HIModeFlags.HImode" \
 --preExec "all:flags.Reco.EnableZDC=False;flags.Reco.EnableTrigger=False" \
---postExec 'all:from IOVDbSvc.IOVDbSvcConfig import addOverride;cfg.merge(addOverride(flags,"/TRT/Calib/PID_NN", "TRTCalibPID_NN_v1"));cfg.merge(addOverride(flags,"/TRT/Onl/Calib/PID_NN", "TRTCalibPID_NN_v1"));' \
+--postExec 'all:from IOVDbSvc.IOVDbSvcConfig import addOverride;cfg.merge(addOverride(flags,"/TRT/Calib/PID_NN", "TRTCalibPID_NN_v1"));cfg.merge(addOverride(flags,"/TRT/Onl/Calib/PID_NN", "TRTCalibPID_NN_v1"));cfg.getCondAlgo("LArADC2MeVCondAlg").CompleteDetector=False' \
 
 RES=$?
 echo "art-result: $RES Reco"
