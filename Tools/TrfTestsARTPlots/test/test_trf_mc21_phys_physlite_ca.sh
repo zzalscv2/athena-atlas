5c0d9d869be93ad4769470f376410262fa8006a6
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2023-12-04 18:42:46 +0100

    TrfTestsARTPlots: Update to new dcube reference files
    TrfTestsARTPlots: Update to new dcube reference files

--- Tools/TrfTestsARTPlots/test/test_trf_mc21_phys_physlite_ca.sh
+++ Tools/TrfTestsARTPlots/test/test_trf_mc21_phys_physlite_ca.sh
@@ -50,7 +50,7 @@ echo "art-result: ${rc4} xAODHistSize"
 
 # dcube references
 echo "============ dcube references"
-dcubeRef="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/dcube/mc21/v5/hist_physlite_2403.root"
+dcubeRef="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/dcube/mc21/v6/hist_physlite_24017.root"
 dcubeXML="/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/TrfTestsART/dcube/mc21/v4/dcube_config_hist_physlite_2402.xml"
 echo ${dcubeRef}
 echo ${dcubeXML}
