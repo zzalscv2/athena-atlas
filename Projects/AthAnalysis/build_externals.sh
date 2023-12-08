31d04e40d72e4180752a28acc3c239c41b3a2de4
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2023-12-08 09:59:33 +0100

    new LCG layer LCG_104c_ATLAS_3
    new LCG layer LCG_104c_ATLAS_3

--- Projects/AthAnalysis/build_externals.sh
+++ Projects/AthAnalysis/build_externals.sh
@@ -10,7 +10,7 @@ ATLAS_PROJECT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 ATLAS_EXT_PROJECT_NAME="AthAnalysisExternals"
 ATLAS_BUILDTYPE="RelWithDebInfo"
 ATLAS_EXTRA_CMAKE_ARGS=(-DLCG_VERSION_NUMBER=104
-                        -DLCG_VERSION_POSTFIX="c_ATLAS_1"
+                        -DLCG_VERSION_POSTFIX="c_ATLAS_3"
                         -DATLAS_ONNXRUNTIME_USE_CUDA=FALSE
                         -DATLAS_GAUDI_SOURCE="URL;https://gitlab.cern.ch/atlas/Gaudi/-/archive/v37r2.000/Gaudi-v37r2.000.tar.gz;URL_MD5;dfeef8eb0b338bff8948b025c4b450cf")
 ATLAS_EXTRA_MAKE_ARGS=()
