f20c17cf478d7d13727005bc3f4fd651b6233ad6
Johannes Elmsheuser <johannes.elmsheuser@cern.ch>
2023-12-13 17:44:22 +0100

    TrfTestsARTPlots: Add CI-like RunWorkflowTests PHYSLITE ART test
    TrfTestsARTPlots: Add CI-like RunWorkflowTests PHYSLITE ART test

---
+++ Tools/TrfTestsARTPlots/test/test_trf_citest_phys_physlite_compare.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#
+# art-description: RunWorkflowTests Derivation PHYSLITE, diff-root with previous nightly
+# art-type: grid
+# art-include: main/Athena
+# art-athena-mt: 8
+
+# Determine current and previous nightly
+currentrel=$AtlasBuildStamp
+releasebasedir=`echo $ATLAS_RELEASE_BASE | sed "s/${AtlasBuildStamp}//g"` 
+previousrel=`ls -r $releasebasedir | sort | grep 20 | tail -2 | head -1`
+
+# Set some defaults
+filename="DAOD_PHYSLITE.myOutput.pool.root"
+diff_root_mode="--branches-of-interest"
+diff_root_list="AnalysisElectronsAuxDyn(.*) AnalysisJetsAuxDyn(.*) AnalysisLargeRJetsAuxDyn(.*) AnalysisMuonsAuxDyn(.*) AnalysisPhotonsAuxDyn(.*) AnalysisTauJetsAuxDyn(.*)"
+max_events=100
+
+# Run2 data ##########
+source $AtlasSetup/scripts/asetup.sh Athena,main,$previousrel
+RunWorkflowTests_Run2.py --CI -d -w Derivation --tag data_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} prev_data_PHYSLITE_Run2"
+mv run_data_PHYSLITE_Run2 prev_run_data_PHYSLITE_Run2
+
+source $AtlasSetup/scripts/asetup.sh Athena,main,$currentrel
+RunWorkflowTests_Run2.py --CI -d -w Derivation --tag data_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} curr_data_PHYSLITE_Run2"
+mv run_data_PHYSLITE_Run2 curr_run_data_PHYSLITE_Run2
+
+reference_file="prev_run_data_PHYSLITE_Run2/"$filename
+validation_file="curr_run_data_PHYSLITE_Run2/"$filename
+acmd.py diff-root $reference_file $validation_file --order-trees --nan-equal --exact-branches --mode semi-detailed --error-mode resilient $diff_root_mode $diff_root_list --entries $max_events
+rc=$?
+echo "art-result: ${rc} diff-root data_PHYSLITE_Run2"
+
+# Run2 MC ##########
+source $AtlasSetup/scripts/asetup.sh Athena,main,$previousrel
+RunWorkflowTests_Run2.py --CI -d -w Derivation --tag mc_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} prev_mc_PHYSLITE_Run2"
+mv run_mc_PHYSLITE_Run2 prev_run_mc_PHYSLITE_Run2
+
+source $AtlasSetup/scripts/asetup.sh Athena,main,$currentrel
+RunWorkflowTests_Run2.py --CI -d -w Derivation --tag mc_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} curr_mc_PHYSLITE_Run2"
+mv run_mc_PHYSLITE_Run2 curr_run_mc_PHYSLITE_Run2
+
+reference_file="prev_run_mc_PHYSLITE_Run2/"$filename
+validation_file="prev_run_mc_PHYSLITE_Run2/"$filename
+acmd.py diff-root $reference_file $validation_file --order-trees --nan-equal --exact-branches --mode semi-detailed --error-mode resilient $diff_root_mode $diff_root_list --entries $max_events
+rc=$?
+echo "art-result: ${rc} diff-root mc_PHYSLITE_Run2"
+
+# Run3 data ##########
+source $AtlasSetup/scripts/asetup.sh Athena,main,$previousrel
+RunWorkflowTests_Run3.py --CI -d -w Derivation --tag data_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} prev_data_PHYSLITE_Run3"
+mv run_data_PHYSLITE_Run3 prev_run_data_PHYSLITE_Run3
+
+source $AtlasSetup/scripts/asetup.sh Athena,main,$currentrel
+RunWorkflowTests_Run3.py --CI -d -w Derivation --tag data_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} curr_data_PHYSLITE_Run3"
+mv run_data_PHYSLITE_Run3 curr_run_data_PHYSLITE_Run3
+
+reference_file="prev_run_data_PHYSLITE_Run3/"$filename
+validation_file="curr_run_data_PHYSLITE_Run3/"$filename
+acmd.py diff-root $reference_file $validation_file --order-trees --nan-equal --exact-branches --mode semi-detailed --error-mode resilient $diff_root_mode $diff_root_list --entries $max_events
+rc=$?
+echo "art-result: ${rc} diff-root data_PHYSLITE_Run3"
+
+# Run3 MC ##########
+source $AtlasSetup/scripts/asetup.sh Athena,main,$previousrel
+RunWorkflowTests_Run3.py --CI -d -w Derivation --tag mc_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} prev_mc_PHYSLITE_Run3"
+mv run_mc_PHYSLITE_Run3 prev_run_mc_PHYSLITE_Run3
+
+source $AtlasSetup/scripts/asetup.sh Athena,main,$currentrel
+RunWorkflowTests_Run3.py --CI -d -w Derivation --tag mc_PHYSLITE --threads 8
+rc=$?
+echo "art-result: ${rc} curr_mc_PHYSLITE_Run3"
+mv run_mc_PHYSLITE_Run3 curr_run_mc_PHYSLITE_Run3
+
+reference_file="prev_run_mc_PHYSLITE_Run3/"$filename
+validation_file="prev_run_mc_PHYSLITE_Run3/"$filename
+acmd.py diff-root $reference_file $validation_file --order-trees --nan-equal --exact-branches --mode semi-detailed --error-mode resilient $diff_root_mode $diff_root_list --entries $max_events
+rc=$?
+echo "art-result: ${rc} diff-root mc_PHYSLITE_Run3"
