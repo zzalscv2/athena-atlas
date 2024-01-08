f94d1059fe3198b39c1d3d89b1db5bfa9271f1af
Nicholas Styles <nicholas.styles@desy.de>
2024-01-08 06:54:36 +0100

    Fixes for executability of ITk alignment tests
    Fixes for executability of ITk alignment tests

--- Tracking/TrkExtrapolation/TrkExUnitTests/scripts/RunExEngineTest.py
+++ Tracking/TrkExtrapolation/TrkExUnitTests/scripts/RunExEngineTest.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (C) 2002-2022 CERN for the benefit of the ATLAS collaboration
 from AthenaConfiguration.AllConfigFlags import ConfigFlags
 from AthenaConfiguration.MainServicesConfig import MainServicesCfg    
